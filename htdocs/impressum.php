4b9d52057a8d848ee802188b3afedf71d809f9aa
Cornelis Kater <kater@zqs.uni-hannover.de>
2002-06-21 19:33:04 +0000

    Auswirkungen des td background kills beseitigt, bmbf Logo eingefügt, Statistikseite sinniger aufgebaut

--- htdocs/impressum.php
+++ htdocs/impressum.php
@@ -99,119 +99,144 @@ function write_toplist($rubrik,$query)
 <? IF ($view=="main") {?>
 	
 	<tr>
-		<td valign="center" class="blank">
-		<blockquote><br /><br />
-		Stud.IP ist ein Open Source Projekt zur Unterst&uuml;tzung von Pr&auml;senzlehre an der Universit&auml;t G&ouml;ttingen. <br>
-		Das System wird federf&uuml;hrend entwickelt vom Zentrum f&uuml;r interdisziplin&auml;re Medienwissenschaft (ZiM) G&ouml;ttingen.<br>
-		Stud.IP steht unter der GNU General Public License, Version 2 oder neuer.<br /><br />
-		Weitere Informationen finden sie auf <a target="_new" href="http://www.studip.de">www.studip.de</a><br /><br />
+		<td valign="top" class="blank">
+			<blockquote><br />
+			Stud.IP ist ein Open Source Projekt zur Unterst&uuml;tzung von Pr&auml;senzlehre an der Universit&auml;t G&ouml;ttingen. <br>
+			Das System wird federf&uuml;hrend entwickelt vom Zentrum f&uuml;r interdisziplin&auml;re Medienwissenschaft (ZiM) G&ouml;ttingen.<br>
+			Stud.IP steht unter der GNU General Public License, Version 2 oder neuer.<br /><br />
+			Weitere Informationen finden sie auf <a target="_new" href="http://www.studip.de">www.studip.de</a><br />
 		</td>
-	<td class="blank" align = right><a target="_new" href="http://www.studip.de"><img src="pictures/studipanim.gif" border="0"></a><br /><b>Version: </b><? echo $SOFTWARE_VERSION?>&nbsp; &nbsp; &nbsp; &nbsp; 
-	</td></tr><tr><td colspan=2>
-	<br>&nbsp; &nbsp; <b>Die folgenden Entwickler</b> sind mit der st&auml;ndigen Pflege und Weiterentwicklung des Systems befasst:<br><br><blockquote>
-	<b>Marco Bohnsack</b>, eMail: <a href="mailto:mbohnsa@stud.uni-goettingen.de">mbohnsa@stud.uni-goettingen.de</a> (Hilfe)
-	<br><b>Oliver Brakel</b>, eMail: <a href="mailto:obrakel@gwdg.de">obrakel@gwdg.de</a> (Distribution)
-	<br><b>Cornelis Kater</b>, eMail: <a href="mailto:ckater@gwdg.de">ckater@gwdg.de</a> (Kernentwicklung, Terminverwaltung, Adminbereich, Design)
-	<br><b>André Noack</b>, eMail: <a href="mailto:andre.noack@gmx.net">andre.noack@gmx.net</a> (Kernentwicklung, Newsverwaltung, Chat)
-	<br><b>Arne Schröder</b>, eMail: <a href="mailto:23arne@web.de">23arne@web.de</a> (Externe Seiten)
-	<br><b>Ralf Stockmann</b>, eMail: <a href="mailto:rstockm@gwdg.de">rstockm@gwdg.de</a> (Kernentwicklung, Forensystem, pers&ouml;nliche Seiten, Design)
-	<br><b>Stefan Suchi</b>, eMail: <a href="mailto:suchi@data-quest.de">suchi@data-quest.de</a> (Kernentwicklung, Datenbankstruktur, Rechtesystem, Adminbereich)
-	<br><b>Peter Thienel</b>, eMail: <a href="mailto:rabeiri@gmx.de">rabeiri@gmx.de</a> (Externe Seiten, Terminplaner)
-      <br></blockquote>
-	
-	</td>
+		<td class="blank" align="right">
+			<a target="_new" href="http://www.studip.de"><img src="pictures/studipanim.gif" border="0"></a><br /><b>Version: </b><? echo $SOFTWARE_VERSION?>
+			&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+			<font size=-1>gef&ouml;rdert von </font>&nbsp; &nbsp; &nbsp; &nbsp; <br />
+			<a target="_new" href="http://www.bmbf.de/"><img src="pictures/bmbf.gif" border="0" /></a>&nbsp; &nbsp; &nbsp; &nbsp; 
+			
+		</td>
+	<tr>
+		<td class="steel1"colspan=2>
+			<br>&nbsp; &nbsp; <b>Die folgenden Entwickler</b> sind mit der st&auml;ndigen Pflege und Weiterentwicklung des Systems befasst:<br><br><blockquote>
+			<b>Marco Bohnsack</b>, eMail: <a href="mailto:mbohnsa@stud.uni-goettingen.de">mbohnsa@stud.uni-goettingen.de</a> (Hilfe)
+			<br><b>Oliver Brakel</b>, eMail: <a href="mailto:obrakel@gwdg.de">obrakel@gwdg.de</a> (Distribution)
+			<br><b>Cornelis Kater</b>, eMail: <a href="mailto:ckater@gwdg.de">ckater@gwdg.de</a> (Kernentwicklung, Terminverwaltung, Adminbereich, Design)
+			<br><b>André Noack</b>, eMail: <a href="mailto:andre.noack@gmx.net">andre.noack@gmx.net</a> (Kernentwicklung, Newsverwaltung, Chat)
+			<br><b>Arne Schröder</b>, eMail: <a href="mailto:23arne@web.de">23arne@web.de</a> (Externe Seiten)
+			<br><b>Ralf Stockmann</b>, eMail: <a href="mailto:rstockm@gwdg.de">rstockm@gwdg.de</a> (Kernentwicklung, Forensystem, pers&ouml;nliche Seiten, Design)
+			<br><b>Stefan Suchi</b>, eMail: <a href="mailto:suchi@data-quest.de">suchi@data-quest.de</a> (Kernentwicklung, Datenbankstruktur, Rechtesystem, Adminbereich)
+			<br><b>Peter Thienel</b>, eMail: <a href="mailto:rabeiri@gmx.de">rabeiri@gmx.de</a> (Externe Seiten, Terminplaner)
+			<br></blockquote>
+		</td>
 	</tr>
 	<?}
 	
 IF ($view=="statistik") {?>
 	
 	<tr>
-		<td valign="center" class="blank">
-		<blockquote><br /><br />
-		<table width=60% align=middle >
-	<?
-	$db=new DB_Seminar;
-	$db->query("SELECT * from seminare");
-	$anzahl = $db->num_rows();
-	echo "<tr><td>Aktive Veranstaltungen:</td><td align=right>$anzahl</td></tr>"; 
+		<td width="70%"  valign="top" class="blank">
+		<blockquote>
+			<b>Top-Listen aller Veranstaltungen</b><br /><br />
+			<table  cellpadding=0 cellspacing=0 class=blank>	
+			<?
+			//Toplists
+			$count = 10;
+			write_toplist("die meisten Teilnehmer","SELECT seminare.seminar_id, seminare.name, count(seminare.seminar_id) as count FROM seminar_user LEFT JOIN seminare USING(seminar_id) ".$sql_where_query_seminare." GROUP BY seminare.seminar_id ORDER BY count DESC LIMIT $count");
+			write_toplist("zuletzt angelegt","SELECT seminare.seminar_id, seminare.name, mkdate as count FROM seminare ".$sql_where_query_seminare." ORDER BY mkdate DESC LIMIT $count");
+			write_toplist("die meisten Materialien (Dokumente)","SELECT seminare.seminar_id, seminare.name, count(seminare.seminar_id) as count FROM dokumente LEFT JOIN seminare USING(seminar_id) ".$sql_where_query_seminare." GROUP BY seminar_id  ORDER BY count DESC LIMIT $count");
+			$week = time()-1209600;
+			IF ($view!="Alle") $tmp = ereg_replace("WHERE","AND",$sql_where_query_seminare);
+				write_toplist("die aktivsten Seminare (Postings der letzten zwei Wochen)","SELECT seminare.seminar_id, seminare.name, count(*) as count FROM px_topics LEFT JOIN seminare USING(seminar_id) WHERE px_topics.mkdate > $week ".$tmp." GROUP BY seminar_id  ORDER BY count DESC LIMIT $count");
+			?>	
+			</table>
+		</blockquote>
+		</td>
+		<td width="30%" valign="top" class="blank">
+			<table  align=middle cellpadding=2 cellspacing=0 border=0 >
+			<b>Statistik</b><br /><br />
+			<?
+			//Statistics
+			$db=new DB_Seminar;
+			$cssSw=new cssClassSwitcher;
 	
-	$db->query("SELECT * from archiv");
-	$anzahl = $db->num_rows();
-	echo "<tr><td nowrap>Archivierte Veranstaltungen:</td><td align=right>$anzahl</td></tr>"; 
+			$db->query("SELECT * from seminare");
+			$anzahl = $db->num_rows();
+			echo "<tr><td class=\"".$cssSw->getClass() ."\">Aktive Veranstaltungen:</td><td class=\"".$cssSw->getClass() ."\" align=right>$anzahl</td></tr>"; 
 
-	echo "<tr><td colspan=2>&nbsp; </td></tr>";
+			$cssSw->switchClass();
+			$db->query("SELECT * from archiv");
+			$anzahl = $db->num_rows();
+			echo "<tr><td class=\"".$cssSw->getClass() ."\" nowrap>Archivierte Veranstaltungen:</td><td class=\"".$cssSw->getClass() ."\" align=right>$anzahl</td></tr>"; 
 
-	$db->query("SELECT * FROM Institute");
-	$anzahl = $db->num_rows();
-	echo "<tr><td>beteiligte Institute:</td><td align=right>$anzahl</td></tr>"; 
+			$cssSw->switchClass();
+			echo "<tr><td class=\"".$cssSw->getClass() ."\" colspan=2>&nbsp; </td></tr>";
 
-	$db->query("SELECT * FROM Fakultaeten");
-	$anzahl = $db->num_rows();
-	echo "<tr><td>beteiligte Fakult&auml;ten:</td><td align=right>$anzahl</td></tr>"; 
+			$cssSw->switchClass();	$db->query("SELECT * FROM Institute");
+			$anzahl = $db->num_rows();
+			echo "<tr><td class=\"".$cssSw->getClass() ."\">beteiligte Institute:</td><td class=\"".$cssSw->getClass() ."\" align=right>$anzahl</td></tr>"; 
 
-	echo "<tr><td colspan=2>&nbsp; </td></tr>";
+			$cssSw->switchClass();	$db->query("SELECT * FROM Institute");
+			$db->query("SELECT * FROM Fakultaeten");
+			$anzahl = $db->num_rows();
+			echo "<tr><td class=\"".$cssSw->getClass() ."\">beteiligte Fakult&auml;ten:</td><td class=\"".$cssSw->getClass() ."\" align=right>$anzahl</td></tr>"; 
 
-	$db->query("SELECT * from auth_user_md5 WHERE perms='admin'");
-	$anzahl = $db->num_rows();
-	echo "<tr><td>registrierte Administratoren:</td><td align=right>$anzahl</td></tr>"; 
-	
-	$db->query("SELECT * from auth_user_md5 WHERE perms='dozent'");
-	$anzahl = $db->num_rows();
-	echo "<tr><td>registrierte Dozenten:</td><td align=right>$anzahl</td></tr>"; 
-	
-	$db->query("SELECT * from auth_user_md5 WHERE perms='tutor'");
-	$anzahl = $db->num_rows();
-	echo "<tr><td>registrierte Tutoren:</td><td align=right>$anzahl</td></tr>"; 
-	
-	$db->query("SELECT * from auth_user_md5 WHERE perms='autor'");
-	$anzahl = $db->num_rows();
-	echo "<tr><td>registrierte Studierende:</td><td align=right>$anzahl</td></tr>"; 
-	
-	echo "<tr><td colspan=2>&nbsp; </td></tr>";
-	
-	$db->query("SELECT * from px_topics");
-	$anzahl = $db->num_rows();
-	echo "<tr><td>Postings:</td><td align=right>$anzahl</td></tr>"; 	
-	
-	$db->query("SELECT * from dokumente");
-	$anzahl = $db->num_rows();
-	echo "<tr><td>Dateien:</td><td align=right>$anzahl</td></tr>"; 	
-	
-	$db->query("SELECT * from literatur");
-	$anzahl = $db->num_rows();
-	echo "<tr><td>Literaturlisten:</td><td align=right>$anzahl</td></tr>"; 	
-	
-	$db->query("SELECT * from termine");
-	$anzahl = $db->num_rows();
-	echo "<tr><td>Termine:</td><td align=right>$anzahl</td></tr>"; 	
-	
-	$db->query("SELECT * from news");
-	$anzahl = $db->num_rows();
-	echo "<tr><td>News:</td><td align=right>$anzahl</td></tr></table>"; 	
-	
-	echo "</td><td class=blank align=center><table class=blank width=100% border=0><tr><td class=blank><a target='_new' href='http://www.studip.de'><img src='pictures/studipanim.gif' border=0></a></td></tr></table></td></tr>";
-	
-	?>		
-	<td colspan=2 class=blank align=center><br><br><table width=80% cellpadding=0 cellspacing=0 class=blank>	
+			$cssSw->switchClass();	$db->query("SELECT * FROM Institute");
+			echo "<tr><td class=\"".$cssSw->getClass() ."\" colspan=2>&nbsp; </td></tr>";
 
-	<?
-	$count = 10;
-	write_toplist("die meisten Teilnehmer","SELECT seminare.seminar_id, seminare.name, count(seminare.seminar_id) as count FROM seminar_user LEFT JOIN seminare USING(seminar_id) ".$sql_where_query_seminare." GROUP BY seminare.seminar_id ORDER BY count DESC LIMIT $count");
-	write_toplist("zuletzt angelegt","SELECT seminare.seminar_id, seminare.name, mkdate as count FROM seminare ".$sql_where_query_seminare." ORDER BY mkdate DESC LIMIT $count");
-	write_toplist("die meisten Materialien (Dokumente)","SELECT seminare.seminar_id, seminare.name, count(seminare.seminar_id) as count FROM dokumente LEFT JOIN seminare USING(seminar_id) ".$sql_where_query_seminare." GROUP BY seminar_id  ORDER BY count DESC LIMIT $count");
-	$week = time()-1209600;
-	IF ($view!="Alle") $tmp = ereg_replace("WHERE","AND",$sql_where_query_seminare);
-	write_toplist("die aktivsten Seminare (Postings der letzten zwei Wochen)","SELECT seminare.seminar_id, seminare.name, count(*) as count FROM px_topics LEFT JOIN seminare USING(seminar_id) WHERE px_topics.mkdate > $week ".$tmp." GROUP BY seminar_id  ORDER BY count DESC LIMIT $count");
+			$cssSw->switchClass();	$db->query("SELECT * FROM Institute");
+			$db->query("SELECT * from auth_user_md5 WHERE perms='admin'");
+			$anzahl = $db->num_rows();
+			echo "<tr><td class=\"".$cssSw->getClass() ."\">registrierte Administratoren:</td><td class=\"".$cssSw->getClass() ."\" align=right>$anzahl</td></tr>"; 
 	
-	?>	
-	</table><br>&nbsp; </td></tr>
-	<?}
+			$cssSw->switchClass();	
+			$db->query("SELECT * FROM Institute");
+			$db->query("SELECT * from auth_user_md5 WHERE perms='dozent'");
+			$anzahl = $db->num_rows();
+			echo "<tr><td class=\"".$cssSw->getClass() ."\">registrierte Dozenten:</td><td class=\"".$cssSw->getClass() ."\" align=right>$anzahl</td></tr>"; 
+	
+			$cssSw->switchClass();	$db->query("SELECT * FROM Institute");
+			$db->query("SELECT * from auth_user_md5 WHERE perms='tutor'");
+			$anzahl = $db->num_rows();
+			echo "<tr><td class=\"".$cssSw->getClass() ."\">registrierte Tutoren:</td><td class=\"".$cssSw->getClass() ."\" align=right>$anzahl</td></tr>"; 
+	
+			$cssSw->switchClass();	$db->query("SELECT * FROM Institute");
+			$db->query("SELECT * from auth_user_md5 WHERE perms='autor'");
+			$anzahl = $db->num_rows();
+			echo "<tr><td class=\"".$cssSw->getClass() ."\">registrierte Studierende:</td><td class=\"".$cssSw->getClass() ."\" align=right>$anzahl</td></tr>"; 
+	
+			$cssSw->switchClass();	$db->query("SELECT * FROM Institute");
+			echo "<tr><td class=\"".$cssSw->getClass() ."\" colspan=2>&nbsp; </td></tr>";
+	
+			$cssSw->switchClass();	$db->query("SELECT * FROM Institute");
+			$db->query("SELECT * from px_topics");
+			$anzahl = $db->num_rows();
+			echo "<tr><td class=\"".$cssSw->getClass() ."\" >Postings:</td><td class=\"".$cssSw->getClass() ."\" align=right>$anzahl</td></tr>"; 	
+	
+			$cssSw->switchClass();	$db->query("SELECT * FROM Institute");
+			$db->query("SELECT * from dokumente");
+			$anzahl = $db->num_rows();
+			echo "<tr><td class=\"".$cssSw->getClass() ."\">Dateien:</td><td class=\"".$cssSw->getClass() ."\" align=right>$anzahl</td></tr>"; 	
+
+			$cssSw->switchClass();	$db->query("SELECT * FROM Institute");
+			$db->query("SELECT * from literatur");
+			$anzahl = $db->num_rows();
+			echo "<tr><td class=\"".$cssSw->getClass() ."\" >Literaturlisten:</td><td class=\"".$cssSw->getClass() ."\" align=right>$anzahl</td></tr>"; 	
+	
+			$cssSw->switchClass();	$db->query("SELECT * FROM Institute");
+			$db->query("SELECT * from termine");
+			$anzahl = $db->num_rows();
+			echo "<tr><td class=\"".$cssSw->getClass() ."\">Termine:</td><td class=\"".$cssSw->getClass() ."\" align=right>$anzahl</td></tr>"; 	
+	
+			$cssSw->switchClass();	$db->query("SELECT * FROM Institute");
+			$db->query("SELECT * from news");
+			$anzahl = $db->num_rows();
+			echo "<tr><td class=\"".$cssSw->getClass() ."\">News:</td><td class=\"".$cssSw->getClass() ."\" align=right>$anzahl</td></tr></blockquote></table></td></tr>"; 	
+	}
 	
 IF ($view=="history") {?>
 	
 	<tr>
 		<td valign="center" class="blank">
-		<blockquote><br /><br />
+		<blockquote>
+		<b>Stud.IP history.txt</b><br /><br />		
 		<? 
 		
 		$history = file("history.txt");
@@ -228,7 +253,7 @@ IF ($view=="ansprechpartner") {?>
 	
 	
 	<tr>
-		<td class="blank" colspan=2><br><br>
+		<td class="blank" valign="top" colspan=2><br><br>
 		<blockquote><b>F&uuml;r diese Stud.IP-Installation sind folgende Administratoren zust&auml;ndig:</b><br>
 		<blockquote><blockquote>
 <?
@@ -251,13 +276,13 @@ IF ($view=="ansprechpartner") {?>
 		<blockquote></blockquote>
 		</td>
 	<tr>
-		<td colspan=2>
+		<td class="steel1" colspan=2>
 		<blockquote><br><b>Folgende Einrichtungen sind beteiligt:</b><br><font size=-1>(Genannt werden die jeweiligen Administratoren der Einrichtungen f&uuml;r entsprechende Anfragen)</font>
 		</td>
 	</table><table width="100%" border=0 cellpadding=0 cellspacing=0>
 	</tr>
 	<tr>
-		<td valign="top"width="55%">
+		<td class="steel1" valign="top"width="55%">
 		<blockquote>
 		
 <?
@@ -270,14 +295,13 @@ IF ($view=="ansprechpartner") {?>
 		{
 		if (($count<$half) && ($change==0) && ($last_inst<>$db->f("Institut_id")))
 			{
-			echo"<br><br></td><td valign=\"top\" width=\"45%\">";
+			echo"<br><br></td><td class=\"steel1\" valign=\"top\" width=\"45%\">";
 			$change=1;
 			}
 		if ($last_inst<>$db->f("Institut_id"))
 			{
 			$inst_id=$db->f("Institut_id");
-//			echo "<br><br><b>".htmlReady($db->f("Name")).":</b><br>";
-			echo "<br><br><b><a href=\"institut_main.php?auswahl=".$db->f("Institut_id")."\">".htmlReady($db->f("Name"))."</a>:</b><br>"; //EXPERIMENTELL!!
+			echo "<br><br><b><a href=\"institut_main.php?auswahl=".$db->f("Institut_id")."\">".htmlReady($db->f("Name"))."</a>:</b><br>";
 			}
 		$last_inst=$inst_id;
 		echo "<a href=\"about.php?username=".$db->f("username")."\">".$db->f("Vorname")." ".$db->f("Nachname")."</a>, eMail: <a href=\"mailto:".$db->f("Email")."\">".$db->f("Email")."</a><br>";
