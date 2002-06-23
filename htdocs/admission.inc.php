3de5f1230cae5b74a907781bd05b674de7e9ff4b
Cornelis Kater <kater@zqs.uni-hannover.de>
2002-06-23 20:18:16 +0000

    Korrekturen, jetzt schonmal auf einige Varianten getestet

--- htdocs/admission.inc.php
+++ htdocs/admission.inc.php
@@ -54,13 +54,13 @@ function check_admission ($send_message=TRUE) {
 			//User aus admission_Seminar_user in seminar_user verschieben
 			while ($db3->next_record())   {
 				$group = select_group ($db->f("start_time"), $db3->f("user_id"));			
-				$db4->query("INSERT INTO seminar_user SET user_id = '".$db3->f("user_id")."', Seminar_id = '".$db->f("Seminar_id")."', status= 'autor', group = '$group', admission_studiengang_id = '".$db3->f("studiengang_id")."', mkdate = '".time()."' ");
-				if ($db4->num_rows())
+				$db4->query("INSERT INTO seminar_user SET Seminar_id = '".$db->f("Seminar_id")."', user_id = '".$db3->f("user_id")."', status= 'autor', gruppe = '$group', admission_studiengang_id = '".$db3->f("studiengang_id")."', mkdate = '".time()."' ");
+				if ($db4->affected_rows())
 					$db5->query("DELETE FROM admission_seminar_user WHERE user_id ='".$db3->f("user_id")."' AND seminar_id = '".$db->f("Seminar_id")."' ");
 					//User benachrichten
-					if (($db5->num_rows()) && ($send_message)) {
-						$message="Sie wurden als Teilnehmer der Veranstaltung <b>".$db->f("Name")."</b> ausgelost. Ab sofort finden Sie die Veranstaltungen in der &Uuml;bersicht ihrer Veranstaltungen. Damit sind sie auch als Teilnehmer der Pr&auml;senzveranstaltung zugelassen.";
-						$messaging->insert_sms ($db3->f("username"), $message);
+					if (($db5->affected_rows()) && ($send_message)) {
+						$message="Sie wurden als Teilnehmer der Veranstaltung **".$db->f("Name")."** ausgelost. Ab sofort finden Sie die Veranstaltungen in der Übersicht ihrer Veranstaltungen. Damit sind sie auch als Teilnehmer der Präsenzveranstaltung zugelassen.";
+						$messaging->insert_sms ($db3->f("username"), $message, "____%system%____");
 					}
 			}
 			//Alle anderen Teilnehmer in der Warteliste losen
@@ -70,9 +70,9 @@ function check_admission ($send_message=TRUE) {
 			while ($db3->next_record()) {
 				$db4->query("UPDATE admission_seminar_user SET position = '$position', status = 'awaiting' ");
 				//User benachrichten
-				if (($db4->num_rows()) && ($send_message)) {
-					$message="Sie wurden leider im Losverfahren der Veranstaltung <b>".$db->f("Name")."</b> nicht ausgelost. Sie wurden jedoch auf Position $position auf die Warteliste gesetzt. Sie werden automatisch eingetragen, sobald ein Platz f&uuml;r Sie frei wird.";
-					$messaging->insert_sms ($db3->f("username"), $message);
+				if (($db4->affected_rows()) && ($send_message)) {
+					$message="Sie wurden leider im Losverfahren der Veranstaltung **".$db->f("Name")."** nicht ausgelost. Sie wurden jedoch auf Position $position auf die Warteliste gesetzt. Das System wird Sie  automatisch tragen, sobald ein Platz für Sie frei wird.";
+					$messaging->insert_sms ($db3->f("username"), $message, "____%system%____");
 				}
 				$position++;
 			}
@@ -100,20 +100,20 @@ function update_admission ($seminar_id, $send_message=TRUE) {
 		$db2->query("SELECT studiengang_id, quota FROM admission_seminar_studiengang WHERE seminar_id = '$seminar_id' ");
 		while ($db2->next_record()) {
 			//Wiieviel Teilnehmer sind eingetragen und wieviele koennten eingetragen werden?
-			$db3->query("SELECT count (user_id) AS count FROM seminar_user WHERE Seminar_id = '".$db->f("Seminar_id")."' AND admission_studiengang_id = '".$db2->f("studiengang_id")."' ");
+			$db3->query("SELECT user_id FROM seminar_user WHERE Seminar_id = '".$db->f("Seminar_id")."' AND admission_studiengang_id = '".$db2->f("studiengang_id")."' ");
 			$db3->next_record();
-			if ($db3->f("count") < round($db->f("admission_turnout") * ($db2->f("quota") / 100))) {
+			if ($db3->num_rows() < round($db->f("admission_turnout") * ($db2->f("quota") / 100))) {
 				//Studis asuwaehlen, die jetzt aufsteigen koennen
-				$db4->query("SELECT admission_seminar_user.user_id, username LEFT JOIN auth_user_md5 USING (user_id) FROM admission_seminar_user WHERE seminar_id =  '".$db->f("Seminar_id")."'  AND studiengang_id = ".$db2->f("studiengang_id")."' ORDER BY position LIMIT ".($db3->f("count") - round($db->f("admission_turnout") * ($db2->f("quota") / 100))));
+				$db4->query("SELECT admission_seminar_user.user_id, username FROM admission_seminar_user LEFT JOIN auth_user_md5 USING (user_id) WHERE seminar_id =  '".$db->f("Seminar_id")."'  AND studiengang_id = '".$db2->f("studiengang_id")."' ORDER BY position LIMIT ".($db3->num_rows() - round($db->f("admission_turnout") * ($db2->f("quota") / 100))));
 				while ($db4->next_record()) {
 					$group = select_group ($db->f("start_time"), $db4->f("user_id"));			
-					$db5->query("INSERT INTO seminar_user SET user_id = '".$db4->f("user_id")."', Seminar_id = '".$db->f("Seminar_id")."', status= 'autor', group = '$group', admission_studiengang_id = '".$db2->f("studiengang_id")."', mkdate = '".time()."' ");
-					if ($db6->num_rows())
+					$db5->query("INSERT INTO seminar_user SET user_id = '".$db4->f("user_id")."', Seminar_id = '".$db->f("Seminar_id")."', status= 'autor', gruppe = '$group', admission_studiengang_id = '".$db2->f("studiengang_id")."', mkdate = '".time()."' ");
+					if ($db6->affected_rows())
 						$db6->query("DELETE FROM admission_seminar_user WHERE user_id ='".$db4->f("user_id")."' AND seminar_id = '".$db->f("Seminar_id")."' ");
 						//User benachrichten
-						if (($db6->num_rows()) && ($send_message)) {
-							$message="Sie als Teilnehmer der Veranstaltung <b>".$db->f("Name")."</b> eingetragen worden, da f&uuml;r Sie ein Platz frei geworden ist. Ab sofort finden Sie die Veranstaltungen in der &Uuml;bersicht ihrer Veranstaltungen. Damit sind sie auch als Teilnehmer der Pr&auml;senzveranstaltung zugelassen.";
-							$messaging->insert_sms ($db3->f("username"), $message);
+						if (($db6->affected_rows()) && ($send_message)) {
+							$message="Sie sind als Teilnehmer der Veranstaltung **".$db->f("Name")."** eingetragen worden, da für Sie ein Platz frei geworden ist. Ab sofort finden Sie die Veranstaltungen in der Übersicht ihrer Veranstaltungen. Damit sind sie auch als Teilnehmer der Präsenzveranstaltung zugelassen.";
+							$messaging->insert_sms ($db3->f("username"), $message, $user_id="____%system%____");
 						}
 				}
 				//Warteposition der restlichen User neu eintragen
