50d693fcc8c3d58da63f2be371533d6ec6fb976e
Cornelis Kater <kater@zqs.uni-hannover.de>
2002-06-24 14:30:57 +0000

    GNU eingebaut, Zebra eingebaut

--- htdocs/forumsettings.inc.php
+++ htdocs/forumsettings.inc.php
@@ -1,6 +1,26 @@
 <?
+/*
+folder.php - Anzeige und Verwaltung des Ordnersystems
+Copyright (C) 2002 Ralf Stockmann <rstockm@gwdg.de>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
 
 //Standard herstellen
+
+$cssSw=new cssClassSwitcher;	
 
 if ($forumsend=="bla"){
 	$forum=array(
@@ -30,9 +50,9 @@ if ($forumsend=="bla"){
 
 <blockquote><br>Auf dieser Seite k&ouml;nnen Sie die Bedienung des Stud.IP Forensystems an Ihre Bed&uuml;rfnisse anpassen.
 </blockquote><p>
-<table width="100%" border=0 cellpadding=2 cellspacing=1  border=0>
+<table width="99%" border=0 cellpadding=2 cellspacing=0 align="center"  border=0>
 <form action="<?echo $PHP_SELF?>?view=Forum" method="POST">
-<tr><td><blockquote><b>Java-Script Hovereffekte</b></td><td>
+<tr <? $cssSw->switchClass() ?>><td class="<? echo $cssSw->getClass() ?>"><blockquote><b>Java-Script Hovereffekte</b></td><td class="<? echo $cssSw->getClass() ?>">
 <?
 IF ($auth->auth["jscript"]) {
 	echo "<input type=CHECKBOX name='jshover' value=1";
@@ -42,13 +62,13 @@ IF ($auth->auth["jscript"]) {
 	}
 ELSE echo "Sie m&uuml;ssen in Ihrem Browser Javascript aktivieren um dieses Feature nutzen zu k&ouml;nnen.";
 ?>
-</td><td><br><blockquote>Mit dieser Funktion k&ouml;nnen sie durch reines &Uuml;berfahren der Themen&uuml;berschriften im Forum den entsprechenden Beitrag lesen. Sie k&ouml;nnen sich so sehr schnell und effizient auch durch l&auml;ngere Diskussionen arbeiten. Da jedoch die Ladezeit der Seite erheblich ansteigt, empfehlen wir diese Einstellung nur f&uuml;r Nutzer die mindestens eine ISDN Verbindung haben.<br><br></td></tr>
-<tr><td><blockquote><b>Neue Beitr&auml;ge immer aufgeklappt</b></td><td><input type="CHECKBOX" name="neuauf" value="1"<?IF($forum["neuauf"]==1) echo " checked";?>></td><td><br><blockquote>Neue Postings sind immer automatisch aufgeklappt<br><br></td></tr>
+</td><td  class="<? echo $cssSw->getClass() ?>"><br><blockquote>Mit dieser Funktion k&ouml;nnen sie durch reines &Uuml;berfahren der Themen&uuml;berschriften im Forum den entsprechenden Beitrag lesen. Sie k&ouml;nnen sich so sehr schnell und effizient auch durch l&auml;ngere Diskussionen arbeiten. Da jedoch die Ladezeit der Seite erheblich ansteigt, empfehlen wir diese Einstellung nur f&uuml;r Nutzer die mindestens eine ISDN Verbindung haben.<br><br></td></tr>
+<tr  <? $cssSw->switchClass() ?>><td  class="<? echo $cssSw->getClass() ?>"><blockquote><b>Neue Beitr&auml;ge immer aufgeklappt</b></td><td class="<? echo $cssSw->getClass() ?>"><input type="CHECKBOX" name="neuauf" value="1"<?IF($forum["neuauf"]==1) echo " checked";?>></td><td  class="<? echo $cssSw->getClass() ?>"><br><blockquote>Neue Postings sind immer automatisch aufgeklappt<br><br></td></tr>
 <input type="HIDDEN" name="forumsend" value="bla">
-<tr><td>&nbsp; </td><td  colspan=2><input type=submit name=Create value="&Auml;nderungen &uuml;bernehmen">&nbsp; </td></tr>		
+<tr  <? $cssSw->switchClass() ?>><td class="<? echo $cssSw->getClass() ?>">&nbsp; </td><td  class="<? echo $cssSw->getClass() ?>" colspan=2><input type=submit name=Create value="&Auml;nderungen &uuml;bernehmen">&nbsp; </td></tr>		
 </form>		
-</table>
+</table><br />
 <? IF ($forumsend=="anpassen") {
-	echo "</td></tr></table>";
+	echo " </td></tr></table>";
 	die;
 	}
