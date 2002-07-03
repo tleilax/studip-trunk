1f621fcb400d04e43247aa059ce3809e3fb4aa4f
Cornelis Kater <kater@zqs.uni-hannover.de>
2002-07-03 14:27:52 +0000

    nocheinmal umfangreiche Änderungen, damit die Anmeldung logischer läuft, Kleinigkeiten

--- htdocs/admission.inc.php
+++ htdocs/admission.inc.php
@@ -23,12 +23,93 @@ require_once ("$ABSOLUTE_PATH_STUDIP/messaging.inc.php");
 require_once ("$ABSOLUTE_PATH_STUDIP/functions.php");
 
 /*
-Die Funktion veranstaltung_beginn errechnet den ersten Seminartermin aus dem Turnus Daten.
-Zurueckgegeben wird ein String oder Timestamp. je nach return_mode (TRUE = Timestamp)
-Evtl. Ergaenzungen werden im Stringmodus mit ausgegeben.
-Die Funktion kann mit einer Seminar_id aufgerufen werden, dann werden saemtliche gespeicherten Daten 
-beruecksichtigt. Im 'ad hoc' Modus koennen der Funktion auch die eizelnen Variabeln des Metadaten-Arrays
-uebergeben werden. Dann werden konkrete Termine nur mit berruecksichtigt, sofern sie schon angelegt wurden.
+Die Funktion renumer_admission numeriert eine Warteliste neu. Anzuwenden wenn Personen
+von der Warteliste entfertnt wurden.
+Die Teilnehmer bekommen eine SMS wie sich die Position veraendert hat, wenn der Parameter
+gesetzt ist.
+*/
+
+function renumber_admission ($seminar_id, $send_message=TRUE) {
+	$db=new DB_Seminar;
+	$db2=new DB_Seminar;
+	$db3=new DB_Seminar;
+	$db4=new DB_Seminar;
+	$messaging=new messaging;
+
+	//Daten holen / Abfrage ob ueberhaupt begrenzt
+	$db->query("SELECT Seminar_id, Name FROM seminare WHERE seminar_id = '$seminar_id' AND ((admission_type = '1'  AND admission_selection_take_place = '1') OR (admission_type = '2'))");
+	if ($db->next_record()) {
+		//Liste einlesen
+		$db2->query("SELECT user_id FROM admission_seminar_user WHERE seminar_id =  '".$db->f("Seminar_id")."' ORDER BY position ");
+		$position=1;
+		//Liste neu numerieren
+		while ($db2->next_record()) {
+			$db3->query("UPDATE admission_seminar_user SET position = '$position' WHERE user_id = '".$db2->f("user_id")."' AND seminar_id = '".$db->f("Seminar_id")."' ");
+			//User benachrichten				
+			if (($db3->affected_rows()) && ($send_message)) {
+				//Username auslesen
+				$db4->query("SELECT username FROM auth_user_md5 WHERE user_id = '".$db2->f("user_id")."' ");
+				$db4->next_record();
+				$message="Sie sind in der Warteliste der Veranstaltung **".$db->f("Name")."** hochgestuft worde. Sie stehen zur Zeit auf Position $position.";
+				$messaging->insert_sms ($db4->f("username"), $message, "____%system%____");
+			}
+			$position++;
+		}
+	}
+}
+
+/*
+Die Funktion update_admission ueberprueft, ob Teilnehmer nachruecken koennen
+Die Teilnehmer bekommen eine SMS wenn es geklappt hat, wenn der Parameter
+gesetzt ist.
+*/
+
+function update_admission ($seminar_id, $send_message=TRUE) {
+
+	$db=new DB_Seminar;
+	$db2=new DB_Seminar;
+	$db3=new DB_Seminar;
+	$db4=new DB_Seminar;
+	$db5=new DB_Seminar;
+	$messaging=new messaging;
+	
+	//Daten holen / Abfrage ob ueberhaupt begrenzt
+	$db->query("SELECT Seminar_id, Name, admission_endtime, admission_turnout, admission_type, start_time FROM seminare WHERE seminar_id = '$seminar_id' AND ((admission_type = '1'  AND admission_selection_take_place = '1') OR (admission_type = '2'))");
+	if ($db->next_record()) {
+		//Alle zugelassenen Studiengaenge auswaehlen um die genaue Platzzahl zu ermitteln
+		$db2->query("SELECT studiengang_id, quota FROM admission_seminar_studiengang WHERE seminar_id = '$seminar_id' ");
+		$count=0;
+		while ($db2->next_record()) {
+			//Wiieviel Teilnehmer koennen eingetragen werden?
+			$db3->query("SELECT user_id FROM seminar_user WHERE Seminar_id = '".$db->f("Seminar_id")."' AND admission_studiengang_id = '".$db2->f("studiengang_id")."' ");
+			$db3->next_record();
+			$count=$count+round($db->f("admission_turnout") * ($db2->f("quota") / 100));
+		}
+			
+		//Studis auswaehlen, die jetzt aufsteigen koennen
+		$db3->query("SELECT admission_seminar_user.user_id, username, studiengang_id FROM admission_seminar_user LEFT JOIN auth_user_md5 USING (user_id) WHERE seminar_id =  '".$db->f("Seminar_id")."' ORDER BY position LIMIT $count");
+		while ($db3->next_record()) {
+			$group = select_group ($db->f("start_time"), $db3->f("user_id"));			
+			$db4->query("INSERT INTO seminar_user SET user_id = '".$db3->f("user_id")."', Seminar_id = '".$db->f("Seminar_id")."', status= 'autor', gruppe = '$group', admission_studiengang_id = '".$db3->f("studiengang_id")."', mkdate = '".time()."' ");
+			if ($db4->affected_rows()) {
+				$db5->query("DELETE FROM admission_seminar_user WHERE user_id ='".$db3->f("user_id")."' AND seminar_id = '".$db->f("Seminar_id")."' ");
+				//User benachrichten
+				if (($db5->affected_rows()) && ($send_message)) {
+					$message="Sie sind als Teilnehmer der Veranstaltung **".$db->f("Name")."** eingetragen worden, da für Sie ein Platz frei geworden ist. Ab sofort finden Sie die Veranstaltung in der Übersicht ihrer Veranstaltungen. Damit sind sie auch als Teilnehmer der Präsenzveranstaltung zugelassen.";
+					$messaging->insert_sms ($db4->f("username"), $message, "____%system%____");
+				}
+			}
+		}
+
+		//Warteposition der restlichen User neu eintragen
+		renumber_admission($db->f("Seminar_id"). $send_message);
+	}
+}
+
+/*
+Die Funktion check_admissionueberprueft, ob Veranstaltungen gelost oder das Kontingent geoefftnet
+werden muss. Die Teilnehmer bekommen eine SMS ob es geklappt hat oder nicht, wenn der Parameter
+gesetzt ist.
 */
 
 function check_admission ($send_message=TRUE) {
@@ -64,68 +145,30 @@ function check_admission ($send_message=TRUE) {
 					}
 				}
 			}
-			//Alle anderen Teilnehmer in der Warteliste losen
-			$db3->query("SELECT admission_seminar_user.user_id, username FROM admission_seminar_user LEFT JOIN auth_user_md5 USING (user_id) WHERE seminar_id = '".$db->f("Seminar_id")."' ORDER BY RAND() ");
-			//Warteposition ablegen
-			$position=1;
-			while ($db3->next_record()) {
-				$db4->query("UPDATE admission_seminar_user SET position = '$position', status = 'awaiting' WHERE user_id = '".$db3->f("user_id")."' AND seminar_id = '".$db->f("Seminar_id")."' ");
-				//User benachrichten
-				if (($db4->affected_rows()) && ($send_message)) {
-					$message="Sie wurden leider im Losverfahren der Veranstaltung **".$db->f("Name")."** <u>nicht</u> ausgelost. Sie wurden jedoch auf Position $position auf die Warteliste gesetzt. Das System wird Sie automatisch eintragen, sobald ein Platz für Sie frei wird.";
-					$messaging->insert_sms ($db3->f("username"), $message, "____%system%____");
-				}
-				$position++;
-			}
 		}
+		
+		//Alle anderen Teilnehmer in der Warteliste losen
+		$db3->query("SELECT admission_seminar_user.user_id, username FROM admission_seminar_user LEFT JOIN auth_user_md5 USING (user_id) WHERE seminar_id = '".$db->f("Seminar_id")."' ORDER BY RAND() ");
+		//Warteposition ablegen
+		$position=1;
+		while ($db3->next_record()) {
+			$db4->query("UPDATE admission_seminar_user SET position = '$position', status = 'awaiting' WHERE user_id = '".$db3->f("user_id")."' AND seminar_id = '".$db->f("Seminar_id")."' ");
+			$position ++;
+		}
+
+		//evtl. verbliebene Plaetze auffuellen
+		update_admission($db->f("Seminar_id"). $send_message);
+		
 		//Veranstaltung lock aufheben und erfolgreichen Losvorgang einragen
 		$db2->query("UPDATE seminare SET admission_selection_take_place ='1' WHERE Seminar_id = '".$db->f("Seminar_id")."' ");
-	}
-}
 
-
-function update_admission ($seminar_id, $send_message=TRUE) {
-
-	$db=new DB_Seminar;
-	$db2=new DB_Seminar;
-	$db3=new DB_Seminar;
-	$db4=new DB_Seminar;
-	$db5=new DB_Seminar;
-	$db6=new DB_Seminar;
-	$messaging=new messaging;
-	
-	//Daten holen / Abfrage ob ueberhaupt begrenzt
-	$db->query("SELECT Seminar_id, Name, admission_endtime, admission_turnout, admission_type, start_time FROM seminare WHERE seminar_id = '$seminar_id' AND ((admission_type = '1'  AND admission_selection_take_place = '1') OR (admission_type = '2'))");
-	if ($db->next_record()) {
-		//Alle zugelassenen Studiengaenge auswaehlen
-		$db2->query("SELECT studiengang_id, quota FROM admission_seminar_studiengang WHERE seminar_id = '$seminar_id' ");
-		while ($db2->next_record()) {
-			//Wiieviel Teilnehmer sind eingetragen und wieviele koennten eingetragen werden?
-			$db3->query("SELECT user_id FROM seminar_user WHERE Seminar_id = '".$db->f("Seminar_id")."' AND admission_studiengang_id = '".$db2->f("studiengang_id")."' ");
-			$db3->next_record();
-			if ($db3->num_rows() < round($db->f("admission_turnout") * ($db2->f("quota") / 100))) {
-				//Studis asuwaehlen, die jetzt aufsteigen koennen
-				$db4->query("SELECT admission_seminar_user.user_id, username FROM admission_seminar_user LEFT JOIN auth_user_md5 USING (user_id) WHERE seminar_id =  '".$db->f("Seminar_id")."'  AND studiengang_id = '".$db2->f("studiengang_id")."' ORDER BY position LIMIT ".($db3->num_rows() - round($db->f("admission_turnout") * ($db2->f("quota") / 100))));
-				while ($db4->next_record()) {
-					$group = select_group ($db->f("start_time"), $db4->f("user_id"));			
-					$db5->query("INSERT INTO seminar_user SET user_id = '".$db4->f("user_id")."', Seminar_id = '".$db->f("Seminar_id")."', status= 'autor', gruppe = '$group', admission_studiengang_id = '".$db2->f("studiengang_id")."', mkdate = '".time()."' ");
-					if ($db5->affected_rows()) {
-						$db6->query("DELETE FROM admission_seminar_user WHERE user_id ='".$db4->f("user_id")."' AND seminar_id = '".$db->f("Seminar_id")."' ");
-						//User benachrichten
-						if (($db6->affected_rows()) && ($send_message)) {
-							$message="Sie sind als Teilnehmer der Veranstaltung **".$db->f("Name")."** eingetragen worden, da für Sie ein Platz frei geworden ist. Ab sofort finden Sie die Veranstaltung in der Übersicht ihrer Veranstaltungen. Damit sind sie auch als Teilnehmer der Präsenzveranstaltung zugelassen.";
-							$messaging->insert_sms ($db4->f("username"), $message, "____%system%____");
-						}
-					}
-				}
+		//User benachrichten
+		if ($send_message) {
+			$db2->query("SELECT admission_seminar_user.user_id, username, position FROM admission_seminar_user LEFT JOIN auth_user_md5 USING (user_id) WHERE seminar_id = '".$db->f("Seminar_id")."' ORDER BY position ");
+			while ($db2->next_record()) {
+				$message="Sie wurden leider im Losverfahren der Veranstaltung **".$db->f("Name")."** __nicht__ ausgelost. Sie wurden jedoch auf Position ".$db2->f("position")." auf die Warteliste gesetzt. Das System wird Sie automatisch eintragen, sobald ein Platz für Sie frei wird.";
+				$messaging->insert_sms ($db2->f("username"), $message, "____%system%____");
 			}
-		}
-		//Warteposition der restlichen User neu eintragen
-		$db4->query("SELECT user_id FROM admission_seminar_user WHERE seminar_id =  '".$db->f("Seminar_id")."' ORDER BY position ");
-		$position=1;
-		while ($db4->next_record()) {
-			$db4->query("UPDATE admission_seminar_user SET position = '$position' WHERE user_id = '".$db4->f("user_id")."' AND seminar_id = '".$db->f("Seminar_id")."' ");
-			$position++;
 		}
 	}
 }
