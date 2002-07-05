1af4dba3ab6e4f4e12dea6afdab8644fcc39dea0
Cornelis Kater <kater@zqs.uni-hannover.de>
2002-07-05 13:19:26 +0000

    Debug on

--- htdocs/admission.inc.php
+++ htdocs/admission.inc.php
@@ -65,11 +65,10 @@ function renumber_admission ($seminar_id, $send_message=TRUE) {
 	$db3=new DB_Seminar;
 	$db4=new DB_Seminar;
 	$messaging=new messaging;
-
+	echo r, $seminar_id;
 	//Daten holen / Abfrage ob ueberhaupt begrenzt
-	echo "SELECT Seminar_id, Name FROM seminare WHERE Seminar_id = '$seminar_id' AND ((admission_type = '1'  AND admission_selection_take_place = '1') OR (admission_type = '2'))";
+	$db->query("SELECT Seminar_id, Name FROM seminare WHERE Seminar_id = '$seminar_id' AND ((admission_type = '1'  AND admission_selection_take_place = '1') OR (admission_type = '2'))");
 	if ($db->next_record()) {
-		echo jaa;
 		//Liste einlesen
 		$db2->query("SELECT user_id FROM admission_seminar_user WHERE seminar_id =  '".$db->f("Seminar_id")."' AND status = 'awaiting' ORDER BY position ");
 		$position=1;
@@ -103,6 +102,8 @@ function update_admission ($seminar_id, $send_message=TRUE) {
 	$db4=new DB_Seminar;
 	$db5=new DB_Seminar;
 	$messaging=new messaging;
+	echo r, $seminar_id;
+	
 	//Daten holen / Abfrage ob ueberhaupt begrenzt
 	$db->query("SELECT Seminar_id, Name, admission_endtime, admission_turnout, admission_type, start_time FROM seminare WHERE Seminar_id = '$seminar_id' AND admission_selection_take_place = '1' ");
 	if ($db->next_record()) {
