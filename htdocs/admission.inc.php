9e2ca8e76fd21b357f629a8814d2ee611bb1e8af
Cornelis Kater <kater@zqs.uni-hannover.de>
2002-07-05 13:24:40 +0000

    Debug off.... WAAH! gefunden, ein PUNKT zuviel.

--- htdocs/admission.inc.php
+++ htdocs/admission.inc.php
@@ -65,7 +65,7 @@ function renumber_admission ($seminar_id, $send_message=TRUE) {
 	$db3=new DB_Seminar;
 	$db4=new DB_Seminar;
 	$messaging=new messaging;
-	echo r, $seminar_id;
+
 	//Daten holen / Abfrage ob ueberhaupt begrenzt
 	$db->query("SELECT Seminar_id, Name FROM seminare WHERE Seminar_id = '$seminar_id' AND ((admission_type = '1'  AND admission_selection_take_place = '1') OR (admission_type = '2'))");
 	if ($db->next_record()) {
@@ -102,7 +102,6 @@ function update_admission ($seminar_id, $send_message=TRUE) {
 	$db4=new DB_Seminar;
 	$db5=new DB_Seminar;
 	$messaging=new messaging;
-	echo u, $seminar_id;
 	
 	//Daten holen / Abfrage ob ueberhaupt begrenzt
 	$db->query("SELECT Seminar_id, Name, admission_endtime, admission_turnout, admission_type, start_time FROM seminare WHERE Seminar_id = '$seminar_id' AND admission_selection_take_place = '1' ");
