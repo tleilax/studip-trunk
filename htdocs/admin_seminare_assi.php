b07e6b2424af94ffb8d1fe9feaf3f1664dc17080
André Noack <noackorama@gmail.com>
2002-07-22 06:53:16 +0000

    =3 ???

--- htdocs/admin_seminare_assi.php
+++ htdocs/admin_seminare_assi.php
@@ -1,4 +1,4 @@
-=3<?
+<?
 /*
 admin_seminare_assi.php - Seminar-Assisten von Stud.IP.
 Copyright (C) 2000 Stefan Suchi <suchi@gmx.de>, Ralf Stockmann <rstockm@gwdg.de>, Cornelis Kater <ckater@gwdg.de>
