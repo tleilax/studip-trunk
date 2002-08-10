e00720c243926022dd0c6e8542dc8abc07c5f7a6
Peter Thienel <thienel@data-quest.de>
2002-08-10 00:06:05 +0000

    Linkerkennung für ftp (username) und mail verbessert

--- htdocs/visual.inc.php
+++ htdocs/visual.inc.php
@@ -451,8 +451,8 @@ function FixLinks($data = "", $fix_nl = TRUE, $nl_to_br = FALSE){
         $replace = array("\\1http://www.", "\\1ftp://ftp.");
         $fixed_text = preg_replace($pattern, $replace, $data);
         
-        $pattern = array("'(\[([^\n\f\]]+)\])?(((http://)|(https://)|(ftp://([_a-z0-9-]+@)?))[_a-z0-9-]+(\.[_a-z0-9-]+)+)'ie",
-                                                                         "'(\[([^\n\f\]]+)\])?([-a-z0-9_]+(\.[_a-z0-9-]+)*@([_a-z0-9-]+(\.[_a-z0-9-]+)+))'ie");
+        $pattern = array("'(\[([^\n\f\]]+)\])?(((http://)|(https://)|(ftp://([_a-z0-9-]+@)?))[_a-z0-9-]+(\.[_a-z0-9-]+)+(/[_a-z0-9-]*)*\.?[_a-z0-9-]*)'ie",
+                                                                         "'(?<=\s)(\[([^\n\f\]]+)\])?([-a-z0-9_]+(\.[_a-z0-9-]+)*@([_a-z0-9-]+(\.[_a-z0-9-]+)+))'ie");
         $replace = array("preg_call_link('\\2', '\\3', 'LINK')", "preg_call_link('\\2', '\\3', 'MAIL')");
         $fixed_text = preg_replace($pattern, $replace, $fixed_text);
         
