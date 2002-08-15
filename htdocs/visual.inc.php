8eaa218e2d5a8aa86b93ae23e1eeb1895d1ee2d3
André Noack <noackorama@gmail.com>
2002-08-15 12:32:39 +0000

    hover änderungen

--- htdocs/visual.inc.php
+++ htdocs/visual.inc.php
@@ -91,50 +91,46 @@ class cssClassSwitcher {
 	var $class = array("steelgraulight", "steel1");                 //Klassen
 	var $headerClass = "steel";
 	var $classcnt = 0;                //Counter
-	var        $hoverclass = array("hover1", "hover2");
+	var $hovercolor = array("#B7C2E2","#CED8F2");
+	var $nohovercolor = array("#E2E2E2","#F2F2F2");
 	var $JSenabled = FALSE;
 	var $hoverenabled = FALSE;
 	
-	function cssClassSwitcher($class = "",$headerClass = "",$hoverclass = ""){
+	function cssClassSwitcher($class = "",$headerClass = "",$hovercolor = "",$nohovercolor = ""){
 		if ($GLOBALS["auth"]->auth["jscript"]) $this->JSenabled = TRUE;
 		if (is_array($class)) $this->class = $class;
 		if ($headerClass) $this->headerClass = $headerClass;
-		if (is_array($hoverclass)) $this->hoverclass = $hoverclass;
+		if (is_array($hovercolor)) $this->hovercolor = $hovercolor;
+		if (is_array($nohovercolor)) $this->nohovercolor = $nohovercolor;
 	}
 	
-	function enableHover($newclass = ""){
-		if (is_array($newclass))
-			$this->class = $newclass;
-		else
-			$this->class = array("nohover1", "nohover2");
-			
-		$this->hoverenabled = TRUE;
+	function enableHover($hovercolor = "",$nohovercolor = ""){
+		if (is_array($hovercolor)) $this->hovercolor = $hovercolor;
+		if (is_array($nohovercolor)) $this->nohovercolor = $nohovercolor;	
+		if ($this->JSenabled)
+			$this->hoverenabled = TRUE;
 	}
 	
-	function disableHover($newclass = ""){
-		if (is_array($newclass))
-			$this->class = $newclass;
-		else
-			$this->class = array("steelgraulight", "steel1");
-			
+	function disableHover(){
 		$this->hoverenabled = FALSE;
 	}
 	
 	function getHover(){
-		$ret = $this->getFullClass();
 		if($this->hoverenabled && $this->JSenabled){
-			$ret .= " onMouseOver='doHover(this,\"".$this->class[$this->classcnt]."\",\"".$this->hoverclass[$this->classcnt]."\")'".
-				" onMouseOut='doHover(this,\"".$this->hoverclass[$this->classcnt]."\",\"".$this->class[$this->classcnt]."\")' ";
+			$ret = $this->getFullClass();
+			$ret .= " onMouseOver='doHover(this,\"".$this->nohovercolor[$this->classcnt]."\",\"".$this->hovercolor[$this->classcnt]."\")'".
+				" onMouseOut='doHover(this,\"".$this->hovercolor[$this->classcnt]."\",\"".$this->nohovercolor[$this->classcnt]."\")' ";
 		}
 		return $ret;
 	}
 	
 	function getFullClass(){
-		return " class=\"" . $this->class[$this->classcnt] . "\"";
+		$ret = ($this->hoverenabled) ?  " style=\"background-color:".$this->nohovercolor[$this->classcnt]."\" " : " class=\"" . $this->class[$this->classcnt] . "\" ";
+		return $ret;
 	}
 	
 	function getClass() {
-		return $this->class[$this->classcnt];
+		return ($this->hoverenabled) ? "\"  style=\"background-color:".$this->nohovercolor[$this->classcnt]." " : $this->class[$this->classcnt];
 	}
 
 	function getHeaderClass() {
@@ -156,25 +152,33 @@ class cssClassSwitcher {
 		$ret = "";
 		if($GLOBALS["auth"]->auth["jscript"] && !$is_called) {
 			$ret = "<script type=\"text/javascript\">
-					function doHover(theRow, theFromClass, theToClass){
-						if (theFromClass == '' || theToClass == '' || typeof(theRow.className) == 'undefined') {
+					function hexToRgb(hexcolor){
+						var rgb = 'rgb(' + parseInt(hexcolor.substr(1,2),16) + ',' + parseInt(hexcolor.substr(3,2),16) + ','
+									+ parseInt(hexcolor.substr(5,2),16) +')';
+						return rgb;
+					}
+					function doHover(theRow, theFromColor, theToColor){
+						if (theFromColor == '' || theToColor == '') {
 							return false;
 						}
-						if (typeof(document.getElementsByTagName) != 'undefined') {
+						if (document.getElementsByTagName) {
 							var theCells = theRow.getElementsByTagName('td');
 						}
-						else if (typeof(theRow.cells) != 'undefined') {
+						else if (theRow.cells) {
 							var theCells = theRow.cells;
 						} else {
 							return false;
 						}
+						hexToRgb(theToColor);
 						if (theRow.tagName.toLowerCase() != 'tr'){
-							theRow.className = theToClass;
+							if ((theRow.style.backgroundColor.toLowerCase() == theFromColor.toLowerCase()) || (theRow.style.backgroundColor == hexToRgb(theFromColor))) {
+								theRow.style.backgroundColor = theToColor;
+							}
 						} else {
 							var rowCellsCnt  = theCells.length;
 							for (var c = 0; c < rowCellsCnt; c++) {
-								if (theCells[c].className == theFromClass) {
-									theCells[c].className = theToClass;
+								if ((theCells[c].style.backgroundColor == theFromColor.toLowerCase()) || (theCells[c].style.backgroundColor == hexToRgb(theFromColor))) {
+									theCells[c].style.backgroundColor = theToColor;
 								}
 							}
 						}
@@ -583,7 +587,7 @@ array  ("kategorie"  => "Information:",
 		)
 	),
 array  ("kategorie" => "Aktionen:",
-	       "eintrag" => array	(	
+		   "eintrag" => array	(	
 						array (	"icon" => "pictures/ausruf_small.gif",
 								"text"  => "es sind noch 19 Veranstaltungen vorhanden."
 								)
