c86e622caeb6a62c666f8f3055b4f7eb9818f392
Stefan Suchi <suchi@data-quest.de>
2002-08-18 16:13:44 +0000

    md5.js wird hier nicht mehr gebraucht

--- htdocs/admin_seminare1.php
+++ htdocs/admin_seminare1.php
@@ -34,9 +34,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 	include ("$ABSOLUTE_PATH_STUDIP/header.php");   // Output of Stud.IP head
 ?>
 
- <script language="javascript" src="./md5.js"></script>
-
-
  <SCRIPT language="JavaScript">
 <!--
 
