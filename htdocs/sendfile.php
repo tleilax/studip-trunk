994a293421bc9de92ff0922d5fd04fefdb32a58a
Cornelis Kater <kater@zqs.uni-hannover.de>
2002-08-19 18:11:40 +0000

    Kann nun auch downloads aus Einrichtungen handlen

--- htdocs/sendfile.php
+++ htdocs/sendfile.php
@@ -1,43 +1,57 @@
-<?php
-/*
-sendfile.php - Datei an Browser senden
-Copyright (C) 2000 Cornelis Kater <ckater@gwdg.de>
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+<?
+/**
+* sendfile.php
+* 
+* Send files to the browser an does permchecks
+*
+* @author		Cornelis Kater <ckater@gwdg.de>, Suchi & Berg GmbH <info@data-quest.de>, André Noack André Noack <andre.noack@gmx.net>
+* @access		public
+* @modulegroup	system_core
+* @module		sendfile.php
 */
+
+// +---------------------------------------------------------------------------+
+// This file is part of Stud.IP
+// sendfile.php - Datei an Browser senden
+// Copyright (C) 2000 - 2002 Cornelis Kater <ckater@gwdg.de>
+// Ralf Stockmann <rstockm@gwdg.de>, André Noack André Noack <andre.noack@gmx.net>
+// +---------------------------------------------------------------------------+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or any later version.
+// +---------------------------------------------------------------------------+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// +---------------------------------------------------------------------------+
+
 
 $dont_put_headers=TRUE;
 page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Default_Auth", "perm" => "Seminar_Perm", "user" => "Seminar_User"));
 
-require_once("config.inc.php");
-require_once("datei.inc.php");
-require_once("visual.inc.php");
+require_once ("$ABSOLUTE_PATH_STUDIP/config.inc.php");
+require_once ("$ABSOLUTE_PATH_STUDIP/datei.inc.php");
+require_once ("$ABSOLUTE_PATH_STUDIP/visual.inc.php");
+require_once ("$ABSOLUTE_PATH_STUDIP/functions.php");
 
 $db=new DB_Seminar;
 $db2=new DB_Seminar;
 
 switch ($type) {
-	//We want download from the archive
+	//We want to download from the archive
 	case 1: 
 		$path_file=$ARCHIV_PATH."/".$file_id;
 	break;
-	//We want download from the tmp-folder
+	//We want to download from the tmp-folder
 	case 2:
 		$path_file=$TMP_PATH."/".$file_id;
 	break;
-	//We want download from the regular upload-folder
+	//We want to download from the regular upload-folder
 	default:
 		$path_file=$UPLOAD_PATH."/".$file_id;
 	break;
@@ -160,58 +174,69 @@ switch (strtolower(getFileExtension ($file_name))) {
 	break;
 	}
 
-//Bevor wir Dateien rausschmeissen, sollte passiert noch mal ein Rechtecheck. Dies erfordert allerdings auch, dass die Dateien ausserhalb des Zugriffs des www liegen!
-if (!$perm->have_perm ("user")) {
-	if (!$type) {
-		$db->query("SELECT Lesezugriff FROM seminare LEFT JOIN dokumente USING (seminar_id) WHERE dokument_id = '".$file_id."' ");
-		$db->next_record();
-		if ($db->f("Lesezugriff") != 0)
-			$no_access=TRUE;
-		}
-	else
-		$no_access=TRUE; //nobody darf nie an das Archiv
-	}
-elseif (!$perm->have_perm("root")) {
-	if ($type) {
-		if ($perm->have_perm("admin") && !$perm->have_perm("root"))
-			$db->query ("SELECT archiv.seminar_id FROM archiv LEFT JOIN archiv_user USING (seminar_id) WHERE archiv_file_id = '".$file_id."' ");
-		else
-			$db->query ("SELECT archiv.seminar_id FROM archiv LEFT JOIN archiv_user USING (seminar_id) WHERE user_id = '".$user->id."' AND archiv_file_id = '".$file_id."' ");
-		if (!$db->next_record())
-			$no_access=TRUE;
-		if ($perm->have_perm("admin") && !$perm->have_perm("root")) {
-			$db2->query("SELECT archiv.seminar_id FROM archiv LEFT OUTER JOIN user_inst ON (heimat_inst_id = institut_id) WHERE user_inst.user_id = '".$user->id."' AND user_inst.inst_perms = 'admin'");
-			while ($db2->next_record())  {
-				if($db->f("seminar_id") == $db2->f("seminar_id"))
-					$admin=TRUE;
-				}
-			if ($admin)
-				$no_access=FALSE;
-			else
-				$no_accss=TRUE;
-			}
-		}
-	else {
-		if ($perm->have_perm("admin") && !$perm->have_perm("root"))
-			$db->query ("SELECT Institut_id FROM dokumente LEFT JOIN seminare USING (seminar_id) WHERE dokument_id = '".$file_id."' ");
-		else
-			$db->query ("SELECT seminar_user.user_id, Institut_id FROM dokumente LEFT JOIN seminar_user USING (seminar_id) LEFT JOIN seminare USING (seminar_id) WHERE seminar_user.user_id = '".$user->id."' AND dokument_id = '".$file_id."' ");		
-		if (!$db->next_record()) 
-			$no_access=TRUE;
-		if ($perm->have_perm("admin") && !$perm->have_perm("root")) {
-			$db2->query("SELECT institut_id FROM user_inst  WHERE user_inst.user_id = '".$user->id."' AND user_inst.inst_perms = 'admin'");
-			while ($db2->next_record())  {
-				if($db->f("Institut_id") == $db2->f("institut_id"))
-					$admin=TRUE;
-				}
-			if ($admin)
-				$no_access=FALSE;
-			else
-				$no_accss=TRUE;
-			}
-		}
-	}
+//determine the type of the object we want to download a file from (only in type=0 mode!)
+if (!$type) {
+	$db->query("SELECT seminar_id AS object_id FROM dokumente WHERE dokument_id = '".$file_id."' ");
+	$db->next_record();
+	$object_type = get_object_type($db->f("object_id"));
+	if ($object_type == "inst")
+		$skip_check=TRUE;
+}
 
+//permcheck
+if (($type != 2) || ($skip_check)) { //if type 2 we donload from the tmp directory and skip permchecks
+	if (!$perm->have_perm ("user")) {
+		if (!$type) {
+			$db->query("SELECT Lesezugriff FROM seminare LEFT JOIN dokumente USING (seminar_id) WHERE dokument_id = '".$file_id."' ");
+			$db->next_record();
+			if ($db->f("Lesezugriff") != 0)
+				$no_access=TRUE;
+			}
+		else
+			$no_access=TRUE; //nobody darf nie an das Archiv
+		}
+	elseif (!$perm->have_perm("root")) {
+		if ($type) {
+			if ($perm->have_perm("admin") && !$perm->have_perm("root"))
+				$db->query ("SELECT archiv.seminar_id FROM archiv LEFT JOIN archiv_user USING (seminar_id) WHERE archiv_file_id = '".$file_id."' ");
+			else
+				$db->query ("SELECT archiv.seminar_id FROM archiv LEFT JOIN archiv_user USING (seminar_id) WHERE user_id = '".$user->id."' AND archiv_file_id = '".$file_id."' ");
+			if (!$db->next_record())
+				$no_access=TRUE;
+			if ($perm->have_perm("admin") && !$perm->have_perm("root")) {
+				$db2->query("SELECT archiv.seminar_id FROM archiv LEFT OUTER JOIN user_inst ON (heimat_inst_id = institut_id) WHERE user_inst.user_id = '".$user->id."' AND user_inst.inst_perms = 'admin'");
+				while ($db2->next_record())  {
+					if($db->f("seminar_id") == $db2->f("seminar_id"))
+						$admin=TRUE;
+					}
+				if ($admin)
+					$no_access=FALSE;
+				else
+					$no_accss=TRUE;
+				}
+			}
+		else {
+			if ($perm->have_perm("admin") && !$perm->have_perm("root"))
+				$db->query ("SELECT Institut_id FROM dokumente LEFT JOIN seminare USING (seminar_id) WHERE dokument_id = '".$file_id."' ");
+			else
+				$db->query ("SELECT seminar_user.user_id, Institut_id FROM dokumente LEFT JOIN seminar_user USING (seminar_id) LEFT JOIN seminare USING (seminar_id) WHERE seminar_user.user_id = '".$user->id."' AND dokument_id = '".$file_id."' ");		
+			if (!$db->next_record()) 
+				$no_access=TRUE;
+			if ($perm->have_perm("admin") && !$perm->have_perm("root")) {
+				$db2->query("SELECT institut_id FROM user_inst  WHERE user_inst.user_id = '".$user->id."' AND user_inst.inst_perms = 'admin'");
+				while ($db2->next_record())  {
+					if($db->f("Institut_id") == $db2->f("institut_id"))
+						$admin=TRUE;
+					}
+				if ($admin)
+					$no_access=FALSE;
+				else
+					$no_accss=TRUE;
+				}
+			}
+		}
+	}
+	
 //Nachricht bei verbotenem Download
 if ($no_access) {
 	if ($type)
