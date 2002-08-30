c9112928b6200adbbe38145623e2f1252a466cfb
Stefan Suchi <suchi@data-quest.de>
2002-08-30 20:56:12 +0000

    Leerzeichen ergaenzt

--- htdocs/sms.php
+++ htdocs/sms.php
@@ -146,7 +146,7 @@ if ($cmd=="write") {
 		<blockquote>Schreiben Sie hier eine Nachricht an einen anderen Benutzer:
 		<?
 		if ($SessSemName[0] && $SessSemName["class"] == "inst")
-			echo "<br /><br /><a href=\"institut_main.php\">Zur&uuml;ck zur ausgew&auml;hltenEinrichtung</a>";
+			echo "<br /><br /><a href=\"institut_main.php\">Zur&uuml;ck zur ausgew&auml;hlten Einrichtung</a>";
 		elseif ($SessSemName[0])
 			echo "<br /><br /><a href=\"seminar_main.php\">Zur&uuml;ck zur ausgew&auml;hlten Veranstaltung</a>";
 		?>
