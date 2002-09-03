b33bbb293cd9be51d07139efc5688a9327e04dfa
rstockm <rstockm>
2002-09-03 16:12:37 +0000

    Anpassungen für Institute

--- htdocs/admin_statusgruppe.php
+++ htdocs/admin_statusgruppe.php
@@ -20,7 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
   page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Auth", "perm" => "Seminar_Perm", user => "Seminar_User"));
 	$auth->login_if($auth->auth["uid"] == "nobody");
-  $perm->check("dozent");
+	$perm->check("tutor");
 
 ## Set this to something, just something different...
   $hash_secret = "dslkjjhetbjs";
@@ -29,17 +29,36 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 // -- here you have to put initialisations for the current page
 
-require_once ("$ABSOLUTE_PATH_STUDIP/msg.inc.php");
-require_once ("$ABSOLUTE_PATH_STUDIP/visual.inc.php");
-require_once ("$ABSOLUTE_PATH_STUDIP/functions.php");
-require_once ("$ABSOLUTE_PATH_STUDIP/admission.inc.php");	//Funktionen der Teilnehmerbegrenzung
-require_once ("$ABSOLUTE_PATH_STUDIP/messaging.inc.php");	//Funktionen des Nachrichtensystems
-
-// Start of Output
 	include ("$ABSOLUTE_PATH_STUDIP/html_head.inc.php"); // Output of html head
 	include ("$ABSOLUTE_PATH_STUDIP/header.php");   // Output of Stud.IP head
 	include ("$ABSOLUTE_PATH_STUDIP/links_admin.inc.php");
 
+	require_once ("$ABSOLUTE_PATH_STUDIP/visual.inc.php");
+	require_once ("$ABSOLUTE_PATH_STUDIP/functions.php");
+		
+// Rechtecheck
+
+
+
+	if ($range_id && !$perm->have_perm("root")) {
+		//Sicherheitscheck
+
+	  $range_perm=get_perm($range_id);
+	  if (($ebene=="sem") && ($range_perm!="admin" && $range_perm!="dozent" && $range_perm!="tutor")) 
+			die;
+		if (($ebene=="inst") && ($range_perm!="admin"))
+			die;
+		if (($ebene=="fak") && ($range_perm!="admin")) 
+			die;
+	}
+
+
+	
+		//Sicherheitscheck ob was zum Bearbeiten gewaehlt ist.
+	if (!$range_id) {
+		echo "</tr></td></table>";
+		die;
+	}
 
 // Hilfsfunktionen
 
@@ -68,22 +87,21 @@ function PrintAllStatusgruppen ()
 }
 
 function GetAllSelected ()
-{	global $SessSemName;
+{	global $range_id;
+	$zugeordnet[] = "";
   	$db3=new DB_Seminar;
-	$db3->query ("SELECT DISTINCT user_id FROM statusgruppe_user LEFT JOIN statusgruppen USING(statusgruppe_id) WHERE range_id = '$SessSemName[1]'");
+	$db3->query ("SELECT DISTINCT user_id FROM statusgruppe_user LEFT JOIN statusgruppen USING(statusgruppe_id) WHERE range_id = '$range_id'");
 	while ($db3->next_record()) {
 		$zugeordnet[] = $db3->f("user_id");
 	}
 	RETURN $zugeordnet;
 }
 
-
-
 function PrintAktualStatusgruppen ()
-{	global $SessSemName, $PHP_SELF;
+{	global $range_id, $PHP_SELF;
 	$db=new DB_Seminar;
 	$db2=new DB_Seminar;
-	$db->query ("SELECT name, statusgruppe_id, size FROM statusgruppen WHERE range_id = '$SessSemName[1]' ORDER BY position ASC");
+	$db->query ("SELECT name, statusgruppe_id, size FROM statusgruppen WHERE range_id = '$range_id' ORDER BY position ASC");
 	$AnzahlStatusgruppen = $db->num_rows();
 	if ($AnzahlStatusgruppen == 0) {
 		$infobox = array	(			
@@ -108,7 +126,7 @@ function PrintAktualStatusgruppen ()
 				          <td width=\"5%\">";
 		printf ("            	  <input type=\"IMAGE\" name=\"%s\" src=\"./pictures/move.gif\" border=\"0\">&nbsp; </td>", $statusgruppe_id);
 		printf ("	          <td width=\"95%%\" class=\"topic\">&nbsp; %s</td>",$db->f("name"));
-		printf ( "	          <td width=\"5%%\"><a href=\"$PHP_SELF?cmd=remove_statusgruppe&statusgruppe_id=%s\"><img src=\"pictures/lighttrash.gif\" width=\"11\" height=\"17\" border=\"0\"></a></td>",$statusgruppe_id);
+		printf ( "	          <td width=\"5%%\"><a href=\"$PHP_SELF?cmd=remove_statusgruppe&statusgruppe_id=%s&range_id=%s\"><img src=\"pictures/lighttrash.gif\" width=\"11\" height=\"17\" border=\"0\"></a></td>",$statusgruppe_id, $range_id);
 		echo 	"</tr>";
 
 		$db2->query ("SELECT statusgruppe_user.user_id, Vorname, Nachname, username FROM statusgruppe_user LEFT JOIN auth_user_md5 USING(user_id) WHERE statusgruppe_id = '$statusgruppe_id'");
@@ -121,7 +139,7 @@ function PrintAktualStatusgruppen ()
 			}
 			printf ("     <tr><td><font color=\"%s\">$k</font></td>", $farbe);
 			printf ("       <td class=\"blank\">%s&nbsp; %s</td>",$db2->f("Vorname"), $db2->f("Nachname"));
-			printf ( "	   <td width=\"5%%\"><a href=\"$PHP_SELF?cmd=remove_person&statusgruppe_id=%s&username=%s\"><img src=\"pictures/trash.gif\" width=\"11\" height=\"17\" border=\"0\"></a></td>", $statusgruppe_id, $db2->f("username"));
+			printf ( "	   <td width=\"5%%\"><a href=\"$PHP_SELF?cmd=remove_person&statusgruppe_id=%s&username=%s&range_id=%s\"><img src=\"pictures/trash.gif\" width=\"11\" height=\"17\" border=\"0\"></a></td>", $statusgruppe_id, $db2->f("username"), $range_id);
 			echo "	</tr>";
 			$k++;
 		}
@@ -136,20 +154,27 @@ function PrintAktualStatusgruppen ()
 		$i++;
 		echo "</table>";
 		if ($i < $AnzahlStatusgruppen) {
-			printf ("<p align=\"center\"><a href=\"$PHP_SELF?cmd=swap&statusgruppe_id=%s\"><img src=\"pictures/move_up.gif\" width=\"13\" height=\"11\" border=\"0\"></a>",$statusgruppe_id); 
-			printf ("&nbsp; &nbsp; &nbsp; <a href=\"$PHP_SELF?cmd=swap&statusgruppe_id=%s\"><img src=\"pictures/move_down.gif\" width=\"13\" height=\"11\" border=\"0\"></a> </p>",$statusgruppe_id);
+			printf ("<p align=\"center\"><a href=\"$PHP_SELF?cmd=swap&statusgruppe_id=%s&range_id=%s\"><img src=\"pictures/move_up.gif\" width=\"13\" height=\"11\" border=\"0\"></a>",$statusgruppe_id, $range_id); 
+			printf ("&nbsp; &nbsp; &nbsp; <a href=\"$PHP_SELF?cmd=swap&statusgruppe_id=%s&range_id=%s\"><img src=\"pictures/move_down.gif\" width=\"13\" height=\"11\" border=\"0\"></a> </p>",$statusgruppe_id, $range_id);
 		}
 	}
 }
 
 function PrintSearchResults ($search_exp)
-{	global $SessSemName;
+{	global $SessSemName, $range_id;
 		$db=new DB_Seminar;
-		$query = "SELECT a.user_id, username, Vorname, Nachname, perms FROM auth_user_md5 a ".		
-			"LEFT JOIN seminar_user b ON (b.user_id=a.user_id AND b.seminar_id='$SessSemName[1]')  ".
+		if (get_object_type($range_id) == "sem") {
+			$query = "SELECT a.user_id, username, Vorname, Nachname, perms FROM auth_user_md5 a ".		
+			"LEFT JOIN seminar_user b ON (b.user_id=a.user_id AND b.seminar_id='$range_id')  ".
 			"WHERE perms IN ('autor','tutor','dozent') AND ISNULL(b.seminar_id) AND ".
 			"(username LIKE '%$search_exp%' OR Vorname LIKE '%$search_exp%' OR Nachname LIKE '%$search_exp%') ".
 			"ORDER BY Nachname";
+		} else {
+			$query = "SELECT DISTINCT auth_user_md5.user_id, Vorname, Nachname, username, perms ".
+			"FROM auth_user_md5 LEFT JOIN user_inst ON user_inst.user_id=auth_user_md5.user_id AND Institut_id = '$inst_id' ".
+			"WHERE perms !='root' AND (user_inst.inst_perms = 'user' OR user_inst.inst_perms IS NULL) ".
+			"AND (Vorname LIKE '%$search_exp%' OR Nachname LIKE '%$search_exp%' OR username LIKE '%$search_exp%') ORDER BY Nachname ";
+		}
 		$db->query($query); // results all users which are not in the seminar
 		if (!$db->num_rows()) {
 			echo "&nbsp; keine Treffer&nbsp; ";
@@ -162,13 +187,19 @@ function PrintSearchResults ($search_exp)
 		}
 }
 
-function PrintAktualMembers ()
-{	global $SessSemName;
+function PrintAktualMembers ($range_id)
+{	
 	$bereitszugeordnet = GetAllSelected();
-	echo "<font size=\"-1\">&nbsp; TeilnehmerInnen der Veranstaltung</font><br>";
+	if (get_object_type($range_id) == "sem") {
+		echo "<font size=\"-1\">&nbsp; TeilnehmerInnen der Veranstaltung</font><br>";
+		$query = "SELECT seminar_user.user_id, username, Nachname, Vorname, perms FROM auth_user_md5 LEFT JOIN seminar_user USING(user_id)  WHERE Seminar_id = '$range_id' ORDER BY Nachname ASC";
+	} else {
+		echo "<font size=\"-1\">&nbsp; MitarbeiterInnen des Instituts</font><br>";
+		$query = "SELECT user_inst.user_id, username, Nachname, Vorname, inst_perms AS perms FROM auth_user_md5 LEFT JOIN user_inst USING(user_id)  WHERE Institut_id = '$range_id' AND inst_perms != 'user' ORDER BY Nachname ASC";
+	}
 	echo "&nbsp; <select size=\"10\" name=\"AktualMembers[]\" multiple>";
 	$db=new DB_Seminar;
-	$db->query ("SELECT seminar_user.user_id, username, Nachname, Vorname, perms FROM auth_user_md5 LEFT JOIN seminar_user USING(user_id)  WHERE Seminar_id = '$SessSemName[1]' ORDER BY Nachname ASC");
+	$db->query ($query);
 	while ($db->next_record()) {
 		if (in_array($db->f("user_id"), $bereitszugeordnet)) {
 			$tmpcolor = "#777777";
@@ -182,8 +213,8 @@ function PrintAktualMembers ()
 
 //Hilfsfunktion, erzeugt eine lsite mit allen ids der einrichtungen
 function get_inst_list(){
-	global $SessSemName;
-	$db = new DB_Seminar("SELECT Institut_id FROM seminar_inst WHERE seminar_id='$SessSemName[1]'"); //beteiligte Einrichtungen
+	global $range_id;
+	$db = new DB_Seminar("SELECT Institut_id FROM seminar_inst WHERE seminar_id='$range_id'"); //beteiligte Einrichtungen
 	$value_list = "";
 	$result[] = $SessSemName[5]; //Heimatinstitut
 	if ($db->num_rows()){
@@ -196,14 +227,14 @@ function get_inst_list(){
 }
 
 function PrintInstitutMembers ()
-{	global $SessSemName;
+{	global $range_id;
 	echo "<font size=\"-1\">&nbsp; MitarbeiterInnen der Institute</font><br>";
 	echo "&nbsp; <select name=\"InstitutMembers\">";
 	$db=new DB_Seminar;
 	$value_list = get_inst_list();
 	$query = "SELECT DISTINCT b.user_id, username, Vorname, Nachname, inst_perms, perms FROM user_inst a ".
 			"LEFT JOIN auth_user_md5  b USING(user_id) ".
-			"LEFT JOIN seminar_user c ON (c.user_id=a.user_id AND c.seminar_id='$SessSemName[1]')  ".
+			"LEFT JOIN seminar_user c ON (c.user_id=a.user_id AND c.seminar_id='$range_id')  ".
 			"WHERE a.Institut_id IN($value_list) AND a.inst_perms IN ('tutor','dozent') AND ISNULL(c.seminar_id) ORDER BY Nachname";
 	$db->query($query); // ergibt alle berufbaren Personen
 		printf ("<option>---</option>");
@@ -216,10 +247,7 @@ function PrintInstitutMembers ()
 
 // Abfrage der Formulare und Aktionen
 
-	if (!$range_id) {
-		$range_id = $SessSemName[1];
-	}
-
+//	echo $range_id;
 
 	// neue Statusgruppe hinzufuegen
 
@@ -228,14 +256,13 @@ function PrintInstitutMembers ()
 		$mkdate = time();
 		$chdate = time();
 		$db=new DB_Seminar;
-		$db->query ("SELECT position FROM statusgruppen WHERE range_id = '$SessSemName[1]' ORDER BY position DESC");
+		$db->query ("SELECT position FROM statusgruppen WHERE range_id = '$range_id' ORDER BY position DESC");
 		if ($db->next_record()) {
 			$position = $db->f("position")+1;
 		} else {
 			$position = "1";
 		}
-		$db->query("INSERT INTO statusgruppen SET statusgruppe_id = '$statusgruppe_id', name = '$new_statusgruppe_name', range_id= '$SessSemName[1]', position='$position', size = '$new_statusgruppe_size', mkdate = '$mkdate', chdate = '$chdate'");
-
+		$db->query("INSERT INTO statusgruppen SET statusgruppe_id = '$statusgruppe_id', name = '$new_statusgruppe_name', range_id= '$range_id', position='$position', size = '$new_statusgruppe_size', mkdate = '$mkdate', chdate = '$chdate'");
 	}
 	
 	// bestehende Statusgruppe in Textfeld
@@ -265,12 +292,12 @@ function PrintInstitutMembers ()
 				}
 			}
 		}
-		if ($InstitutMembers != "---") {
+		if (isset($InstitutMembers) && $InstitutMembers != "---") {
 			$user_id = get_userid($InstitutMembers);
 			$db->query("SELECT * FROM statusgruppe_user WHERE statusgruppe_id = '$statusgruppe_id' AND user_id = '$user_id'");
 			if (!$db->next_record()) {			
 				$db->query("INSERT INTO statusgruppe_user SET statusgruppe_id = '$statusgruppe_id', user_id = '$user_id'");
-				$db->query("INSERT INTO seminar_user SET Seminar_id = '$SessSemName[1]', user_id = '$user_id', status = 'autor', gruppe = '6' , mkdate = '$mkdate'");
+				$db->query("INSERT INTO seminar_user SET Seminar_id = '$range_id', user_id = '$user_id', status = 'autor', gruppe = '6' , mkdate = '$mkdate'");
 			}
 		}
 		if ($Freesearch != "") {
@@ -279,11 +306,12 @@ function PrintInstitutMembers ()
 				$db->query("SELECT * FROM statusgruppe_user WHERE statusgruppe_id = '$statusgruppe_id' AND user_id = '$user_id'");
 				if (!$db->next_record()) {
 					$db2->query("INSERT INTO statusgruppe_user SET statusgruppe_id = '$statusgruppe_id', user_id = '$user_id'");
-					$db2->query("INSERT INTO seminar_user SET Seminar_id = '$SessSemName[1]', user_id = '$user_id', status = 'autor', gruppe = '6' , mkdate = '$mkdate'");
+					if (get_object_type($range_id) == "sem") {
+						$db2->query("INSERT INTO seminar_user SET Seminar_id = '$range_id', user_id = '$user_id', status = 'autor', gruppe = '6' , mkdate = '$mkdate'");
+					} 
 				}
 			}
 		}
-		
 	}
 	
 	// Entfernen von Personen aus einer Statusgruppe
@@ -308,14 +336,13 @@ function PrintInstitutMembers ()
 
 		// Neusortierung
 		
-		$db->query("SELECT * FROM statusgruppen WHERE range_id = '$SessSemName[1]' AND position > '$position'");
+		$db->query("SELECT * FROM statusgruppen WHERE range_id = '$range_id' AND position > '$position'");
 		while ($db->next_record()) {
 			$new_position = $db->f("position")-1;
 			$statusgruppe_id = $db->f("statusgruppe_id");
 			$db2=new DB_Seminar;
 			$db2->query("UPDATE statusgruppen SET position =  '$new_position' WHERE statusgruppe_id = '$statusgruppe_id'");
 		}
-		
 	}
 	
 	// Aendern der Position
@@ -327,7 +354,7 @@ function PrintInstitutMembers ()
 			$next_position = $current_position + 1;
 			$db2=new DB_Seminar;
 			$db2->query("UPDATE statusgruppen SET position =  '$next_position' WHERE statusgruppe_id = '$statusgruppe_id'");
-			$db2->query("UPDATE statusgruppen SET position =  '$current_position' WHERE range_id = '$SessSemName[1]' AND position = '$next_position' AND statusgruppe_id != '$statusgruppe_id'");
+			$db2->query("UPDATE statusgruppen SET position =  '$current_position' WHERE range_id = '$range_id' AND position = '$next_position' AND statusgruppe_id != '$statusgruppe_id'");
 		}
 	}
 
@@ -366,6 +393,9 @@ function PrintInstitutMembers ()
   <tr>
     <td align="right" width="50%" class="steel1">
  	<form action="<? echo $PHP_SELF ?>?cmd=move_old_statusgruppe" method="POST">
+ 	<?
+  	  echo"<input type=\"HIDDEN\" name=\"range_id\" value=\"$range_id\">";
+  	?>
             &nbsp;
 	<? 
 	PrintAllStatusgruppen (); 
@@ -374,6 +404,9 @@ function PrintInstitutMembers ()
         </form><br></td>
     <td align="right" width="50%" NOWRAP class="steel1">
 	<form action="<? echo $PHP_SELF ?>?cmd=add_new_statusgruppe" method="POST">
+	<?
+  	  echo"<input type=\"HIDDEN\" name=\"range_id\" value=\"$range_id\">";
+  	?>
         <font size="2">Name: </font>
         <input type="text" name="new_statusgruppe_name" value="<? echo $statusgruppe_name;?>">
         &nbsp; &nbsp; &nbsp; <font size="2">Anzahl:</font> 
@@ -392,11 +425,15 @@ function PrintInstitutMembers ()
     <td class="steelgraudunkel" valign="top" width="50%">
     	<br>
 	<?
-	PrintAktualMembers ();
+	if (get_object_type($range_id) == "sem" || get_object_type($range_id) == "inst") {
+		PrintAktualMembers ($range_id);
+	}
 	?>
 	<br><br>
 	<?
-	PrintInstitutMembers ();
+	if (get_object_type($range_id) == "sem") {
+		PrintInstitutMembers ();
+	}
 	?>
           <br><br>
 	<?
