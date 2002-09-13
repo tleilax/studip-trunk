766d5d1498ec4214bf2e722b5aabc9c2bcc9ef76
Cornelis Kater <kater@zqs.uni-hannover.de>
2002-09-13 11:32:14 +0000

    Bugfix Veranstaltungstermin

--- htdocs/dates.inc.php
+++ htdocs/dates.inc.php
@@ -80,32 +80,41 @@ function veranstaltung_beginn ($seminar_id='', $art='', $semester_start_time='',
 					$return_int=$vorles_beginn+604800;
 				}
 			}
-		else //anderer Startzeitpunkt gewaehlt
-			{
+		//anderer Startzeitpunkt gewaehlt
+		else {
+			//kein gueltiger Termin bekannt
 			if ($term_data["start_termin"]<1) {
 				$return_string.="nicht bekannt";
 				$return_int=-1;
-			}	
-			else {
+			//gueltiger Termin bekannt
+			} else {
 				$return_string.=date ("d.m.Y", $term_data["start_termin"]);
 				$return_int=$term_data["start_termin"];
 			}
-			if ($term_data["turnus_data"][0]["start_stunde"]) {
-				$return_string.=", ". $term_data["turnus_data"][0]["start_stunde"]. ":"; 
-				if (($term_data["turnus_data"][0]["start_minute"] > 0)  &&  ($term_data["turnus_data"][0]["start_minute"] < 10))
-					$return_string.="0". $term_data["turnus_data"][0]["start_minute"];
-				elseif ($term_data["turnus_data"][0]["start_minute"] > 10)
-					$return_string.=$term_data["turnus_data"][0]["start_minute"];
-				if (!$term_data["turnus_data"][0]["start_minute"])
-					$return_string.="00";
-				if (($term_data["turnus_data"][0]["end_stunde"] != $term_data["turnus_data"][0]["start_stunde"]) && ($term_data["turnus_data"][0]["end_minute"] !=$term_data["turnus_data"][0]["start_minute"])) {
-					$return_string.= " - ". $term_data["turnus_data"][0]["end_stunde"]. ":";
-					if (($term_data["turnus_data"][0]["end_minute"] > 0)  &&  ($term_data["turnus_data"][0]["end_minute"] < 10))
-						$return_string.="0".$term_data["turnus_data"][0]["end_minute"];
-					elseif ($term_data["turnus_data"][0]["end_minute"] > 10)
-						$return_string.=$term_data["turnus_data"][0]["end_minute"];
-					if (!$term_data["turnus_data"][0]["end_minute"])
-						$return_string.="00";
+			foreach ($term_data["turnus_data"] as $val) {
+				$dow = $val["day"];
+				if ($dow == 7)
+					$dow=0;
+				if ($dow == date("w", $term_data["start_termin"])) {
+					if ($val["start_stunde"]) {
+						$return_string.=", ". $val["start_stunde"]. ":"; 
+						if (($val["start_minute"] > 0)  &&  ($val["start_minute"] < 10))
+							$return_string.="0". $val["start_minute"];
+						elseif ($val["start_minute"] > 10)
+							$return_string.=$val["start_minute"];
+						if (!$val["start_minute"])
+							$return_string.="00";
+						if (!(($val["end_stunde"] != $val["start_stunde"]) && ($val["end_minute"] !=$val["start_minute"]))) {
+							$return_string.= " - ". $val["end_stunde"]. ":";
+							if (($val["end_minute"] > 0)  &&  ($val["end_minute"] < 10))
+								$return_string.="0".$val["end_minute"];
+							elseif ($val["end_minute"] > 10)
+								$return_string.=$val["end_minute"];
+							if (!$val["end_minute"])
+								$return_string.="00";
+							}	
+						}
+					break;
 					}
 				}
 			}
