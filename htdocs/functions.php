03f6a4f469b835d7473aaf8e22abf0b417f5545a
Cornelis Kater <kater@zqs.uni-hannover.de>
2002-09-26 20:32:17 +0000

    Zu Ende kommentiert....

--- htdocs/functions.php
+++ htdocs/functions.php
@@ -38,19 +38,19 @@
 * This function "opens" a Veranstaltung to work with it
 *
 * The following variables will bet set:
-*	$SessionSeminar					Veranstaltung id
-*	$SessSemName[0]					Veranstaltung name
-*	$SessSemName[1]					Veranstaltung id
-*	$SessSemName[2]					Veranstaltung ort (room)
-*	$SessSemName[3]					Veranstaltung Untertitel (subtitle)
-*	$SessSemName[4]					Veranstaltung start_time (the Semester start_time)
-*	$SessSemName[5]					Veranstaltung institut_id (the home-intitute)
-*	$SessSemName["art"]				Veranstaltung type in alphanumeric form
-*	$SessSemName["art_num"]			Veranstaltung type in numeric form
-*	$SessSemName["art_generic"]		Veranstaltung generic type in alhanumeric form (self description)
-*	$SessSemName["class"]				Veranstaltung class (sem or inst, in this function always sem)
-*	$loginfilelast[$sem_id]				last login-time to the Veranstaltung
-*	$loginfilenowt[$sem_id]				current login-time to the Veranstaltung
+*	$SessionSeminar					Veranstaltung id<br>
+*	$SessSemName[0]					Veranstaltung name<br>
+*	$SessSemName[1]					Veranstaltung id<br>
+*	$SessSemName[2]					Veranstaltung ort (room)<br>
+*	$SessSemName[3]					Veranstaltung Untertitel (subtitle)<br>
+*	$SessSemName[4]					Veranstaltung start_time (the Semester start_time)<br>
+*	$SessSemName[5]					Veranstaltung institut_id (the home-intitute)<br>
+*	$SessSemName["art"]				Veranstaltung type in alphanumeric form<br>
+*	$SessSemName["art_num"]			Veranstaltung type in numeric form<br>
+*	$SessSemName["art_generic"]		Veranstaltung generic type in alhanumeric form (self description)<br>
+*	$SessSemName["class"]				Veranstaltung class (sem or inst, in this function always sem)<br>
+*	$loginfilelast[$sem_id]				last login-time to the Veranstaltung<br>
+*	$loginfilenowt[$sem_id]				current login-time to the Veranstaltung<br>
 *
 * @param		string	the id of the Veranstaltung
 *
@@ -82,7 +82,6 @@ function openSem ($sem_id) {
 	}
 }
 
-
 /**
 * This function "opens" an Einrichtung to work with it
 *
@@ -90,15 +89,15 @@ function openSem ($sem_id) {
 * especially if you look at the variable names....
 *
 * The following variables will bet set:
-*	$SessionSeminar					Einrichtung id
-*	$SessSemName[0]					Einrichtung name
-*	$SessSemName[1]					Einrichtung id
-*	$SessSemName["art"]				Einrichtung type in alphanumeric form
-*	$SessSemName["art_num"]			Einrichtung type in numeric form
-*	$SessSemName["art_generic"]		Einrichtung generic type in alhanumeric form (self description)
-*	$SessSemName["class"]				Einrichtung class (sem or inst, in this function always inst)
-*	$loginfilelast[$sem_id]				last login-time to the Einrichtung
-*	$loginfilenowt[$sem_id]				current login-time to the Einrichtung
+*	$SessionSeminar					Einrichtung id<br>
+*	$SessSemName[0]					Einrichtung name<br>
+*	$SessSemName[1]					Einrichtung id<br>
+*	$SessSemName["art"]				Einrichtung type in alphanumeric form<br>
+*	$SessSemName["art_num"]			Einrichtung type in numeric form<br>
+*	$SessSemName["art_generic"]		Einrichtung generic type in alhanumeric form (self description)<br>
+*	$SessSemName["class"]				Einrichtung class (sem or inst, in this function always inst)<br>
+*	$loginfilelast[$sem_id]				last login-time to the Einrichtung<br>
+*	$loginfilenowt[$sem_id]				current login-time to the Einrichtung<br>
 *
 * @param		string	the id of the Veranstaltung
 *
@@ -212,7 +211,6 @@ function lastActivity ($sem_id) {
 * @return		array		returns an array of my object_ids
 *
 */
-
 function get_my_administrable_objects($user_id='') {
 	global $user, $perm;
 	
@@ -249,10 +247,9 @@ function get_my_administrable_objects($user_id='') {
 * Einrichtungen, Veranstaltungen and Fakultaeten
 * 
 * @param		string	seminar_id	the seminar_id of the seminar to calculate
-* @return		string	return "ins" (Institut), "sem" (Veranstaltung), "fak" (Fakultaeten)
+* @return		string	return "ins" (Einrichtung), "sem" (Veranstaltung), "fak" (Fakultaeten)
 *
 */
-
 function get_object_type($seminar_id) {
 	 $db=new DB_Seminar;
 	
@@ -274,15 +271,18 @@ function get_object_type($seminar_id) {
 	return FALSE;
 }
 
-// functions.php - This file contains various functions
-// functions.php - author: studip crew
-// functions.php - version: $Id$
-
-////
-// !dunno
-// A longer, more complete description would go here
-// We might include details of what the function parameters do and what the 
-// result should be, etc.
+/**
+* The function calculate one of the group colors unique for the Semester of the Veranstaltung
+*
+* It calculate a unique color number to create the initial entry for a new user in a Veranstaltung.
+* It will create a unique number for every Semester and will start over, if the the max. numer 
+* (7) is reached.
+* 
+* @param		integer	the timestamp of the start time from the Semester
+* @param		string	this field is no more necessary but only for compatibilty reasons here
+* @return		integer	the color number
+*
+*/
 function select_group($sem_start_time, $user_id='') {
 	global $SEMESTER;
 	//Farben Algorhytmus, erzeugt eindeutige Farbe fuer jedes Semester. Funktioniert ab 2001 die naechsten 1000 Jahre.....
@@ -295,8 +295,19 @@ function select_group($sem_start_time, $user_id='') {
 	return $group;
 }
 
-//////////////////////////////////////////////////////////////////////////
-
+/**
+* The function shortens a string, but it uses the first 2/3 and the last 1/3
+*
+* The parts will be divided by a "[...]". The functions is to use like php's
+* substr function.
+* 
+* @param		string	the original string
+* @param		integer	start pos, 0 is the first pos
+* @param		integer	end pos
+* @return		string
+*
+*
+*/
 function my_substr($what, $start, $end) {
 	$length=$end-$start;
 	if (strlen($what) > $length) {
@@ -306,8 +317,15 @@ function my_substr($what, $start, $end) {
 }
 
 
-//////////////////////////////////////////////////////////////////////////
-
+/**
+* The function determines, if the current user have write perm in a Veranstaltung or Einrichtung
+*
+* It uses the Variables $SemSecLevelWrite, $SemUserStatus and $rechte, which are created in the
+* modul check_sem_entry.inc.php and $perm from PHP-lib
+* 
+* @return		string	the error msg. If no masg is returned, the user has write permission
+*
+*/
 function have_sem_write_perm () {
 
 global $SemSecLevelWrite, $SemUserStatus, $perm, $rechte;
@@ -340,10 +358,18 @@ if (!($perm->have_perm("root"))) {
 	}
 return $error_msg;
 }
-//////////////////////////////////////////////////////////////////////////
-////
-// !Returns global perm
-// deprecated	use $auth->auth["perm"]
+
+/**
+* The function gives the global perm of an user
+*
+* It ist recommended to use $auth->auth["perm"] for this query,
+* but the function is useful, if you want to query an user_id from another user
+* (which ist not the current user)
+* 
+* @param		string	if omitted, current user_id is used
+* @return		string	the perm level or an error msg
+*
+*/
 function get_global_perm($user_id="") {
 	 global $user;
 
@@ -356,17 +382,18 @@ function get_global_perm($user_id="") {
 	 else
 	 	return ("Fehler");
 }
-// TABLES: auth_user_md5
 
-//////////////////////////////////////////////////////////////////////////
-
-////
-// !Returns permission for given range_id and user_id
-// Function works for seminare,institute, fakultaeten
-// admins get status 'admin' if range_id is a seminar
-// param:	string 	$range_id 	an id from a seminar,institut, fakultaet
-// param:	string 	$user_id 	if omitted,current user_id is used
-// return:	string 	
+/**
+* Returns permission for given range_id and user_id
+*
+* Function works for Veranstaltungen, Einrichtunge, Fakultaeten.
+* admins get status 'admin' if range_id is a seminar
+* 
+* @param		string	an id a Veranstaltung, Einrichtung or Fakultaet
+* @param		string	if omitted,current user_id is used
+* @return		string	the perm level
+*
+*/
 function get_perm($range_id,$user_id="")
 {
  global $user,$auth;
@@ -411,14 +438,16 @@ function get_perm($range_id,$user_id="")
 
  return $status;
 }
-// TABLES: seminar_user,seminare,user_inst,fakultaet_user
-//////////////////////////////////////////////////////////////////////////
 
-////
-// !Retrieves fullname for a given user_id
-// uses global $online array if user is online
-// param:	string 	$user_id 	if omitted,current user_id is used
-// return:	string 	
+/**
+* Retrieves the fullname for a given user_id
+*
+* uses global $online array if user is online
+* 
+* @param		string	if omitted, current user_id is used
+* @return		string	
+*
+*/
 function get_fullname($user_id="")
 {
  global $user,$online;
@@ -441,14 +470,14 @@ if (!$author) {
  if ($author=="") $author="unbekannt";
  return $author;
  }
-// TABLES: auth_user_md5 
- /////////////////////////////////////////////////////////////////////////
 
-////
-// !Retrieves fullname for a given username
-// uses global $online array if user is online
-// param:	string 	$uname 	if omitted,current username is used
-// return:	string 	
+/**
+* Retrieves the fullname for a given username
+* 
+* @param		string	if omitted, current user_id is used
+* @return		string	
+*
+*/
 function get_fullname_from_uname($uname="")
 {
  global $auth,$online;
@@ -469,15 +498,15 @@ if (!$author) {
 
  return $author;
  }
- // TABLES: auth_user_md5
  
- //////////////////////////////////////////////////////////////////////////
-
-////
-// !Retrieves vorname for a given user_id
-// param:	string 	$user_id 	if omitted,current user_id is used
-// return:	string 	
-function get_vorname($user_id="")
+/**
+* Retrieves the Vorname for a given user_id
+*
+* @param		string	if omitted, current user_id is used
+* @return		string	
+*
+*/
+ function get_vorname($user_id="")
 {
  global $user;
  if (!($user_id)) $user_id=$user->id;
@@ -489,14 +518,14 @@ function get_vorname($user_id="")
 
  return $author;
  }
-// TABLES: auth_user_md5
- 
- //////////////////////////////////////////////////////////////////////////
 
-////
-// !Retrieves nachname for a given user_id
-// param:	string 	$user_id 	if omitted,current user_id is used
-// return:	string 	
+/**
+* Retrieves the Nachname for a given user_id
+* 
+* @param		string	if omitted, current user_id is used
+* @return		string	
+*
+*/
 function get_nachname($user_id="")
 {
  global $user;
@@ -509,14 +538,14 @@ function get_nachname($user_id="")
 
  return $author;
  }
- // TABLES: auth_user_md5
- //////////////////////////////////////////////////////////////////////////
 
- ////
-// !Retrieves username for a given user_id
-// uses global $online array if user is online
-// param:	string 	$user_id 	if omitted,current user_id is used
-// return:	string 	
+/**
+* Retrieves the username for a given user_id
+* 
+* @param		string	if omitted, current username will be returned
+* @return		string	
+*
+*/
 function get_username($user_id="")
 {
   global $auth,$online;
@@ -538,14 +567,14 @@ if (!$author) {
 }
  return $author;
 }
-// TABLES: auth_user_md5
- //////////////////////////////////////////////////////////////////////////
 
-////
-// !Retrieves user_id for a given username
-// uses global $online array if user is online
-// param:	string 	$username 	if omitted,current username is used
-// return:	string 	
+/**
+* Retrieves the userid for a given username
+* 
+* @param		string	if omitted, current user_id will be returned
+* @return		string	
+*
+*/
 function get_userid($username="")
 {
  global $user,$online;
@@ -562,14 +591,15 @@ $db=new DB_Seminar;
 }
  return $author;
 }
-//TABLES auth_user_md5 
- //////////////////////////////////////////////////////////////////////////
 
-////
-// !Retrieves titel for a given studip score
-// param:	integer	$score
-// return:	string 	
-FUNCTION gettitel($score)
+/**
+* Retrieves the titel for a given studip score
+*
+* @param		integer	a score value
+* @return		string	the titel
+*
+*/
+function gettitel($score)
 
 {
 	IF ($score==0) $titel =		"Unbeschriebenes Blatt";
@@ -589,15 +619,17 @@ FUNCTION gettitel($score)
 	IF ($score>65536) $titel =	"Lichtgestalt";
 	IF ($score>131072) $titel =	"Gott";
 
-	RETURN $titel;
+	return $titel;
 }
 
-//////////////////////////////////////////////////////
-
-////
-// !calculate score for current user
-// score is also stored in DB
-// return:	integer
+/**
+* Retrieves the score for the current user
+*
+* uses global $online array if user is online
+* 
+* @return		integer	the score
+*
+*/
 function getscore()
 
 { global $user,$auth;
@@ -647,6 +679,4 @@ $db->query($query);
 	
 RETURN $score;
 }
-// TABLES: user_info,news,user_inst,archiv_user,seminar_user,dokumente,px_topics
-///////////////////////////////////////////////////////////////
 ?>
