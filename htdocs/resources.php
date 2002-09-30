a3b5a6d5ed5d291b6a976f416a5ad68fac74f5dd
Cornelis Kater <kater@zqs.uni-hannover.de>
2002-09-30 19:56:14 +0000

    Kommentierung Stud.IP Kopf

--- htdocs/resources.php
+++ htdocs/resources.php
@@ -1,30 +1,41 @@
 <?
-/*
-resourcesControl.php - 0.8
-Steuerung fuer Ressourcenverwaltung von Stud.IP.
-Copyright (C) 2002 Cornelis Kater <ckater@gwdg.de>
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+/**
+* resources.php
+* 
+* The startscript for the resources module
+*
+* @author		Cornelis Kater <ckater@gwdg.de>, Suchi & Berg GmbH <info@data-quest.de>
+* @version		$Id$
+* @access		public
+* @package		resources
+* @modulegroup	resources_modules
+* @module		resources.php
 */
+
+// +---------------------------------------------------------------------------+
+// This file is part of Stud.IP
+// resources.php
+// Steuerung fuer Ressourcenverwaltung von Stud.IP
+// Copyright (C) 2002 Cornelis Kater <ckater@gwdg.de>, Suchi & Berg GmbH <info@data-quest.de>, 
+// +---------------------------------------------------------------------------+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or any later version.
+// +---------------------------------------------------------------------------+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// +---------------------------------------------------------------------------+
 
 page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Auth", "perm" => "Seminar_Perm", "user" => "Seminar_User"));
 $perm->check("user"); //Noch anpassen!!!
 
 include ("$ABSOLUTE_PATH_STUDIP/seminar_open.php"); // initialise Stud.IP-Session
-
-// -- here you have to put initialisations for the current page
 
 if ($RESOURCES_ENABLE) {
 	//Steuerung der Ressourcenverwaltung einbinden
