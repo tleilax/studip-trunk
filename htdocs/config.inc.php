10ff128e2c29ada3b23bf0a13934261f94a28ed3
Cornelis Kater <kater@zqs.uni-hannover.de>
2002-09-30 20:02:33 +0000

    ;-) zur Versionsnummer

--- htdocs/config.inc.php
+++ htdocs/config.inc.php
@@ -37,7 +37,7 @@
 
 //Generische Systemeinstellungen
 $AUTH_LIFETIME=30;						//Zeit bis zu einem automatischem Logout in Minuten (wird zur Zeit nur zu Anzeigezwecken verwendet...)
-$SOFTWARE_VERSION="0.8.15";
+$SOFTWARE_VERSION="0.8.15 ;-)";
 
 
 //Daten ueber die Uni
