fd5d3ad5c3224d15f8539c447b7053a7668a9b9f
Cornelis Kater <kater@zqs.uni-hannover.de>
2002-10-14 22:19:37 +0000

    Sprung auf Version 0.9

--- htdocs/config.inc.php
+++ htdocs/config.inc.php
@@ -37,7 +37,7 @@
 
 //Generische Systemeinstellungen
 $AUTH_LIFETIME=30;						//Zeit bis zu einem automatischem Logout in Minuten (wird zur Zeit nur zu Anzeigezwecken verwendet...)
-$SOFTWARE_VERSION="0.8.15 ;-)";
+$SOFTWARE_VERSION="0.9 rc 4";
 
 
 //Daten ueber die Uni
