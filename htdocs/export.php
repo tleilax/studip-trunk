80ef258c31ec393ec834793abe586f8ecd36d133
arne <arne>
2002-11-02 22:07:57 +0000

    Exportfunktionen: XML und XSLT

--- htdocs/export.php
+++ htdocs/export.php
@@ -18,29 +18,33 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Auth", "perm" => "Seminar_Perm", user => "Seminar_User"));
-$perm->check("user");
+$perm->check("dozent");
 
 include ("$ABSOLUTE_PATH_STUDIP/seminar_open.php"); // initialise Stud.IP-Session
+
 $i_page = "meine_seminare.php";
+$EXPORT_ENABLE = true;
+$XSLT_ENABLE = false;
+$PATH_EXPORT = "export";
 // -- here you have to put initialisations for the current page
 
 require_once ("$ABSOLUTE_PATH_STUDIP/visual.inc.php");
 require_once ("$ABSOLUTE_PATH_STUDIP/functions.php");
 
-require_once ("$ABSOLUTE_PATH_STUDIP" . "export/export_config.inc.php");
+require_once ("$ABSOLUTE_PATH_STUDIP/$PATH_EXPORT" . "/export_config.inc.php");
 
 if ($EXPORT_ENABLE)
 {
 	if (($page==2) AND $XSLT_ENABLE AND $skip_page_2) $page=3;
 	//Exportmodul einbinden
 	if ($range_id != "")	
-		include($RELATIVE_PATH_EXPORT."./export/export.inc.php");
+		include($ABSOLUTE_PATH_STUDIP ."/" . $PATH_EXPORT . "/export.inc.php");
 	elseif (($xml_file_id != "") AND ($page != 3))
-		include($RELATIVE_PATH_EXPORT."./export/export_choose_xslt.inc.php");
+		include($ABSOLUTE_PATH_STUDIP ."/" . $PATH_EXPORT . "/export_choose_xslt.inc.php");
 
 	if ( (isset($choose)) AND (isset($format)) AND ($XSLT_ENABLE) AND 
 		(((($o_mode == "processor") OR ($o_mode == "passthrough")) AND ($object_counter > 0)) OR ($page == 3)))
-		include($RELATIVE_PATH_EXPORT."./export/export_run_xslt.inc.php");
+		include($ABSOLUTE_PATH_STUDIP ."/" . $PATH_EXPORT . "/export_run_xslt.inc.php");
 	
 }
 else 
