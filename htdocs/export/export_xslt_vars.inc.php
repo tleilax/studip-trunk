e5921c971fc2b9262b4239d7c54487da9159e58d
arne <arne>
2002-11-02 23:14:38 +0000

    Daten zu den XSLT-Skripten

---
+++ htdocs/export/export_xslt_vars.inc.php
@@ -0,0 +1,63 @@
+<?
+$output_formats = array(
+	"htm"		=>		"Hypertext (HTML)", 
+	"txt"			=>		"Text (TXT)", 
+	"blafasel"		=>		"Blafasel Text Format (BTF)", 
+	"rtf"			=>		"Rich Text Format (RTF)", 
+	"xsl"			=>		"Adobe Postscript (PDF)"
+);
+
+$xslt_files[0]["name"] = "Standardmodul";
+$xslt_files[0]["desc"] = "Dies ist ein Standardmodul zur einfachen Ausgabe von Veranstaltungs- oder Personen-Daten als HTML-Datei.";
+$xslt_files[0]["file"] = "html-v-1.xsl";
+$xslt_files[0]["htm"] = true;
+$xslt_files[0]["person"] = true;
+$xslt_files[0]["veranstaltung"] = true;
+
+$xslt_files[1]["name"] = "Standardmodul";
+$xslt_files[1]["desc"] = "Dies ist ein Standardmodul zur einfachen Ausgabe von Veranstaltungs- oder Personen-Daten als Textdatei.";
+$xslt_files[1]["file"] = "txt-v-1.xsl";
+$xslt_files[1]["txt"] = true;
+$xslt_files[1]["person"] = true;
+$xslt_files[1]["veranstaltung"] = true;
+
+$xslt_files[2]["name"] = "kombi2";
+$xslt_files[2]["desc"] = "Diese Datei ist rein virtuell 1";
+$xslt_files[2]["file"] = "5.xsl";
+$xslt_files[2]["rtf"] = true;
+$xslt_files[2]["pdf"] = true;
+$xslt_files[2]["htm"] = true;
+$xslt_files[2]["person"] = true;
+$xslt_files[2]["veranstaltung"] = true;
+
+$xslt_files[3]["name"] = "kombi3";
+$xslt_files[3]["desc"] = "Diese Datei ist rein virtuell 2";
+$xslt_files[3]["file"] = "6.xsl";
+$xslt_files[3]["rtf"] = true;
+$xslt_files[3]["txt"] = true;
+$xslt_files[3]["veranstaltung"] = true;
+
+$xslt_files[4]["name"] = "super";
+$xslt_files[4]["desc"] = "dudeldi";
+$xslt_files[4]["file"] = "7.xsl";
+$xslt_files[4]["rtf"] = true;
+$xslt_files[4]["txt"] = true;
+$xslt_files[4]["htm"] = true;
+$xslt_files[4]["veranstaltung"] = true;
+
+$xslt_files[5]["name"] = "super1";
+$xslt_files[5]["desc"] = "layoutfunktionen";
+$xslt_files[5]["file"] = "8.xsl";
+$xslt_files[5]["htm"] = true;
+$xslt_files[5]["person"] = true;
+
+$xslt_files[6]["name"] = "standard";
+$xslt_files[6]["desc"] = "test 3333333333333333333333333333333";
+$xslt_files[6]["file"] = "1.xsl";
+$xslt_files[6]["rtf"] = true;
+$xslt_files[6]["txt"] = true;
+$xslt_files[6]["htm"] = true;
+$xslt_files[6]["person"] = true;
+
+
+?>
