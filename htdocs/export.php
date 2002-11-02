28240a7af552809d8615e7bb5af5c9f41fadbc79
arne <arne>
2002-11-02 22:20:22 +0000

    Konfigurationsdatei für die Exportmodule

--- htdocs/export.php
+++ htdocs/export.php
@@ -31,20 +31,20 @@ $PATH_EXPORT = "export";
 require_once ("$ABSOLUTE_PATH_STUDIP/visual.inc.php");
 require_once ("$ABSOLUTE_PATH_STUDIP/functions.php");
 
-require_once ("$ABSOLUTE_PATH_STUDIP/$PATH_EXPORT" . "/export_config.inc.php");
+require_once ("$ABSOLUTE_PATH_STUDIP$PATH_EXPORT" . "/export_config.inc.php");
 
 if ($EXPORT_ENABLE)
 {
 	if (($page==2) AND $XSLT_ENABLE AND $skip_page_2) $page=3;
 	//Exportmodul einbinden
 	if ($range_id != "")	
-		include($ABSOLUTE_PATH_STUDIP ."/" . $PATH_EXPORT . "/export.inc.php");
+		include($ABSOLUTE_PATH_STUDIP ."" . $PATH_EXPORT . "/export.inc.php");
 	elseif (($xml_file_id != "") AND ($page != 3))
-		include($ABSOLUTE_PATH_STUDIP ."/" . $PATH_EXPORT . "/export_choose_xslt.inc.php");
+		include($ABSOLUTE_PATH_STUDIP ."" . $PATH_EXPORT . "/export_choose_xslt.inc.php");
 
 	if ( (isset($choose)) AND (isset($format)) AND ($XSLT_ENABLE) AND 
 		(((($o_mode == "processor") OR ($o_mode == "passthrough")) AND ($object_counter > 0)) OR ($page == 3)))
-		include($ABSOLUTE_PATH_STUDIP ."/" . $PATH_EXPORT . "/export_run_xslt.inc.php");
+		include($ABSOLUTE_PATH_STUDIP ."" . $PATH_EXPORT . "/export_run_xslt.inc.php");
 	
 }
 else 
