0515d1529836239d6a468462e718ee442993e6a8
arne <arne>
2002-11-08 23:50:37 +0000

    HTML-Teil der Exportmodule

---
+++ htdocs/export/export_view.inc.php
@@ -0,0 +1,128 @@
+<?
+if (($o_mode != "direct") AND ($o_mode != "passthrough"))
+{
+// Start of Output
+	include ("$ABSOLUTE_PATH_STUDIP/html_head.inc.php"); // Output of html head
+	include ("$ABSOLUTE_PATH_STUDIP/header.php");   // Output of Stud.IP head
+	if ($page == 1)
+		echo "\n" . $cssSw->GetHoverJSFunction() . "\n";
+	include ("$ABSOLUTE_PATH_STUDIP/links_admin.inc.php");
+	if ($page == 1)
+		$cssSw->enableHover();
+
+ ?>
+<table cellspacing="0" cellpadding="0" border="0" width="100%">
+	<tr>
+		<td class="topic" colspan="2"><b><? echo $export_pagename; ?></b>
+		</td>
+	</tr>
+	<tr>
+		<td class="blank" colspan="2">&nbsp; 
+		</td>
+	</tr>
+	<tr valign="top">
+     		<td width="90%" class="blank">
+
+			<table>		
+<?
+
+				if (isset($export_error))
+					my_error($export_error);
+				if (isset($export_msg))
+					my_msg($export_msg);
+				if (isset($export_info))
+					my_info($export_info);
+?>
+			</table>
+			<?
+			echo $export_pagecontent;
+
+			if (isset($xml_printlink))
+			{
+			?>
+			<table cellspacing="0" cellpadding="0" border="0" width="100%">
+				<tr>
+					<?
+					printhead ("99%", FALSE, "", "close", true, $xml_printimage, $xml_printlink, $xml_printdesc);
+					?>
+				</tr>
+			</table>
+			<table cellspacing="0" cellpadding="0" border="0" width="100%">
+				<tr>
+					<?
+					printcontent("99%", FALSE, $xml_printcontent, "");
+					?>
+				</tr>
+			</table>
+			<?
+			}
+			if (isset($xslt_printlink))
+			{
+			?>
+			<table cellspacing="0" cellpadding="0" border="0" width="100%">
+				<tr>
+					<?
+					printhead ("99%", FALSE, "", "close", true, $xslt_printimage, $xslt_printlink, $xslt_printdesc);
+					?>
+				</tr>
+			</table>
+			<table cellspacing="0" cellpadding="0" border="0" width="100%">
+				<tr>
+					<?
+					printcontent("99%", FALSE, $xslt_printcontent, "");
+					?>
+				</tr>
+			</table>
+			<?
+			}
+			if (isset($result_printlink))
+			{
+			?>
+			<table cellspacing="0" cellpadding="0" border="0" width="100%">
+				<tr>
+					<?
+					printhead ("99%", FALSE, "", "close", true, $result_printimage, $result_printlink, $result_printdesc);
+					?>
+				</tr>
+			</table>
+			<table cellspacing="0" cellpadding="0" border="0" width="100%">
+				<tr>
+					<?
+					printcontent("99%", FALSE, $result_printcontent, "");
+					?>
+				</tr>
+			</table>
+			<br>
+			<?
+			}
+			if (isset($export_weiter_button))
+			{	
+			?>
+			<br>
+			<br>
+			<?
+				echo $export_weiter_button;
+			}
+			?>
+		</td>
+		<td width="270" NOWRAP class="blank" align="center" valign="top">
+			<? 
+			print_infobox ($infobox,"pictures/export.jpg");
+			?>		
+		</td>		
+	</tr>
+	<tr>
+		<td class="blank" colspan="2">&nbsp; 
+		</td>
+	</tr>
+	</table>
+	<p>&nbsp;</p>
+	</body>
+	</html>
+<?
+}
+elseif ($export_error_num > 0)
+{
+	echo $export_error;
+}
+?>
