2649a66b6c403680d3323240a59a7dc95208b359
Cornelis Kater <kater@zqs.uni-hannover.de>
2002-11-21 15:59:22 +0000

    Enthält sämtliche (Fehler)Meldungen der Ressourcenverwaltung

---
+++ htdocs/resources/views/msgs_resources.inc.php
@@ -0,0 +1,35 @@
+<?
+$this->msg[1] = array (
+		"mode" => "error",
+		"titel" => _("Fehlende Berechtigung"),
+		"msg"=> _("Sie haben leider keine Berechtigung, das Objekt zu bearbeiten"));
+$this->msg[2] = array (
+		"mode" => "error",
+		"titel" => _("Nutzer hat keine Berechtigung")	,
+		"msg"=> _("Sie versuchen, einen Nutzer einzutragen, der nicht selbst in der Lage ist, die Belegung zu bearbeiten oder zu l&ouml;schen. Sie k&ouml;nnen diesen Nutzer leider nicht eintragen"));
+$this->msg[3] = array (
+		"mode" => "msg",
+		"titel" => _("Belegung eingetragen"),
+		"msg"=> _("Die Belegung wurde eingetragen"));
+$this->msg[4] = array (
+		"mode" => "msg",
+		"titel" => _("Belegung ver&auml;ndert"),
+		"msg"=> _("Die Belegung wurde ver&auml;ndert"));
+$this->msg[5] = array (
+		"mode" => "msg",
+		"titel" => _("Belegung gel&ouml;scht"),
+		"msg"=> _("Die Belegung wurde gel&ouml;scht"));
+$this->msg[6] = array (
+		"mode" => "msg",
+		"titel" => _("Eigenschaften ver&auml;ndert"),
+		"msg"=> _("Die Eigenschaften der Ressource wurden ver&auml;ndert"));
+$this->msg[7] = array (
+		"mode" => "msg",
+		"titel" => _("Ressource gel&ouml;scht"),
+		"msg"=> _("Die Ressource wurde gel&ouml;scht"));
+$this->msg[8] = array (
+		"mode" => "msg",
+		"titel" => _("Berechtigungen ver&auml;ndert"),
+		"msg"=> _("Die Berechtigungseinstellungen der Ressource wurden ver&auml;ndert"));
+
+?>
