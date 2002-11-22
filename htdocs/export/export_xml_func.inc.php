6ccd98e9acd83a2ba1d84454957457e7c5a0c674
arne <arne>
2002-11-22 21:26:22 +0000

    Sonderzeichen in ISO-8859-1

--- htdocs/export/export_xml_func.inc.php
+++ htdocs/export/export_xml_func.inc.php
@@ -2,7 +2,7 @@
 
 function xml_header()
 {
-	$xml_tag_string = "<" . "?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+	$xml_tag_string = "<" . "?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n";
 //	$xml_tag_string .= "<!DOCTYPE StudIP SYSTEM \"http://goettingen.studip.de/studip.dtd\">\n";
 	$xml_tag_string .= "<studip>\n";
 	return $xml_tag_string;
@@ -11,7 +11,7 @@ function xml_header()
 function xml_open_tag($tag_name, $tag_key = "")
 {
 	if ($tag_key != "")  
-		$xml_tag_string .= " key=\"" . utf8_encode( htmlspecialchars ($tag_key ) ) ."\"" ;
+		$xml_tag_string .= " key=\"" . htmlspecialchars ($tag_key ) ."\"" ;
 	$xml_tag_string = "<" . $tag_name . $xml_tag_string .  ">\n";
 	return $xml_tag_string;
 }
@@ -25,7 +25,7 @@ function xml_close_tag($tag_name)
 function xml_tag($tag_name, $tag_content)
 {
 	$xml_tag_string = "<" . $tag_name . $xml_tag_string .  ">" 
-		. utf8_encode( htmlspecialchars (kill_format( $tag_content ) ) )
+		. htmlspecialchars (kill_format( $tag_content ) )
 		. "</" . $tag_name .  ">\n";
 	return $xml_tag_string;
 }
