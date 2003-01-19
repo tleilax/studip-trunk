ff1ae9a3d28e7ffd2a1cc9f8e15f31b021972be1
Cornelis Kater <kater@zqs.uni-hannover.de>
2003-01-19 22:30:52 +0000

    Bugfix

--- htdocs/resources/lib/list_assign.inc.php
+++ htdocs/resources/lib/list_assign.inc.php
@@ -69,17 +69,17 @@ function create_assigns($assign_object, &$this, $begin='', $end='') {
 	$quantity=0;
 	$temp_ts=0;
 
+	//if no begin/end-date submitted, we create all the assigns from the given assign-object
+	if (!$begin)
+		$begin = $assign_object->getBegin();
+	if (!$end)
+		$end = $assign_object->getRepeatEnd();
+
 	if ($assign_object->getRepeatMode() == "na") {
 		// date without repeatation, we have to create only one event (object = event)
 		$this->events[] = new AssignEvent($assign_object->getId(), $assign_object->getBegin(), $assign_object->getEnd(),
 								$assign_object->getResourceId(), $assign_object->getAssignUserId(), 
 								$assign_object->getUserFreeName());
-	
-	//if no begin/enddate submitted, we create all the assigs from the given assin-object
-	if (!$begin)
-		$begin = $assign_object->getBegin();
-	if (!$end)
-		$end = $assign_object->getEnd();
 	
 	} elseif (($assign_object -> getRepeatEnd() >= $begin) && ($assign_object -> getBegin() <= $end))
 		do { 
