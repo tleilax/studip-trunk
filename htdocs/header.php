5118175297e7f93c1ff1e6708a88b52e852a8213
Stefan Suchi <suchi@data-quest.de>
2003-01-22 18:23:40 +0000

    Internationalisierung

--- htdocs/header.php
+++ htdocs/header.php
@@ -83,8 +83,7 @@ if ($auth->auth["uid"] == "nobody") { ?>
 		</table>
 
 <?php
-}
-else {   // Benutzer angemeldet
+} else {   // Benutzer angemeldet
 
 		$db=new DB_Seminar;
 
@@ -92,7 +91,7 @@ else {   // Benutzer angemeldet
 		$now = time(); // nach eingestellter Zeit (default = 5 Minuten ohne Aktion) zaehlt man als offline
 		$query = "SELECT " . $_fullname_sql['full'] . " AS full_name,($now-UNIX_TIMESTAMP(changed)) AS lastaction,a.username,a.user_id FROM active_sessions LEFT JOIN auth_user_md5 a ON (a.user_id=sid) LEFT JOIN user_info USING(user_id) WHERE changed > '".date("YmdHis",$now - ($my_messaging_settings["active_time"] * 60))."' AND sid != 'nobody' AND sid != '".$auth->auth["uid"]."' AND active_sessions.name = 'Seminar_User' ORDER BY changed DESC";
 		$db->query($query);
-		while ($db->next_record()){
+		while ($db->next_record()) {
 			$online[$db->f("username")] = array("name"=>$db->f("full_name"),"last_action"=>$db->f("lastaction"),"userid"=>$db->f("user_id"));      
 		}
 		
@@ -101,12 +100,11 @@ else {   // Benutzer angemeldet
 		$db->query("SELECT *  FROM globalmessages WHERE user_id_rec LIKE '$myuname'");
 		$i=0;
 		$chatm=false;
-		while ($db->next_record())
-		{
-			if (preg_match("/chat_with_me/i", $db->f("message")))  {
+		while ($db->next_record()) {
+			if (preg_match("/chat_with_me/i", $db->f("message"))) {
 				if ($online[$db->f("user_id_snd")]) {
-				    $chatm=true;
-					}
+					$chatm=true;
+				}
 			}
 			elseif ($my_messaging_settings["last_visit"] < $db->f("mkdate"))
 				$neum++; // das ist eine neue Nachricht.
@@ -120,68 +118,63 @@ else {   // Benutzer angemeldet
 		<table class="header" border="0" width="100%" background="pictures/fill1.gif" cellspacing="0" cellpadding="0" bordercolor="#999999" height="25">
 			<tr>
 				<td class="header" width="33%" valign="bottom" background="pictures/fill1.gif">
-					&nbsp;<a href="index.php" target="_top"><img border="0" src="pictures/home.gif" <?=tooltip("zurück zur Startseite")?> width="24" height="21"></a>
-					&nbsp;<a href="./help/index.php<?echo $help_query?>" target="_new"><img border="0" src="pictures/hilfe.gif" <?=tooltip("Hilfe")?> width="24" height="21"></a>
-					&nbsp;<a href="meine_seminare.php"><img border="0" src="pictures/meinesem.gif" <?=tooltip("Meine Veranstaltungen & Einrichtungen")?> width="24" height="21"></a>
+					&nbsp;<a href="index.php" target="_top"><img border="0" src="pictures/home.gif" <?=tooltip(_("zurück zur Startseite"))?> width="24" height="21"></a>
+					&nbsp;<a href="./help/index.php<?echo $help_query?>" target="_new"><img border="0" src="pictures/hilfe.gif" <?=tooltip(_("Hilfe"))?> width="24" height="21"></a>
+					&nbsp;<a href="meine_seminare.php"><img border="0" src="pictures/meinesem.gif" <?=tooltip(_("Meine Veranstaltungen & Einrichtungen"))?> width="24" height="21"></a>
 				<?
 				if (!($perm->have_perm("admin") || $perm->have_perm("root"))) {
 					echo "&nbsp;<a href=\"./calendar.php\">";
 					echo "<img border=\"0\" src=\"pictures/meinetermine.gif\" ";
-					echo tooltip("Meine Terminverwaltung") . "width=\"24\" height=\"21\"></a>";
+					echo tooltip(_("Meine Terminverwaltung")) . "width=\"24\" height=\"21\"></a>";
 				}
 				?>
 					&nbsp;&nbsp;&nbsp;
 
 
 <?	if ((($altm) && (!$neum)) || ((($altm+$neum) >0) && ($i_page == "sms.php")))
-		if ($altm > 1)
-			echo "&nbsp;<a href=\"sms.php\" ><img border='0' src='pictures/nachricht1.gif' ".tooltip("Sie haben $altm alte Nachrichten!")."></a>&nbsp;&nbsp;&nbsp;";
+			if ($altm > 1)
+				echo "&nbsp;<a href=\"sms.php\" ><img border='0' src='pictures/nachricht1.gif' ".tooltip(sprintf(_("Sie haben %s alte Nachrichten!"), $altm))."></a>&nbsp;&nbsp;&nbsp;";
+			else
+				echo "&nbsp;<a href=\"sms.php\" ><img border='0' src='pictures/nachricht1.gif' ".tooltip(_("Sie haben eine alte Nachricht!"))."></a>&nbsp;&nbsp;&nbsp;";
+		elseif (($neum) && ($i_page != "sms.php"))
+			if ($neum > 1)
+				echo "&nbsp;<a href=\"sms.php\" ><img border='0' src='pictures/nachricht2.gif' ".tooltip(sprintf(_("Sie haben %s neue Nachrichten!"), $neum))."></a>&nbsp;&nbsp;&nbsp;";
+			else
+				echo "&nbsp;<a href=\"sms.php\" ><img border='0' src='pictures/nachricht2.gif' ".tooltip(_("Sie haben eine neue Nachricht!"))."></a>&nbsp;&nbsp;&nbsp;";
 		else
-			echo "&nbsp;<a href=\"sms.php\" ><img border='0' src='pictures/nachricht1.gif' ".tooltip("Sie haben eine alte Nachricht!")."></a>&nbsp;&nbsp;&nbsp;";
-	elseif (($neum) && ($i_page != "sms.php"))
-		if ($neum > 1)
-			echo "&nbsp;<a href=\"sms.php\" ><img border='0' src='pictures/nachricht2.gif' ".tooltip("Sie haben $neum neue Nachrichten!")."></a>&nbsp;&nbsp;&nbsp;";
-		else
-			echo "&nbsp;<a href=\"sms.php\" ><img border='0' src='pictures/nachricht2.gif' ".tooltip("Sie haben eine neue Nachricht!")."></a>&nbsp;&nbsp;&nbsp;";
-	else
-		echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+			echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
 
 		// wurde ich zum Chat eingeladen? Wenn nicht, nachsehen ob wer im Chat ist
           //Version für neuen Chat (vorläufig)
-  if ($CHAT_ENABLE) {
-
-          if (($chatm) && ($i_page != "sms.php") && (!$chatServer->isActiveUser($user->id,"studip"))) {
-			echo "<a href=\"javascript:open_chat();\"><img border='0' src='pictures/chateinladung.gif' ".tooltip("Sie wurden zum Chatten eingeladen!")."></a>\n";
+  	if ($CHAT_ENABLE) {
+    	if (($chatm) && ($i_page != "sms.php") && (!$chatServer->isActiveUser($user->id,"studip"))) {
+				echo "<a href=\"javascript:open_chat();\"><img border='0' src='pictures/chateinladung.gif' ".tooltip(_("Sie wurden zum Chatten eingeladen!"))."></a>\n";
+			} else {
+      	$chatter=$chatServer->getActiveUsers("studip");
+   			if ($chatter == 1)
+   		  	if ($chatServer->isActiveUser($user->id,"studip"))	
+						printf ("<a href=\"javascript:open_chat();\"><img border='0' src='pictures/chat3.gif' ".tooltip(_("Nur Sie sind im Chat"))."></a>");
+					else
+						printf ("<a href=\"javascript:open_chat();\"><img border='0' src='pictures/chat2.gif' ".tooltip(_("Es ist eine Person im Chat"))."></a>");
+				elseif ($chatter > 1)
+					print ("<a href=\"javascript:open_chat();\"><img border='0' src='pictures/chat2.gif' ".tooltip(sprintf(_("Es sind %s Personen im Chat"), $chatter))."></a>");
+      	else
+					echo "<a href=\"javascript:open_chat();\"><img border='0' src='pictures/chat1.gif' ".tooltip(_("Es ist niemand im Chat"))."></a>";
+			}
 		} else {
-
-               $chatter=$chatServer->getActiveUsers("studip");
-   		     if ($chatter == 1)
-   		     		if ($chatServer->isActiveUser($user->id,"studip"))	
-			       		printf ("<a href=\"javascript:open_chat();\"><img border='0' src='pictures/chat3.gif' ".tooltip("Nur Sie sind im Chat")."></a>");
-			       	else
-			       		printf ("<a href=\"javascript:open_chat();\"><img border='0' src='pictures/chat2.gif' ".tooltip("Es ist eine Person im Chat")."></a>");
-			elseif ($chatter > 1)
-			       print ("<a href=\"javascript:open_chat();\"><img border='0' src='pictures/chat2.gif' ".tooltip("Es sind $chatter Personen im Chat")."></a>");
-               else
-                    echo "<a href=\"javascript:open_chat();\"><img border='0' src='pictures/chat1.gif' ".tooltip("Es ist niemand im Chat")."></a>";
-          }
-     }
-     else echo "&nbsp;&nbsp;";
+			echo "&nbsp;&nbsp;";
+		}
 
 
+		echo "&nbsp;&nbsp;&nbsp;";
 
-?>
-
-&nbsp;
-
-<?	// Ist sonst noch wer da?
-		if (!count($online)) print "<a href=\"online.php\"><img src='pictures/nutzer.gif' ".tooltip("Nur Sie sind online")." border='0'></a>";
+		// Ist sonst noch wer da?
+		if (!count($online))
+			print "<a href=\"online.php\"><img src='pictures/nutzer.gif' ".tooltip(_("Nur Sie sind online"))." border='0'></a>";
 		else {
-			if (count($online)==1) print "<a href=\"online.php\"><img src='pictures/nutzeronline.gif' ".tooltip("Ausser Ihnen ist eine Person online")." border='0'></a>";
+			if (count($online)==1) print "<a href=\"online.php\"><img src='pictures/nutzeronline.gif' ".tooltip(_("Ausser Ihnen ist eine Person online"))." border='0'></a>";
 			else {
-				?>
-				<a href="online.php"><img src="pictures/nutzeronline.gif" <?=tooltip("Es sind ausser Ihnen ".count($online)." Personen online")?> border='0'></a>
-				<?
+				print("<a href=\"online.php\"><img src=\"pictures/nutzeronline.gif\" " . tooltip(sprintf(_("Es sind ausser Ihnen %s Personen online"), count($online))) . " border='0'></a>");
 			}
 		}
 
@@ -189,8 +182,9 @@ else {   // Benutzer angemeldet
 		</td>
 
 		<td class="angemeldet" width="20%" nowrap bgcolor="#C0C0C0" valign="middle" align="center" background="pictures/kaverl1b.jpg">
-			<font color="#000080">angemeldet als	<? printf ("%s", $auth->auth["uname"]);?>
-
+			<font color="#000080">
+<?		printf(_("angemeldet als %s"), $auth->auth["uname"]);
+?>
 			<img border="0" src="pictures/info.gif"
 			<? //create (javascript) info tooltip/window
 				$infotext = sprintf (_("Sie sind angemeldet als: %s mit der Berechtigung: %s. Beginn der Session: %s,  letztes Login: %s, %s,  Auflösung: %sx%s, eingestellte Sprache: %s"),
@@ -205,20 +199,18 @@ else {   // Benutzer angemeldet
 		<td class="header" width="33%" nowrap valign="bottom" align="right" background="pictures/fill1.gif">
 
 <?
-		IF ($perm->have_perm("autor"))
-		{
-			echo"&nbsp;<a href=\"about.php\"><img border='0' src='pictures/einst.gif' ".tooltip("Zu Ihrer Einstellungsseite")."></a>\n";
-			echo"&nbsp;<a href=\"auswahl_suche.php\"><img border='0' src='pictures/suchen.gif' ".tooltip("Im System suchen")."></a>\n";
+		if ($perm->have_perm("autor")) {
+			echo"&nbsp;<a href=\"about.php\"><img border='0' src='pictures/einst.gif' ".tooltip(_("Zu Ihrer Einstellungsseite"))."></a>\n";
+			echo"&nbsp;<a href=\"auswahl_suche.php\"><img border='0' src='pictures/suchen.gif' ".tooltip(_("Im System suchen"))."></a>\n";
 		}
 
-		IF ($perm->have_perm("tutor"))
-		{
-			echo"&nbsp;<a href=\"adminarea_start.php?list=TRUE\"><img border='0' src='pictures/admin.gif' ".tooltip("Zu Ihrer Administrationsseite")."></a>\n";
+		if ($perm->have_perm("tutor")) {
+			echo"&nbsp;<a href=\"adminarea_start.php?list=TRUE\"><img border='0' src='pictures/admin.gif' ".tooltip(_("Zu Ihrer Administrationsseite"))."></a>\n";
 		}
 ?>
 
-			&nbsp;&nbsp;<a href="impressum.php"><img border="0" src="pictures/logo2.gif" <?=tooltip("Impressum")?>></a>
-			&nbsp;&nbsp;<a href="logout.php"><img border="0" src="pictures/logout.gif" <?=tooltip("Aus dem System abmelden")?>></a>&nbsp;
+			&nbsp;&nbsp;<a href="impressum.php"><img border="0" src="pictures/logo2.gif" <?=tooltip(_("Impressum"))?>></a>
+			&nbsp;&nbsp;<a href="logout.php"><img border="0" src="pictures/logout.gif" <?=tooltip(_("Aus dem System abmelden"))?>></a>&nbsp;
 
 		</td>
 	</tr>
