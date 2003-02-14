d99e18fcdd6e3ee9e4835e8035c36f617486c1bb
Cornelis Kater <kater@zqs.uni-hannover.de>
2003-02-14 23:16:33 +0000

    kommentiert; mangels Sprachausgabe gleich auf 2.0 gesetzt

--- htdocs/resources/lib/evaluate_values.php
+++ htdocs/resources/lib/evaluate_values.php
@@ -1,4 +1,39 @@
 <?
+/**
+* evaluate_values.php
+* 
+* handles all values, which are sent from the resources-management
+* 
+*
+* @author		Cornelis Kater <ckater@gwdg.de>
+* @version		$Id$
+* @access		public
+* @package		resources
+* @modulegroup	resources
+* @module		ScheduleWeek.class.php
+*/
+
+// +---------------------------------------------------------------------------+
+// This file is part of Stud.IP
+// ScheduleWeek.class.php
+// Auswerten der Werte aus der Ressourcenverwaltung
+// Copyright (C) 2003 Cornelis Kater <ckater@gwdg.de>
+// +---------------------------------------------------------------------------+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or any later version.
+// +---------------------------------------------------------------------------+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// +---------------------------------------------------------------------------+
+
+
 //a temp session-variable...
 $sess->register("new_assign_object");
 
