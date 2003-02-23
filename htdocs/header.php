bce4a22aa48957cb08846b001ce353e7930711c4
rstockm <rstockm>
2003-02-23 18:50:00 +0000

    Abstände

--- htdocs/header.php
+++ htdocs/header.php
@@ -51,11 +51,11 @@ function MakeToolbar($icon,$URL,$text,$tooltip,$size,$target="_top",$align="cent
 	}
 	$toolbar = "<td class=\"toolbar\" align=\"$align\">";
 
-	$toolbar .= "<img border=\"0\" src=\"pictures/blank.gif\" height=\"1\" width=\"40\"><br>"
+	$toolbar .= "<img border=\"0\" src=\"pictures/blank.gif\" height=\"1\" width=\"30\"><br>"
 			  ."<a class=\"toolbar\" href=\"$URL\" target=\"$target\"><img border=\"0\" src=\"$icon\" ".$tool."><br>"
 			  ."<img border=\"0\" src=\"pictures/blank.gif\" height=\"4\" width=\"$size\"><br>"
 			  ."<b>$text</b></a><br>"
-			  ."<img border=\"0\" src=\"pictures/blank.gif\" height=\"7\" width=\"40\">";
+			  ."<img border=\"0\" src=\"pictures/blank.gif\" height=\"7\" width=\"30\">";
 	$toolbar .= "</td>\n";
 	return $toolbar;
 }
