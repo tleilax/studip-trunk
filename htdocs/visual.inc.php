099eb0500b5ff3c355408605071c9081e5b6ce6f
bzeiss <bzeiss>
2003-02-26 11:37:54 +0000

    latexrender patches

--- htdocs/visual.inc.php
+++ htdocs/visual.inc.php
@@ -243,10 +243,41 @@ function quotes_encode($description,$author)
 ////////////////////////////////////////////////////////////////////////////////
 
 function formatReady($what, $trim = TRUE){
-	return symbol(smile(FixLinks(format(htmlReady($what, $trim, FALSE)), FALSE)));
+	return symbol(smile(FixLinks(format(latex(htmlReady($what, $trim, FALSE))), FALSE)));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+
+function latex($text) {
+	global $ABSOLUTE_PATH_STUDIP,$CANONICAL_RELATIVE_PATH_STUDIP,$TEXCACHE_PATH,$LATEXRENDER_ENABLE;
+	global $LATEX_PATH,$DVIPS_PATH,$CONVERT_PATH,$IDENTIFY_PATH,$TMP_PATH;
+
+	if ($LATEXRENDER_ENABLE) {
+	    require_once($ABSOLUTE_PATH_STUDIP."/class.latexrender.php");
+    	$latex = new LatexRender($ABSOLUTE_PATH_STUDIP."/".$TEXCACHE_PATH,$CANONICAL_RELATIVE_PATH_STUDIP."/".$TEXCACHE_PATH);
+		$latex->_latex_path = $LATEX_PATH;
+		$latex->_dvips_path = $DVIPS_PATH;
+		$latex->_convert_path = $CONVERT_PATH;
+		$latex->_identify_path = $IDENTIFY_PATH;
+		$latex->_tmp_dir = $TMP_PATH;
+    
+		preg_match_all("#\[tex\](.*?)\[/tex\]#si",$text,$tex_matches);
+    
+    	for ($i=0; $i < count($tex_matches[0]); $i++) {
+        	$pos = strpos($text, $tex_matches[0][$i]);
+	        $latex_formula = $tex_matches[1][$i];
+
+    	    $url = $latex->getFormulaURL($latex_formula);
+
+        	if ($url != false) {
+            	$text = substr_replace($text, "<img src='".$url."'>",$pos,strlen($tex_matches[0][$i]));
+	        } else {
+    	        $text = substr_replace($text, "[unparseable or potentially dangerous latex formula]",$pos,strlen($tex_matches[0][$i]));
+        	}
+	    }	
+    }
+   	return $text;
+}
 
 // ermöglicht einfache Formatierungen in Benutzereingaben
 
