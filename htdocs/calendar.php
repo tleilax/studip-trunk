1fed04c1d2e300b2ed15793f7e8a547a0c5ef27f
Peter Thienel <thienel@data-quest.de>
2003-03-05 21:56:12 +0000

    internationalisiert

--- htdocs/calendar.php
+++ htdocs/calendar.php
@@ -1,23 +1,46 @@
 <?
-/*
-calendar.php - 0.8
-Bindet Terminkalender ein.
-Copyright (C) 2002 Peter Thienel <pthienel@web.de>
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+/**
+* Calendar-mainfile. Calls the submodules.
+* 
+* 
+*
+* @author		Peter Thienel <pthienel@data.quest.de>
+* @version		$Id$
+* @access		public
+* @modulegroup	extern_modules
+* @module		extern
+* @package		Extern
 */
+
+
+// +---------------------------------------------------------------------------+
+// This file is part of Stud.IP
+// calendar.php
+//
+// Copyright (c) 2002 Peter Tienel <pthienel@data-quest.de> 
+// +---------------------------------------------------------------------------+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or any later version.
+// +---------------------------------------------------------------------------+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// +---------------------------------------------------------------------------+
+
+/**
+* workaround for PHPDoc
+*
+* Use this if module contains no elements to document !
+* @const PHPDOC_DUMMY
+*/
+define("PHPDOC_DUMMY",true);
+
 
 // Default_Auth
 page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Auth", "perm" => "Seminar_Perm", "user" => "Seminar_User"));
@@ -39,8 +62,8 @@ else {
 	// Start of Output
 	include ("$ABSOLUTE_PATH_STUDIP/html_head.inc.php"); // Output of html head
 	include ("$ABSOLUTE_PATH_STUDIP/header.php");   // Output of Stud.IP head
-	parse_window ("error§Der Terminkalender ist nicht eingebunden. Der Terminkalender wurde in den Systemeinstellungen nicht freigeschaltet. Wenden Sie sich bitte an den Administrator.", "§",
-				"Terminkalender nicht eingebunden");
+	$message = _("Der Terminkalender ist nicht eingebunden. Der Terminkalender wurde in den Systemeinstellungen nicht freigeschaltet. Wenden Sie sich bitte an den Administrator.");
+	parse_window ("error§$message", "§", _("Terminkalender ist nicht eingebunden!"));
 	print("</body></html>");
 }
 ?>
