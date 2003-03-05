fa5b14d54f3ee3cd0dab7f316c6ca9e942ca7453
Peter Thienel <thienel@data-quest.de>
2003-03-05 02:50:31 +0000

    Die neuen externen Seiten.

---
+++ htdocs/admin_extern.php
@@ -0,0 +1,51 @@
+<?
+/**
+* Extern-admin-pages-mainfile. Calls the submodules.
+* 
+* 
+*
+* @author		Peter Thienel <pthienel@data.quest.de>
+* @version		$Id$
+* @access		public
+* @modulegroup	extern_modules
+* @module		extern
+* @package		Extern
+*/
+/**
+* workaround for PHPDoc
+*
+* Use this if module contains no elements to document !
+* @const PHPDOC_DUMMY
+*/
+define("PHPDOC_DUMMY",true);
+// +---------------------------------------------------------------------------+
+// This file is part of Stud.IP
+// extern_admin.php
+//
+// Copyright (c) 2003 Peter Tienel <pthienel@data-quest.de> 
+// Suchi & Berg GmbH <info@data-quest.de>
+// +---------------------------------------------------------------------------+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or any later version.
+// +---------------------------------------------------------------------------+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// +---------------------------------------------------------------------------+
+
+page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Auth", "perm" => "Seminar_Perm", "user" => "Seminar_User", "ses" => "ses"));
+$perm->check("admin");
+
+if ($EXTERN_ENABLE)
+	include($ABSOLUTE_PATH_STUDIP . $RELATIVE_PATH_EXTERN . "/admin_extern.inc.php");
+else {
+	$perm->check("GOTT");
+	page_close();
+}
+?>
---
+++ htdocs/extern.php
@@ -0,0 +1,57 @@
+<?
+/**
+* Extern-Pages-mainfile. Calls the submodules.
+* 
+* 
+*
+* @author		Peter Thienel <pthienel@data.quest.de>
+* @version		$Id$
+* @access		public
+* @modulegroup	extern_modules
+* @module		extern
+* @package		Extern
+*/
+/**
+* workaround for PHPDoc
+*
+* Use this if module contains no elements to document !
+* @const PHPDOC_DUMMY
+*/
+define("PHPDOC_DUMMY",true);
+// +---------------------------------------------------------------------------+
+// This file is part of Stud.IP
+// extern.php
+//
+// Copyright (c) 2003 Peter Tienel <pthienel@data-quest.de> 
+// Suchi & Berg GmbH <info@data-quest.de>
+// +---------------------------------------------------------------------------+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or any later version.
+// +---------------------------------------------------------------------------+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// +---------------------------------------------------------------------------+
+
+page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Auth", "perm" => "Seminar_Perm", "user" => "Seminar_User"));
+
+if (!$EXTERN_ENABLE) {
+	if ($perm->have_perm("nobody"))
+			$perm->perm_invalid($auth->auth["perm"], "");
+		else {
+			echo "<br><br><br><blockquote><b>This page is not available!<br>The module \"extern\"";
+			echo " is not enabled in this Stud.IP-installation.</b></blockquote>";
+		}
+		
+		exit;
+}
+include($ABSOLUTE_PATH_STUDIP . $RELATIVE_PATH_EXTERN . "/extern.inc.php");
+	
+page_close();
+?>
