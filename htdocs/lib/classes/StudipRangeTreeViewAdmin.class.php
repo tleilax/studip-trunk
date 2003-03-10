b3212907ad86a173b05b14b501659fbc965c20fa
André Noack <noackorama@gmail.com>
2003-03-10 15:00:06 +0000

    bugfix internationalisierung

--- htdocs/lib/classes/StudipRangeTreeViewAdmin.class.php
+++ htdocs/lib/classes/StudipRangeTreeViewAdmin.class.php
@@ -500,8 +500,7 @@ class StudipRangeTreeViewAdmin extends TreeView{
 		if ($item_id == $this->edit_item_id )
 			return $this->getEditItemContent();
 		if ($item_id == $this->move_item_id){
-			$this->msg[$item_id] = "info§" . sprintf(_("Dieses Element wurde zum Verschieben markiert. Bitte w&auml;hlen Sie ein Einfügesymbol %s aus,"
-								." um das Element zu verschieben."), "<img src=\"pictures/move.gif\" border=\"0\" " .tooltip(_("Einfügesymbol")) . ">");
+			$this->msg[$item_id] = "info§" . sprintf(_("Dieses Element wurde zum Verschieben markiert. Bitte w&auml;hlen Sie ein Einfügesymbol %s aus, um das Element zu verschieben."), "<img src=\"pictures/move.gif\" border=\"0\" " .tooltip(_("Einfügesymbol")) . ">");
 			}
 		$content = "\n<table width=\"90%\" cellpadding=\"2\" cellspacing=\"2\" align=\"center\" style=\"font-size:10pt\">";
 		$content .= $this->getItemMessage($item_id);
