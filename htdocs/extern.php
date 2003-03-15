f546f82e734e9d7a0d3d46c48d8580ae3910572d
Peter Thienel <thienel@data-quest.de>
2003-03-15 01:06:40 +0000

    Rechtecheck entfernt

--- htdocs/extern.php
+++ htdocs/extern.php
@@ -39,20 +39,12 @@ define("PHPDOC_DUMMY",true);
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // +---------------------------------------------------------------------------+
 
-page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Auth", "perm" => "Seminar_Perm", "user" => "Seminar_User"));
-
 if (!$EXTERN_ENABLE) {
-	if ($perm->have_perm("nobody"))
-			$perm->perm_invalid($auth->auth["perm"], "");
-	else {
-		echo "<br><br><br><blockquote><b>This page is not available!<br>The module \"extern\"";
-		echo " is not enabled in this Stud.IP-installation.</b></blockquote>";
-	}
-	page_close();
+	echo "<br><br><br><blockquote><b>This page is not available!<br>The module \"extern\"";
+	echo " is not enabled in this Stud.IP-installation.</b></blockquote>";
 	exit;
 }
-else
-	include($ABSOLUTE_PATH_STUDIP . $RELATIVE_PATH_EXTERN . "/extern.inc.php");
-	
-page_close();
+
+include($ABSOLUTE_PATH_STUDIP . $RELATIVE_PATH_EXTERN . "/extern.inc.php");
+
 ?>
