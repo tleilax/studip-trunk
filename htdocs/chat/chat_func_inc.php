d12d2b9462ca78f2c4d54cc45a42c522f6987f60
André Noack <noackorama@gmail.com>
2003-03-17 14:34:28 +0000

    ...

--- htdocs/chat/chat_func_inc.php
+++ htdocs/chat/chat_func_inc.php
@@ -127,7 +127,6 @@ function chat_show_info($chatid){
 			$chatServer =& ChatServer::GetInstance($GLOBALS['CHAT_SERVER_NAME']);
 			$sms = new messaging();
 			$chatter = $chatServer->isActiveChat($chatid);
-			$pic_path = $GLOBALS['CANONICAL_RELATIVE_PATH_STUDIP'] . "/pictures/";
 			$chatinv = $sms->check_chatinv($chatid);
 			$is_active = $chatServer->isActiveUser($auth->auth['uid'],$chatid);
 			$chatname = ($chatter) ? $chatServer->chatDetail[$chatid]['name'] : chat_get_name($chatid);
@@ -136,52 +135,57 @@ function chat_show_info($chatid){
 			echo "\n<tr><td class=\"topic\" colspan=\"2\" width=\"100%\">";
 			echo "\n" . chat_get_chat_icon($chatter,$chatinv,$is_active);
 			echo "\n<b>&nbsp;" . _("Chatraum:") . "&nbsp;" . htmlReady($chatname) . "</b></td></tr>";
-			echo "\n<tr><td class=\"steel1\" colspan=\"2\" width=\"100%\">&nbsp;</td></tr>";
-			echo "\n<tr><td class=\"steel1\" width=\"50%\"><blockquote><font size=\"-1\">";
-			if (chat_get_entry_level($chatid) || $chatinv){
-				if (!$is_active){
-					echo "<a href=\"#\" onClick=\"javascript:return open_chat('$chatid');\">";
-					echo "<img border=\"0\" align=\"absmiddle\" src=\"$pic_path/chat1.gif\" " . tooltip(_("Diesen Chatraum betreten")) ." ></a>&nbsp;&nbsp;";
-					echo _("Sie k&ouml;nnen diesen Chatraum betreten.");
-					if ($chatinv){
-						echo "&nbsp;" . _("(Sie wurden eingeladen.)");
-					}
-				} else {
-					echo "<img border=\"0\" align=\"absmiddle\" src=\"$pic_path/chat1.gif\" " . tooltip(_("Sie haben diesen Chatraum bereits betreten.")) ." >&nbsp;&nbsp;";
-					echo _("Sie haben diesen Chatraum bereits betreten.");
-				}
-				if ($chatServer->chatDetail[$chatid]['password']){
-					echo "<br><img border=\"0\" align=\"absmiddle\" src=\"$pic_path/closelock.gif\" >&nbsp;&nbsp;";
-					echo _("Dieser Chatraum ist mit einem Passwort gesichert.");
-				}
-			} else {
-				echo "<img border=\"0\" align=\"absmiddle\" src=\"$pic_path/nochat.gif\" >&nbsp;&nbsp;";
-				echo _("Um diesen Chatraum zu betreten, brauchen sie eine g&uuml;ltige Einladung.");
-			}
-			echo "\n</font></blockquote></td><td class=\"steel1\" width=\"50%\"><blockquote><font size=\"-1\">";
-			if (!$chatter){
-				echo _("Dieser Chatraum ist leer.");
-			} else {
-				echo ($chatter == 1) ? _("Es ist eine Person in diesem Chatraum.") : sprintf(_("Es sind %s Personen in diesem Chatraum"),$chatter);
-				echo "<br>(";
-				$chat_user = $chatServer->getUsers($chatid);
-				$c = 0;
-				foreach ($chat_user as $chat_user_id => $detail){
-					echo "<a href=\"{$GLOBALS['CANONICAL_RELATIVE_PATH_STUDIP']}/about.php?username={$detail['nick']}\">"
-						. htmlReady($detail['fullname']) . "</a>";
-					if (++$c != $chatter){
-						echo ", ";
-					}
-				}
-				echo ")";
-			}
-				
-			echo "</font></blockquote></td></tr>";
-			echo "\n<tr><td class=\"steel1\" colspan=\"2\" width=\"100%\">&nbsp;</td></tr>";
+			echo chat_get_content($chatid,$chatter,$chatinv,$chatServer->chatDetail['password'],$is_active,$chatServer->getUsers($chatid));
 			echo "\n</table>";
 			return true;
 		} else {
 			return false;
 		}
+}
+
+function chat_get_content($chatid,$chatter,$chatinv,$password,$is_active,$chat_user){
+	$pic_path = $GLOBALS['CANONICAL_RELATIVE_PATH_STUDIP'] . "/pictures/";
+	$ret = "\n<tr><td class=\"steel1\" colspan=\"2\" width=\"100%\">&nbsp;</td></tr>";
+	$ret .= "\n<tr><td class=\"steel1\" width=\"50%\" valign=\"center\"><blockquote><font size=\"-1\">";
+	if (chat_get_entry_level($chatid) || $chatinv){
+		if (!$is_active){
+			$ret .= "<a href=\"#\" onClick=\"javascript:return open_chat('$chatid');\">";
+			$ret .= "<img border=\"0\" align=\"absmiddle\" src=\"$pic_path/chat1.gif\" " . tooltip(_("Diesen Chatraum betreten")) ." ></a>&nbsp;&nbsp;";
+			$ret .= sprintf(_("Sie k&ouml;nnen diesen Chatraum %sbetreten%s."),"<a href=\"#\" onClick=\"javascript:return open_chat('$chatid');\">","</a>");
+			if ($chatinv){
+				$ret .= "&nbsp;" . _("(Sie wurden eingeladen.)");
+			}
+		} else {
+			$ret .= "<img border=\"0\" align=\"absmiddle\" src=\"$pic_path/chat1.gif\" " . tooltip(_("Sie haben diesen Chatraum bereits betreten.")) ." >&nbsp;&nbsp;";
+			$ret .= _("Sie haben diesen Chatraum bereits betreten.");
+		}
+		if ($password){
+			$ret .= "<br><img border=\"0\" align=\"absmiddle\" src=\"$pic_path/closelock.gif\" >&nbsp;&nbsp;";
+			$ret .= _("Dieser Chatraum ist mit einem Passwort gesichert.");
+		}
+	} else {
+		$ret .= "<img border=\"0\" align=\"absmiddle\" src=\"$pic_path/nochat.gif\" >&nbsp;&nbsp;";
+		$ret .= _("Um diesen Chatraum zu betreten, brauchen sie eine g&uuml;ltige Einladung.");
+	}
+	$ret .= "\n</font></blockquote></td><td class=\"steel1\" width=\"50%\" valign=\"center\"><blockquote><font size=\"-1\">";
+	if (!$chatter){
+		$ret .= _("Dieser Chatraum ist leer.");
+	} else {
+		$ret .= ($chatter == 1) ? _("Es ist eine Person in diesem Chatraum.") : sprintf(_("Es sind %s Personen in diesem Chatraum"),$chatter);
+		$ret .= "<br>(";
+		$c = 0;
+		foreach ($chat_user as $chat_user_id => $detail){
+			$ret .= "<a href=\"{$GLOBALS['CANONICAL_RELATIVE_PATH_STUDIP']}/about.php?username={$detail['nick']}\">"
+			. htmlReady($detail['fullname']) . "</a>";
+			if (++$c != $chatter){
+				$ret .= ", ";
+			}
+		}
+		$ret .= ")";
+	}
+	
+	$ret .= "</font></blockquote></td></tr>";
+	$ret .= "\n<tr><td class=\"steel1\" colspan=\"2\" width=\"100%\">&nbsp;</td></tr>";
+	return $ret;
 }
 ?>
