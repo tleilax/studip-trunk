95edcc7c039918055f186325d27857121b3d94d0
arne <arne>
2003-03-18 02:45:33 +0000

    Umstellung im Veranstaltngsbereich auf ph/pc

--- htdocs/lernmodule/lernmodul_view_functions.inc.php
+++ htdocs/lernmodule/lernmodul_view_functions.inc.php
@@ -37,29 +37,49 @@ function show_user_modules($benutzername)
 
 function show_seminar_modules($seminar_id)
 {
-	global $cssSw;
+	global $PHP_SELF, $print_open;
+
 	$module_count = 0;
 	$mod_array = get_seminar_modules($seminar_id);
 	if ($mod_array != false)
 	{	
 		echo "<b>" . _("Der Veranstaltung sind folgende Lernmodule zugeordnet:") . "</b><br><br>";
-		?>
-		<table cellspacing="0" cellpadding="0" border="0" width="100%">
-			<tr align="center" valign="top">
-				<th width="80%" align="left"><b><? echo _("Name");?></b></th>
-				<th width="20%"><b><? echo _("Entfernen");?></b></th>
-			</tr>		
-		<?
+
 		while ($module_count < sizeof($mod_array))
 		{
-			$cssSw->switchClass();
-			$module_info = get_module_info($mod_array[$module_count]["inst"], $mod_array[$module_count]["id"]);
 			$link_del = $PHP_SELF . "?view=edit&seminar_id=" . $seminar_id . "&do_op=clear&op_co_inst=" . $mod_array[$module_count]["inst"] . "&op_co_id=". $mod_array[$module_count]["id"];
-			echo "<tr><td class=\"" . $cssSw->getClass() . "\"><b>" . $module_info["title"] . "</b> - " . $module_info["description"] . "</td><td align=\"center\" class=\"" . $cssSw->getClass() . "\">" .
-			"<a href=\"" . $link_del . "\"><img src='pictures/trash.gif' border=0 alt=\"" .  _("Verknüpfung aufheben") . "\" title=\"" . _("Verknüpfung aufheben") . "\"></a></td></tr>";
+
+			$module_info = get_module_info($mod_array[$module_count]["inst"], $mod_array[$module_count]["id"]);
+			$ph_key = $mod_array[$module_count]["id"] . "@" . $mod_array[$module_count]["inst"] . "@" . "sem";
+			$printlink = $module_info["title"];
+			$printimage = "<img src=\"pictures/icon-lern.gif\">";
+			$printcontent = $module_info["description"] . "<br><br><center><a href=\"$link_del\">" . makeButton("entfernen", "img") . "</a></center>";
+			$mod_author = get_module_author($mod_array[$module_count]["inst"], $mod_array[$module_count]["id"]);
+			$printdesc = implode($mod_author, ", ");
+			?>
+			<table cellspacing="0" cellpadding="0" border="0" width="100%">
+				<tr>
+					<?
+					if ($print_open[$ph_key] == true)
+						printhead ("99%", FALSE, $PHP_SELF . "?do_close=" . $ph_key . "&view=edit&seminar_id=$seminar_id", "open", true, $printimage, $printlink, $printdesc);
+					else
+						printhead ("99%", FALSE, $PHP_SELF . "?do_open=" . $ph_key . "&view=edit&seminar_id=$seminar_id", "close", true, $printimage, $printlink, $printdesc);
+					?>
+				</tr>
+			</table>
+			<? if ($print_open[$ph_key] == true) 
+			{ ?>
+			<table cellspacing="0" cellpadding="0" border="0" width="100%">
+				<tr>
+					<?
+					printcontent("99%", FALSE, $printcontent, "");
+					?>
+				</tr>
+			</table>
+			<? }
 			$module_count ++;
 		}
-		echo "</table><br>";
+		echo "<br>";
 	}
 	else
 		echo "<b>" . _("Mit dieser Veranstaltung sind keine ILIAS-Lernmodule verknüpft.") . "</b><br><br>";
@@ -67,31 +87,49 @@ function show_seminar_modules($seminar_id)
 	
 function show_all_modules($seminar_id)
 {
-	global $cssSw;
+	global $PHP_SELF, $print_open;
+
 	$module_count = 0;
 	$hide_mod = get_seminar_modules($seminar_id);
 	$mod_array = get_all_modules($hide_mod);
 	if ($mod_array != false)
 	{	
 		echo "<b>" . _("Folgende Lernmodule können eingebunden werden:") . "</b><br><br>";
-		?> 
-		<table cellspacing="0" cellpadding="0" border="0" width="100%">
-			<tr align="center" valign="top">
-				<th width="80%" align="left"><b><? echo _("Name"); ?></b></th>
-				<th width="20%"><b><? echo _("Hinzufügen");?></b></th>
-			</tr>		
-		<?
+
 		while ($module_count < sizeof($mod_array))
 		{
-			$cssSw->switchClass();
 			$link_con = $PHP_SELF . "?view=edit&seminar_id=" . $seminar_id . "&do_op=connect&op_co_inst=" . $mod_array[$module_count]["inst"] . "&op_co_id=". $mod_array[$module_count]["id"];
-			?><tr><td class="<? echo $cssSw->getClass(); ?>"><? echo "<b>" . $mod_array[$module_count]["title"] . "</b> - " . $mod_array[$module_count]["description"]; ?>
-			</td><td class="<? echo $cssSw->getClass(); ?>" align="center">
-			<a href="<? echo $link_con;?>"><img src='pictures/icon-posting.gif' border=0  alt="<? echo _("Hinzufügen") ?>" title="<? echo _("Hinzufügen") ?>" ></a>&nbsp; 
-			</td></tr><?
+
+			$module_info = get_module_info($mod_array[$module_count]["inst"], $mod_array[$module_count]["id"]);
+			$ph_key = $mod_array[$module_count]["id"] . "@" . $mod_array[$module_count]["inst"] . "@" . "all";
+			$printlink = $module_info["title"];
+			$printimage = "<img src=\"pictures/icon-lern.gif\">";
+			$printcontent = $module_info["description"] . "<br><br><center><a href=\"$link_con\">" . makeButton("hinzufuegen", "img") . "</a></center>";
+			$mod_author = get_module_author($mod_array[$module_count]["inst"], $mod_array[$module_count]["id"]);
+			$printdesc = implode($mod_author, ", ");
+			?>
+			<table cellspacing="0" cellpadding="0" border="0" width="100%">
+				<tr>
+					<?
+					if ($print_open[$ph_key] == true)
+						printhead ("99%", FALSE, $PHP_SELF . "?do_close=" . $ph_key . "&view=edit&seminar_id=$seminar_id", "open", true, $printimage, $printlink, $printdesc);
+					else
+						printhead ("99%", FALSE, $PHP_SELF . "?do_open=" . $ph_key . "&view=edit&seminar_id=$seminar_id", "close", true, $printimage, $printlink, $printdesc);
+					?>
+				</tr>
+			</table>
+			<? if ($print_open[$ph_key] == true) 
+			{ ?>
+			<table cellspacing="0" cellpadding="0" border="0" width="100%">
+				<tr>
+					<?
+					printcontent("99%", FALSE, $printcontent, "");
+					?>
+				</tr>
+			</table>
+			<? }
 			$module_count ++;
 		}
-		echo "</table>";
 	}
 	elseif ($hide_mod != "")
 		echo "<b>" . _("Alle verf&uuml;gbaren Lernmodule sind der Veranstaltung zugeordnet.") . "</b><br><br>";
@@ -136,7 +174,7 @@ function show_all_modules_admin()
 
 function show_seminar_modules_links($seminar_id)
 {
-	global $cssSw, $PHP_SELF, $print_open;
+	global $PHP_SELF, $print_open;
 	$out_str = link_seminar_modules($seminar_id);
  	if ($out_str == false)
  	{
