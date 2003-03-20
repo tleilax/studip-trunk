d153aec67fab2b18e4fc9913dc7974159212daa5
Stefan Suchi <suchi@data-quest.de>
2003-03-20 21:35:13 +0000

    I18N

--- htdocs/resources.php
+++ htdocs/resources.php
@@ -45,7 +45,7 @@ if ($RESOURCES_ENABLE) {
 	include ("$ABSOLUTE_PATH_STUDIP/html_head.inc.php"); // Output of html head
 	include ("$ABSOLUTE_PATH_STUDIP/header.php");   // Output of Stud.IP head
 	require_once ($ABSOLUTE_PATH_STUDIP."msg.inc.php");
-	parse_window ("error§Die Ressurcenverwaltung ist nicht eingebunden. Bitte aktivieren Sie sie in den Systemeinstellungen oder wenden Sie sich an den Systemadministrator.", "§",
-				"Ressourcenverwaltung nicht eingebunden");
+	parse_window ("error§" . _("Die Ressurcenverwaltung ist nicht eingebunden. Bitte aktivieren Sie sie in den Systemeinstellungen oder wenden Sie sich an die Systemadministratoren."), "§",
+				_("Ressourcenverwaltung nicht eingebunden"));
 }
 ?>
