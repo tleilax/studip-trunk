ab8449aded85b74fc7c16c8cfe0e702948414899
Peter Thienel <thienel@data-quest.de>
2003-03-27 14:03:07 +0000

    editMainSetting() erlaubt das Ausblenden von Feldern. Fehleranzeige eingebaut.

--- htdocs/extern/views/ExternEditModule.class.php
+++ htdocs/extern/views/ExternEditModule.class.php
@@ -44,14 +44,18 @@ class ExternEditModule extends ExternEditHtml {
 		ExternEdit::ExternEdit(&$config, $form_values, $faulty_values, $edit_element);
 	}
 	
-	function editMainSettings ($field_names, $no_sort = "") {
+	function editMainSettings ($field_names, $hide_fields = "", $hide = "") {
 		$order = $this->getValue("order");
 		$aliases = $this->getValue("aliases");
 		$visible = $this->getValue("visible");
 		$widths = $this->getValue("width");
 		$sort = $this->getValue("sort");
-		if (!is_array($no_sort))
-			$no_sort = array();
+		if (!is_array($hide_fields["sort"]))
+			$hide_fields["sort"] = array();
+		if (!is_array($hide_fields["aliases"]))
+			$hide_fields["aliases"] = array();
+		if (!is_array($hide))
+			$hide = array();
 		
 		$this->css->resetClass();
 		$this->css->switchClass();
@@ -59,10 +63,18 @@ class ExternEditModule extends ExternEditHtml {
 		$out = "<tr><td><table width=\"100%\" border=\"0\" cellpadding=\"2\" cellspacing=\"0\">\n";
 		$out .= "<tr" . $this->css->getFullClass() . ">\n";
 		$out .= "<td><font size=\"2\"><b>" . _("Spaltenname") . "</b></font></td>\n";
-		$out .= "<td><font size=\"2\"><b>" . _("&Uuml;berschrift") . "</b></font>$error_sign</td>\n";
-		$out .= "<td><font size=\"2\"><b>" . _("Breite") . "</b></font></td>\n";
-		$out .= "<td><font size=\"2\"><b>" . _("Sortierung") . "</b></font></td>\n";
-		$out .= "<td><font size=\"2\"><b>" . _("Reihenfolge/<br>Sichtbarkeit") . "</b></font></td>\n";
+		$out .= "<td><font size=\"2\"><b>" . _("&Uuml;berschrift") . "</b></font>";
+		$out .= $this->faulty_values[$this->element_name . "_aliases"] ? $this->error_sign : "";
+		$out .= "</td>\n";
+		if (!in_array("width", $hide)) {
+			$out .= "<td><font size=\"2\"><b>" . _("Breite") . "</b></font>";
+			$out .= $this->faulty_values[$this->element_name . "_width"] ? $this->error_sign : "";
+			$out .= "</td>\n";
+		}
+		if (!in_array("sort", $hide))
+			$out .= "<td><font size=\"2\"><b>" . _("Sortierung") . "</b></font></td>\n";
+		if (!in_array("visible", $hide))
+			$out .= "<td><font size=\"2\"><b>" . _("Reihenfolge/<br>Sichtbarkeit") . "</b></font></td>\n";
 		$out .= "</tr>\n";
 		$this->css->switchClass();
 		
@@ -73,60 +85,70 @@ class ExternEditModule extends ExternEditHtml {
 			$out .= "<td><font size=\"2\">&nbsp;{$field_names[$order[$i]]}</font></td>";
 			
 			// column headline
-			$out .= "<td><input type=\"text\" name=\"{$this->element_name}_aliases[$order[$i]]\"";
-			$out .= "\" size=\"12\" maxlength=\"50\" value=\"";
-			$out .= $aliases[$order[$i]] . "\"></td>\n";
-			
-			// width
-			$width = str_replace("%", "", $widths[$order[$i]]);
-			$out .= "<td><input type=\"text\" name=\"{$this->element_name}_width[$order[$i]]";
-			$out .= "\" size=\"3\" maxlength=\"3\" value=\"$width\">\n</td>\n";
-			
-			// sort
-			if (!in_array($order[$i], $no_sort)) {
-				$out .= "<td><select name=\"{$this->element_name}_sort[$order[$i]]\" ";
-				$out .= "size=\"1\">\n";
-				for ($j = 0; $j <= (sizeof($order) - sizeof($no_sort)); $j++) {
-					if ($sort[$order[$i]] == $j)
-						$selected = " selected";
-					else
-						$selected = "";
-					
-					if ($j == 0)
-						$out .= "<option value=\"0\"$selected>keine</option>";
-					else
-						$out .= "<option value=\"$j\"$selected>$j</option>";
-				}
-				$out .= "\n</select>\n</td>\n";
+			if (!in_array($order[$i], $hide_fields["aliases"])) {
+				$out .= "<td><input type=\"text\" name=\"{$this->element_name}_aliases[$order[$i]]\"";
+				$out .= "\" size=\"12\" maxlength=\"50\" value=\"";
+				$out .= $aliases[$order[$i]] . "\"></td>\n";
 			}
 			else
 				$out .= "<td>&nbsp;</td>\n";
 			
-			// move left
-			$out .= "<td valign=\"top\" nowrap=\"nowrap\">";
-			$out .= "<input type=\"image\" name=\"move_left[$i]\" ";
-			$out .= "img src=\"" . $GLOBALS["CANONICAL_RELATIVE_PATH_STUDIP"] . "pictures/move_up.gif\"";
-			$out .= tooltip(_("nach links verschieben"));
-			$out .= "border=\"0\" align=\"bottom\">\n";
-			
-			// move right
-			$out .= "<input type=\"image\" name=\"move_right[$i]\" ";
-			$out .= "img src=\"" . $GLOBALS["CANONICAL_RELATIVE_PATH_STUDIP"] . "pictures/move_down.gif\"";
-			$out .= tooltip(_("nach rechts verschieben"));
-			$out .= "border=\"0\" align=\"bottom\">\n&nbsp;";
-			
-			// visible
-			if ($visible[$order[$i]]) {
-				$out .= "<input type=\"image\" name=\"hide[{$order[$i]}]\" ";
-				$out .= "img src=\"" . $GLOBALS["CANONICAL_RELATIVE_PATH_STUDIP"] . "pictures/on_small.gif\"";
-				$out .= tooltip(_("Spalte ausblenden"));
-				$out .= "border=\"0\" align=\"middle\">\n";
+			// width
+			if (!in_array("width", $hide)) {
+				$width = str_replace("%", "", $widths[$order[$i]]);
+				$out .= "<td><input type=\"text\" name=\"{$this->element_name}_width[$order[$i]]";
+				$out .= "\" size=\"3\" maxlength=\"3\" value=\"$width\">\n</td>\n";
 			}
-			else {
-				$out .= "<input type=\"image\" name=\"show[{$order[$i]}]\" ";
-				$out .= "img src=\"" . $GLOBALS["CANONICAL_RELATIVE_PATH_STUDIP"] . "pictures/off_small.gif\"";
-				$out .= tooltip(_("Spalte einblenden"));
-				$out .= "border=\"0\" align=\"middle\">\n</td>\n";
+			
+			// sort
+			if (!in_array("sort", $hide)) {
+				if (!in_array($order[$i], $hide_fields["sort"])) {
+					$out .= "<td><select name=\"{$this->element_name}_sort[$order[$i]]\" ";
+					$out .= "size=\"1\">\n";
+					for ($j = 0; $j <= (sizeof($order) - sizeof($no_sort)); $j++) {
+						if ($sort[$order[$i]] == $j)
+							$selected = " selected";
+						else
+							$selected = "";
+					
+						if ($j == 0)
+							$out .= "<option value=\"0\"$selected>keine</option>";
+						else
+							$out .= "<option value=\"$j\"$selected>$j</option>";
+					}
+					$out .= "\n</select>\n</td>\n";
+				}
+				else
+					$out .= "<td>&nbsp;</td>\n";
+			}
+			
+			if (!in_array("visible", $hide)) {
+				// move left
+				$out .= "<td valign=\"top\" nowrap=\"nowrap\">";
+				$out .= "<input type=\"image\" name=\"{$this->element_name}_move_left[$i]\" ";
+				$out .= "img src=\"" . $GLOBALS["CANONICAL_RELATIVE_PATH_STUDIP"] . "pictures/move_up.gif\"";
+				$out .= tooltip(_("nach links verschieben"));
+				$out .= "border=\"0\" align=\"bottom\">\n";
+				
+				// move right
+				$out .= "<input type=\"image\" name=\"{$this->element_name}_move_right[$i]\" ";
+				$out .= "img src=\"" . $GLOBALS["CANONICAL_RELATIVE_PATH_STUDIP"] . "pictures/move_down.gif\"";
+				$out .= tooltip(_("nach rechts verschieben"));
+				$out .= "border=\"0\" align=\"bottom\">\n&nbsp;";
+				
+				// visible
+				if ($visible[$order[$i]]) {
+					$out .= "<input type=\"image\" name=\"{$this->element_name}_hide[{$order[$i]}]\" ";
+					$out .= "img src=\"" . $GLOBALS["CANONICAL_RELATIVE_PATH_STUDIP"] . "pictures/on_small.gif\"";
+					$out .= tooltip(_("Spalte ausblenden"));
+					$out .= "border=\"0\" align=\"middle\">\n";
+				}
+				else {
+					$out .= "<input type=\"image\" name=\"{$this->element_name}_show[{$order[$i]}]\" ";
+					$out .= "img src=\"" . $GLOBALS["CANONICAL_RELATIVE_PATH_STUDIP"] . "pictures/off_small.gif\"";
+					$out .= tooltip(_("Spalte einblenden"));
+					$out .= "border=\"0\" align=\"middle\">\n</td>\n";
+				}
 			}
 			
 			$out .= "</tr>\n";
@@ -134,23 +156,25 @@ class ExternEditModule extends ExternEditHtml {
 		}
 		
 		// width in pixels or percent
-		$title = _("Breite in:");
-		$info = _("Wählen Sie hier, ob die Breiten der Tabellenspalten als Prozentwerte oder Pixel interpretiert werden sollen.");
-		$width_values = array("%", "");
-		$width_names = array(_("Prozent"), _("Pixel"));
-		$out .= "<tr" . $this->css->getFullClass() . ">\n";
-		$out .= "<td><font size=\"2\">&nbsp;$title</font></td>";
-		$out .= "<td colspan=\"4\"><input type=\"radio\" name=\"Main_widthpp\" value=\"%\"";
-		if (substr($widths[0], -1) == "%")
-			$out .= " checked=\"checked\"";
-		$out .= " /><font size=\"2\">" . _("Prozent") . "&nbsp; &nbsp;</font><input type=\"radio\" name=\"";
-		$out .= "Main_widthpp\" value=\"\"";
-		if (substr($widths[0], -1) != "%")
-			$out .= " checked=\"checked\"";
-		$out .= " /><font size=\"2\">" . _("Pixel") . "&nbsp; &nbsp;</font>\n";
-		$out .= "<img src=\"" . $GLOBALS["CANONICAL_RELATIVE_PATH_STUDIP"] . "pictures/info.gif\"";
-		$out .= tooltip($info, TRUE, TRUE) . ">$error_sign</td></tr>\n";
-		$this->css->switchClass();
+		if (!in_array("widthpp", $hide)) {
+			$colspan = 4 - sizeof($hide);
+			$title = _("Breite in:");
+			$info = _("Wählen Sie hier, ob die Breiten der Tabellenspalten als Prozentwerte oder Pixel interpretiert werden sollen.");
+			$width_values = array("%", "");
+			$width_names = array(_("Prozent"), _("Pixel"));
+			$out .= "<tr" . $this->css->getFullClass() . ">\n";
+			$out .= "<td><font size=\"2\">&nbsp;$title</font></td>";
+			$out .= "<td colspan=\"$colspan\"><input type=\"radio\" name=\"{$this->element_name}_widthpp\" value=\"%\"";
+			if (substr($widths[0], -1) == "%")
+				$out .= " checked=\"checked\"";
+			$out .= " /><font size=\"2\">" . _("Prozent") . "&nbsp; &nbsp;</font><input type=\"radio\" name=\"";
+			$out .= "{$this->element_name}_widthpp\" value=\"\"";
+			if (substr($widths[0], -1) != "%")
+				$out .= " checked=\"checked\"";
+			$out .= " /><font size=\"2\">" . _("Pixel") . "&nbsp; &nbsp;</font>\n";
+			$out .= "<img src=\"" . $GLOBALS["CANONICAL_RELATIVE_PATH_STUDIP"] . "pictures/info.gif\"";
+			$out .= tooltip($info, TRUE, TRUE) . ">$error_sign</td></tr>\n";
+		}
 		
 		$out .= "</table>\n</td></tr>\n";
 		
@@ -190,13 +214,14 @@ class ExternEditModule extends ExternEditHtml {
 		$out = "<tr><td><table width=\"100%\" border=\"0\" cellpadding=\"2\" cellspacing=\"0\">\n";
 		$out .= "<tr" . $this->css->getFullClass() . ">\n";
 		$out .= "<td width=\"42%\"><font size=\"2\"><b>" . _("Gruppenname") . "</b></font></td>\n";
-		$out .= "<td width=\"48%\"><font size=\"2\"><b>" . _("alternativer Gruppenname") . "</b></font>$error_sign</td>\n";
+		$out .= "<td width=\"48%\"><font size=\"2\"><b>" . _("alternativer Gruppenname") . "</b></font>";
+		$out .= $this->faulty_values[$this->element_name . "_groupsalias"] ? $this->error_sign : "";
+		$out .= "</td>\n";
 		$out .= "<td width=\"1%\"><font size=\"2\"><b>" . _("Sichtbarkeit") . "</b></font></td>\n";
 		$out .= "<td width=\"9%\"><font size=\"2\">&nbsp;</font></td>\n";
 		$out .= "</tr>\n";
 		$this->css->switchClass();
 		
-		reset($groups_db);
 		foreach ($groups_db as $id => $name) {
 		
 			// name of group
@@ -206,19 +231,19 @@ class ExternEditModule extends ExternEditHtml {
 			$out .= "<td nowrap=\"nowrap\"><font size=\"2\">&nbsp;" . htmlReady($name) . "</font></td>";
 			
 			// column headline
-			$out .= "<td nowrap=\"nowrap\"><input type=\"text\" name=\"Main_groupsalias[]\"";
+			$out .= "<td nowrap=\"nowrap\"><input type=\"text\" name=\"{$this->element_name}_groupsalias[]\"";
 			$out .= "\" size=\"25\" maxlength=\"150\" value=\"";
 			$out .= $groups[$id] . "\"></td>\n";
 			
 			// visible
 			if (in_array($id, $groups_visible)) {
-				$out .= "<td align=\"center\"><input type=\"image\" name=\"hide_group[$id]\" ";
+				$out .= "<td align=\"center\"><input type=\"image\" name=\"{$this->element_name}_hide_group[$id]\" ";
 				$out .= "img src=\"" . $GLOBALS["CANONICAL_RELATIVE_PATH_STUDIP"] . "pictures/on_small.gif\"";
 				$out .= tooltip(_("Spalte ausblenden"));
 				$out .= "border=\"0\" align=\"middle\">\n</td>\n";
 			}
 			else {
-				$out .= "<td align=\"center\"><input type=\"image\" name=\"show_group[$id]\" ";
+				$out .= "<td align=\"center\"><input type=\"image\" name=\"{$this->element_name}_show_group[$id]\" ";
 				$out .= "img src=\"" . $GLOBALS["CANONICAL_RELATIVE_PATH_STUDIP"] . "pictures/off_small.gif\"";
 				$out .= tooltip(_("Spalte einblenden"));
 				$out .= "border=\"0\" align=\"middle\">\n</td>\n";
@@ -226,9 +251,8 @@ class ExternEditModule extends ExternEditHtml {
 			$out .= "<td>&nbsp;</td></tr>\n";
 			$this->css->switchClass();
 		}
-		reset($groups_db);
 		foreach ($groups_db as $id => $name)
-			$out .= "<input type=\"hidden\" name=\"Main_groups[]\" value=\"$id\">\n";
+			$out .= "<input type=\"hidden\" name=\"{$this->element_name}_groups[]\" value=\"$id\">\n";
 			
 		$out .= "</table>\n</td></tr>\n";
 		
