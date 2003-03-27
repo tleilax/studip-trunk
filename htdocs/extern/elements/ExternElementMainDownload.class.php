7d9dbe73ed1a692a1a3c97d60deb8d49e13b59d3
Peter Thienel <thienel@data-quest.de>
2003-03-27 13:52:06 +0000

    Verlinkung zu Mitarbeiterdetails usw.

--- htdocs/extern/elements/ExternElementMainDownload.class.php
+++ htdocs/extern/elements/ExternElementMainDownload.class.php
@@ -40,8 +40,9 @@ require_once($GLOBALS["ABSOLUTE_PATH_STUDIP"].$GLOBALS["RELATIVE_PATH_EXTERN"]."
 class ExternElementMainDownload extends ExternElementMain {
 
 	var $attributes = array("name", "order", "visible", "aliases", "width", "width_pp", "sort",
-			"wholesite", "lengthdesc", "nameformat", "urlcss", "title", "nodatatext",
-			"iconpic", "icontxt", "iconpdf", "iconppt", "iconxls", "iconrtf", "iconzip", "icondefault");
+			"wholesite", "lengthdesc", "nameformat", "urlcss", "title", "nodatatext", "dateformat",
+			"datelanguage", "srilink", "config", "iconpic", "icontxt", "iconpdf", "iconppt", "iconxls",
+			"iconrtf", "iconzip", "icondefault");
 	var $edit_function = "editMainSettings";
 	
 	/**
@@ -66,10 +67,14 @@ class ExternElementMainDownload extends ExternElementMain {
 			"sort" => "|0|0|0|1|0|0",
 			"wholesite" => "",
 			"lengthdesc" => "",
-			"nametitle" => "1",
+			"nameformat" => "no_title",
 			"urlcss" => "",
 			"title" => "",
 			"nodatatext" => "",
+			"dateformat" => "%d. %b. %Y",
+			"datelanguage" => "de_DE",
+			"config" => "",
+			"srilink" => "",
 			"iconpic" => "",
 			"icontxt" => "",
 			"iconpdf" => "",
@@ -109,7 +114,7 @@ class ExternElementMainDownload extends ExternElementMain {
 		$headline = $edit_form->editHeadline(_("Allgemeine Angaben zum Tabellenaufbau"));
 		
 		$edit_function = $this->edit_function;
-		$table = $edit_form->$edit_function($this->field_names, array(0));
+		$table = $edit_form->$edit_function($this->field_names, array("sort" => array(0)));
 		
 		$content_table .= $edit_form->editContentTable($headline, $table);
 		$content_table .= $edit_form->editBlankContent();
@@ -118,9 +123,9 @@ class ExternElementMainDownload extends ExternElementMain {
 		
 		$title = _("HTML-Header/Footer:");
 		$info = _("Anwählen, wenn die Seite als komplette HTML-Seite ausgegeben werden soll, z.B. bei direkter Verlinkung oder in einem Frameset.");
-		$wholesite_values = "1";
-		$wholesite_names = "";
-		$table = $edit_form->editCheckboxGeneric("wholesite", $title, $info, $wholesite_values, $wholesite_names);
+		$values = "1";
+		$names = "";
+		$table = $edit_form->editCheckboxGeneric("wholesite", $title, $info, $values, $names);
 		
 		$title = _("Max. L&auml;nge der Beschreibung:");
 		$info = _("Geben Sie an, wieviele Zeichen der Beschreibung der Datei ausgegeben werden sollen.");
@@ -128,10 +133,23 @@ class ExternElementMainDownload extends ExternElementMain {
 		
 		$title = _("Namensformat:");
 		$info = _("Wählen Sie, wie Personennamen formatiert werden sollen.");
-		$nametitle_values = array("no_title", "no_title_rev", "full", "full_rev");
-		$nametitle_names = array(_("Vorname Nachname"), _("Nachname Vorname"),
+		$values = array("no_title", "no_title_rev", "full", "full_rev");
+		$names = array(_("Vorname Nachname"), _("Nachname Vorname"),
 				_("Titel Vorname Nachname"), _("Nachname Vorname Titel"));
-		$table .= $edit_form->editOptionGeneric("nameformat", $title, $info, $nametitle_values, $nametitle_names);
+		$table .= $edit_form->editOptionGeneric("nameformat", $title, $info, $values, $names);
+		
+		$title = _("Datumsformat:");
+		$info = _("Wählen Sie, wie Datumsangaben formatiert werden sollen.");
+		$values = array("%d. %b. %Y", "%d.%m.%Y", "%d.%m.%y", "%d. %B %Y", "%m/%d/%y");
+		$names = array(_("25. Nov. 2003"), _("25.11.2003"), _("25.11.03"),
+				_("25. November 2003"), _("11/25/03"));
+		$table .= $edit_form->editOptionGeneric("dateformat", $title, $info, $values, $names);
+		
+		$title = _("Sprache Datum");
+		$info = ("Wählen Sie eine Sprache für die Datumsangaben aus.");
+		$values = array("de_DE", "en_US");
+		$names = array(_("Deutsch"), _("Englisch (US)"));
+		$table .= $edit_form->editOptionGeneric("datelanguage", $title, $info, $values, $names);
 		
 		$title = _("Stylesheet-Datei:");
 		$info = _("Geben Sie hier die URL Ihrer Stylesheet-Datei an.");
@@ -144,6 +162,26 @@ class ExternElementMainDownload extends ExternElementMain {
 		$title = _("Keine Dateien:");
 		$info = _("Dieser Text wird an Stelle der Tabelle ausgegeben, wenn keine Dateien zum Download verfügbar sind.");
 		$table .= $edit_form->editTextareaGeneric("nodatatext", $title, $info, 3, 50);
+		
+		$title = _("Konfiguration Mitarbeiterdetails:");
+		$info = ("Der Link auf die Seite zur Anzeige der Mitarbeiterdaten wird die gewählte Konfiguration aufrufen. Wählen Sie \"Standard\", um die von Ihnen gesetzte Standardkonfiguration zu benutzen. Ist für das Mitarbeitermodul noch keine Konfiguration erstellt worden, wird die Stud.IP-Default-Konfiguration verwendet.");
+		if ($configs = get_all_configurations($this->config->range_id, 6)) {
+			$values = array_keys($configs["Persondetails"]);
+			unset($names);
+			foreach ($configs["Persondetails"] as $config)
+				$names[] = $config["name"];
+		}
+		else {
+			$values = array();
+			$names = array();
+		}
+		array_unshift($values, "");
+		array_unshift($names, _("Standardkonfiguration"));
+		$table .= $edit_form->editOptionGeneric("config", $title, $info, $values, $names);
+		
+		$title = _("SRI-Link:");
+		$info = _("Wenn Sie die SRI-Schnittstelle benutzen, müssen Sie hier die vollständige URL (mit http://) der Seite angeben, in der das Ausgabemodul für die Mitarbeiterdaten eingebunden wird. Lassen Sie dieses Feld unbedingt leer, falls Sie die SRI-Schnittstelle nicht nutzen.");
+		$table .= $edit_form->editTextfieldGeneric("srilink", $title, $info, 50, 250);
 		
 		$content_table .= $edit_form->editContentTable($headline, $table);
 		$content_table .= $edit_form->editBlankContent();
