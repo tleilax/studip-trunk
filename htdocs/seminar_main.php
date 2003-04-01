6897db7bb98e79dce5c1709f93dccfcdad187de1
André Noack <noackorama@gmail.com>
2003-04-01 15:27:17 +0000

    spaces -> tabs

--- htdocs/seminar_main.php
+++ htdocs/seminar_main.php
@@ -35,38 +35,38 @@ if ($GLOBALS['CHAT_ENABLE']){
 }
 
 if (isset($auswahl) && $auswahl!="") {
-        //just opened Veranstaltung... here follows the init
-        openSem($auswahl);
+		//just opened Veranstaltung... here follows the init
+		openSem($auswahl);
 } else {
-        $auswahl=$SessSemName[1];
+		$auswahl=$SessSemName[1];
 }
 
 
 // gibt es eine Anweisung zur Umleitung?
 if(isset($redirect_to) && $redirect_to != "") {
-        $take_it = 0;
+		$take_it = 0;
 
-        for ($i = 0; $i < count($i_query); $i++) { // alle Parameter durchwandern
-                $parts = explode('=',$i_query[$i]);
-                if ($parts[0] == "redirect_to") {
-                        // aha, wir haben die erste interessante Angabe gefunden
-                        $new_query = $parts[1];
-                        $take_it ++;
-                }        elseif ($take_it) {
-                        // alle weiteren Parameter mit einsammeln
-                        if ($take_it == 1) { // hier kommt der erste
-                                $new_query .= '?';
-                        } else { // hier kommen alle weiteren
-                                $new_query .= '&';
-                        }
-                        $new_query .= $i_query[$i];
-                        $take_it ++;
-                }
-        }
-        header("Location: $new_query");
-        unset($redirect_to);
-        page_close();
-        die;
+		for ($i = 0; $i < count($i_query); $i++) { // alle Parameter durchwandern
+				$parts = explode('=',$i_query[$i]);
+				if ($parts[0] == "redirect_to") {
+						// aha, wir haben die erste interessante Angabe gefunden
+						$new_query = $parts[1];
+						$take_it ++;
+				} elseif ($take_it) {
+						// alle weiteren Parameter mit einsammeln
+						if ($take_it == 1) { // hier kommt der erste
+								$new_query .= '?';
+						} else { // hier kommen alle weiteren
+								$new_query .= '&';
+						}
+						$new_query .= $i_query[$i];
+						$take_it ++;
+				}
+		}
+		header("Location: $new_query");
+		unset($redirect_to);
+		page_close();
+		die;
 }
 
 // Start of Output
@@ -82,84 +82,84 @@ include "show_dates.inc.php";
 $sess->register("smain_data");
 //Auf und Zuklappen Termine
 if ($dopen)
-        $smain_data["dopen"]=$dopen;
+		$smain_data["dopen"]=$dopen;
 
 if ($dclose)
-        $smain_data["dopen"]='';
+		$smain_data["dopen"]='';
 
 //Auf und Zuklappen News
 if ($nopen)
-        $smain_data["nopen"]=$nopen;
+		$smain_data["nopen"]=$nopen;
 
 if ($nclose)
-        $smain_data["nopen"]='';
+		$smain_data["nopen"]='';
 
 ?>
-        <table width="100%" border=0 cellpadding=0 cellspacing=0>
-        <tr><td class="topic" colspan=2><b>&nbsp;<? echo $SessSemName["header_line"]. " - " . _("Kurzinfo"); ?>
-        </b></td></tr>
-        <tr><td class="blank" valign="top"><blockquote>
-        <?
+		<table width="100%" border=0 cellpadding=0 cellspacing=0>
+		<tr><td class="topic" colspan=2><b>&nbsp;<? echo $SessSemName["header_line"]. " - " . _("Kurzinfo"); ?>
+		</b></td></tr>
+		<tr><td class="blank" valign="top"><blockquote>
+		<?
 
-        if ($SessSemName[3]) {
-                echo "<br /><b>" . _("Untertitel:") . " </b>"; echo htmlReady($SessSemName[3]); echo"<br>";
-        }
+		if ($SessSemName[3]) {
+				echo "<br /><b>" . _("Untertitel:") . " </b>"; echo htmlReady($SessSemName[3]); echo"<br>";
+		}
 
-        echo "<br><b>" . _("Zeit:") . " </b>", view_turnus($SessionSeminar, FALSE);
+		echo "<br><b>" . _("Zeit:") . " </b>", view_turnus($SessionSeminar, FALSE);
 
-        if (getRoom($SessSemName[1])) {
-                echo "<br><b>" . _("Ort:") . " </b>".getRoom($SessSemName[1]);
-        }
+		if (getRoom($SessSemName[1])) {
+				echo "<br><b>" . _("Ort:") . " </b>".getRoom($SessSemName[1]);
+		}
 
-        $db=new DB_Seminar;
-        $db->query ("SELECT seminar_user.user_id, " . $_fullname_sql['full'] . " AS fullname, username, status FROM seminar_user LEFT JOIN auth_user_md5 USING (user_id)  LEFT JOIN user_info USING(user_id) WHERE seminar_user.Seminar_id = '$SessionSeminar' AND status = 'dozent' ORDER BY Nachname");
-        if ($db->affected_rows() > 1)
-                printf ("<br><b>%s: </b>", ($SEM_CLASS[$SEM_TYPE[$SessSemName["art_num"]]["class"]]["workgroup_mode"]) ? _("LeiterInnen") : _("DozentInnen"));
-        else
-                printf ("<br><b>%s: </b>", ($SEM_CLASS[$SEM_TYPE[$SessSemName["art_num"]]["class"]]["workgroup_mode"]) ? _("LeiterIn") : _("DozentIn"));
+		$db=new DB_Seminar;
+		$db->query ("SELECT seminar_user.user_id, " . $_fullname_sql['full'] . " AS fullname, username, status FROM seminar_user LEFT JOIN auth_user_md5 USING (user_id)  LEFT JOIN user_info USING(user_id) WHERE seminar_user.Seminar_id = '$SessionSeminar' AND status = 'dozent' ORDER BY Nachname");
+		if ($db->affected_rows() > 1)
+				printf ("<br><b>%s: </b>", ($SEM_CLASS[$SEM_TYPE[$SessSemName["art_num"]]["class"]]["workgroup_mode"]) ? _("LeiterInnen") : _("DozentInnen"));
+		else
+				printf ("<br><b>%s: </b>", ($SEM_CLASS[$SEM_TYPE[$SessSemName["art_num"]]["class"]]["workgroup_mode"]) ? _("LeiterIn") : _("DozentIn"));
 
-        $i=0;
-        while ($db->next_record()) {
-                if ($i)
-                        print( ", <a href = about.php?username=" . $db->f("username") . ">");
-                else
-                        print( "<a href = about.php?username=" . $db->f("username") . ">");
-                print(htmlReady($db->f("fullname")) ."</a>");
-                $i++;
-        }
-        ?>
-        </blockquote><br><br>
-        </td>
+		$i=0;
+		while ($db->next_record()) {
+				if ($i)
+						print( ", <a href = about.php?username=" . $db->f("username") . ">");
+				else
+						print( "<a href = about.php?username=" . $db->f("username") . ">");
+				print(htmlReady($db->f("fullname")) ."</a>");
+				$i++;
+		}
+		?>
+		</blockquote><br><br>
+		</td>
 
-        <td class="blank" align = right><img src="pictures/board2.jpg" border="0"></td>
-        </tr></table><br>
+		<td class="blank" align = right><img src="pictures/board2.jpg" border="0"></td>
+		</tr></table><br>
 
 
-        <?php
+		<?php
 
-        // Anzeige von News
+		// Anzeige von News
 
-        ($rechte) ? $show_admin=TRUE : $show_admin=FALSE;
-        if (show_news($auswahl,$show_admin, 0, $smain_data["nopen"], "100%", $loginfilelast[$SessSemName[1]]))
-                echo"<br>";
-        // Anzeige von Terminen
-        $start_zeit=time();
-        $end_zeit=$start_zeit+1210000;
-        $name = rawurlencode($SessSemName[0]);
-        ($rechte) ? $show_admin="admin_dates.php?range_id=$SessSemName[1]&ebene=sem&new_sem=TRUE" : $show_admin=FALSE;
-        if (show_dates($auswahl, $start_zeit, $end_zeit, 0, 0, $show_admin, $smain_data["dopen"]))
-                echo"<br>";
-        //show chat info
-        if ($GLOBALS['CHAT_ENABLE']){
-                if (chat_show_info($auswahl))
-                        echo "<br>";
-        }
+		($rechte) ? $show_admin=TRUE : $show_admin=FALSE;
+		if (show_news($auswahl,$show_admin, 0, $smain_data["nopen"], "100%", $loginfilelast[$SessSemName[1]]))
+				echo"<br>";
+		// Anzeige von Terminen
+		$start_zeit=time();
+		$end_zeit=$start_zeit+1210000;
+		$name = rawurlencode($SessSemName[0]);
+		($rechte) ? $show_admin="admin_dates.php?range_id=$SessSemName[1]&ebene=sem&new_sem=TRUE" : $show_admin=FALSE;
+		if (show_dates($auswahl, $start_zeit, $end_zeit, 0, 0, $show_admin, $smain_data["dopen"]))
+				echo"<br>";
+		//show chat info
+		if ($GLOBALS['CHAT_ENABLE']){
+				if (chat_show_info($auswahl))
+						echo "<br>";
+		}
 
 ?>
 </body>
 </html>
 <?php
-  // Save data back to database.
-  page_close();
+// Save data back to database.
+page_close();
 ?>
 <!-- $Id$ -->
