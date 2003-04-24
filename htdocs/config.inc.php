4f23dd255dba5d6e8ce9c4088f043fcd0da4899e
arne <arne>
2003-04-24 11:20:36 +0000

    Lernmodul-Konfig-Variablen ausgelagert

--- htdocs/config.inc.php
+++ htdocs/config.inc.php
@@ -379,4 +379,33 @@ $export_icon["htm"] = "txt-icon.gif";
 $export_icon["txt"] = "txt-icon.gif";
 // weitere Icons und Formate können hier angefügt werden
 // EXPORT -->>
+
+// <<-- LERNMODULE
+// Zeichenkette, die vor Ilias-Usernamen gesetzt wird:
+// IM LAUFENDEN BETRIEB NICHT MEHR ÄNDERN!!!
+$username_prefix = "studip_";
+
+// Zuordnung von Stud.IP-Status zu ILIAS-Status
+// DEFAULT: 1 = Gast, 2 = Superuser, 3 = StudentIn, 4 = MitarbeiterIn
+$ilias_status = array(
+"user" => "1",
+"autor" => "3",
+"tutor" => "3",
+"dozent" => "4",
+"admin" => "2",
+"root" => "2",
+);
+
+// Zuordnung von Stud.IP-Status zu ILIAS-System-Gruppe
+// DEFAULT: 1 = AdministratorIn, 2 = AutorIn, 3 = LernerIn, 4 = Gast
+$ilias_systemgroup = array(
+"user" => "4",
+"autor" => "2",
+"tutor" => "2",
+"dozent" => "2",
+"admin" => "1",
+"root" => "1",
+);
+// LERNMODULE -->>
+
 ?>
