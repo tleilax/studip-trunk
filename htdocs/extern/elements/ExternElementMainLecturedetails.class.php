f69452f86c6dedb513db3e455a5ab35243f1e2ce
Peter Thienel <thienel@data-quest.de>
2003-05-07 21:01:14 +0000

    Bugfix

--- htdocs/extern/elements/ExternElementMainLecturedetails.class.php
+++ htdocs/extern/elements/ExternElementMainLecturedetails.class.php
@@ -63,7 +63,7 @@ class ExternElementMainLecturedetails extends ExternElementMain {
 			"visible" => "|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1|1",
 			"aliases" => "|"._("Untertitel:")." |"._("DozentIn:")." |"._("Veranstaltungsart:")
 				." |"._("Veranstaltungstyp:")." |"._("Beschreibung:")." |"._("Ort:")." |"._("Semester:")
-				." |"._("Zeiten:")." |"." |"._("Veranstaltungsnummer:")." |"._("TeilnehmerInnen:")
+				." |"._("Zeiten:")." |"._("Veranstaltungsnummer:")." |"._("TeilnehmerInnen:")
 				." |"._("Voraussetzungen:")." |"._("Lernorganisation:")." |"._("Leistungsnachweis:")
 				." |"._("Bereichseinordnung:")." |"._("Sonstiges:")." |"._("ECTS-Kreditpunkte:"),
 			"aliaspredisc" => _("Vorbesprechung:") . " ",
