7077e544b4d29c25fcfe22c0e2e1399c5c1905c2
André Noack <noackorama@gmail.com>
2003-05-08 09:32:40 +0000

    bugfix: dubletten entfernt

--- db/studip_demo_data.sql
+++ db/studip_demo_data.sql
@@ -114,25 +114,6 @@ INSERT INTO `resources_assign` (`assign_id`, `resource_id`, `assign_user_id`, `u
 INSERT INTO `resources_assign` (`assign_id`, `resource_id`, `assign_user_id`, `user_free_name`, `begin`, `end`, `repeat_end`, `repeat_quantity`, `repeat_interval`, `repeat_month_of_year`, `repeat_day_of_month`, `repeat_month`, `repeat_week_of_month`, `repeat_day_of_week`, `repeat_week`, `mkdate`, `chdate`) VALUES ('19d02a3ff03a68b1be353f64303fd7c5', '47107f2140bdfa0ba0352c32af45535f', 'd1718fcb6e0fc19e1fd1e0d4b3790c15', '', 1057047300, 1057052700, 1057052700, 0, 0, 0, 0, 0, 0, 0, 0, 1051715234, 1051715234);
 INSERT INTO `resources_assign` (`assign_id`, `resource_id`, `assign_user_id`, `user_free_name`, `begin`, `end`, `repeat_end`, `repeat_quantity`, `repeat_interval`, `repeat_month_of_year`, `repeat_day_of_month`, `repeat_month`, `repeat_week_of_month`, `repeat_day_of_week`, `repeat_week`, `mkdate`, `chdate`) VALUES ('727de649fde6eb97af39e10a9d9e4823', '47107f2140bdfa0ba0352c32af45535f', '0577536619fe3d6c4a8536e23954df83', '', 1058256900, 1058262300, 1058262300, 0, 0, 0, 0, 0, 0, 0, 0, 1051715234, 1051715234);
 #
-# Dumping data for table `resources_categories`
-#
-INSERT INTO `resources_categories` (`category_id`, `name`, `description`, `system`, `iconnr`) VALUES ('1cf2a34de92c06137ecdfcef4a29e4bc', 'Raum', '', 1, 3);
-INSERT INTO `resources_categories` (`category_id`, `name`, `description`, `system`, `iconnr`) VALUES ('82bdd20907e914de72bbfc8043dd3a46', 'Gebäude', '', 0, 1);
-INSERT INTO `resources_categories` (`category_id`, `name`, `description`, `system`, `iconnr`) VALUES ('891662c701078186c857fca25d34ade6', 'Gerät', '', 0, 2);
-#
-# Dumping data for table `resources_categories_properties`
-#
-INSERT INTO `resources_categories_properties` (`category_id`, `property_id`, `system`) VALUES ('82bdd20907e914de72bbfc8043dd3a46', '8772d6757457c8b4a05b180e1c2eba9c', 0);
-INSERT INTO `resources_categories_properties` (`category_id`, `property_id`, `system`) VALUES ('82bdd20907e914de72bbfc8043dd3a46', '5753ab43945ae787f983f5c8a036712d', 0);
-INSERT INTO `resources_categories_properties` (`category_id`, `property_id`, `system`) VALUES ('891662c701078186c857fca25d34ade6', '1b86b5026052fd3d8624fead31204cba', 0);
-INSERT INTO `resources_categories_properties` (`category_id`, `property_id`, `system`) VALUES ('891662c701078186c857fca25d34ade6', '9c0658891b95fe962d013f1308feb80d', 0);
-INSERT INTO `resources_categories_properties` (`category_id`, `property_id`, `system`) VALUES ('891662c701078186c857fca25d34ade6', '7bff1a7d45bc37280e988f6e8d007bad', 0);
-INSERT INTO `resources_categories_properties` (`category_id`, `property_id`, `system`) VALUES ('1cf2a34de92c06137ecdfcef4a29e4bc', '0ef8a73d95f335cdfbaec50cae92762a', 0);
-INSERT INTO `resources_categories_properties` (`category_id`, `property_id`, `system`) VALUES ('1cf2a34de92c06137ecdfcef4a29e4bc', '5753ab43945ae787f983f5c8a036712d', 0);
-INSERT INTO `resources_categories_properties` (`category_id`, `property_id`, `system`) VALUES ('1cf2a34de92c06137ecdfcef4a29e4bc', '31abad810703df361d793361bf6b16e5', 0);
-INSERT INTO `resources_categories_properties` (`category_id`, `property_id`, `system`) VALUES ('1cf2a34de92c06137ecdfcef4a29e4bc', 'ef4ba565e635b45c3f43ecdc69fb4aca', 1);
-INSERT INTO `resources_categories_properties` (`category_id`, `property_id`, `system`) VALUES ('1cf2a34de92c06137ecdfcef4a29e4bc', '648b8579ffca64a565459fd6ea0313c5', 0);
-#
 # Dumping data for table `resources_objects`
 #
 INSERT INTO `resources_objects` (`resource_id`, `root_id`, `parent_id`, `category_id`, `owner_id`, `level`, `name`, `description`, `inventar_num`, `parent_bind`, `mkdate`, `chdate`) VALUES ('6e06400579d08e356608972fae08206d', '6e06400579d08e356608972fae08206d', '0', '', '8eec88158b9742e868dd47104620f614', '0', 'Veranstaltungsräume', '', '', 0, 1048795905, 1048795966);
@@ -148,21 +129,7 @@ INSERT INTO `resources_objects_properties` (`resource_id`, `property_id`, `state
 INSERT INTO `resources_objects_properties` (`resource_id`, `property_id`, `state`) VALUES ('47107f2140bdfa0ba0352c32af45535f', '31abad810703df361d793361bf6b16e5', 'Übungsraum');
 INSERT INTO `resources_objects_properties` (`resource_id`, `property_id`, `state`) VALUES ('47107f2140bdfa0ba0352c32af45535f', '5753ab43945ae787f983f5c8a036712d', 'on');
 INSERT INTO `resources_objects_properties` (`resource_id`, `property_id`, `state`) VALUES ('47107f2140bdfa0ba0352c32af45535f', '648b8579ffca64a565459fd6ea0313c5', 'on');
-#
-# Dumping data for table `resources_properties`
-#
-INSERT INTO `resources_properties` (`property_id`, `name`, `description`, `type`, `options`, `system`) VALUES ('ef4ba565e635b45c3f43ecdc69fb4aca', 'Sitzplätze', '', 'num', '', 1);
-INSERT INTO `resources_properties` (`property_id`, `name`, `description`, `type`, `options`, `system`) VALUES ('8772d6757457c8b4a05b180e1c2eba9c', 'Adresse', '', 'text', '', 0);
-INSERT INTO `resources_properties` (`property_id`, `name`, `description`, `type`, `options`, `system`) VALUES ('0ef8a73d95f335cdfbaec50cae92762a', 'Ausstattung', '', 'text', '', 0);
-INSERT INTO `resources_properties` (`property_id`, `name`, `description`, `type`, `options`, `system`) VALUES ('7bff1a7d45bc37280e988f6e8d007bad', 'Seriennummer', '', 'num', '', 0);
-INSERT INTO `resources_properties` (`property_id`, `name`, `description`, `type`, `options`, `system`) VALUES ('31abad810703df361d793361bf6b16e5', 'Raumtyp', '', 'select', 'Hörsaal;Übungsraum;Sitzungszimmer', 0);
-INSERT INTO `resources_properties` (`property_id`, `name`, `description`, `type`, `options`, `system`) VALUES ('5753ab43945ae787f983f5c8a036712d', 'behindertengerecht', '', 'bool', '', 0);
-INSERT INTO `resources_properties` (`property_id`, `name`, `description`, `type`, `options`, `system`) VALUES ('648b8579ffca64a565459fd6ea0313c5', 'Verdunklung', '', 'bool', 'vorhanden', 0);
-INSERT INTO `resources_properties` (`property_id`, `name`, `description`, `type`, `options`, `system`) VALUES ('9c0658891b95fe962d013f1308feb80d', 'Hersteller', '', 'num', '', 0);
-INSERT INTO `resources_properties` (`property_id`, `name`, `description`, `type`, `options`, `system`) VALUES ('1b86b5026052fd3d8624fead31204cba', 'Kaufdatum', '', 'num', '', 0);
-#
-# Dumping data for table `resources_user_resources`
-#
+
 #
 # Dumping data for table `sem_tree`
 #
