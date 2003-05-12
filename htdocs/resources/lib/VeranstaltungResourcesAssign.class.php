567d8b61cbca7bb93eae4a2fde922df6787b6757
André Noack <noackorama@gmail.com>
2003-05-12 08:54:49 +0000

    bugfix: $ABSOLUTE_PATH_STUDIP eingefügt

--- htdocs/resources/lib/VeranstaltungResourcesAssign.class.php
+++ htdocs/resources/lib/VeranstaltungResourcesAssign.class.php
@@ -34,8 +34,8 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // +---------------------------------------------------------------------------+
 
-require_once ("dates.inc.php");
-require_once ("config.inc.php");
+require_once ("$ABSOLUTE_PATH_STUDIP/dates.inc.php");
+require_once ("$ABSOLUTE_PATH_STUDIP/config.inc.php");
 
 class VeranstaltungResourcesAssign {
 	var $db;
