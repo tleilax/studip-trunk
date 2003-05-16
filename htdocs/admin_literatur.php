424f3a38ea33b9d40fbfc3f4e14b48f55f3db196
Stefan Suchi <suchi@data-quest.de>
2003-05-16 21:09:00 +0000

    I18N, entruempelt

--- htdocs/admin_literatur.php
+++ htdocs/admin_literatur.php
@@ -44,104 +44,93 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 	}
 
  	//maximale spaltenzahl berechnen
-	if ($auth->auth["jscript"]) $max_col = round($auth->auth["xres"] / 12 );
-		else $max_col =  64 ; //default für 640x480
+	if ($auth->auth["jscript"])
+		$max_col = round($auth->auth["xres"] / 12 );
+	else
+		$max_col =  64 ; //default für 640x480
 
 
-	 if($aendern && $range_id)
-		 {
-		 if ($new_entry)
-			 {
-			 $db->query("INSERT INTO literatur VALUES ('$lit_id','$range_id','$user->id','$literatur','$links', '".time()."', '".time()."')");
-			 if ($db->affected_rows()) $result="msg§Listen ge&auml;ndert";
-			 }
-
-		 else
-			 {
-			 $db->query("UPDATE literatur SET user_id='$user->id', literatur='$literatur', links='$links' WHERE literatur_id='$lit_id'");
-			 if ($db->affected_rows()) {
-			 	$result="msg§Literatur und Links ge&auml;ndert";
+	if($aendern && $range_id) {
+		if ($new_entry) {
+			$db->query("INSERT INTO literatur VALUES ('$lit_id','$range_id','$user->id','$literatur','$links', '".time()."', '".time()."')");
+			if ($db->affected_rows())
+				$result="msg§" . _("Listen ge&auml;ndert");
+		} else {
+			$db->query("UPDATE literatur SET user_id='$user->id', literatur='$literatur', links='$links' WHERE literatur_id='$lit_id'");
+			if ($db->affected_rows()) {
+				$result="msg§" . _("Literatur und Links ge&auml;ndert");
 			 	$db->query("UPDATE literatur SET chdate='".time()."' WHERE literatur_id='$lit_id'");
-			 	}
-			 }
+			}
+		}
+	}
 
-		 }
 ?>
 	<table cellspacing="0" cellpadding="0" border="0" width="100%">
 	<tr><td class="topic" colspan=2>&nbsp;<b>
 	<?
-	echo htmlReady($SessSemName["header_line"]) . " -  Literatur und Links";
+	echo $SessSemName["header_line"] . " -  " . _("Literatur und Links");
 	?></b></td></tr>
 	<tr><td class="blank" colspan=2>&nbsp;</td></tr>
 	<?
 	
-       if (isset($result)) 
-	     	{
-	     	$result=rawurldecode($result);
-		parse_msg($result);
-		$result="";
-		}
+	parse_msg($result);
 	
-	?>
-	<tr>
-		<td class="blank" colspan=2>
-			<blockquote>Sie k&ouml;nnen hier die Literaturliste und Links bearbeiten.
-			Im Feld Links k&ouml;nnen Sie Links normal eingeben, diese werden anschliessend automatisch als Hyperlinks dargestellt.
-			</blockquote>
-	<? 
-	  echo"<form action=\"$PHP_SELF\" method=\"POST\">";
-	  ?><table border="0" cellpadding="2" cellspacing="0" width="99%" align="center">
+	print ("<tr><td class=\"blank\" colspan=2><blockquote>");
+	print(_("Sie k&ouml;nnen hier die Literaturliste und Links bearbeiten. Im Feld Links k&ouml;nnen Sie Links normal eingeben, diese werden anschliessend automatisch als Hyperlinks dargestellt."));
+	print("</blockquote>");
+	print("<form action=\"$PHP_SELF\" method=\"POST\">");
+	?><table border="0" cellpadding="2" cellspacing="0" width="99%" align="center">
 	    <tr valign="top" align="center">
-	    <th width="100%" colspan=2 class="steelgraudunkel">Literaturliste</th>
-	    </tr><?
-	  $db->query("SELECT * FROM literatur WHERE range_id='$range_id'");
-	  if (!$db->num_rows())
-		{
+	    <th width="100%" colspan=2 class="steelgraudunkel"><?=_("Literaturliste")?></th>
+	    </tr>
+	<?
+	$db->query("SELECT * FROM literatur WHERE range_id='$range_id'");
+	if (!$db->num_rows()) {
 		$new_entry=TRUE;
 		$hash_secret = "blafasel25";
 		$lit_id=md5(uniqid($hash_secret));
 		$autor=get_fullname();
-		}
-	     else
-		 {
-		 $db->next_record();
-		 $db2->query("SELECT username FROM auth_user_md5 WHERE user_id = '".$db->f("user_id")."'");
-		 $db2->next_record();
-		 $new_entry=FALSE;
-		 $literatur=htmlReady($db->f("literatur"));
-		 $links=htmlReady($db->f("links"));
-		 $lit_id=$db->f("literatur_id");
-		 $autor=get_fullname($db->f("user_id"));
-		 }
-	  echo"<tr><td width=\"100%\" align=\"center\" colspan=2>";
-	  echo"<textarea  name=\"literatur\"  style=\"width: 100%\" cols=$max_col rows=10 wrap=virtual >$literatur</textarea></td></tr>";
-	  echo"<tr><td width=\"50%\" align=\"left\" class=\"steel1\">&nbsp; &Auml;nderungen <input type=\"IMAGE\" align=\"absmiddle\" name=\"send_button\" value=\"Änderungen vornehmen\" border=0 src=\"pictures/buttons/uebernehmen-button.gif\"></td>";
-	  echo"<td width=\"50%\" align=\"center\" class=\"steel1\"><font size=-1>Eingestellt von: <b><a href=\"about.php?username=".$db2->f("username")."\">$autor</a></b>";
-	  if ($db->f("chdate"))
-	  	echo ", letzte &Auml;nderung am ".date("d.m.y", $db->f("chdate"))." um ",date("H:i", $db->f("chdate"));
-	  echo "</font></td>";
-	  echo"<input type=\"HIDDEN\" name=\"range_id\" value=\"$range_id\">";
-	  echo"<input type=\"HIDDEN\" name=\"lit_id\" value=\"$lit_id\">";
-	  echo"<input type=\"HIDDEN\" name=\"new_entry\" value=\"$new_entry\">";
-	  echo"<input type=\"HIDDEN\" name=\"ebene\" value=\"$ebene\">";
-	  echo"<input type=\"HIDDEN\" name=\"aendern\" value=\"TRUE\">";	  
-	  echo"</td></tr>";
-	  echo"</table><br><br>";
+	} else {
+		$db->next_record();
+		$db2->query("SELECT username FROM auth_user_md5 WHERE user_id = '".$db->f("user_id")."'");
+		$db2->next_record();
+		$new_entry=FALSE;
+		$literatur=htmlReady($db->f("literatur"));
+		$links=htmlReady($db->f("links"));
+		$lit_id=$db->f("literatur_id");
+		$autor=get_fullname($db->f("user_id"));
+	}
+	echo"<tr><td width=\"100%\" align=\"center\" colspan=2>";
+	echo"<textarea  name=\"literatur\"  style=\"width: 100%\" cols=$max_col rows=10 wrap=virtual >$literatur</textarea></td></tr>";
+	echo"<tr><td width=\"50%\" align=\"left\" class=\"steel1\">&nbsp; ";
+	printf(_("&Auml;nderungen %s"), "<input type=\"IMAGE\" align=\"absmiddle\" name=\"send_button\" value=\"Änderungen vornehmen\" border=0 " . makeButton("uebernehmen", "src") . "></td>");
+	echo"<td width=\"50%\" align=\"center\" class=\"steel1\"><font size=-1>" . _("Eingestellt von:") . " <b><a href=\"about.php?username=".$db2->f("username")."\">$autor</a></b>";
+	if ($db->f("chdate"))
+		print(", " . _("letzte &Auml;nderung am") . " ".date("d.m.y", $db->f("chdate"))." " . _("um") . " " . date("H:i", $db->f("chdate")));
+	echo "</font>";
+	echo"<input type=\"HIDDEN\" name=\"range_id\" value=\"$range_id\">";
+	echo"<input type=\"HIDDEN\" name=\"lit_id\" value=\"$lit_id\">";
+	echo"<input type=\"HIDDEN\" name=\"new_entry\" value=\"$new_entry\">";
+	echo"<input type=\"HIDDEN\" name=\"aendern\" value=\"TRUE\">";	  
+	echo"</td></tr>";
+	echo"</table><br><br>";
 
-    ?><table border="0" cellpadding="2" cellspacing="0" width="99%" align="center">
+  ?><table border="0" cellpadding="2" cellspacing="0" width="99%" align="center">
 	    <tr valign="top" align="center">
-	    <th width="100%" colspan=2 class="steelgraudunkel">Links</th>
+	    <th width="100%" colspan=2 class="steelgraudunkel"><?=_("Links")?></th>
 	    </tr><?
 
-	  echo"<form action=\"$PHP_SELF\" method=\"POST\"><tr><td width=\"100%\" align=\"center\" colspan=\"2\">";
-	  echo"<textarea  name=\"links\"  style=\"width: 100%\" cols=$max_col rows=10 wrap=virtual >$links</textarea></td></tr>";
- 	  echo"<tr><td width=\"50%\" align=\"left\" class=\"steel1\">&nbsp; &Auml;nderungen <input type=\"IMAGE\" align=\"absmiddle\" name=\"send_button\" value=\"Änderungen vornehmen\" border=0 src=\"pictures/buttons/uebernehmen-button.gif\"></td>";
-	  echo"<td width=\"50%\" align=\"center\" class=\"steel1\"><font size=-1>Eingestellt von: <b><a href=\"about.php?username=".$db2->f("username")."\">$autor</a></b>";
-		  if ($db->f("chdate"))
-	  	echo ", letzte &Auml;nderung am ".date("d.m.y", $db->f("chdate"))." um ",date("H:i", $db->f("chdate"));
-	  echo "</font></td>";
-	  echo"</td></tr>";
-	  echo"</table><br></form></td></tr></table>";
+	echo"<tr><td width=\"100%\" align=\"center\" colspan=\"2\">";
+	echo"<textarea  name=\"links\"  style=\"width: 100%\" cols=$max_col rows=10 wrap=virtual >$links</textarea></td></tr>";
+	echo"<tr><td width=\"50%\" align=\"left\" class=\"steel1\">&nbsp; ";
+	printf(_("&Auml;nderungen %s"), "<input type=\"IMAGE\" align=\"absmiddle\" name=\"send_button\" value=\"Änderungen vornehmen\" border=0 " . makeButton("uebernehmen", "src") . "></td>");
+	echo"<td width=\"50%\" align=\"center\" class=\"steel1\"><font size=-1>" . _("Eingestellt von:") . " <b><a href=\"about.php?username=".$db2->f("username")."\">$autor</a></b>";
+	if ($db->f("chdate"))
+		print(", " . _("letzte &Auml;nderung am") . " ".date("d.m.y", $db->f("chdate"))." " . _("um") . " " . date("H:i", $db->f("chdate")));
+	echo "</font>";
+	echo"</td></tr>";
+	echo"</table><br></form>";
+	echo"</td></tr></table>";
 	  
 	page_close();
  ?>
