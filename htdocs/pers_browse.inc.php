50a8c13de0e0f888aeeedb6fbb949d91c38275d9
Stefan Suchi <suchi@data-quest.de>
2003-05-20 16:25:15 +0000

    I18N, Graphikbuttons

--- htdocs/pers_browse.inc.php
+++ htdocs/pers_browse.inc.php
@@ -19,34 +19,34 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 
-function perm_select($name,$default)
-{
-$possible_perms=array("alle","user","autor","tutor","dozent","admin","root");
-$counter=0;
-echo "<select name=\"$name\">";
-while ($counter <= 6 ) {
-	echo "<option";
-	if ($default==$possible_perms[$counter])  echo" selected";
-	echo ">$possible_perms[$counter]</option>";
-	$counter++;
-}
-echo "</select>";
-return;
+function perm_select($name,$default) {
+	$possible_perms=array(_("alle"),"user","autor","tutor","dozent","admin","root");
+	$counter=0;
+	echo "<select name=\"$name\">";
+	while ($counter <= 6 ) {
+		echo "<option";
+		if ($default==$possible_perms[$counter])
+			echo" selected";
+		echo ">$possible_perms[$counter]</option>";
+		$counter++;
+	}
+	echo "</select>";
+	return;
 }
 
-function crit_select($name,$default)
-{
-$possible_crits=array(">=","=","<","nie");
-$counter=0;
-echo "<select name=\"$name\">";
-while ($counter <= 3 ) {
-	echo "<option";
-	if ($default==$possible_crits[$counter])  echo" selected";
-	echo ">$possible_crits[$counter]</option>";
-	$counter++;
-}
-echo "</select>";
-return;
+function crit_select($name,$default) {
+	$possible_crits=array(">=","=","<",_("nie"));
+	$counter=0;
+	echo "<select name=\"$name\">";
+	while ($counter <= 3 ) {
+		echo "<option";
+		if ($default==$possible_crits[$counter])
+			echo" selected";
+		echo ">$possible_crits[$counter]</option>";
+		$counter++;
+	}
+	echo "</select>";
+	return;
 }
 
 
@@ -71,7 +71,7 @@ if (isset($pers_browse_search)) {
 		$pers_browse_search_string .= "Email LIKE '%" . $pers_browse_Email ."%' AND ";
 	if (isset($pers_browse_Nachname) && $pers_browse_Nachname != "")
 		$pers_browse_search_string .= "Nachname LIKE '%" . $pers_browse_Nachname ."%' AND ";
-	if (isset($pers_browse_perms) && $pers_browse_perms != "alle")
+	if (isset($pers_browse_perms) && $pers_browse_perms != _("alle"))
 		$pers_browse_search_string .= "perms = '$pers_browse_perms' AND ";
 	if (isset($pers_browse_changed) && $pers_browse_changed != "" && $pers_browse_changed >=0) {
 		$searchdate = date("YmdHis",time()-$pers_browse_changed*3600*24);
@@ -88,7 +88,7 @@ if (isset($pers_browse_search)) {
 				$pers_browse_search_string .= "changed < '$searchdate' AND changed > '$searchdate2' AND ";
 			}
 		}
-	if (isset($pers_browse_crit) && $pers_browse_crit == "nie")
+	if (isset($pers_browse_crit) && $pers_browse_crit == _("nie"))
 		$pers_browse_search_string .= "changed IS NULL AND ";
 	
 	if ($pers_browse_search_string != "") {
@@ -98,7 +98,7 @@ if (isset($pers_browse_search)) {
 	} else {
 		$sess->unregister("pers_browse_search_string");
 		unset($pers_browse_search_string);
-		$msg = "error§Bitte geben Sie einen Suchbegriff ein.§";
+		$msg = "error§" . _("Bitte geben Sie einen Suchbegriff ein.") . "§";
 	}
 }
 
@@ -115,28 +115,27 @@ if (isset($pers_browse_clear)) {
 // Suchformular
 print "<form action=\"$PHP_SELF\" method=\"post\">\n";
 print "<table border=0 align=\"center\" cellspacing=0 cellpadding=2 width = \"80%\">\n";
-print "<tr><th colspan=5>Suchformular</th></tr>";
-print "<tr><td class=steel1 align=\"right\" width=\"15%\">Benutzername: </td>";
+print "<tr><th colspan=5>" . _("Suchformular") . "</th></tr>";
+print "<tr><td class=steel1 align=\"right\" width=\"15%\">" . _("Benutzername:") . " </td>";
 print "<td class=steel1 align=\"left\" width=\"35%\"><input name=\"pers_browse_username\" type=\"text\" value=\"$pers_browse_old[username]\" size=30 maxlength=255></td>\n";
-print "<td class=steel1 align=\"right\" width=\"15%\">Vorname: </td>";
+print "<td class=steel1 align=\"right\" width=\"15%\">" . _("Vorname:") . " </td>";
 print "<td class=steel1 colspan=2 align=\"left\" width=\"35%\"><input name=\"pers_browse_Vorname\" type=\"text\" value=\"$pers_browse_old[Vorname]\" size=30 maxlength=255></td></tr>\n";
-print "<td class=steel1 align=\"right\" width=\"15%\">E-Mail: </td>";
+print "<td class=steel1 align=\"right\" width=\"15%\">" . _("E-Mail:") . " </td>";
 print "<td class=steel1 align=\"left\" width=\"35%\"><input name=\"pers_browse_Email\" type=\"text\" value=\"$pers_browse_old[Email]\" size=30 maxlength=255></td>\n";
-print "<td class=steel1 align=\"right\" width=\"15%\">Nachname: </td>";
+print "<td class=steel1 align=\"right\" width=\"15%\">" . _("Nachname:") . " </td>";
 print "<td class=steel1 colspan=2 align=\"left\" width=\"35%\"><input name=\"pers_browse_Nachname\" type=\"text\" value=\"$pers_browse_old[Nachname]\" size=30 maxlength=255></td></tr>\n";
-print "<td class=steel1 align=\"right\" width=\"15%\">Status: </td>";
+print "<td class=steel1 align=\"right\" width=\"15%\">" . _("Status:") . " </td>";
 print "<td class=steel1 align=\"left\" width=\"35%\">";
 	perm_select("pers_browse_perms",$pers_browse_old[perms]);
 print "</td>\n";
-print "<td class=steel1 align=\"right\" width=\"15%\">inaktiv: </td>";
+print "<td class=steel1 align=\"right\" width=\"15%\">" . _("inaktiv:") . " </td>";
 print "<td class=steel1 align=\"left\" width=\"10%\">";
 	crit_select("pers_browse_crit",$pers_browse_old[crit]);
 print "</td>";
 print "<td class=steel1 align=\"left\" width=\"25%\"><input name=\"pers_browse_changed\" type=\"text\" value=\"$pers_browse_old[changed]\" size=10 maxlength=50> Tage</td></tr>\n";
-print "<tr><td class=steel1>&nbsp</td><td class=steel1 align=\"left\"><input type=submit name=\"pers_browse_search\" value=\" Suche starten \"></td>\n";
-print "<td class=steel1>&nbsp</td><td class=steel1 colspan=2 align=\"left\"><input type=submit name=\"pers_browse_clear\" value=\" Formular zur&uuml;cksetzen \"></td></tr>\n";
+print "<tr><td class=steel1>&nbsp</td><td class=steel1 align=\"left\"><input type=\"IMAGE\" " . makeButton("suchestarten", "src") .  tooltip(_("Suche starten")) . " border=0><input type=\"HIDDEN\" name=\"pers_browse_search\" value=\"TRUE\"></td>\n";
+print "<td class=steel1>&nbsp</td><td class=steel1 colspan=2 align=\"left\"><a href=\"$PHP_SELF?pers_browse_clear=TRUE\"" . tooltip(_("Formular zurücksetzen")) . ">" . makeButton("zuruecksetzen", "img") . "</a></td></tr>\n";
 
 print "</table></form>\n";
-
 
 ?>
