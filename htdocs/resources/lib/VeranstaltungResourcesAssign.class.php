e4a30fbed0bde46d3a808856298aabc3372bc9cb
Cornelis Kater <kater@zqs.uni-hannover.de>
2003-06-18 10:07:34 +0000

    nochmal Fix der Abbruchbedingung

--- htdocs/resources/lib/VeranstaltungResourcesAssign.class.php
+++ htdocs/resources/lib/VeranstaltungResourcesAssign.class.php
@@ -101,9 +101,10 @@ class VeranstaltungResourcesAssign {
 		} else
 			$sem_begin = $term_data["start_termin"];
 			
-                //if there happens a mistake with the $sem_beginn, cancel.
-                if (!$sem_beginn)
-                        return FALSE;
+		//if there happens a mistake with the $sem_beginn, cancel.
+		if ($sem_begin <= 0) {
+			return FALSE;
+		}
 
 		$dow = date("w", $sem_begin);
 	
