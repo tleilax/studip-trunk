de0bf49add04a55c8b75cffd92a39ca2f8d9ea66
Cornelis Kater <kater@zqs.uni-hannover.de>
2003-07-02 07:54:39 +0000

    Bugfix

--- htdocs/seminar_main.php
+++ htdocs/seminar_main.php
@@ -27,7 +27,7 @@ require_once("$ABSOLUTE_PATH_STUDIP/dates.inc.php"); //Funktionen zur Anzeige de
 require_once("$ABSOLUTE_PATH_STUDIP/config.inc.php");
 require_once("$ABSOLUTE_PATH_STUDIP/visual.inc.php");
 require_once("$ABSOLUTE_PATH_STUDIP/functions.php");
-if (($GLOBALS['CHAT_ENABLE']) && ($modules["forum"])){
+if ($GLOBALS['CHAT_ENABLE']){
 	include_once $ABSOLUTE_PATH_STUDIP.$RELATIVE_PATH_CHAT."/chat_func_inc.php";
 	if ($_REQUEST['kill_chat']){
 		chat_kill_chat($_REQUEST['kill_chat']);
