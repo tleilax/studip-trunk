a0edae45eeb62e46637ab89f8fdba80416749d89
Cornelis Kater <kater@zqs.uni-hannover.de>
2003-07-09 16:23:45 +0000

    neuen Checks für Literatur eingebaut

--- htdocs/meine_einrichtungen.php
+++ htdocs/meine_einrichtungen.php
@@ -65,16 +65,12 @@ function get_my_inst_values(&$my_inst) {
 			if((chdate > b.loginfilenow AND user_id !='".$user->id."' AND (literatur !='' OR links != '')),1,0) AS neue 
 			FROM loginfilenow_".$user->id." b  LEFT JOIN literatur ON (range_id = b.Seminar_id)");
 	while($db2->next_record()) {
-		$my_inst[$db2->f("Seminar_id")]["neueliteratur"]=$db2->f("neue");
-		$my_inst[$db2->f("Seminar_id")]["literatur"]=$db2->f("literatur");
+		if ($my_inst[$db2->f("Seminar_id")]["modules"]["literature"]) {			
+			$my_inst[$db2->f("Seminar_id")]["neueliteratur"]=$db2->f("neue");
+			$my_inst[$db2->f("Seminar_id")]["literatur"]=$db2->f("literatur");
+		}
 	}
 
-	$db2->query("SELECT b.Seminar_id,count(termin_id) as count, count(if((chdate > b.loginfilenow AND autor_id !='".$user->id."'),a.termin_id,NULL)) AS neue 
-				FROM loginfilenow_".$user->id." b  LEFT JOIN termine a ON (b.Seminar_id=range_id) GROUP BY b.Seminar_id");
-	while($db2->next_record()) {
-		$my_inst[$db2->f("Seminar_id")]["neuetermine"]=$db2->f("neue");
-		$my_inst[$db2->f("Seminar_id")]["termine"]=$db2->f("count");
-	}
 	return;
 }
 
