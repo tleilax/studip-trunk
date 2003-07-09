693738c816ea17d8b9f21ed9d9496c28dc7fccbc
Stefan Suchi <suchi@data-quest.de>
2003-07-09 17:24:18 +0000

    favicon

--- htdocs/calendar/views/header.inc.php
+++ htdocs/calendar/views/header.inc.php
@@ -41,6 +41,7 @@ define("PHPDOC_DUMMY",true);
 
 echo "<html>\n<head>\n<title>Stud.IP</title>\n";
 echo "<link rel=\"stylesheet\" href=\"style.css\" type=\"text/css\">";
+echo "<link rel=\"SHORTCUT ICON\" href=\"http://www.studip.de/favicon.ico\">";
 echo "</head>\n<body bgcolor=\"#FFFFFF\">\n";
 
 if($cmd == "showmonth"){
