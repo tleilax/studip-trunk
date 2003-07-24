126da7588da8a97a5752ab4e3756f95d4140a843
Cornelis Kater <kater@zqs.uni-hannover.de>
2003-07-24 14:45:43 +0000

    Weitere Checks; präzisere Bereichschecks, bessere Meldungen

--- htdocs/lib/classes/StartupChecks.class.php
+++ htdocs/lib/classes/StartupChecks.class.php
@@ -39,40 +39,52 @@ require_once $ABSOLUTE_PATH_STUDIP.("config.inc.php");
 class StartupChecks {
 	var $registered_checks = array (
 		"institutes" => array("perm" => "root"),
-		"institutesRange" => array("perm" => "root"),
+		"institutesRange" => array("perm" => "admin"),
 		"myInstituteRange" => array("perm" => "dozent"),
+		"myAdminInstitute" => array("perm" => "admin"),
 		"dozent" => array("perm" => "admin"),
 		"institutesDozent" => array("perm" => "admin"),
-		"myInstitutesDozent" => array("perm" => "dozent")
+		"myInstitutesDozent" => array("perm" => "admin"),
+		"myInstitutes" => array("perm" => "dozent")
 	);
 	var $db;
+	var $db2;
 	
 	function StartupChecks() {
 		$this->registered_checks["institutes"]["msg"] = _("Sie ben&ouml;tigen mindestens eine Einrichtung, an der Veranstaltungen angeboten werden k&ouml;nnen. Legen Sie bitte zun&auml;chst eine Einrichtung an.");
 		$this->registered_checks["institutes"]["link"] = "admin_institut.php?i_view=new";
 		$this->registered_checks["institutes"]["link_name"] = _("neue Einrichtung anlegen");
 	
-		$this->registered_checks["institutesRange"]["msg"] = _("Sie m&uuml;ssen der Einrichtung, f&uuml;r die sie eine Veranstaltung anlegen wollen, mindestens einen Studienbereich zuordnen. Nutzen sie daf&uuml;r die Veranstaltunghierarchie, um Studienbereiche f&uuml;r die entsprechende Einrichtung anzulegen.");
+		$this->registered_checks["institutesRange"]["msg"] = _("Sie m&uuml;ssen in der Veranstaltungshierarchie mindestens einen Studienbereich unterhalb der Fakult&auml;t, zu der die Einrichtung geh&ouml;rt, f&uuml;r die sie eine Veranstaltung anlegen wollen, anlegen. Nutzen sie daf&uuml;r die Veranstaltunghierarchie, um Studienbereiche zu verwalten.");
 		$this->registered_checks["institutesRange"]["link"] = "admin_sem_tree.php";
-		$this->registered_checks["institutesRange"]["link_name"] = _("Studienbereiche zuordnen");
+		$this->registered_checks["institutesRange"]["link_name"] = _("Studienbereiche verwalten");
 
 		$this->registered_checks["myInstituteRange"]["msg"] = _("Um Veranstaltungen anlegen zu k&ouml;nnen, muss der Einrichtung, f&uuml;r die Sie eine Veranstaltung anlegen m&ouml;chten, mindestens ein Studienbereich zugeordnet werden. Bitte wenden Sie sich an einen der Administratoren des Systems.");
 		$this->registered_checks["myInstituteRange"]["link"] = "impressum.php?view=ansprechpartner";
 		$this->registered_checks["myInstituteRange"]["link_name"] = _("Kontakt zu den Administratoren");
 
-		$this->registered_checks["dozent"]["msg"] = _("Sie m&uuml;ssen der Einrichtung, f&uuml;r die sie eine Veranstaltung anlegen wollen, mindestens einen Dozenten zuordnen. Nutzen sie daf&uuml;r die globale Nutzerverwaltung, um einen neuen Nutzer mit dem Status Dozent anzulegen oder den Satus eines bestehenden Nutzers auf &raquo;dozent&laquo; zu setzen.");
+		$this->registered_checks["myAdminInstitute"]["msg"] = _("Um eine Veranstaltung anlegen zu k&ouml;nnen, muss Ihr Account mit einer Einrichtung verkn&uuml;pft werden.");
+		$this->registered_checks["myAdminInstitute"]["link"] = "impressum.php?view=ansprechpartner";
+		$this->registered_checks["myAdminInstitute"]["link_name"] = _("Kontakt zu den Administratoren");
+
+		$this->registered_checks["dozent"]["msg"] = _("Sie ben&ouml;tigen mindestens einen Dozentenaccount, um Veranstaltungen anlegen zu k&ouml;nnen. Nutzen sie die globale Nutzerverwaltung, um einen neuen Nutzer mit dem Status Dozent anzulegen oder den Satus eines bestehenden Nutzers auf &raquo;dozent&laquo; zu setzen.");
 		$this->registered_checks["dozent"]["link"] = "new_user_md5";
 		$this->registered_checks["dozent"]["link_name"] = _("Dozentenaccount anlegen oder anderen Account hochstufen");
 
 		$this->registered_checks["institutesDozent"]["msg"] = _("Um Veranstaltungen anlegen zu k&ouml;nnen, muss der Einrichtung, f&uuml;r die Sie eine Veranstaltung anlegen m&ouml;chten, mindestens ein Dozentenaccount zugeordnet werden. Nutzen Sie daf&uuml;r die Mitarbeiterverwaltung f&uuml;r Einrichtungen.");
 		$this->registered_checks["institutesDozent"]["link"] = "inst_admin.php?list=TRUE";
 		$this->registered_checks["institutesDozent"]["link_name"] = _("Dozent der Einrichtung zuordnen");
+
+		$this->registered_checks["myInstitutesDozent"]["msg"] = _("Um Veranstaltungen anlegen zu k&ouml;nnen, muss der Einrichtung, f&uuml;r die Sie eine Veranstaltung anlegen m&ouml;chten, mindestens ein Dozentenaccount zugeordnet werden. Nutzen Sie daf&uuml;r die Mitarbeiterverwaltung f&uuml;r Einrichtungen.");
+		$this->registered_checks["myInstitutesDozent"]["link"] = "inst_admin.php?list=TRUE";
+		$this->registered_checks["myInstitutesDozent"]["link_name"] = _("Dozent der Einrichtung zuordnen");
 	
-		$this->registered_checks["myInstitutesDozent"]["msg"] = _("Um Veranstaltungen anlegen zu k&ouml;nnen, muss ihr Account der Einrichtung, f&uuml;r die Sie eine Veranstaltung anlegen m&ouml;chten, zugeordnet werden. Bitte wenden Sie sich an einen der Administratoren des Systems.");
-		$this->registered_checks["myInstitutesDozent"]["link"] = "impressum.php?view=ansprechpartner";
-		$this->registered_checks["myInstitutesDozent"]["link_name"] = _("Kontakt zu den Administratoren");
+		$this->registered_checks["myInstitutes"]["msg"] = _("Um Veranstaltungen anlegen zu k&ouml;nnen, muss ihr Account der Einrichtung, f&uuml;r die Sie eine Veranstaltung anlegen m&ouml;chten, zugeordnet werden. Bitte wenden Sie sich an einen der Administratoren des Systems.");
+		$this->registered_checks["myInstitutes"]["link"] = "impressum.php?view=ansprechpartner";
+		$this->registered_checks["myInstitutes"]["link_name"] = _("Kontakt zu den Administratoren");
 
 		$this->db = new DB_Seminar;
+		$this->db2 = new DB_Seminar;
 	}
 	
 	function institutes() {
@@ -87,7 +99,30 @@ class StartupChecks {
 	}
 
 	function institutesRange() {
-		$query = "SELECT studip_object_id FROM sem_tree LEFT JOIN Institute ON (Institute.Institut_id = sem_tree.studip_object_id)";
+		global $user, $perm;
+	
+		if (!$perm->have_perm ("root")) {
+			$query = sprintf ("SELECT a.Institut_id, IF(a.Institut_id=fakultaets_id,1,0) AS is_fak,inst_perms FROM user_inst  a LEFT JOIN Institute USING (institut_id) WHERE (user_id = '%s' AND inst_perms = 'admin')", $user->id);
+	
+			$this->db->query($query);
+			while ($this->db->next_record()) {
+				$tmp_inst_ids[]=$this->db->f("Institut_id");
+				if ($this->db->f("is_fak")) {
+					$query2 = sprintf ("SELECT a.Institut_id, a.Name FROM Institute a WHERE fakultaets_id='%s' AND a.Institut_id!='%s' ", $this->db->f("Institut_id"), $this->db->f("Institut_id"));
+					$this->db2->query($query2);
+					while ($this->db2->next_record()) {
+						$tmp_inst_ids[]=$this->db2->f("Institut_id");
+					}
+				}
+			}
+	
+			$clause = implode("', '", $tmp_inst_ids);
+
+			$query = sprintf ("SELECT studip_object_id FROM user_inst LEFT JOIN Institute USING (Institut_id) LEFT JOIN sem_tree ON (Institute.fakultaets_id = sem_tree.studip_object_id) WHERE user_inst.Institut_id IN ('%s') AND studip_object_id IS NOT NULL ", $clause);
+		} else {
+			$query = "SELECT studip_object_id FROM sem_tree LEFT JOIN Institute ON (Institute.fakultaets_id = sem_tree.studip_object_id)";
+		}
+
 		$this->db->query ($query);
 		
 		if ($this->db->nf()) {
@@ -100,13 +135,30 @@ class StartupChecks {
 	function myInstituteRange() {
 		global $user;
 
-		$query = sprintf ("SELECT studip_object_id FROM user_inst LEFT JOIN Institute USING (Institut_id) LEFT JOIN sem_tree ON (Institute.Institut_id = sem_tree.studip_object_id) WHERE user_id = '%s' ", $user->id);
+		$query = sprintf ("SELECT studip_object_id FROM user_inst LEFT JOIN Institute USING (Institut_id) LEFT JOIN sem_tree ON (Institute.fakultaets_id = sem_tree.studip_object_id) WHERE user_id = '%s' ", $user->id);
 		$this->db->query ($query);
 		
 		if ($this->db->nf()) {
 			return FALSE;
 		} else {
 			return TRUE;
+		}
+	}
+
+	function myAdminInstitute() {
+		global $user, $perm;
+		
+		if ($perm->have_perm ("root")) {
+			return FALSE;
+		} else {
+			$query = sprintf ("SELECT user_id FROM user_inst WHERE user_id = '%s' AND inst_perms = 'admin' ", $user->id);
+			$this->db->query ($query);
+		
+			if ($this->db->nf()) {
+				return FALSE;
+			} else {
+				return TRUE;
+			}
 		}
 	}
 
@@ -133,6 +185,41 @@ class StartupChecks {
 	}
 
 	function myInstitutesDozent() {
+		global $user, $perm;
+		
+
+		if (!$perm->have_perm ("root")) {
+			$query = sprintf ("SELECT a.Institut_id, IF(a.Institut_id=fakultaets_id,1,0) AS is_fak,inst_perms FROM user_inst  a LEFT JOIN Institute USING (institut_id) WHERE (user_id = '%s' AND inst_perms = 'admin')", $user->id);
+	
+			$this->db->query($query);
+			while ($this->db->next_record()) {
+				$tmp_inst_ids[]=$this->db->f("Institut_id");
+				if ($this->db->f("is_fak")) {
+					$query2 = sprintf ("SELECT a.Institut_id, a.Name FROM Institute a WHERE fakultaets_id='%s' AND a.Institut_id!='%s' ", $this->db->f("Institut_id"), $this->db->f("Institut_id"));
+					$this->db2->query($query2);
+					while ($this->db2->next_record()) {
+						$tmp_inst_ids[]=$this->db2->f("Institut_id");
+					}
+				}
+			}
+	
+			$clause = implode("', '", $tmp_inst_ids);
+			
+			$query = sprintf ("SELECT user_id FROM user_inst WHERE inst_perms = 'dozent' AND Institut_id IN ('%s')", $clause);
+		} else {
+			$query = "SELECT user_id FROM user_inst WHERE inst_perms = 'dozent'";
+		}
+		
+		$this->db->query ($query);
+
+		if ($this->db->nf()) {
+			return FALSE;
+		} else {
+			return TRUE;
+		}
+	}
+
+	function myInstitutes() {
 		global $user;
 		
 		$query = sprintf ("SELECT user_id FROM user_inst WHERE inst_perms = 'dozent' AND user_id = '%s'", $user->id);
