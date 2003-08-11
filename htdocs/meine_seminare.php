060f6fc0833abd4a1130b9f7d1a30a2ab9bf33cd
Tobias Thelen <tobias.thelen@uni-osnabrueck.de>
2003-08-11 09:31:40 +0000

    bugfix: direkter Link auf neue Wiki-Seiten, Text geaendert

--- htdocs/meine_seminare.php
+++ htdocs/meine_seminare.php
@@ -137,7 +137,7 @@ function print_seminar_content($semid,$my_sem_values) {
 
   // Wikiseiten
   if ($my_sem_values["neuewikiseiten"])
-		echo "&nbsp; <a href=\"seminar_main.php?auswahl=$semid&redirect_to=wiki.php\"><img src='pictures/icon-wiki2.gif' border=0 ".tooltip(sprintf(_("%s WikiSeiten, %s neue"), $my_sem_values["wikiseiten"], $my_sem_values["neuewikiseiten"]))."></a>";
+		echo "&nbsp; <a href=\"seminar_main.php?auswahl=$semid&redirect_to=wiki.php&view=listnew\"><img src='pictures/icon-wiki2.gif' border=0 ".tooltip(sprintf(_("%s WikiSeiten, %s Änderungen"), $my_sem_values["wikiseiten"], $my_sem_values["neuewikiseiten"]))."></a>";
   elseif ($my_sem_values["wikiseiten"])
 		echo "&nbsp; <a href=\"seminar_main.php?auswahl=$semid&redirect_to=wiki.php\"><img src='pictures/icon-wiki.gif' border=0 ".tooltip(sprintf(_("%s WikiSeiten"), $my_sem_values["wikiseiten"]))."></a>";
   else
