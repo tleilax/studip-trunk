da6b6961da2ccfc22418608e57bb74a453498d72
arne <arne>
2003-08-22 17:01:17 +0000

    Bei Fakultäten auch untergeordnete Institute exortieren

--- htdocs/export/export_studipdata_func.inc.php
+++ htdocs/export/export_studipdata_func.inc.php
@@ -58,14 +58,13 @@ function export_range($range_id)
 	}
 
 //	Ist die Range-ID eine Fakultaets-ID? Dann auch untergeordnete Institute exportieren!
-	$db->query('SELECT * FROM Institute WHERE fakultaets_id = "' . $range_id . '" AND  Institut_id != "' . $range_id . '" ');
-	if (($db->next_record()) And ($db->f("Name") != "")) 
+	$db->query('SELECT * FROM Institute WHERE fakultaets_id = "' . $range_id . '" ');
+	if (($db->next_record()) And ($db->f("Name") != "") And ($db->f("Institut_id") != $range_id)) 
 	{
 		output_data ( xml_header(), $o_mode);
 		export_inst( $range_id );
 		
 	}
-	
 
 //    Ist die Range-ID eine Seminar-ID?
 	$db->query('SELECT * FROM seminare WHERE Seminar_id = "' . $range_id . '"');
