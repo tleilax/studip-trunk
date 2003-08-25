0f89ca6ec546e211546493926f66c3d3c274c898
rstockm <rstockm>
2003-08-25 14:12:32 +0000

    kleine Anpassung

--- htdocs/object.inc.php
+++ htdocs/object.inc.php
@@ -62,7 +62,7 @@ function object_print_rate ($object_id) {
 	$db=new DB_Seminar;
 	$db->query("SELECT avg(rate) as mittelwert FROM object_rate WHERE object_id = '$object_id'");
 	if ($db->next_record())
-		$tmp = round($db->f("mittelwert"),1);
+		$tmp = round($db->f("mittelwert"),2);
 		if ($tmp == 0)
 			$tmp = "?";
 	return $tmp;
