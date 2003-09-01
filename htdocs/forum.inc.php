f033142d2ce60f29fc3ac1dee2f0e041cc5bd090
rstockm <rstockm>
2003-09-01 17:04:19 +0000

    neuer header

--- htdocs/forum.inc.php
+++ htdocs/forum.inc.php
@@ -1,25 +1,35 @@
 <?
-/*
-forum.php - Anzeige und Verwaltung des Ordnersystems
-Copyright (C) 2002 Ralf Stockmann <rstockm@gwdg.de>, Stefan Suchi <suchi@gmx.de>
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+/**
+* helper functions for handling the board 
+* 
+* helper functions for handling boards
+*
+* @author			Ralf Stockmann <rstockm@uni-goettingen.de>
+* @version			$Id$
+* @access			public
+* @package			studip_core
+* @modulegroup			library
+* @module			forum.inc.php
 */
+// +---------------------------------------------------------------------------+
+// This file is part of Stud.IP
+// forum.inc.php
+// Copyright (c) 2003 Ralf Stockmann <rstockm@gwdg.de>
+// +---------------------------------------------------------------------------+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or any later version.
+// +---------------------------------------------------------------------------+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// +---------------------------------------------------------------------------+
 
-
-//////////////////////////////////////////////////////////////////////////
 
 /**
 * Builds the edit-Area for created postings or postings being re-editet
