ea22ab4d8170a4147b39bac9d8dc12e29a69b191
Stefan Suchi <suchi@data-quest.de>
2003-09-10 12:37:30 +0000

    Content-Type gesetzt

--- htdocs/wap/wap_adm.inc.php
+++ htdocs/wap/wap_adm.inc.php
@@ -184,6 +184,7 @@
 
 		$session_expired = FALSE;
 
+		header("Content-Type: text/vnd.wap.wml;charset=iso-8859-1");
 		echo '<?xml version="1.0"?>';
 		echo '<!DOCTYPE wml PUBLIC "-//WAPFORUM//DTD WML 1.1//EN" "http://www.wapforum.org/DTD/wml_1.1.xml">';
 		echo "<wml>";
