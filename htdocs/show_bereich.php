468990cae30ad44bf7caca8abe8479bc200e9c28
Cornelis Kater <kater@zqs.uni-hannover.de>
2003-09-17 08:16:53 +0000

    Bugfix: Auch für nobodys zulässig (Konsitenz zur Veranstaltungs-Timetable)

--- htdocs/show_bereich.php
+++ htdocs/show_bereich.php
@@ -19,7 +19,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Auth", "perm" => "Seminar_Perm", "user" => "Seminar_User"));
-$perm->check("user");
 
 include ("$ABSOLUTE_PATH_STUDIP/seminar_open.php"); // initialise Stud.IP-Session
 
