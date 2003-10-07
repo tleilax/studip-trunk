3b856c2218ba02469698823c163c391b871a9288
André Noack <noackorama@gmail.com>
2003-10-07 16:23:29 +0000

    login / registration abbrechen umgebaut

--- htdocs/index.php
+++ htdocs/index.php
@@ -18,9 +18,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-if (!isset($order)) $order = "";
-
-page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Default_Auth", "perm" => "Seminar_Perm", "user" => "Seminar_User"), $order);
+page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Default_Auth", "perm" => "Seminar_Perm", "user" => "Seminar_User"));
 
 $auth->login_if($again && ($auth->auth["uid"] == "nobody"));
 
