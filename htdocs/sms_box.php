70e873d7948f2fdd5aba1da556e35a0a97c358e2
nkwindisch <nkwindisch>
2003-11-03 20:06:46 +0000

    messaging - alle aufklappen als option - zurück zur letzten ... in der infobox

--- htdocs/sms_box.php
+++ htdocs/sms_box.php
@@ -19,8 +19,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-// start functions
-
+// functions
 function count_snd_messages_from_user($user_id) {
 	global $db;
 	$x = "0";
@@ -36,14 +35,15 @@ function count_snd_messages_from_user($user_id) {
 	return $x;
 }
 
-function count_rec_messages_from_user($user_id) {
+function count_rec_messages_from_user($user_id, $where="") {
 	global $db;
 	$x = "0";
 	$query = "SELECT DISTINCT message_id 
 		FROM message_user 
 			WHERE snd_rec = 'rec'
 			AND user_id = '".$user_id."' 
-			AND deleted = '0'";
+			AND deleted = '0'
+			".$where."";
 	$db->query($query);
 	while ($db->next_record()) {
 		$x = $x+1;
@@ -58,24 +58,27 @@ function set_read($message_id) {
 	$db7->query($query);
 }
 
+function show_icon($sms_show, $value) {
+	if ($sms_show == $value) {
+		$x = "forumgrau.gif";
+	} else {
+		$x = "blank.gif";
+	}
+	return $x;
+}
+
 // print_snd_message
 
 function print_snd_message($mkdate, $message_id, $message, $sms_data_open, $sms_data_view) {
-	global $n, $LastLogin, $my_messaging_settings, $cmd, $db7, $PHP_SELF, $msging;	
+	global $n, $LastLogin, $my_messaging_settings, $cmd, $db7, $PHP_SELF, $msging, $cmd_show;	
 	//Kopfzeile erstellen
 	$icon = "&nbsp;<img src=\"pictures/cont_nachricht.gif\">";
-	if ($cmd == "select_all") {
-		$checked = "checked";
-	} else {
-		$checked = "";
-	}
 	$x = "0";
 	$query = "SELECT * FROM message_user WHERE message_id = '".$message_id."' AND snd_rec = 'rec'";
 	$db7->query($query);
 	while ($db7->next_record()) {
 		$x = $x+1;
 	}
-
 	$zusatz = "<font size=-1>";
 	if ($x == "1") {
 		$query = "SELECT message_user.* 
@@ -87,18 +90,19 @@ function print_snd_message($mkdate, $message_id, $message, $sms_data_open, $sms_
 		while ($db7->next_record()) {
 			$rec_userid = $db7->f("user_id");
 		}
-		$zusatz .= sprintf(_("gesendet an %s am %s %s"), "</font><a href=\"about.php?username=".get_username($rec_userid)."\"><font size=-1 color=\"#333399\">".get_fullname($rec_userid)."</font></a><font size=-1>", date("d.m.Y, H:i",$mkdate), "<input type=\"checkbox\" name=\"sel_delsms[]\" value=\"".$message_id."\" ".$checked.">");
+		$zusatz .= sprintf(_("gesendet an %s am %s %s"), "</font><a href=\"about.php?username=".get_username($rec_userid)."\"><font size=-1 color=\"#333399\">".get_fullname($rec_userid)."</font></a><font size=-1>", date("d.m.Y, H:i",$mkdate), "<input type=\"checkbox\" name=\"sel_delsms[]\" value=\"".$message_id."\" ".CheckChecked($cmd, "select_all").">");
 	} else if ($x >= "2") {
-		$zusatz .= sprintf(_("gesendet an %s Empf&auml;nger am %s %s"), $x, date("d.m.Y, H:i",$mkdate), "<input type=\"checkbox\" name=\"sel_delsms[]\" value=\"".$message_id."\" ".$checked.">");
+		$zusatz .= sprintf(_("gesendet an %s Empf&auml;nger am %s %s"), $x, date("d.m.Y, H:i",$mkdate), "<input type=\"checkbox\" name=\"sel_delsms[]\" value=\"".$message_id."\" ".CheckChecked($cmd, "select_all").">");
 	}
-	$zusatz .= "</font>";	
-	
+	$zusatz .= "</font>";		
+
 	if (strpos($message,$msging->sig_string)) {
 		$titel = mila(kill_format(substr($message, 0, strpos($message,$msging->sig_string))));
 	} else {
 		$titel = mila(kill_format($message));
 	}
 	
+
 	$content = quotes_decode(formatReady($message));
 	if ($x >= "2") {
 		$content .= "<br><br>"._("gesendet an:")."<br>";
@@ -112,23 +116,23 @@ function print_snd_message($mkdate, $message_id, $message, $sms_data_open, $sms_
 			$content .= "<a href=\"about.php?username=".get_username($db7->f("user_id"))."\"><font size=-1 color=\"#333399\">".get_fullname($db7->f("user_id"))."</font></a> / ";
 		}
 	}
-
 	$edit = "&nbsp;<a href=\"".$PHP_SELF."?cmd=delete_selected&sel_delsms[1]=".$message_id."\">".makeButton("loeschen", "img")."</a><br><br>";
 	
 	if ($sms_data_open == $message_id) {
 		$open = "open";
 		$link = $PHP_SELF."?mclose=TRUE";
+	} else if ($cmd_show == "openall" || $my_messaging_settings["openall"] == "1") {
+		$open = "open";
+		$link = $PHP_SELF."?mopen=".$message_id."#".$message_id;
 	} else {
 		$open = "close";
 		$link = $PHP_SELF."?mopen=".$message_id."#".$message_id;
 	}
-	
 	if (strlen($titel) >= "50") {
 		$titel = "<a name=".$message_id."><a href=\"$link\" class=\"tree\" >".substr($titel, 0, 30)." ...</a></a>";
 	} else {
 		$titel = "<a name=".$message_id."><a href=\"$link\" class=\"tree\" >".$titel."</a></a>";
-	}
-
+	}	
 	// ausgabe der ueberschrift		
 	echo "\n<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"99%\" align=\"center\" class=\"steel1\"><tr>";
 	printhead(0, 0, $link, $open, FALSE, $icon, $titel, $zusatz, $mkdate);
@@ -145,28 +149,23 @@ function print_snd_message($mkdate, $message_id, $message, $sms_data_open, $sms_
 // print_rec_message
 
 function print_rec_message($user_id_snd, $mkdate, $message_id, $message, $fullname, $sms_data_open, $read) {
-	global $n, $LastLogin, $my_messaging_settings, $cmd, $PHP_SELF, $msging;	
-
+	global $n, $LastLogin, $my_messaging_settings, $cmd, $PHP_SELF, $msging, $cmd_show;	
 	$uname_snd = get_username($user_id_snd);
-
-	if ($cmd == "select_all") {
-		$checked = "checked";
-	} else {
-		$checked = "";
-	}
-
 	// open if unread
-	if ($read != "1") {
+	if ($read != "1" && $my_messaging_settings["opennew"] == "1") {
 		$open = "open";
 		$link = $PHP_SELF."?mclose=TRUE";
 	} else if ($sms_data_open == $message_id) {
 		$open = "open";
 		$link = $PHP_SELF."?mclose=TRUE";
+	} else if ($cmd_show == "openall" || $my_messaging_settings["openall"] == "1") {
+		$open = "open";
+		$link = $PHP_SELF."?mopen=".$message_id."#".$message_id;
 	} else {
 		$open = "close";
 		$link = $PHP_SELF."?mopen=".$message_id."#".$message_id;
 	}
-
+	// unread=new ... is message new? if new and opened=set readed
 	if ($read == "1") {
 		$red = FALSE;
 		$icon = "&nbsp;<img src=\"pictures/cont_nachricht.gif\">";
@@ -180,7 +179,7 @@ function print_rec_message($user_id_snd, $mkdate, $message_id, $message, $fullna
 
 	if ($user_id_snd == "____%system%____") {
 		$zusatz = "<font size=-1>";
-		$zusatz .= sprintf(_("automatische Systemnachricht, gesendet am %s %s"), date("d.m.Y, H:i", $mkdate), "<input type=\"checkbox\" name=\"sel_delsms[]\" value=\"".$message_id."\" ".$checked.">");
+		$zusatz .= sprintf(_("automatische Systemnachricht, gesendet am %s %s"), date("d.m.Y, H:i", $mkdate), "<input type=\"checkbox\" name=\"sel_delsms[]\" value=\"".$message_id."\" ".CheckChecked($cmd, "select_all").">");
 		$zusatz .= "</font>";
 	} else {
 		if ($cmd == "select_all") {
@@ -193,12 +192,17 @@ function print_rec_message($user_id_snd, $mkdate, $message_id, $message, $fullna
 		$zusatz .= "</font>";			
 	}
 	
-	if (strpos($message, $msging->sig_string)) {
-		$titel = mila(kill_format(substr($message, 0, strpos($message, $msging->sig_string))));
+	if (strpos($message,$msging->sig_string)) {
+		$titel = mila(kill_format(substr($message, 0, strpos($message,$msging->sig_string))));
 	} else {
 		$titel = mila(kill_format($message));
 	}
-	
+	if (strlen($titel) >= "50") {
+		$titel = "<a name=".$message_id."><a href=\"$link\" class=\"tree\" >".substr($titel, 0, 30)." ...</a></a>";
+	} else {
+		$titel = "<a name=".$message_id."><a href=\"$link\" class=\"tree\" >".$titel."</a></a>";
+	}	
+
 	$content = quotes_decode(formatReady($message));
 
 	$edit = "";
@@ -207,12 +211,6 @@ function print_rec_message($user_id_snd, $mkdate, $message_id, $message, $fullna
 		$edit .= "&nbsp;<a href=\"sms_send.php?cmd=write&quote=".$message_id."&rec_uname=".$uname_snd."\">".makeButton("zitieren", "img")."</a>";
 	}
 	$edit.= "&nbsp;<a href=\"".$PHP_SELF."?cmd=delete_selected&sel_delsms[1]=".$message_id."\">".makeButton("loeschen", "img")."</a><br><br>";
-
-	if (strlen($titel) >= "50") {
-		$titel = "<a name=".$message_id."><a href=\"$link\" class=\"tree\" >".substr($titel, 0, 30)." ...</a></a>";
-	} else {
-		$titel = "<a name=".$message_id."><a href=\"$link\" class=\"tree\" >".$titel."</a></a>";
-	}
 
 	// ausgabe der ueberschrift		
 	echo "\n<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"99%\" align=\"center\" class=\"steel1\"><tr>";
@@ -225,15 +223,6 @@ function print_rec_message($user_id_snd, $mkdate, $message_id, $message, $fullna
 		echo "</tr></table>	";		
 	}
 	return $n++;
-}
-
-function show_icon($sms_show, $value) {
-	if ($sms_show == $value) {
-		$x = "forumgrau.gif";
-	} else {
-		$x = "blank.gif";
-	}
-	return $x;
 }
 
 // end of functions
@@ -264,6 +253,10 @@ $msging = new messaging;
 $db = new DB_Seminar;
 $db6 = new DB_Seminar;
 $db7 = new DB_Seminar;
+
+if (empty($my_messaging_settings["timefilter"])) {
+	$my_messaging_settings["timefilter"] ="all";
+}
 
 // delete selected messages
 if ($cmd == "delete_selected") {
@@ -299,10 +292,17 @@ if ($sms_inout) {
 	$sms_data["view"] = "in";
 }
 
+if (empty($my_messaging_settings["openall"])) {
+	$my_messaging_settings["openall"] = "0";
+}
+
 if ($sms_time) {
 	$sms_data["time"] = $sms_time;
-} else if ($sms_data["time"] == "") {
+} else if ($sms_data["time"] == "" && empty($my_messaging_settings["timefilter"])) {
 	$sms_data["time"] = "all";
+	$my_messaging_settings["timefilter"] = "all";
+} else if ($sms_data["time"] == "" && !empty($my_messaging_settings["timefilter"])) {
+	$sms_data["time"] = $my_messaging_settings["timefilter"];
 }
 
 if ($sms_data['view'] == "in") {
@@ -382,11 +382,6 @@ if (($change_view) || ($delete_user) || ($view=="Messaging")) {
 	}
 	print("<table cellpadding=\"5\" border=\"0\" width=\"100%\"><tr><td colspan=\"2\" valign=\"top\">");
 	print("<font size=-1>".$info_text_002.$info_text_003);
-	if ($SessSemName[0] && $SessSemName["class"] == "inst") {
-		echo "<br><br><a href=\"institut_main.php\">"._("Zur&uuml;ck zur ausgew&auml;hlten Einrichtung")."</a>";
-	} elseif ($SessSemName[0]) {
-		echo "<br><br><a href=\"seminar_main.php\">"._("Zur&uuml;ck zur ausgew&auml;hlten Veranstaltung")."</a>";
-	}
 	print("</font></td><td class=\"blank\" align=\"right\" valign=\"bottom\">");
 	print("<a href=\"".$PHP_SELF."?cmd=select_all\">".makeButton("alleauswaehlen", "img")."</a><br><br>");
 	print("&nbsp;&nbsp;");
@@ -401,7 +396,15 @@ if (($change_view) || ($delete_user) || ($view=="Messaging")) {
 	if ($sms_data['view'] == "in") {
 		$info_text_001 = "<img src=\"pictures/nachricht1.gif\" border=\"0\" align=\"texttop\"><b>&nbsp;"._("empfangene systeminterne Nachrichten anzeigen")."</b>";
 		$info_text_002 = _("Alle systeminternen Nachrichten, die an Sie gesendet wurden werden hier angezeigt.<br>");
-		$query .= "SELECT message.*, message_user.*, ".$_fullname_sql['full']." AS fullname FROM message_user LEFT JOIN message USING (message_id) LEFT JOIN auth_user_md5 ON (autor_id=auth_user_md5.user_id) LEFT JOIN user_info USING(user_id) WHERE message_user.user_id = '".$user->id."' AND message_user.snd_rec = 'rec' AND message_user.deleted = '0' ".$query_time;		
+		$query .= "
+		SELECT message.*, message_user.*, ".$_fullname_sql['full']." AS fullname 
+		FROM message_user 
+			LEFT JOIN message USING (message_id) 
+			LEFT JOIN auth_user_md5 ON (autor_id=auth_user_md5.user_id) 
+			LEFT JOIN user_info USING(user_id) 
+		WHERE message_user.user_id = '".$user->id."' 
+			AND message_user.snd_rec = 'rec' 
+			AND message_user.deleted = '0' ".$query_time;		
 		$db->query($query);		
 		while ($db->next_record()) {
 			print_rec_message($db->f("autor_id"), $db->f("mkdate"), $db->f("message_id"), $db->f("message"), get_fullname($db->f("autor_id")), $sms_data["open"], $db->f("readed"));	
@@ -409,13 +412,18 @@ if (($change_view) || ($delete_user) || ($view=="Messaging")) {
 	} else if ($sms_data['view'] == "out") {
 		$info_text_001 = "<img src=\"pictures/nachricht1.gif\" border=\"0\" align=\"texttop\"><b>&nbsp;"._("gesendete systeminterne Nachrichten anzeigen")."</b>";
 		$info_text_002 = _("Alle systeminternen Nachrichten, die Sie gesendet haben werden hier angezeigt.<br>");
-		$query .= "SELECT message.*, message_user.* FROM message_user LEFT JOIN message USING (message_id) WHERE message_user.user_id = '".$user->id."' AND message_user.snd_rec = 'snd' AND message_user.deleted = '0' ".$query_time;
+		$query .= "
+		SELECT message.*, message_user.* 
+		FROM message_user 
+			LEFT JOIN message USING (message_id) 
+		WHERE message_user.user_id = '".$user->id."' 
+			AND message_user.snd_rec = 'snd' 
+			AND message_user.deleted = '0' ".$query_time;
 		$db->query($query);
 		while ($db->next_record()) {
 			print_snd_message($db->f("mkdate"), $db->f("message_id"), $db->f("message"), $sms_data["open"], $sms_data["view"]);		
 		}	
 	}
-
 	echo "</form>"; // close form "delete selected messages"
 
 	// wenn keine nachrichten zum anzeigen
@@ -437,13 +445,22 @@ if (($change_view) || ($delete_user) || ($view=="Messaging")) {
 	$time_by_links .= "&nbsp;<a href=\"".$PHP_SELF."?sms_time=30d\"><img src=\"pictures/".show_icon($sms_data["time"], "30d")."\" width=\"10\" height=\"20\" border=\"0\">&nbsp;"._("letzte 30 Tage")."</a><br>";
 	$time_by_links .= "&nbsp;<a href=\"".$PHP_SELF."?sms_time=older\"><img src=\"pictures/".show_icon($sms_data["time"], "older")."\" width=\"10\" height=\"20\" border=\"0\">&nbsp;"._("&auml;lter als 30 Tage")."</a>";
 
+	if ($SessSemName[0] && $SessSemName["class"] == "inst") {
+		$x = array("kategorie" => _("Zur&uuml;ck:"),"eintrag" => array(array("icon" => "pictures/ausruf_small.gif", "text" => "<a href=\"institut_main.php\">"._("Zur&uuml;ck zur ausgew&auml;hlten Einrichtung")."</a>")));
+	} else if ($SessSemName[0]) {
+		$x = array("kategorie" => _("Zur&uuml;ck:"),"eintrag" => array(array("icon" => "pictures/ausruf_small.gif", "text" => "<a href=\"seminar_main.php\">"._("Zur&uuml;ck zur ausgew&auml;hlten Veranstaltung")."</a>")));
+	}
+
 	$infobox = array(
+		$x,
 		array("kategorie" => _("Information:"),"eintrag" => array(
 			array("icon" => "pictures/ausruf_small.gif", "text" => sprintf(_("Sie haben %s empfangene und %s gesendete Nachrichten."), count_rec_messages_from_user($user->id), count_snd_messages_from_user($user->id)))
 		)),
-
 		array("kategorie" => _("Anzeigezeitraum ausw&auml;hlen:"),"eintrag" => array(
 			array("icon" => "pictures/suchen.gif", "text" => $time_by_links)
+		)),
+		array("kategorie" => _("Weitere M&ouml;glichkeiten:"),"eintrag" => array(
+			array("icon" => "pictures/blank", "text" => sprintf("<a href=\"%s?cmd_show=openall\">"._("Alle Nachrichten aufklappen.")."</a>", $PHP_SELF))
 		))
 	);
 	print_infobox($infobox,"pictures/sms3.jpg");
