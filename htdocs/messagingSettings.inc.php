c6bf7928f68a433d5771f8a3bc5d69c36685478a
nkwindisch <nkwindisch>
2003-11-03 20:04:04 +0000

    messaging - erweiterte optionen zum konfigurieren des messaging -- zeit auswahl -- signatur senden -- immer aufklappen -- immer neue aufklappen

--- htdocs/messagingSettings.inc.php
+++ htdocs/messagingSettings.inc.php
@@ -11,18 +11,40 @@ $msging=new messaging;
 
 //vorgenommene Anpassungen der Ansicht in Uservariablen schreiben
 if ($messaging_cmd=="change_view_insert") {
-
-	$my_messaging_settings["changed"]=TRUE;
-	$my_messaging_settings["show_only_buddys"]=$show_only_buddys;
-	$my_messaging_settings["delete_messages_after_logout"]=$delete_messages_after_logout;
-	$my_messaging_settings["start_messenger_at_startup"]=$start_messenger_at_startup;
-	$my_messaging_settings["active_time"]=$active_time;
-	$my_messaging_settings["sms_sig"]=$sms_sig;
-	$my_messaging_settings["changed"]="TRUE";
+	$my_messaging_settings["changed"] = TRUE;
+	$my_messaging_settings["show_only_buddys"] = $show_only_buddys;
+	$my_messaging_settings["delete_messages_after_logout"] = $delete_messages_after_logout;
+	$my_messaging_settings["start_messenger_at_startup"] = $start_messenger_at_startup;
+	$my_messaging_settings["active_time"] = $active_time;
+	$my_messaging_settings["sms_sig"] = $sms_sig;
+	$my_messaging_settings["timefilter"] = $timefilter;
+	$my_messaging_settings["openall"] = $openall;
+	$my_messaging_settings["opennew"] = $opennew;
+	$my_messaging_settings["hoover"] = $hoover;
+	$my_messaging_settings["addsignature"] = $addsignature;
+	$my_messaging_settings["changed"] = "TRUE";
+} else {
+	if (empty($my_messaging_settings["hoover"])) {
+		$my_messaging_settings["hoover"] ="0";
 	}
+	if (empty($my_messaging_settings["openall"])) {
+		$my_messaging_settings["openall"] ="0";
+	}
+	if (empty($my_messaging_settings["timefilter"])) {
+		$my_messaging_settings["timefilter"] ="all";
+	}
+	if (empty($my_messaging_settings["addsignature"])) {
+		$my_messaging_settings["addsignature"] ="0";
+	}
+	if (empty($my_messaging_settings["opennew"])) {
+		$my_messaging_settings["opennew"] ="1";
+	}
+}
 
 if ($do_add_user_x)
 	$msging->add_buddy ($add_user);
+
+
 
 //Anpassen der Ansicht
 function change_messaging_view() {
@@ -50,6 +72,7 @@ function change_messaging_view() {
 		<td class="blank" colspan=2>
 			<form method="POST" action="<? echo $PHP_SELF ?>?messaging_cmd=change_view_insert">
 			<table width ="99%" align="center" cellspacing=0 cellpadding=2 border=0>
+				
 				<tr <? $cssSw->switchClass() ?>>
 					<td class="<? echo $cssSw->getClass() ?>" colspan=2>
 					&nbsp; &nbsp; <b><?=_("Systeminterne Kurznachrichten")?></b>
@@ -61,29 +84,99 @@ function change_messaging_view() {
 					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 					<?=_("automatisches L&ouml;schen:")?>
 					</td>
-					
 					<td class="<? echo $cssSw->getClass() ?>" width="70%">&nbsp; 
 					<input type="CHECKBOX" 
 					<? if ($my_messaging_settings["delete_messages_after_logout"]) echo " checked"; ?>
 					 name="delete_messages_after_logout">
 					&nbsp;<font size=-1> <?=_("Alle Nachrichten automatisch nach dem Logout l&ouml;schen")?></font>
 					</td>
-					
+				</tr>
+<!-- 
+				<tr <? #$cssSw->switchClass() ?>>
+					<td class="<? #echo $cssSw->getClass() ?>" width="30%">
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<?=_("Nachrichten hoovern")?>
+					</td>
+					<td class="<? #echo $cssSw->getClass() ?>" width="70%">&nbsp;<font size=-1> 
+					<input type="radio" name="hoover" value="1" <? #echo CheckChecked("1", $my_messaging_settings["hoover"]) ?>>ja
+					<input type="radio" name="hoover" value="0" <? #echo CheckChecked("0", $my_messaging_settings["hoover"]) ?>>nein</font>
+					</td>
+				</tr>
+ -->				
+				<tr <? $cssSw->switchClass() ?>>
+					<td class="<? echo $cssSw->getClass() ?>" width="30%">
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<?=_("Neue immer aufgeklappen")?>
+					</td>
+					<td class="<? echo $cssSw->getClass() ?>" width="70%">&nbsp;<font size=-1> 
+					<input type="radio" name="opennew" value="1" <? echo CheckChecked("1", $my_messaging_settings["opennew"]) ?>>ja
+					<input type="radio" name="opennew" value="0" <? echo CheckChecked("0", $my_messaging_settings["opennew"]) ?>>nein</font>
+					</td>
+				</tr>
+				
+				<tr <? $cssSw->switchClass() ?>>
+					<td class="<? echo $cssSw->getClass() ?>" width="30%">
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<?=_("Alle immer aufgeklappt")?>
+					</td>
+					<td class="<? echo $cssSw->getClass() ?>" width="70%">&nbsp;<font size=-1> 
+					<input type="radio" name="openall" value="1" <? echo CheckChecked("1", $my_messaging_settings["openall"]) ?>>ja
+					<input type="radio" name="openall" value="0" <? echo CheckChecked("0", $my_messaging_settings["openall"]) ?>>nein</font>					
+					</td>
+				</tr>
+<!-- 
+				<tr <? #$cssSw->switchClass() ?>>
+					<td class="<? #echo $cssSw->getClass() ?>" width="30%">
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<?=_("Vorschau")?>
+					</td>
+					<td class="<? #echo $cssSw->getClass() ?>" width="70%">&nbsp;<font size=-1> 
+					<input type="radio" name="preview" value="1" <? #echo CheckChecked("1", $my_messaging_settings["preview"]) ?>>ja
+					<input type="radio" name="preview" value="0" <? #echo CheckChecked("0", $my_messaging_settings["preview"]) ?>>nein
+					&nbsp;<?=_("Zeige Vorschau bei Schreiben von Nachrichten")?></font>
+					</td>
+				</tr>
+ -->
+				<tr <? $cssSw->switchClass() ?>>
+					<td class="<? echo $cssSw->getClass() ?>" width="30%">
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<?=_("Zeitfilter der Anzeige")?>
+					</td>
+					<td class="<? echo $cssSw->getClass() ?>" width="70%">&nbsp; 
+					<select name="timefilter">
+					<?  
+					printf("<option value=\"%s\" %s>%s</option>", "new", CheckSelected($my_messaging_settings["timefilter"], "new"), _("neue Nachrichten"));
+					printf("<option value=\"%s\" %s>%s</option>", "all", CheckSelected($my_messaging_settings["timefilter"], "all"), _("alle Nachrichten"));
+					printf("<option value=\"%s\" %s>%s</option>", "24h", CheckSelected($my_messaging_settings["timefilter"], "24h"), _("letzte 24 Stunden"));
+					printf("<option value=\"%s\" %s>%s</option>", "7d", CheckSelected($my_messaging_settings["timefilter"], "7d"), _("letzte 7 Tage"));
+					printf("<option value=\"%s\" %s>%s</option>", "30d", CheckSelected($my_messaging_settings["timefilter"], "30d"), _("letzte 30 Tage"));
+					printf("<option value=\"%s\" %s>%s</option>", "older", CheckSelected($my_messaging_settings["timefilter"], "older"), _("&auml;lter als 30 Tage"));
+					?>
+					</select>
+					&nbsp;<font size=-1> <?=_("Voreingestellter Anzeige-Filter der anzeigten Nachrichten.")?></font>
+					</td>
 				</tr>
 
 				<tr <? $cssSw->switchClass() ?>>
 					<td class="<? echo $cssSw->getClass() ?>">
 					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 					<?=_("Signatur:")?>
-					<td class="<? echo $cssSw->getClass() ?>">&nbsp; 
-					<textarea name="sms_sig" rows=3 cols=40><? echo htmlready($my_messaging_settings["sms_sig"]); ?></textarea>
+					<td class="<? echo $cssSw->getClass() ?>">&nbsp;<font size=-1> 
+					<input type="radio" name="addsignature" value="1" <? echo CheckChecked("1", $my_messaging_settings["addsignature"]) ?>>ja
+					<input type="radio" name="addsignature" value="0" <? echo CheckChecked("0", $my_messaging_settings["addsignature"]) ?>>nein
+					&nbsp;&nbsp;<?=_("gesendeten Nachrichten eine Signatur anhängen") ?>
+					</font>
+					<br>&nbsp;
+					<textarea name="sms_sig" rows=5 cols=40><? echo htmlready($my_messaging_settings["sms_sig"]); ?></textarea>
 					</td>
 				</tr>
+
 				<tr <? $cssSw->switchClass() ?>>
 					<td class="<? echo $cssSw->getClass() ?>" colspan=2>
 					&nbsp; &nbsp; <b><?=_("Stud.IP-Messenger")?></b>
 					</td>
 				</tr>
+
 				<tr <? $cssSw->switchClass() ?>>
 					<td class="<? echo $cssSw->getClass() ?>">
 					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -96,6 +189,7 @@ function change_messaging_view() {
 					&nbsp; <font size=-1><?=_("Stud.IP-Messenger automatisch nach dem Login starten")?></font>
 					</td>
 				</tr>
+
 				<tr <? $cssSw->switchClass() ?>>
 					<td class="<? echo $cssSw->getClass() ?>" colspan=2>
 					&nbsp; &nbsp; <b><?=_("Buddies / Wer ist online")?></b><a name="buddy_anker"></a>
