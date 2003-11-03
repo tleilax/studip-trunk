4a6a08805fd3ea958097223185f046dd3b3b84b9
Cornelis Kater <kater@zqs.uni-hannover.de>
2003-11-03 16:58:50 +0000

    neue Dateien für das Studienbuch-Modul

---
+++ htdocs/export/recordofstudy.lib.php
@@ -0,0 +1,318 @@
+<?php
+
+/**
+
+ * Creates a record of study and exports the data to pdf (html-outpu)
+
+ *
+
+ * @author      Christian Bauer <alfredhitchcock@gmx.net>
+
+ * @version     $Exp
+
+ * @copyright   2003 Stud.IP-Project
+
+ * @access      public
+
+ * @module      recordofstudy
+
+ */
+
+/**
+ * displays the site title
+ *
+ * @access  private
+ * @param   string $semester	the current semester (edit-mode) (optional)
+ *
+ */
+function printSiteTitle($semester = NULL){
+   	$html = "<table border=0 class=blank align=center cellspacing=0 cellpadding=0 width=\"100%\">\n"
+    	  . "	<tr valign=top align=center>\n"
+    	  . "    <td class=topic align=left colspan=\"2\">\n"
+		  . "	  <img src=\"PICTURES/meinesem.gif\" alt=\"Studienbuch erstellen\" align=\"texttop\">\n"
+		  . "	  &nbsp;<b>Studienbuch erstellen:</b>\n"
+		  . "	  <font size=\"-1\">$semester</font>\n"
+    	  . "    </td>\n"
+    	  . "   </tr>\n"
+    	  . "</table>\n";
+   	echo $html;
+}
+
+/**
+ * displays the semester selection page
+ *
+ * @access  private
+ * @param   array $infobox		the infobox for this site
+ * @param   array $semestersAR	the array with the semesters to select
+ *
+ */
+function printSelectSemester($infobox,$semestersAR){
+	$html = "<table border=\"0\" class=\"blank\" cellspacing=\"0\" cellpadding=\"0\" width=\"100%\">\n"
+		  . " <tr valign=\"top\">\n"
+		  . "  <td width=\"99%\" NOWRAP class=\"blank\">&nbsp;\n"
+		  . "   <table align=\"center\" width=\"99%\" class=\"blank\" border=\"0\" cellpadding=\"0\" cellspacing=0>\n"
+		  . "	 <tr>"
+		  . "	  <td align=\"left\" valign=\"top\"><font size=\"-1\">\n"
+		  . _("Bitte wählen sie ein Semster aus:")."\n"
+		  . "	   <form action=\"$PHPSELF\" method=post>\n"
+		  . "       &nbsp;<select name=\"semesterid\" style=\"vertical-align:middle;\">\n";
+	// the semester
+	for ($i=1;$i<=sizeof($semestersAR);$i++){
+		$html .= "        <option value=\"".$semestersAR[$i]["id"]."\">".$semestersAR[$i]["name"]."</option>\n";
+	}
+	$html .="       </select>\n"
+		  . createButton("auswaehlen",_("Semester und Kriterium auswählen."),"semester_selected")
+		  . "       <br><br>&nbsp;<select name=\"onlyseminars\" style=\"vertical-align:middle;\">\n"
+		  . "        <option value=\"1\" selected>"._("nur Lehrveranstaltungen")."</option>\n"
+		  . "        <option value=\"0\">"._("alle Veranstaltungen")."</option>\n"
+		  . "       </select>\n"
+		  . "      </form>\n"
+		  . "	  </font></td>\n"
+		  . "	  <td align=\"right\" width=\"250\" valign=\"top\">\n";
+	echo $html;
+	print_infobox($infobox,"pictures/folders.jpg");
+	$html = "	  </td>\n"
+		  . "	 </tr>\n"
+		  . "	</table>\n"
+		  . "  <br></td>\n"
+		  . " </tr>\n"
+		  . "</table>\n";
+	echo $html;
+}
+
+/**
+ * displays the edit page
+ *
+ * @access  private
+ * @param   array  $infobox		the infobox for this site
+ * @param   array  $basicdata	the basic data for the form
+ * @param   array  $seminare	the seminars for the form
+ * @param   string $notice		a notice for the user (optional)
+ *
+ */
+function printRecordOfStudies($infobox, $basicdata, $seminare, $notice = NULL){
+	global $semesterid;
+	$html = "<table border=\"0\" class=\"blank\" cellspacing=\"0\" cellpadding=\"0\" width=\"100%\">\n"
+		  . " <form action=\"$PHP_SELF\" method=post>\n"
+		  . " <input type=\"hidden\" name=\"semesterid\" value=\"".$semesterid."\">\n"
+		  . " <tr valign=\"top\">\n"
+		  . "  <td width=\"99%\" NOWRAP class=\"blank\">&nbsp;\n"
+		  . "   <table align=\"center\" width=\"99%\" class=\"blank\" border=\"0\" cellpadding=\"0\" cellspacing=0>\n"
+		  . "	 <tr>"
+		  . "	  <td valign=\"top\">"
+		  . "	   <table align=\"center\" width=\"100%\" class=\"blank\" border=\"0\" cellpadding=\"0\" cellspacing=0>\n";
+	
+	// displays some infos for the user
+	if ($notice){
+		$html .="	    <tr>\n"
+			  . "		 <td align=\"center\">\n"
+			  . "		  <img src=\"".$ABSOLUTE_PATH_STUDIP."pictures/ausruf.gif\" alt=\"ausruf\" style=\"vertical-align:middle;\">\n"
+			  . "		  </td>\n"
+			  . "		  <td colspan=\"3\">\n";
+		if ($notice == "empty")
+			$html .="		  <font size=\"-1\" color=\"red\"><b>"._("Keine Vorlesungen zum Anzeigen vorhanden.")."</b><br>"._("Bitte fügen sie Vorlesungen mit Hilfe des Buttons \"hinzufügen\" ein oder ändern Sie ihre ihre Auswahl.")."\n";
+		elseif ($notice == "above_limit")
+			$html .="		  <font size=\"-1\"><b>"._("Sie haben mehr als 10 Vorlesungen, Seminare oder Übungen für ihre Studienbuchseite vorbereitet.")."</b><font size=\"-2\" color=\"yello\"><br>"._("Es werden automatisch mehrere Studienbuchseiten erstellt.")."</font>\n";
+		$html .="		 <br><br></font>\n"
+			  . "		 </td>\n"
+			  . "		</tr>\n";
+	}
+
+		  
+	$html .=createInputBox(_("Hochschule: "), $basicdata["university"],	"university", "steelgraulight", 	"60")
+		  . createInputBox(_("Studienfach: "), $basicdata["fieldofstudy"],	 "fieldofstudy",  "steelgraulight", 	"60")
+		  . createInputBox(_("Name (Vor- und Zuname): "), $basicdata["studentname"],	 "studentname",	  "steelgraulight", 	"60")
+		  . createInputBox(_("Semester: "), $basicdata["semester"],		 "semester",	  "steel1kante", 		"30")
+		  . createInputBox(_("Fachsemester: "), $basicdata["semesternumber"],"semesternumber","steel1", 			"2", "tes Fachsemester");
+
+	$html .="	    <tr>\n"
+		  . "		 <td colspan=\"4\"><font size=\"-1\"><b><br>\n"
+		  . _("Vorlesungen, Seminare und Übungen:")."\n"
+		  . "		 </b></font></td>\n"
+		  . "		</tr>\n"
+		  . "		<tr>\n"		  
+		  . createSeminarHeadTD(_("Kenn.-Nr"))
+		  . createSeminarHeadTD(_("Name des Dozenten"))
+		  . createSeminarHeadTD(_("Wochenstundenzahl"), "center")
+		  . createSeminarHeadTD(_("löschen"), "center")
+		  . "		</tr>\n";	 
+
+  if (!empty($seminare)){	
+	for($i=0;$i+1<=sizeof($seminare);$i++){
+	  	if (($i % 2) == 0)	$displayclass = "steel1";
+	  	else				$displayclass = "steelgraulight";
+	$html .="		<tr>\n"
+		  . "		 <td class=\"$displayclass\" height=\"40\"><font size=\"-1\">\n"
+		  . "	   	  &nbsp;<input name=\"seminarnumber$i\" type=\"text\" size=\"6\" maxlength=\"6\" value=\"".$seminare[$i]["seminarnumber"]."\">\n"
+		  . "		 </td>\n"
+		  . "		 <td class=\"$displayclass\"><font size=\"-1\">\n"
+		  . "	   	  &nbsp;<input name=\"tutor$i\" type=\"text\" size=\"70\" maxlength=\"70\" value=\"".$seminare[$i]["tutor"]."\">\n"
+		  . "		  \n"
+		  . "		 </td>\n"
+		  . "		 <td class=\"$displayclass\" align=\"center\"><font size=\"-1\">\n"
+		  . "	   	  &nbsp;<input name=\"sws$i\" type=\"text\" size=\"2\" maxlength=\"2\" value=\"".$seminare[$i]["sws"]."\">"._("SWS")."\n"
+		  . "		 </td>\n"
+		  . "		 <td class=\"$displayclass\" rowspan=\"2\" align=\"center\">\n"
+		  . "		  &nbsp;<input type=\"checkbox\" name=\"delete$i\" value=\"1\">\n"
+		  . "		 </td>\n"
+		  . "		</tr>\n"
+		  . "		<tr>\n"
+		  . "		 <td class=\"$displayclass\" colspan=\"3\"><font size=\"-2\" align=\"top\">\n"
+		  . "		  &nbsp;<b>"._("Genaue Bezeichnung:")."</b><br>&nbsp;<textarea name=\"description$i\" cols=\"60\" rows=\"2\">".$seminare[$i]["description"]."</textarea>\n"
+		  . "		 &nbsp;<br><br></td>\n"
+		  . "		</tr>\n";
+	}
+	// delivers the seminar_max
+	$seminare_max = $i;
+	$html.="		 <input type=\"hidden\" name=\"seminare_max\" value=\"".$seminare_max."\">\n";
+	
+	}
+		  
+	$html .="		<tr>\n"
+		  . "		 <td colspan=\"4\"><font size=\"-1\"><br><table width=\"100%\"><tr><td align=\"left\">\n"
+		  . createButton("hinzufuegen",_("Neue Veranstaltung hinzufügen."),"add_seminars")
+		  . "		  <select style=\"vertical-align:middle;\" name=\"newseminarfields\" size=1>\n";
+	for( $i=1; $i<=10; $i++ )
+		$html .= "		  <option value=\"$i\">$i</option>\n";
+	$html .="		  </select>\n"
+		  . "		 </font></td>\n"
+		  . "		 <td align=right><font size=\"-1\" style=\"vertical-align:middle;\">\n";
+	
+	// only show delete-button if there are any seminars
+	if(!empty($seminare))
+		$html .= _("Markierte Vorlesungen löschen")."\n" . createButton("loeschen",_("Markierte Antworten löschen."),"delete_seminars");
+	$html .="	     </font></td></tr></table>\n"
+		  . "	    </tr>\n"
+		  . "	   </table>\n"
+		  . "	  </td>\n";
+		  
+	// the right site of the page
+	$html .="	  <td class=\"blank\" width=\"256\" valign=\"top\" align=\"center\"><font size=\"-1\">\n";
+	echo $html;
+	print_infobox($infobox,"pictures/folders.jpg");
+	$html = "	   <br>\n"
+		  . createButton("speichern",_("Erstellt sie mit diesem Button ein PDF, wenn sie die benötigten Daten eingegeben haben."),"create_pdf")
+		  . createButton("zurueck",_("Abbrechen und eine Studienbuchseite für ein anderes Semester erstellen."),"select_new_semester")
+		  . "	  <br><br></td>\n"
+		  . "	 </tr>\n"
+		  . "	</table>\n"
+		  . "  </td>\n"
+		  . " </tr>\n"
+		  . " </form>\n"
+		  . "</table>\n";
+	echo $html;		  
+}
+
+/**
+ * displays the site in which the user can download the pdf
+ *
+ * @access  private
+ * @param   array  $infobox		the infobox for this site
+ * @param   array  $seminars	the seminars to export
+ *
+ */
+function printPdfAssortment($infobox,$seminars){
+	$html = "<table border=\"0\" class=\"blank\" cellspacing=\"0\" cellpadding=\"0\" width=\"100%\">\n"
+		  . " <tr valign=\"top\">\n"
+		  . "  <td width=\"99%\" NOWRAP class=\"blank\">&nbsp;\n"
+		  . "   <table align=\"center\" width=\"99%\" class=\"blank\" border=\"0\" cellpadding=\"0\" cellspacing=0>\n"
+		  . "	 <tr>\n"
+		  . "	  <td align=\"left\" valign=\"top\"><font size=\"-1\">\n"
+		  . sprintf(_("Sie haben %s Einträge für ihre Studienbuchseite ausgewählt. "),$seminars["numberofseminars"]);
+	$html .= ($seminars["numberofpages"]>1)
+		  ? sprintf(_("Deshalb werden ihre Einträge auf %s Studienbuchseiten verteilt."),$seminars["numberofpages"])."\n"
+		  : sprintf(_("Ihre Einträge können auf einer Studienbuchseite untergebracht werden."),$seminars["numberofseminars"])."\n";
+	$html .="	  <br><br>\n"
+		  . _("Ihre Studiendaten:")."<br>\n"
+		  . "&nbsp;" . _("Hochschule: ") . $seminars["university"] . "<br>\n"
+		  . "&nbsp;" . _("Studienfach: ") . $seminars["fieldofstudy"] . "<br>\n"
+		  . "&nbsp;" . _("Name (Vor- und Zuname): ") . $seminars["studentname"] . "<br>\n"
+		  . "&nbsp;" . _("Semester: ") . $seminars["semester"] . "<br>\n"
+		  . "&nbsp;" . _("Fachsemester: ") . $seminars["semesternumber"] . "<br>\n"
+		  . "<br><br>\n";
+
+	$html .= ($seminars["numberofpages"]>1)
+		  ? sprintf(_("Klicken sie nun auf die einzelnen Links, um ihre Studienbuchseiten zu erstellen."),$seminars["numberofpages"])."\n"
+		  : sprintf(_("Klicken sie nun auf den Link, um ihre Studienbuchseite zu erstellen."),$seminars["numberofseminars"])."\n";
+
+	$html .="	  <br>\n";
+	if ($seminars["numberofpages"]>1)
+		$html .= _("Studienbuch: ");
+	for($i=1;$i<=$seminars["numberofpages"];$i++){
+		$html .="	  <a href=\"recordofstudy.php?create_pdf=1&page=$i\" target=\"_blank\">\n";
+		$html .= ($seminars["numberofpages"]>1)
+			  ? sprintf(_("Seite %s"),$i)
+			  : _("Studienbuch");
+		$html .=" </a>";
+	}
+
+	$html .="	  </font></td>\n"
+		  . "	  <td align=\"right\" width=\"250\" valign=\"top\">\n";
+	echo $html;
+	print_infobox($infobox,"pictures/folders.jpg");
+	$html = "	  <form action=\"$PHP_SELF\" method=post>"
+		  . "	  <center>\n"
+// 		  . createButton("speichern",_("Erstellt sie mit diesem Button ein PDF, wenn sie die benötigten Daten eingegeben haben."),"create_pdf")
+		  . createButton("zurueck",_("Abbrechen und eine Studienbuchseite für ein anderes Semester erstellen."),"select_new_semester")
+		  . "	  <br><br></center></form></td>\n"
+		  . "	 </tr>\n"
+		  . "	</table>\n"
+		  . "  <br></td>\n"
+		  . " </tr>\n"
+		  . "</table>\n";
+	echo $html;
+}
+
+/**
+ * creates a complete <tr> with a label and an input-box
+ *
+ * @access  private
+ * @param   string $text	the label
+ * @param   string $value	the input box value
+ * @param   string $name	the input box name
+ * @param   string $class	the <td> class
+ * @param   string $size	the $size of the input box
+ * @param   string $additionaltext	an additonal text (optional)
+ * @returns string         	the button
+ */
+function createInputBox($text, $value, $name, $class, $size, $additionaltext = NULL){
+	$html = "	 <tr>\n"
+		  . "	  <td class=\"".$class."\" colspan=\"4\" width=\"99%\"><font size=\"-1\">\n"
+		  . "	   <br>&nbsp;".$text."<input name=\"".$name."\" type=\"text\" size=\"".$size."\" maxlength=\"".$size."\" value=\"".$value."\">".$additionaltext."\n"
+		  . "	  <br>&nbsp;</font></td>\n"
+		  . "	 </tr>\n";
+	return $html;
+}
+
+/**
+ * creates a <td> with a label
+ *
+ * @access  private
+ * @param   string $text	the label
+ * @param   string $align	the align (optional)
+ * @returns string          the <td> head
+ */
+function createSeminarHeadTD($text, $align = "left"){
+	$html = "		 <td class=\"steel\" height=\"26\" align=\"".$align."\" style=\"vertical-align:bottom;\" ><font size=\"-1\"><b>\n"
+		  . "		  &nbsp;".$text."\n"
+		  . "		 </font></b></td>\n";
+	return $html;
+}
+
+/**
+ * creates an image-button
+ *
+ *
+ * @access  private
+ * @param   string $button	the button name (send to makeButton())
+ * @param   string $title	the label
+ * @param   string $button	the button name (optional)
+ * @param   string $align	the button value (optional)
+ * @returns string         	the button
+ */
+function createButton($button, $title, $name = NULL, $value = NULL){
+	$html = "      <input type=\"image\" action=\"http://selfaktuell.teamone.de/\" name=\"".$name."\" value=\"".$value."\" style=\"vertical-align:middle;\""
+		  . 	   makeButton($button,"src") ." alt=\"".$title."\" title=\"".$title."\" border=0>\n";
+	return $html;
+}
+?>
---
+++ htdocs/export/recordofstudyDB.php
@@ -0,0 +1,267 @@
+<?php
+
+/**
+
+ * Creates a record of study and exports the data to pdf (database)
+
+ *
+
+ * @author      Christian Bauer <alfredhitchcock@gmx.net>
+
+ * @version     $Exp
+
+ * @copyright   2003 Stud.IP-Project
+
+ * @access      public
+
+ * @module      recordofstudy
+
+ */
+require_once("$ABSOLUTE_PATH_STUDIP/dates.inc.php");
+require_once("$ABSOLUTE_PATH_STUDIP/config.inc.php");
+
+/**
+ * collect the current seminars and concerning semesters from the archiv	
+ *
+ * @access  private
+ * @returns array the semesters
+ *
+ */
+function getSemesters(){
+	global $SEMESTER,$user;
+	// creating the list of avaible semester
+	for ($i=1;$i<=sizeof($SEMESTER); $i++){
+//		$position = sizeof($SEMESTER)+1-$i;
+		$semestersAR[$i]["beginn"] = $SEMESTER[$i]["beginn"];
+		$semestersAR[$i]["id"] = $i;
+		$semestersAR[$i]["idname"] = $SEMESTER[$i]["name"];
+		$semestersAR[$i]["name"] = convertSemester($SEMESTER[$i]["name"]);
+	}
+
+	// adding the semester from avaible archiv-items
+	$db = &new DB_Seminar ();
+	$db->query ("SELECT archiv.start_time, archiv.semester, archiv.start_time "
+		. "FROM archiv_user LEFT "
+		. "JOIN archiv  USING (seminar_id) "
+		. "WHERE archiv_user.user_id = '".$user->id."' "
+		. "GROUP BY archiv.semester ORDER BY start_time DESC");
+
+	while ($db->next_record()) {
+		$found = 0;
+		for ($j=1; $j<=sizeof($SEMESTER); $j++){
+			if (in_array($db->f("semester"), $SEMESTER[$j],1))
+				$found++;
+		}
+		if ($found == 0){
+			$semestersAR[$i]["beginn"] = $db->f("start_time");
+			$semestersAR[$i]["id"] = $i;
+			$semestersAR[$i]["idname"] = $db->f("semester");
+			$semestersAR[$i]["name"] = convertSemester($db->f("semester"));
+			$semestersAR[$i]["onlyarchiv"] = 1;
+			$i++;
+		}
+
+	}
+	return $semestersAR;
+}
+
+/**
+ * collects the basic data from the db
+ *
+ * @access  private
+ * @returns array 	the basic data
+ *
+ */
+function getBasicData(){
+global $user;
+	$db = &new DB_Seminar ();
+	// Für eigene Fehlerroutine am Ende auf "no" schalten!...
+	$db->Halt_On_Error = "yes";
+
+
+	// get field of study
+	$db->query("SELECT user_studiengang.*,studiengaenge.name "
+		. "FROM user_studiengang LEFT JOIN studiengaenge USING (studiengang_id) "
+		. "WHERE user_id = '".$user->id."' "
+		. "ORDER BY studiengang_id");
+	$db->query ($query);
+
+	while ($db->next_record()) {
+		$fieldofstudy .= $db->f("name")." ";
+	}
+
+	//get fullname
+	$db->query("SELECT user_info.title_front as tv"
+		.", user_info.title_rear as tr"
+		.", auth_user_md5.Vorname vn"
+		.", auth_user_md5.Nachname as nn"
+		." FROM auth_user_md5 LEFT JOIN user_info USING (user_id)"
+		." WHERE auth_user_md5.user_id = '".$user->id."'");
+//		." HAVING (sem_number <= $i AND (sem_number_end >= $i OR sem_number_end = -1))");
+	$db->next_record();
+	$fullname = htmlReady($db->f("tv")." ".$db->f("tr")." ".$db->f("vn")." ".$db->f("nn"));
+
+	return array(
+		"fieldofstudy"	=> $fieldofstudy,
+		"studentname"	=> $fullname,
+	);
+}
+
+/**
+ * gets the field of study of the current user from the db
+ *
+ * @access  private
+ * @returns string 	the field of study 
+ *
+ */
+function getFieldOfStudy(){
+	global $user;
+	
+	$db = &new DB_Seminar ();
+	// Für eigene Fehlerroutine am Ende auf "no" schalten!...
+	$db->Halt_On_Error = "yes";
+	
+
+	// get field of study
+	$db->query("SELECT user_studiengang.*,studiengaenge.name FROM user_studiengang LEFT JOIN studiengaenge USING (studiengang_id) WHERE user_id = '".$user->id."' ORDER BY studiengang_id");
+
+	while ($db->next_record()) {
+		$fieldofstudy .= $db->f("name")." ";
+	}
+	return $fieldofstudy;
+}
+ 
+/**
+ * gets the complete name of the student
+ *
+ * @access  private
+ * @returns string 	the complete name
+ *
+ */
+function getStudentname(){
+	global $user;
+	
+	$db = &new DB_Seminar ();
+	// Für eigene Fehlerroutine am Ende auf "no" schalten!...
+	$db->Halt_On_Error = "yes";
+	
+	//get fullname
+	$db->query("SELECT user_info.title_front as tv"
+		.", user_info.title_rear as tr"
+		.", auth_user_md5.Vorname vn"
+		.", auth_user_md5.Nachname as nn"
+		." FROM auth_user_md5 LEFT JOIN user_info USING (user_id)"
+		." WHERE auth_user_md5.user_id = '".$user->id."'");
+	$db->next_record();
+	$fullname = htmlReady($db->f("tv")." ".$db->f("tr")." ".$db->f("vn")." ".$db->f("nn"));
+
+	return $fullname;
+}
+
+/**
+ * gets the seminars of the currents user from the db
+ *
+ * @access  private
+ * @param   string $semesterid		the selected semester id
+ * @param   boolean $onlyseminars	could reduce the assortment
+ * @returns array 	the seminars
+ *
+ */
+function getSeminare($semesterid,$onlyseminars){
+	global $user,$SEMESTER,$semestersAR,$SEM_CLASS,$SEM_TYPE,$_fullname_sql;
+	
+	$db = &new DB_Seminar ();
+	$i = 0;
+	// if its not an archiv-only-semester, get the current ones
+	if(!$semestersAR[$semesterid]["onlyarchiv"]){
+
+		// the status the user should have in the seminar
+		$status = "autor";
+		
+		// some stolen code from a.noack :)
+		foreach ($SEMESTER as $key => $value){
+			$sem_start_times[] = $value['beginn'];
+		}
+		foreach ($SEM_CLASS as $key => $value){
+			if ($value['bereiche']){
+				foreach($SEM_TYPE as $type_key => $type_value){
+					if($type_value['class'] == $key)
+						$allowed_sem_status[] = $type_key;
+				}
+			}
+		}	
+
+
+		// new seminars
+		$db2 = &new DB_Seminar ();
+		
+		$query = "SELECT b.Seminar_id,b.Name,b.Untertitel,b.VeranstaltungsNummer, "
+			. "INTERVAL(start_time," . join(",",$sem_start_times) .") AS sem_number , "
+			. "IF(duration_time=-1,-1,INTERVAL(start_time+duration_time," . join(",",$sem_start_times) .")) AS sem_number_end "
+			. "FROM seminar_user a LEFT JOIN seminare b USING(Seminar_id) WHERE ";
+		
+		if($onlyseminars)
+			$query .= ((is_array($allowed_sem_status)) ? " b.status IN('" . join("','",$allowed_sem_status) . "') AND " : "") ." ";
+			
+		$query .= " a.user_id='".$user->id."' AND a.status='".$status."' "
+			. "HAVING (sem_number <= ".$semestersAR[$semesterid]["id"]." AND (sem_number_end >= ".$semestersAR[$semesterid]["id"]." OR sem_number_end = -1))";
+		$db->query($query);	
+
+		while ($db->next_record()) {
+			$seminarid = $db->f("Seminar_id");
+			$name = $db->f("Name");
+			$seminarnumber = $db->f("VeranstaltungsNummer");
+			$description = $db->f("Untertitel");
+				if ($description)
+					$name .= ": ".$description;
+			$sem_number_start = $db->f("sem_number");
+			$sem_number_end = $db->f("sem_number_end");
+
+			$db2->query ("SELECT seminar_user.user_id, " . $_fullname_sql['full'] . " AS fullname, username, status FROM seminar_user LEFT JOIN auth_user_md5 USING (user_id)  LEFT JOIN user_info USING(user_id) WHERE seminar_user.Seminar_id = '".$seminarid."' AND status = 'dozent' ORDER BY Nachname");
+			while($db2->next_record()){
+				if ($tutor) $tutor .= "; ";
+				$tutor .= $db2->f("fullname");
+			}
+
+			$seminare[$i] = array(
+				"id" 			=> $i,
+				"seminarid" 	=> $seminarid,
+				"seminarnumber" => $seminarnumber,
+				"tutor" 		=> $tutor,
+				"sws"			=> "",
+				"description" 	=> $name 
+				);
+			$i++;
+		}
+	}
+
+	//archiv seminars
+	$db->query ("SELECT archiv.name, archiv.seminar_id, archiv_user.status, archiv.VeranstaltungsNummer, archiv.name, archiv.semester, archiv.untertitel, archiv.studienbereiche, archiv.dozenten "
+		. "FROM archiv_user LEFT JOIN archiv  USING (seminar_id) "
+		. "WHERE archiv_user.user_id = '".$user->id."' AND archiv.semester = '".$semestersAR[$semesterid]["idname"]."'");
+	while($db->next_record()){
+
+		$seminarid = $db->f("seminar_id");
+		$name = $db->f("name");
+		$seminarnumber = $db->f("VeranstaltungsNummer");
+		$description = $db->f("untertitel");
+			if ($description)
+				$name .= ": ".$description;	
+		$tutor = $db->f("dozenten");
+		$semesterDB = $db->f("semester");
+
+		if( (!$onlyseminars) || 
+			($onlyseminars && $db->f("studienbereiche")))
+			$seminare[$i] = array(
+			"id" 			=> $i,
+			"seminarid" 	=> $seminarid,
+			"seminarnumber" => $seminarnumber,
+			"tutor" 		=> $tutor,
+			"description" 	=> $name 
+			);
+		$i++;
+	}
+	
+	return $seminare;
+}
+?>
