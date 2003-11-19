c7a0df4a87f0202bfcddb7365135d46243b3b662
phuegelm <phuegelm>
2003-11-19 08:29:37 +0000

    Fix für den Dateidownload mit ssl, bitte testen.

--- htdocs/sendfile.php
+++ htdocs/sendfile.php
@@ -295,7 +295,10 @@ header("Expires: Mon, 12 Dec 2001 08:00:00 GMT");
 header("Last-Modified: " . gmdate ("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate");   // HTTP/1.1
 header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
+if ($_SERVER['HTTPS'] == "on")
+	header("Pragma: public");
+else
+	header("Pragma: no-cache");
 header("Cache-Control: private");
 header("Expires: 0");
 
