493ccdbc57cb002b85bf3efb9dda45fd6be2ade6
Cornelis Kater <kater@zqs.uni-hannover.de>
2003-11-21 19:58:38 +0000

    Update: JS nur wenn in der Treeansicht

--- htdocs/folder.php
+++ htdocs/folder.php
@@ -48,13 +48,6 @@ if ($download_selected_x) {
 	}
 }
 
-// Start of Output
-include ("$ABSOLUTE_PATH_STUDIP/html_head.inc.php"); // Output of html head
-//JS Routinen einbinden, wenn benoetigt. Wird in der Funktion gecheckt, ob noetig...
-JS_for_upload();
-//we need this <body> tag, sad but true :)
-echo "\n<body onUnLoad=\"upload_end()\">"; 
-
 //Switch fuer die Ansichten
 if ($cmd=="tree") {
 	$folder_system_data='';
@@ -64,6 +57,17 @@ if ($cmd=="all") {
 	$folder_system_data='';
 	$folder_system_data["cmd"]="all";
 	}
+
+// Start of Output
+include ("$ABSOLUTE_PATH_STUDIP/html_head.inc.php"); // Output of html head
+
+if (!$folder_system_data["cmd"] == "all") {
+	//JS Routinen einbinden, wenn benoetigt. Wird in der Funktion gecheckt, ob noetig...
+	JS_for_upload();
+	//we need this <body> tag, sad but true :)
+	echo "\n<body onUnLoad=\"upload_end()\">"; 
+}
+
 
 include ("$ABSOLUTE_PATH_STUDIP/header.php");   // Output of Stud.IP head
 
