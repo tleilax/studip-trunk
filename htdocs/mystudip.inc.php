a13139f36b3f16eab3b115e2eb262c576d69e653
Cornelis Kater <kater@zqs.uni-hannover.de>
2003-12-05 21:36:13 +0000

    Update: An neues Design im My Stud.IP-Bereich angepasst

--- htdocs/mystudip.inc.php
+++ htdocs/mystudip.inc.php
@@ -76,54 +76,63 @@ function change_general_view() {
 		
 	$db=new DB_Seminar;
 	$cssSw=new cssClassSwitcher;
-	
-	echo "<table width =\"100%\" cellspacing=0 cellpadding=0 border=0><tr>\n";
-	echo "<td class=\"topic\" colspan=2><img src=\"pictures/einst.gif\" border=\"0\" align=\"texttop\"><b>&nbsp;" . _("Allgemeine Stud.IP-Einstellungen anpassen") . "</b></td></tr>\n";
-	echo "<tr><td class=\"blank\" colspan=2>&nbsp;\n";
-	echo "<blockquote><p>\n";
-	echo _("Hier k&ouml;nnen Sie die Ansicht von Stud.IP nach Ihren Vorstellungen anpassen.<br>Sie k&ouml;nnen z.B. Ihre bevorzugte Sprache einstellen.");
-	echo "<br></blockquote></td></tr>\n";	
-	echo "<tr><td class=\"blank\" colspan=2>\n";
 	?>
+	<table width="100%" border="0" cellpadding="0" cellspacing="0" align="center">
+		<tr>
+			<td class="topic" colspan=2><img src="pictures/einst.gif" border="0" align="texttop"><b>&nbsp;<?print _("Allgemeine Einstellungen anpassen");?></b></td>
+		</tr>
+		<tr>
+			<td class="blank" colspan=2>&nbsp;
+			</td>
+		</tr>
+		<tr>
+			
+			<td class="blank" width="100%" colspan="2" align="center">
+			<blockquote>
+				<font size="-1"><b><?print _("Hier k&ouml;nnen Sie die Ansicht von Stud.IP nach Ihren Vorstellungen anpassen.");?>
+			</blockquote>			
 			<form method="POST" action="<? echo $PHP_SELF ?>?cmd=change_general">
-			<table width ="99%" align="center" cellspacing=0 cellpadding=2 border=0>
-				<tr <? $cssSw->switchClass() ?>>
-					<td class="<? echo $cssSw->getClass() ?>" width="5%">&nbsp;
+			<table width="70%" align="center"cellpadding=8 cellspacing=0 border=0>
+				<tr>
+					<th width="50%" align=center><?=_("Option")?></th>
+					<th align=center><?=_("Auswahl")?></th>
+				</tr>
+				<tr  <? $cssSw->switchClass() ?>>
+					<td  align="right" class="blank" style="border-bottom:1px dotted black;">
+						<font size="-1"><?print _("Sprache");?></font>
 					</td>
-					<td class="<? echo $cssSw->getClass() ?>" width="20%">
-					<br><b><? echo _("Sprache:") ?></b><br><br>
-					</td>
-					<td class="<? echo $cssSw->getClass() ?>" width="75%" colspan="2"> 
-					<?	    
-					select_language($_language);
-					?>
+					<td <?=$cssSw->getFullClass()?>>
+						<? select_language($_language); ?>
 					</td>
 				</tr>
-				
-				<tr <? $cssSw->switchClass() ?>>
-					<td class="<? echo $cssSw->getClass() ?>" width="5%">&nbsp;
+				<tr  <? $cssSw->switchClass() ?>>
+					<td  align="right" class="blank" style="border-bottom:1px dotted black;">
+						<font size="-1"><?print _("Java-Script Hovereffekte");?></font><br />
+						<br><div align="left"><font size="-1">
+						<?print _("Mit dieser Funktion k&ouml;nnen Sie durch reines &Uuml;berfahren bestimmter Icons mit dem Mauszeiger (z.B. in den Foren oder im Adressbuch) die entsprechenden Eintr&auml;ge anzeigen lassen. Sie k&ouml;nnen sich so sehr schnell und effizient auch durch gr&ouml;&szlig;ere Informationsmengen arbeiten. Da jedoch die Ladezeiten der Seiten erheblich ansteigen, empfehlen wir diese Einstellung nur für NutzerInnen die mindestens &uuml;ber eine ISDN Verbindung verf&uuml;gen.");?>
+						</font></div>
 					</td>
-				<td class="<? echo $cssSw->getClass() ?>" width="20%"><b><?print _("Java-Script Hovereffekte");?></b></td><td  width="20%" class="<? echo $cssSw->getClass() ?>">
-				<?
-				IF ($auth->auth["jscript"]) {
-					echo "<input type=CHECKBOX name='jshover' value=1";
-					IF($forum["jshover"]==1) 
-					    echo " checked";
-					echo ">";
-				} else
-					echo _("Sie müssen in Ihrem Browser Javascript aktivieren um dieses Feature nutzen zu können.");
-				?>
-				</td><td   width="55%" class="<? echo $cssSw->getClass() ?>"><br><font size="2"><?print _("Mit dieser Funktion k&ouml;nnen sie durch reines &Uuml;berfahren bestimmter Icons mit dem Mauszeiger (z.B. in den Foren oder im Addresbuch) die entsprechenden Eintr&auml;ge anzeigen lassen. Sie k&ouml;nnen sich so sehr schnell und effizient auch durch gr&ouml;&szlig;ere Informationsmengen arbeiten. Da jedoch die Ladezeiten der Seiten erheblich ansteigen, empfehlen wir diese Einstellung nur für NutzerInnen die mindestens &uuml;ber eine ISDN Verbindung verf&uuml;gen.");?></font><br><br>
-				</td>
+					<td <?=$cssSw->getFullClass()?>>
+						<?
+						IF ($auth->auth["jscript"]) {
+							echo "<input type=CHECKBOX name='jshover' value=1";
+						IF($forum["jshover"]==1) 
+							echo " checked";
+						echo ">";
+						} else
+						echo "<font size=\"-1\">"._("Sie müssen in Ihrem Browser Javascript aktivieren um dieses Feature nutzen zu können.")."</font>";
+						?>
+						</font><br><br>
+					</td>
 				</tr>
-
-				<tr <? $cssSw->switchClass() ?>>
-					<td class="<? echo $cssSw->getClass() ?>" width="5%">&nbsp;
+				<tr  <? $cssSw->switchClass() ?>>
+					<td  align="right" class="blank" style="border-bottom:1px dotted black;">
+						<font size="-1"><?print _("pers&ouml;nliche Startseite");?></font><br />
+						<br><div align="left"><font size="-1">
+						<?print _("Sie k&ouml;nnen hier einstellen, welcher Systembereich automatisch nach dem Login oder Autologin aufgerufen wird. Wenn Sie zum Beispiel regelm&auml;&szlig;ig die Seite &raquo;Meine Veranstaltungen&laquo;. nach dem Login aufrufen, so k&ouml;nnen Sie dies hier direkt einstellen.");?></font><br><br>
+						</font></div>						
 					</td>
-					<td class="<? echo $cssSw->getClass() ?>" width="20%">
-					<br><b><? echo _("pers&ouml;nliche Startseite:") ?></b><br><br>
-					</td>
-					<td class="<? echo $cssSw->getClass() ?>" width="25%">
+					<td <?=$cssSw->getFullClass()?>>
 						<select name="personal_startpage">
 							<?	    
 							printf ("<option %s value=\"\">"._("keine")."</option>", (!$my_studip_settings["startpage_redirect"]) ? "selected" : "");
@@ -135,21 +144,18 @@ function change_general_view() {
 							?>
 						</select>
 					</td>
-					<td  width="55%" class="<? echo $cssSw->getClass() ?>">
-					<br><font size="2"><?print _("Sie k&ouml;nnen hier einstellen, welcher Systembereich automatisch nach dem Login oder Autologin aufgerufen wird. Wenn Sie zum Beispiel regelm&auml;&szlig;ig die Seite &raquo;Meine Veranstaltungen&laquo;. nach dem Login aufrufen, so k&ouml;nnen Sie dies hier direkt einstellen.");?></font><br><br>
 				</tr>
-				
-				
 				<tr <? $cssSw->switchClass() ?>>
-					<td class="<? echo $cssSw->getClass() ?>" width="5%">&nbsp;
+					<td  <?=$cssSw->getFullClass()?> colspan=2 align="middle">
+						<font size=-1><input type="IMAGE" <?=makeButton("uebernehmen", "src") ?> border=0 value="<?_("&Auml;nderungen &uuml;bernehmen")?>"></font>&nbsp;	
+						<input type="HIDDEN" name="view" value="allgemein">
+						</form>						
 					</td>
-					<td class="<? echo $cssSw->getClass() ?>" width="20%">&nbsp;
-					</td>
-					<td class="<? echo $cssSw->getClass() ?>" width="75%" colspan="2"><br>	&nbsp; 				
-	<?
-	echo "<font size=\"-1\"><input type=\"IMAGE\" " . makeButton("uebernehmen", "src") . " border=0 value=" . _("&Auml;nderungen &uuml;bernehmen") . "></font>&nbsp;"; 
-	echo "<input type=\"HIDDEN\" name=\"view\" value=\"allgemein\">\n";
-	echo "</td></tr></table></form>\n";	
+				</tr>
+			</table>
+			</td>
+		</tr>
+	</table> 
+<?
 }
-
 ?>
