b67342653625d4b76a4b1ca9ae1d8919c0562f39
André Noack <noackorama@gmail.com>
2004-01-20 15:38:23 +0000

    print view für Literatur

--- htdocs/links_openobject.inc.php
+++ htdocs/links_openobject.inc.php
@@ -201,10 +201,13 @@ if ($modules["scm"]) {
 }
 //
 if ($modules["literature"]) {
-	if ($SessSemName["class"]=="sem")
+	if ($SessSemName["class"]=="sem"){
 		$structure["_literatur"]=array (topKat=>"literatur", name=>_("Literatur"), link=>"literatur.php?view=literatur_sem", active=>FALSE);
-	else
+		$structure["_literatur_print"]=array (topKat=>"literatur", name=>_("Druckansicht"), link=>"lit_print_view.php?_range_id=" . $SessSemName[1], target=>"_blank", active=>FALSE);
+	}else{
 		$structure["_literatur"]=array (topKat=>"literatur", name=>_("Literatur"), link=>"literatur.php?view=literatur_inst", active=>FALSE);
+		$structure["_literatur_print"]=array (topKat=>"literatur", name=>_("Literatur"), link=>"lit_print_view.php?_range_id=" . $SessSemName[1], target=>"_blank", active=>FALSE);
+	}
 }
 
 if ($SessSemName["class"]=="sem" && $modules["participants"] && (!is_array($AUTO_INSERT_SEM) || !in_array($SessSemName[1], $AUTO_INSERT_SEM)  || $rechte))
