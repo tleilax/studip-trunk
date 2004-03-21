91af127e8188a4364eb758f46c5217fe38d6e2ae
rstockm <rstockm>
2004-03-21 12:05:19 +0000

    testing

--- htdocs/sendfile.php
+++ htdocs/sendfile.php
@@ -31,6 +31,8 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // +---------------------------------------------------------------------------+
 
+echo "test";
+
 if(ini_get('zlib.output_compression'))
       ini_set('zlib.output_compression', 'Off');
 
