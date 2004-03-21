0e05646797a86cc8cd3a28803e8086c4d403b25e
rstockm <rstockm>
2004-03-21 11:29:10 +0000

    FTP-Zugriff

--- htdocs/sendfile.php
+++ htdocs/sendfile.php
@@ -349,8 +349,8 @@ if (substr($path_file,0,6) != "ftp://") {
 	
 	header("Expires: Mon, 12 Dec 2001 08:00:00 GMT");
 	header("Last-Modified: " . gmdate ("D, d M Y H:i:s") . " GMT");
-	// header("Cache-Control: no-store, no-cache, must-revalidate");   // HTTP/1.1
-	// header("Cache-Control: post-check=0, pre-check=0", false);
+	header("Cache-Control: no-store, no-cache, must-revalidate");   // HTTP/1.1
+	header("Cache-Control: post-check=0, pre-check=0", false);
 	if ($_SERVER['HTTPS'] == "on")
 		header("Pragma: public");
 	else
