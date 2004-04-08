ff46cb8d7c293e2b99597b57db533763cc21dc2a
Stefan Suchi <suchi@data-quest.de>
2004-04-08 12:20:05 +0000

    UPDATE new version number

--- htdocs/phplib_local.inc.php
+++ htdocs/phplib_local.inc.php
@@ -71,7 +71,7 @@ $_fullname_sql['no_title_rev'] = "CONCAT(Nachname ,', ', Vorname)";
 $_fullname_sql['no_title_short'] = "CONCAT(Nachname,', ',UCASE(LEFT(TRIM(Vorname),1)),'.')";
 
 //software version - please leave it as it as!
-$SOFTWARE_VERSION="1.0.5 alpha cvs";
+$SOFTWARE_VERSION="1.1.0 beta cvs";
 
 /*classes for database access
 ----------------------------------------------------------------
