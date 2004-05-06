a5d3e66757cf94f352701b9ff789453410b1f391
rstockm <rstockm>
2004-05-06 14:57:09 +0000

    bugfix: Zählen personalisierter Votings

--- htdocs/lib/classes/score.class.php
+++ htdocs/lib/classes/score.class.php
@@ -302,6 +302,11 @@ function GetMyScore() {
 		$db->next_record();
 		$vote += $db->f(0);
 		
+		$db->query("SELECT count(*) FROM voteanswers_user LEFT JOIN voteanswers USING(answer_id) WHERE user_id = '$user_id' GROUP BY vote_id");
+		$db->next_record();
+		$vote += $db->f(0);
+		echo $db->f(0);
+				
 		$db->query("SELECT count(*) FROM eval WHERE author_id = '$user_id'");
 		$db->next_record();
 		$vote += 2*$db->f(0);
