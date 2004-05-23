127dba8d619406a853a3d2f146f2ba1f36e12cef
Cornelis Kater <kater@zqs.uni-hannover.de>
2004-05-23 11:48:55 +0000

    Bugfix: Falscher Variablenname

--- htdocs/resources/views/msgs_resources.inc.php
+++ htdocs/resources/views/msgs_resources.inc.php
@@ -157,8 +157,8 @@ $this->msg[35] = array (
 $this->msg[36] = array (
 		"mode" => "info",
 		"msg"=> _("Sie haben alle Anfragen, die Sie ausgew&auml;hlt haben, bearbeitet. Wollen Sie jetzt Nachrichten zu allen aufgel&ouml;sten Anfragen versenden?")
-			."<br /><a href=\"%s?snd_closed_request_sms=1&edit_all=1\">".makeButton("ja2")."</a>"
-			."&nbsp;<a href=\"%s?edit_all=1\">".makeButton("nein")."</a>");
+			."<br /><a href=\"%s?snd_closed_request_sms=1&reset_set=1\">".makeButton("ja2")."</a>"
+			."&nbsp;<a href=\"%s?reset_set=1\">".makeButton("nein")."</a>");
 $this->msg[37] = array (
 		"mode" => "msg",
 		"msg"=> _("Die regelm&auml;&szlig;ige Belegung wurde in Einzeltermine umgewandelt und ein Ablaufplan angelegt."));
