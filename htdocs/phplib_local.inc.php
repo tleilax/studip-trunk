516310b5d97557bab2f707b11ee285241781d323
schmelzer <schmelzer>
2004-08-25 19:32:48 +0000

    Maillink korrigiert wenn Server nicht auf Port 80 läuft

--- htdocs/phplib_local.inc.php
+++ htdocs/phplib_local.inc.php
@@ -107,7 +107,7 @@ class studip_smtp_class extends smtp_class {
 		$this->from = ($GLOBALS['MAIL_FROM'] == "") ? "\"Stud.IP\" <" . $this->env_from . ">" : $this->QuotedPrintableEncode('"' . $GLOBALS['MAIL_FROM'] . '"',1) . ' <' . $this->env_from . '>'; // From: Mailheader
 		$this->abuse = ($GLOBALS['MAIL_ABUSE'] == "") ? "abuse@" . $this->localhost : $GLOBALS['MAIL_ABUSE']; // Reply-To: Mailheader
 		$this->url = (($_SERVER["SERVER_PORT"] == 443 || $_SERVER["HTTPS"] == "on") ? "https://" : "http://") 
-					. $_SERVER["HTTP_HOST"] . (($_SERVER["SERVER_PORT"] != 443 && $_SERVER["SERVER_PORT"] != 80) ? ":" . $_SERVER["SERVER_PORT"] : "") 
+					. $_SERVER["SERVER_NAME"] . (($_SERVER["SERVER_PORT"] != 443 && $_SERVER["SERVER_PORT"] != 80) ? ":" . $_SERVER["SERVER_PORT"] : "") 
 					. $GLOBALS['CANONICAL_RELATIVE_PATH_STUDIP']; // link to system
 		$this->additional_headers = array("MIME-Version: 1.0",
 										"Content-Type: text/plain; charset=\"{$this->charset}\"",
