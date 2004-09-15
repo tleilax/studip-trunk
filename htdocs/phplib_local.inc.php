260c0f51c1ac644ab8c63865fc031542ea4bd6b6
Cornelis Kater <kater@zqs.uni-hannover.de>
2004-09-15 10:17:38 +0000

    Update: Tippfehler

--- htdocs/phplib_local.inc.php
+++ htdocs/phplib_local.inc.php
@@ -44,7 +44,7 @@ $GLOBALS['_fullname_sql']['no_title'] = "CONCAT(Vorname ,' ', Nachname)";
 $GLOBALS['_fullname_sql']['no_title_rev'] = "CONCAT(Nachname ,', ', Vorname)";
 $GLOBALS['_fullname_sql']['no_title_short'] = "CONCAT(Nachname,', ',UCASE(LEFT(TRIM(Vorname),1)),'.')";
 
-//software version - please leave it as it as!
+//software version - please leave it as it is!
 $SOFTWARE_VERSION="1.1.5 alpha cvs";
 
 /*classes for database access
