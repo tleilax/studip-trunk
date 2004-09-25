529a4567ea3f4ed5430cd15be1e113a79886b924
Cornelis Kater <kater@zqs.uni-hannover.de>
2004-09-25 20:23:19 +0000

    Update 1.1 -> 1.1.5 ACHTUNG, NOCH BETA

---
+++ db/update/convert_1.1to1.1.5/migration_1.1to1.1.5.sql
@@ -0,0 +1,93 @@
+# mysql migration script
+# base version: 1.1
+# update version: 1.1.5
+
+PLEASE NOTE: Since there exists no migration-tool, please use this script MANUEL to convert your old database to
+a newer version.
+Dont't paste this script directly into your SQL-client, because you have to excute the convert scripts and/or delete-queries 
+in the specified order! 
+
+# For detailed informations, please take a look at the update protocol from our installation in Goettingen!
+# (Should be located in the same folder)
+#
+#
+# #1
+# create new table for visits
+#
+
+CREATE TABLE `object_user_visits` (
+`object_id` varchar(32) NOT NULL default '',
+`user_id` varchar(32) NOT NULL default '',
+`type` varchar(20) NOT NULL default '',
+`visitdate` int(20) NOT NULL default '0',
+`last_visitdate` int(20) NOT NULL default '0',
+PRIMARY KEY (`object_id`,`user_id`,`type`)
+) TYPE=MyISAM;
+
+# #2
+# >>>please use the script convert_loginfile.php at this point
+#
+
+# #3
+# changes to the messaging system
+#
+
+ALTER TABLE `message` ADD `subject` VARCHAR( 255 ) NOT NULL AFTER `autor_id` ;
+ALTER TABLE `message` ADD `readed` TINYINT( 1 ) DEFAULT '0' NOT NULL AFTER `mkdate` ;
+ALTER TABLE `message` ADD `reading_confirmation` INT ( 1 ) DEFAULT '0' NOT NULL;
+ALTER TABLE `message_user` ADD `confirmed_read` TINYINT( 1 ) DEFAULT '0' NOT NULL ;
+ALTER TABLE `message_user` ADD `answered` TINYINT( 1 ) DEFAULT '0' NOT NULL ;
+
+# #4
+# change the indexes of messaging for better performance
+#
+
+ALTER TABLE `message_user` DROP PRIMARY KEY ;
+ALTER TABLE `message_user` ADD PRIMARY KEY ( `user_id` , `snd_rec` , `message_id` ) ;
+ALTER TABLE `message_user` ADD INDEX ( `message_id` ) ;
+ALTER TABLE `message_user` ADD INDEX ( `user_id` , `snd_rec` , `deleted` , `folder` ) ;
+ALTER TABLE `message_user` ADD INDEX ( `user_id` , `snd_rec` , `deleted` , `folder` ) ;
+
+# #5
+# >>>please use the script convert_<NILS_SCRIPT!FEHLT>
+#
+
+# #6
+# changes to the wiki
+#
+
+ALTER TABLE `wiki` CHANGE `keyword` `keyword` VARCHAR( 128 ) BINARY NOT NULL ;
+ALTER TABLE `wiki_links` CHANGE `to_keyword` `to_keyword` CHAR( 128 ) BINARY NOT NULL ;
+ALTER TABLE `wiki_links` CHANGE `from_keyword` `from_keyword` CHAR( 128 ) BINARY NOT NULL ;
+ALTER TABLE `wiki_locks` CHANGE `keyword` `keyword` VARCHAR( 128 ) BINARY NOT NULL ;
+
+# #7
+# create new table for new smiley-management
+#
+
+CREATE TABLE smiley (
+  smiley_id bigint(20) NOT NULL auto_increment,
+  smiley_name varchar(50) NOT NULL default '',
+  smiley_width int(11) NOT NULL default '0',
+  smiley_height int(11) NOT NULL default '0',
+  short_name varchar(50) NOT NULL default '',
+  smiley_counter bigint(20) NOT NULL default '0',
+  short_counter bigint(20) NOT NULL default '0',
+  fav_counter bigint(20) NOT NULL default '0',
+  timestamp timestamp(14) NOT NULL,
+  PRIMARY KEY  (smiley_id),
+  UNIQUE KEY name (smiley_name),
+  KEY short (short_name)
+) TYPE=MyISAM AUTO_INCREMENT=1 ;
+
+# #8
+# initialize new smiley-management <SCRIPT?admin_smileys.php?cmd=updatetable>
+#
+
+# #9
+# changes to the table user_inst
+#
+
+ALTER TABLE `user_inst` ADD `externdefault` TINYINT( 3 ) UNSIGNED DEFAULT '0' NOT NULL ,
+ADD `priority` TINYINT( 3 ) UNSIGNED DEFAULT '0' NOT NULL ;
+ALTER TABLE `user_inst` CHANGE `raum` `raum` VARCHAR( 200 ) NOT NULL ;
