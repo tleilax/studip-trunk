2d5c36d47155e27445198ff244a738c123e2c6cb
rstockm <rstockm>
2004-11-10 13:31:40 +0000

    Bugfix bei Einstellung über myStud.IP Seite

--- htdocs/forumsettings.inc.php
+++ htdocs/forumsettings.inc.php
@@ -30,6 +30,7 @@ if ($forumsend=="bla"){
 	$forum["showimages"] = $showimages;
 	$forum["sortthemes"] = $sortthemes;
 	$forum["themeview"] = $themeview;
+	$forum["presetview"] = $presetview;
 	$forum["shrink"] = $shrink*604800; // Anzahl der Sekunden pro Woche
 	$forum["changed"] = "TRUE";
 }
@@ -138,7 +139,7 @@ if ($forumsend=="bla"){
 				</td>
 				<td <?=$cssSw->getFullClass()?> align="left">
 					<font size=-1>
-					<input type=radio value="theme" name=presetview <?if ($forum["presetview"]=="tree" || $forum["presetview"]=="mixed") echo "checked"; echo "> "._("Themenansicht");?><br>
+					<input type=radio value="theme" name=presetview <?if ($forum["presetview"]=="theme" || $forum["presetview"]=="mixed") echo "checked"; echo "> "._("Themenansicht");?><br>
 					<input type=radio value="neue" name=presetview <?if ($forum["presetview"]=="neue") echo "checked";echo "> "._("Neue Beiträge");?><br>
 					<input type=radio value="flat" name=presetview <?if ($forum["presetview"]=="flat") echo "checked";echo "> "._("Letzte Beiträge");?><br>
 				</td>
