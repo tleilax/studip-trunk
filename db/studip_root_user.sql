eb0bdb9cd44be2fbf1d9db830886b314cf4571f4
André Noack <noackorama@gmail.com>
2005-09-30 09:29:31 +0000

    *** empty log message ***

--- db/studip_root_user.sql
+++ db/studip_root_user.sql
@@ -1,19 +1,19 @@
 # Anlegen des Benutzers root 
 
 # Benutzer: root@studip ; Password: testing
-INSERT IGNORE INTO auth_user_md5 VALUES( '76ed43ef286fb55cf9e41beadb484a9f', 'root@studip', 'ae2b1fca515949e5d54fb22b8ed95575', 'root', 'Root', 'Studip', 'root@localhost',NULL);
-INSERT IGNORE INTO user_info SET user_id ='76ed43ef286fb55cf9e41beadb484a9f';
+REPLACE INTO auth_user_md5 VALUES( '76ed43ef286fb55cf9e41beadb484a9f', 'root@studip', 'ae2b1fca515949e5d54fb22b8ed95575', 'root', 'Root', 'Studip', 'root@localhost',NULL);
+REPLACE INTO user_info SET user_id = '76ed43ef286fb55cf9e41beadb484a9f';
 
 #
 # wichtige News
 #
 
-INSERT IGNORE INTO news VALUES ('29f2932ce32be989022c6f43b866e744', 'Herzlich Willkommen!', 'Das Stud.IP-Team heisst sie herzlich willkommen. \r\nBitte schauen Sie sich ruhig um!\r\n\r\nWenn Sie das System selbst installiert haben und diese News sehen, haben Sie die Demonstrationsdaten bzw. den Standard-Account für root in die Datenbank eingefügt. Wenn Sie produktiv mit dem System arbeiten wollen, sollten Sie diese Daten später wieder löschen, da die Passwörter der Accounts (vor allem des root-Accounts) öffentlich bekannt sind.', 'Root Studip', UNIX_TIMESTAMP(NOW()), '76ed43ef286fb55cf9e41beadb484a9f', 7343999);
+REPLACE INTO news VALUES ('29f2932ce32be989022c6f43b866e744', 'Herzlich Willkommen!', 'Das Stud.IP-Team heisst sie herzlich willkommen. \r\nBitte schauen Sie sich ruhig um!\r\n\r\nWenn Sie das System selbst installiert haben und diese News sehen, haben Sie die Demonstrationsdaten bzw. den Standard-Account für root in die Datenbank eingefügt. Wenn Sie produktiv mit dem System arbeiten wollen, sollten Sie diese Daten später wieder löschen, da die Passwörter der Accounts (vor allem des root-Accounts) öffentlich bekannt sind.', 'Root Studip', UNIX_TIMESTAMP(NOW()), '76ed43ef286fb55cf9e41beadb484a9f', 7343999);
 
 #
 # Daten für Tabelle `news_range`
 #
 
-INSERT INTO news_range VALUES ('29f2932ce32be989022c6f43b866e744', '76ed43ef286fb55cf9e41beadb484a9f');
-INSERT INTO news_range VALUES ('29f2932ce32be989022c6f43b866e744', 'studip');
+REPLACE INTO news_range VALUES ('29f2932ce32be989022c6f43b866e744', '76ed43ef286fb55cf9e41beadb484a9f');
+REPLACE INTO news_range VALUES ('29f2932ce32be989022c6f43b866e744', 'studip');
 
