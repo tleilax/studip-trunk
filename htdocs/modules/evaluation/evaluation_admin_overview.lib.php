2005794a74973eeaec627aec43bb7e032b3a82a0
André Noack <noackorama@gmail.com>
2006-02-07 08:31:08 +0000

    datei.inc.php wird benötigt für GetDownloadLink()

--- htdocs/modules/evaluation/evaluation_admin_overview.lib.php
+++ htdocs/modules/evaluation/evaluation_admin_overview.lib.php
@@ -21,6 +21,7 @@
 
 # Include all required files ================================================ #
 require_once($ABSOLUTE_PATH_STUDIP."modules/evaluation/evaluation.config.php");
+require_once($ABSOLUTE_PATH_STUDIP."datei.inc.php");
 require_once (HTML);
 require_once (EVAL_LIB_COMMON);
 require_once (EVAL_LIB_SHOW);
