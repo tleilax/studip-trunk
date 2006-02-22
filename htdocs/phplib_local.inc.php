16ad461d6f5f5e2c596209cbbecff2813ef59400
André Noack <noackorama@gmail.com>
2006-02-22 11:37:43 +0000

    ab heute beta Test in Gö :)

--- htdocs/phplib_local.inc.php
+++ htdocs/phplib_local.inc.php
@@ -62,7 +62,7 @@ $GLOBALS['_fullname_sql']['no_title_rev'] = "CONCAT(Nachname ,', ', Vorname)";
 $GLOBALS['_fullname_sql']['no_title_short'] = "CONCAT(Nachname,', ',UCASE(LEFT(TRIM(Vorname),1)),'.')";
 
 //software version - please leave it as it is!
-$SOFTWARE_VERSION="1.3 alpha cvs";
+$SOFTWARE_VERSION="1.3 beta cvs";
 
 /*classes for database access
 ----------------------------------------------------------------
