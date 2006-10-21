ec27ca1f60810ff8bbf4caf323c5924c1c3c68dd
André Noack <noackorama@gmail.com>
2006-10-21 16:04:00 +0000

    noch  ein test

--- htdocs/admin_xml_import.php
+++ htdocs/admin_xml_import.php
@@ -8,7 +8,7 @@
 page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Auth", "perm" => "Seminar_Perm", "user" => "Seminar_User", "ses" => "ses"));
 $perm->check("admin");
 
-//test
+//nochntest
 
 include ("$ABSOLUTE_PATH_STUDIP/seminar_open.php"); // initialise Stud.IP-Session
 
