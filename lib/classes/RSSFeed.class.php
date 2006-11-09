36a0d30f78f2b378e9c60a0d694e6c7f3611b42e
luniki <github@luniki.de>
2006-11-09 15:45:39 +0000

    moving magpierss to vendor including a patch from magpie 0.7.2 to studip-magpie

--- lib/classes/RSSFeed.class.php
+++ lib/classes/RSSFeed.class.php
@@ -29,7 +29,7 @@
 // +---------------------------------------------------------------------------+
 define('MAGPIE_CACHE_DIR', $TMP_PATH.'/magpie_cache');
 
-require_once('magpierss/rss_fetch.inc');
+require_once('vendor/magpierss/rss_fetch.inc');
 require_once('visual.inc.php');
 
 class RSSFeed {
