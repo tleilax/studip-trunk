f0565bed1b42e2ebca1a18de02e290e7c5ba4bbc
André Noack <noackorama@gmail.com>
2007-02-16 13:56:17 +0000

    aktueller Stand für 1.5

--- db/update/convert_1.4to1.5/migration_1.4_to_1.5.sql
+++ db/update/convert_1.4to1.5/migration_1.4_to_1.5.sql
@@ -23,4 +23,38 @@ CREATE TABLE `seminar_user_schedule` (
 # StEP00075: Dozentenreihenfolge und -bezeichnung
 #
 
-ALTER TABLE `seminar_user` ADD `position` INT NOT NULL AFTER `status`;
+ALTER TABLE `seminar_user` ADD `position` TINYINT UNSIGNED NOT NULL AFTER `status` ;
+
+#
+# StEP00078: Performanceoptimierungen, Konfigurationsoptionen
+#
+ALTER TABLE `user_info` DROP INDEX `guestbook`;
+INSERT INTO `config` (`config_id`, `parent_id`, `field`, `value`, `is_default`, `type`, `range`, `section`, `position`, `mkdate`, `chdate`, `description`, `comment`, `message_template`) VALUES ('4cd2cd3cc207ffc0ae92721c291cd906', '', 'RESOURCES_SHOW_ROOM_NOT_BOOKED_HINT', '0', 1, 'boolean', 'global', '', 0, 1168444600, 1168444600, 'Einstellung, ob bei aktivierter Raumverwaltung Raumangaben die nicht gebucht sind gekennzeichnet werden', '', '');
+INSERT INTO `config` ( `config_id` , `parent_id` , `field` , `value` , `is_default` , `type` , `range` , `section` , `position` , `mkdate` , `chdate` , `description` , `comment` , `message_template` )
+VALUES (
+MD5( 'SEM_CREATE_PERM' ) , '', 'SEM_CREATE_PERM', 'dozent', '1', 'string', 'global', '', '0', UNIX_TIMESTAMP( ) , UNIX_TIMESTAMP( ) , 'Bestimmt den globalen Nutzerstatus, ab dem Veranstaltungen angelegt werden dürfen (root,admin,dozent)', '', ''
+);
+INSERT INTO `config` ( `config_id` , `parent_id` , `field` , `value` , `is_default` , `type` , `range` , `section` , `position` , `mkdate` , `chdate` , `description` , `comment` , `message_template` )
+VALUES (
+MD5( 'SEM_VISIBILITY_PERM' ) , '', 'SEM_VISIBILITY_PERM', 'root', '1', 'string', 'global', '', '0', UNIX_TIMESTAMP( ) , UNIX_TIMESTAMP( ) , 'Bestimmt den globalen Nutzerstatus, ab dem versteckte Veranstaltungen in der Suche gefunden werden (root,admin,dozent)', '', ''
+);
+INSERT INTO `config` (`config_id`, `parent_id`, `field`, `value`, `is_default`, `type`, `range`, `section`, `position`, `mkdate`, `chdate`, `description`, `comment`, `message_template`) VALUES (MD5('ENABLE_SKYPE_INFO'), '', 'ENABLE_SKYPE_INFO', '1', '1', 'boolean', 'global', '', '0', UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Ermöglicht die Eingabe / Anzeige eines Skype Namens ', '', '');
+
+#
+# StEP00079: Themenordner im Forum schreibgeschützt
+#
+ALTER TABLE `px_topics` ADD `read_only` TINYINT UNSIGNED NOT NULL AFTER `user_id` ;
+
+#
+# StEP00082: Erweiterung SCM
+#
+ALTER TABLE `scm` DROP INDEX `range_id` ,
+ADD INDEX `range_id` ( `range_id` ) ;
+
+#
+# StEP00085: Erweiterung SCM
+#
+ALTER TABLE `seminare` ADD `admission_disable_waitlist` TINYINT UNSIGNED NOT NULL DEFAULT 0 AFTER `admission_endtime_sem` ;
+INSERT INTO `config` (`config_id`, `parent_id`, `field`, `value`, `is_default`, `type`, `range`, `section`, `position`, `mkdate`, `chdate`, `description`, `comment`, `message_template`) VALUES (MD5('ADMISSION_ALLOW_DISABLE_WAITLIST'), '', 'ADMISSION_ALLOW_DISABLE_WAITLIST', '1', '1', 'boolean', 'global', '', '0', UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Schaltet ein oder aus, ob die Warteliste in Zugangsbeschränkten Veranstaltungen deaktiviert werden kann', '', '');
+
+
