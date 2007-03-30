c1a832d4aa83353ac474dcaeadca62b91966579a
luniki <github@luniki.de>
2007-03-30 18:34:26 +0000

    importing flexi into goe trunk

---
+++ vendor/flexi/flexi.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * flexi.php - bootstrapping flexi
+ *
+ * Copyright (C) 2006 - Marcus Lunzenauer <mlunzena@uos.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ */
+
+
+/**
+ * Bootstrapping file for flexi. Just include this to get going.
+ *
+ * @package   flexi
+ */
+
+require_once 'template.php';
+require_once 'template_factory.php';
+require_once 'js_template.php';
+require_once 'php_template.php';
+require_once 'helper/helper.php';
+
