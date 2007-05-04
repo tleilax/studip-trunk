b8c371771776f3b093b7d3d6ed569828c2415bca
theinrich <theinrich>
2007-05-04 14:46:29 +0000

    RZS-Bugfix: Fixed calendar popup; refs #5

--- lib/calendar/views/insert_date_popup.php
+++ lib/calendar/views/insert_date_popup.php
@@ -29,9 +29,9 @@
 // +---------------------------------------------------------------------------+
 
 page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Auth", "perm" => "Seminar_Perm", "user" => "Seminar_User"));
-include ('seminar_open.php'); // initialise Stud.IP-Session
-require_once ('config.inc.php');
-require_once ('visual.inc.php');
+include ('lib/seminar_open.php'); // initialise Stud.IP-Session
+require_once ('config/config.inc.php');
+require_once ('lib/visual.inc.php');
 
 $element_switch = (isset($_REQUEST['element_switch']))? $_REQUEST['element_switch']:0; // Wert von 1 - 7 für Auswahl der Feldbezeichner
 $c = (isset($_REQUEST['c']))? $_REQUEST['c'] : 0;                   // Zähler wenn mehrere gleiche Eingabefelder im Zielformular
