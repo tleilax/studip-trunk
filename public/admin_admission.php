e8219efb3ecfd5eebaabef38111ab118e135e5b7
jankul <jankul>
2007-07-23 11:52:15 +0000

    StEP00086: Sperrung von Veranstaltung gegen Selbsteintrag

--- public/admin_admission.php
+++ public/admin_admission.php
@@ -639,7 +639,7 @@ if (is_array($admin_admission_data["studg"]) && $admin_admission_data["admission
 			</td>
 			<td class="<? echo $cssSw->getClass() ?>"  colspan=2 align="left">
 				<font size=-1><b><?=_("Anmeldeverfahren:")?></b><br /></font>
-				<? if (($admin_admission_data["admission_type_org"]) && (!$perm->have_perm("admin"))) {
+				<? if (($admin_admission_data["admission_type_org"] && $admin_admission_data["admission_type_org"] != 3) && (!$perm->have_perm("admin"))) {
 					$db->query("SELECT username, ". $_fullname_sql['full'] . "  as fullname FROM user_inst LEFT JOIN auth_user_md5 USING (user_id) LEFT JOIN user_info USING(user_id) WHERE institut_id ='".$admin_admission_data["heimat_inst_id"]."' AND perms = 'admin'");
 					if  (!$db->num_rows())
 						printf ("<font size=-1>"._("Sie haben ein Anmeldeverfahren aktiviert. Dieser Schritt kann %s nicht %s r&uuml;ckg&auml;ngig gemacht werden! Bei Problemen wenden sie sich bitte an eine Administratorin oder einen Administrator.")."<br /></font>", "</font><font size=-1 color=\"red\"><b>", "</b></font><font size=-1>");
