51a080a93750edbc1c750b0ebdd2207eed373467
theinrich <theinrich>
2007-08-02 15:00:40 +0000

    StEP00025: bugfix, forgot to remove comments :) ; refs #5

--- db/migrations/05_step_25_raumzeit_migrations.php
+++ db/migrations/05_step_25_raumzeit_migrations.php
@@ -15,7 +15,7 @@ class Step25RaumzeitMigrations extends DBMigration
         }
         
         $this->write( get_class($this)." - Creating db schema...");
-/*        
+        
         $this->db->query("
             CREATE TABLE IF NOT EXISTS `themen` (
               `issue_id` varchar(32) NOT NULL default '',
@@ -82,7 +82,7 @@ class Step25RaumzeitMigrations extends DBMigration
             '93da66ca9e2d17df5bc61bd56406add7' , '', 'RESOURCES_ROOM_REQUEST_DEFAULT_ACTION', 'NO_ROOM_INFO_ACTION', '1', 'string', 'global', '', '0', '0', '0', 'Designates the pre-selected action for the room request dialog', 'Valid values are: NO_ROOM_INFO_ACTION, ROOM_REQUEST_ACTION, BOOKING_OF_ROOM_ACTION, FREETEXT_ROOM_ACTION', ''
             );
         ");
-*/        
+        
         $this->write( get_class($this)."Finished with creating db schema.");
         $this->write( get_class($this)."Starting data conversion... - this may take a very long time");
 
