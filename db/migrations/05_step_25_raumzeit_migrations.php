80b0ad3746f3e550cf2462112b609a1de8bd9650
theinrich <theinrich>
2007-08-14 15:04:55 +0000

    RZS: moving RESOURCES_ENABLE from config_local.inc.php to config table

--- db/migrations/05_step_25_raumzeit_migrations.php
+++ db/migrations/05_step_25_raumzeit_migrations.php
@@ -76,11 +76,24 @@ class Step25RaumzeitMigrations extends DBMigration
             ALTER TABLE `resources_requests` ADD `reply_comment` TEXT AFTER `comment`;        
         ");
         
+        // move "RESOURCES_ENABLE" from config_local.inc.php to config table
+        if( $GLOBALS["RESOURCES_ENABLE"] ){
+            // use existing value
+            $this->db->query("
+                INSERT INTO `config` ( `config_id` , `parent_id` , `field` , `value` , `is_default` , `type` , `range` , `section` , `position` , `mkdate` , `chdate` , `description` , `comment` , `message_template` )
+                VALUES ( '06cdb765fb8f0853e3ebe08f51c3596e' , '', 'RESOURCES_ENABLE', '".$GLOBALS["RESOURCES_ENABLE"]."', '1', 'boolean', 'global', '', '0', '0', '0', 'Enable the Stud.IP resource management module', '', '');
+            ");
+        } else {
+            // use default value (=false)
+            $this->db->query("
+                INSERT INTO `config` ( `config_id` , `parent_id` , `field` , `value` , `is_default` , `type` , `range` , `section` , `position` , `mkdate` , `chdate` , `description` , `comment` , `message_template` )
+                VALUES ( '06cdb765fb8f0853e3ebe08f51c3596e' , '', 'RESOURCES_ENABLE', '0', '1', 'boolean', 'global', '', '0', '0', '0', 'Enable the Stud.IP resource management module', '', '');
+            ");
+        }
+        
         $this->db->query("
             INSERT INTO `config` ( `config_id` , `parent_id` , `field` , `value` , `is_default` , `type` , `range` , `section` , `position` , `mkdate` , `chdate` , `description` , `comment` , `message_template` )
-            VALUES (
-            '93da66ca9e2d17df5bc61bd56406add7' , '', 'RESOURCES_ROOM_REQUEST_DEFAULT_ACTION', 'NO_ROOM_INFO_ACTION', '1', 'string', 'global', '', '0', '0', '0', 'Designates the pre-selected action for the room request dialog', 'Valid values are: NO_ROOM_INFO_ACTION, ROOM_REQUEST_ACTION, BOOKING_OF_ROOM_ACTION, FREETEXT_ROOM_ACTION', ''
-            );
+            VALUES ( '93da66ca9e2d17df5bc61bd56406add7' , '', 'RESOURCES_ROOM_REQUEST_DEFAULT_ACTION', 'NO_ROOM_INFO_ACTION', '1', 'string', 'global', '', '0', '0', '0', 'Designates the pre-selected action for the room request dialog', 'Valid values are: NO_ROOM_INFO_ACTION, ROOM_REQUEST_ACTION, BOOKING_OF_ROOM_ACTION, FREETEXT_ROOM_ACTION', '');
         ");
         
         $this->write( get_class($this)."Finished with creating db schema.");
