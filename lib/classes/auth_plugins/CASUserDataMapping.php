7450d14327b22151e762ef6d5677dbc367101394
theinrich <theinrich>
2007-08-16 15:49:43 +0000

    added interface for CAS data mapping. removed existing abstract class.

---
+++ lib/classes/auth_plugins/CASUserDataMapping.php
@@ -0,0 +1,10 @@
+<?
+/**
+ * Abstract interface for the user mapping used by StudIPAuthCAS
+ */
+interface CASUserDataMapping {
+
+    // reads one attribute identified by a key of a given user
+	function getUserData( $key, $username);
+}
+?>
