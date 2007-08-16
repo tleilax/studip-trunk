642ea8b568d5d546225ca3f6cf62633864034117
theinrich <theinrich>
2007-08-16 15:52:22 +0000

    cosmetic update

--- lib/classes/auth_plugins/CASUserDataMapping.php
+++ lib/classes/auth_plugins/CASUserDataMapping.php
@@ -1,10 +1,10 @@
 <?
 /**
- * Abstract interface for the user mapping used by StudIPAuthCAS
+ * Interface for the user mapping used by StudIPAuthCAS
  */
 interface CASUserDataMapping {
 
     // reads one attribute identified by a key of a given user
-	function getUserData( $key, $username);
+	function getUserData ($key, $username);
 }
 ?>
