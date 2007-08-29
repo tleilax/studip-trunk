81efa0dc66f48368a43a84dbeac9e8c4ede327fc
André Noack <noackorama@gmail.com>
2007-08-29 15:04:20 +0000

    hmm, besser so

--- lib/raumzeit/SingleDateDB.class.php
+++ lib/raumzeit/SingleDateDB.class.php
@@ -58,7 +58,7 @@ class SingleDateDB {
 		if ($termin->isUpdate()) {
 			$db->query($query = "UPDATE $table SET metadate_id = '".$termin->getMetaDateID()."', date_typ = '".$termin->getDateType()."', date = '".$termin->getStartTime()."', end_time = '".$termin->getEndTime()."', range_id = '".$termin->getRangeID()."', autor_id = '".$termin->getAuthorID()."',raum = '".mysql_escape_string($termin->getFreeRoomText())."', content = '".$termin->getComment()."'  WHERE termin_id = '".$termin->getTerminID()."'");
 			if ($db->affected_rows()) {
-				$db->query("UPDATE $table SET chdate = {$termin->getChDate()}");
+				$db->query("UPDATE $table SET chdate = '".$termin->getChDate()."'");
 			}
 		} else {
 			$db->query($query = "REPLACE INTO $table (metadate_id, date_typ, date, end_time, mkdate, chdate, termin_id, range_id, autor_id, raum, content) VALUES ('".$termin->getMetaDateID()."', '".$termin->getDateType()."', '".$termin->getStartTime()."', '".$termin->getEndTime()."', '".$termin->getMkDate()."', '".$termin->getChDate()."', '".$termin->getTerminID()."', '".$termin->getRangeID()."', '".$termin->getAuthorID()."', '".mysql_escape_string($termin->getFreeRoomText())."', '".$termin->getComment()."')");
