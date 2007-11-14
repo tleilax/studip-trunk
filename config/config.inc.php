b296c2a86a556a1e07798b961987a8fb03621ff5
André Noack <noackorama@gmail.com>
2007-11-14 10:51:44 +0000

    upload Konfiguration für develop angepasst

--- config/config.inc.php
+++ config/config.inc.php
@@ -225,13 +225,13 @@ $SEM_CLASS[5]=array("name"=>_("Arbeitsgruppen"),
 
 //Festlegen der erlaubten oder verbotenen Dateitypen
 $UPLOAD_TYPES=array( 	"default" =>												//Name bezeichnet den zugehoerigen SEM_TYPE, name "1" waere entsprechend die Definition der Dateiendungen fuer SEM_TYPE[1]; default wird verwendet, wenn es keine spezielle Definition fuer einen SEM_TYPE gibt
-						array(	"type"=>"deny", 									//Type bezeichnet den grundsetzlichen Typ der Deklaration: deny verbietet alles ausser den angegebenen file_types, allow erlaubt alle ausser den angegebenen file_types
-								"file_types" => array ("rtf", "xls", "ppt", "zip", "pdf", "txt"),	//verbotene bzw. erlaubte Dateitypen
-								"file_sizes" => array (	"root" => 7 * 1048576,			//Erlaubte Groesse je nach Rechtestufe
-													"admin" => 7 * 1048576,
-													"dozent" => 7 * 1048576,
-													"tutor" => 7 * 1048576,
-													"autor" => 1.38 * 1048576,
+						array(	"type"=>"allow", 									//Type bezeichnet den grundsetzlichen Typ der Deklaration: deny verbietet alles ausser den angegebenen file_types, allow erlaubt alle ausser den angegebenen file_types
+								"file_types" => array ("exe"),	//verbotene bzw. erlaubte Dateitypen
+								"file_sizes" => array (	"root" => 14 * 1048576,			//Erlaubte Groesse je nach Rechtestufe
+													"admin" => 14 * 1048576,
+													"dozent" => 14 * 1048576,
+													"tutor" => 14 * 1048576,
+													"autor" => 7 * 1048576,
 													"nobody" => 1.38 * 1048576
 												)
 							),
