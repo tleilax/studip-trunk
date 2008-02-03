8cf40a69dfc3f7f2f818c727feb5a3e491f9da54
luniki <github@luniki.de>
2008-02-03 14:39:20 +0000

    using PDO for class StudIPUser, refs #26

--- lib/plugins/core/StudIPUser.class.php
+++ lib/plugins/core/StudIPUser.class.php
@@ -1,99 +1,92 @@
 <?php
 
 /**
-* User-Object which should be used in plugins
-* @author Dennis Reil, <dennis.reil@offis.de>
-* @version $Revision$
-* $Id$
-* @package pluginengine
-* @subpackage core
-*/
+ * User-Object which should be used in plugins
+ *
+ * @author Dennis Reil, <dennis.reil@offis.de>
+ * @version $Revision$
+ * $Id$
+ * @package pluginengine
+ * @subpackage core
+ */
 
 class StudIPUser {
-	var $userid;
-	var $username;
-	var $permission;
-	var $surname;
-	var $givenname;
-	var $assignedroles;
 
-	/**
-	* Automatically reads in the uid of the current user
-	*/
-    function StudIPUser() {
-    	$auth = $GLOBALS["auth"];
-	    $this->setUserid($auth->auth['uid']);
-    }
+  var $userid;
+  var $username;
+  var $permission;
+  var $surname;
+  var $givenname;
+  var $assignedroles;
 
-    function getSurname(){
-    	return $this->surname;
-    }
+  /**
+   * Automatically reads in the uid of the current user
+   */
+  function StudIPUser() {
+    $this->setUserid($GLOBALS["auth"]->auth['uid']);
+  }
 
-    function getGivenname(){
-    	return $this->givenname;
-    }
+  function getSurname() {
+    return $this->surname;
+  }
 
-    function setUserid($newuserid){
-	    $this->userid = $newuserid;
-		$dbconn = PluginEngine::getPluginDatabaseConnection();
-		if ($GLOBALS['PLUGINS_CACHING']){
-    		$result =& $dbconn->CacheExecute($GLOBALS['PLUGINS_CACHE_TIME'],"SELECT * FROM auth_user_md5 WHERE user_id=?",array($newuserid));
-		}
-		else {
-			$result =& $dbconn->Execute("SELECT * FROM auth_user_md5 WHERE user_id=?",array($newuserid));
-		}
-		if (!$result->EOF){
-			$this->givenname = $result->fields("Vorname");
-			$this->surname = $result->fields("Nachname");
-			$this->username = $result->fields("username");
-		}
-    	$result->Close();
-	   	$this->permission = new Permission($this->userid);
-    }
+  function getGivenname() {
+    return $this->givenname;
+  }
 
-    function getUserid(){
-	    return $this->userid;
-    }
+  # TODO (mlunzena) what a bad design, the whole idea of filling a user by
+  #                 setting an ID smells
+  function setUserid($id) {
 
-    function getPermission(){
-    	return $this->permission;
-    }
+    $this->userid = $id;
+    $this->permission = new Permission($id);
 
-    function getUsername(){
-    	return $this->username;
+    $row = PluginEngine::cachedCallback('plugins/StudIPUser/setUserid/' . $id,
+                                        array(__CLASS__, '_setUserid'),
+                                        array($id));
+    if ($row !== FALSE) {
+      $this->givenname = $row['Vorname'];
+      $this->surname   = $row['Nachname'];
+      $this->username  = $row['username'];
     }
-    /*
-    function setUsername($newusername){
-    	$this->username = $newusername;
-    }
-    */
+  }
 
-    /**
-     * checks, if this user is identical to the otheruser
-     *
-     * @param StudIPUser $otheruser
-     * @return false - other user is not the same as this user
-     * 		   true - both user are the same
-     */
-    function isSameUser($otheruser){
-    	if (is_a($otheruser,"StudIPUser") || is_subclass_of($otheruser,"StudIPUser")){
-    		if ($otheruser->getUserid() == $this->getUserid()){
-    			return true;
-    		}
-    		else {
-    			return false;
-    		}
-    	}
-    	else {
-    		return false;
-    	}
-    }
+  # TODO (mlunzena) see above..
+  static function _setUserid($id) {
+    $stmt = DBManager::get()->prepare("SELECT Vorname, Nachname, username ".
+                                      "FROM auth_user_md5 ".
+                                      "WHERE user_id=?");
+    $stmt->execute(array($id));
+    return $stmt->fetch();
+  }
 
-    function getAssignedRoles($withimplicit=false){
-    	$rolemgmt = new de_studip_RolePersistence();
-	    $this->assignedroles = $rolemgmt->getAssignedRoles($this->userid,$withimplicit);
-    	return $this->assignedroles;
-    }
+  function getUserid() {
+    return $this->userid;
+  }
 
+  function getPermission() {
+    return $this->permission;
+  }
+
+  function getUsername() {
+    return $this->username;
+  }
+
+  /**
+   * checks, if this user is identical to the otheruser
+   *
+   * @param StudIPUser $otheruser
+   * @return false - other user is not the same as this user
+   *         true - both user are the same
+   */
+  function isSameUser(StudIPUser $otheruser) {
+    return $otheruser->getUserid() === $this->getUserid();
+  }
+
+  function getAssignedRoles($withimplicit = false) {
+    $rolemgmt = new de_studip_RolePersistence();
+    $this->assignedroles = $rolemgmt->getAssignedRoles($this->userid,
+                                                       $withimplicit);
+    return $this->assignedroles;
+  }
 }
-?>
