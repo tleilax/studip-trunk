96c65893ca839d6f6ad745824458afa83f442b5e
Peter Thienel <thienel@data-quest.de>
2008-04-08 10:04:16 +0000

    Migration fuer Datenbankoptimierung (keys fuer Abfrage von Kontingentierungen)

---
+++ db/migrations/17_db_optimierung_kontingentierung.php
@@ -0,0 +1,30 @@
+<?
+class DbOptimierungKontingentierung extends DBMigration {
+
+	function description () {
+		return 'adds keys in admission_seminar_studiengang, admission_seminar_user and seminar_user';
+	}
+
+	function up () {
+		set_time_limit(0);
+		$this->announce("add keys...");
+		$this->db->query("ALTER TABLE `admission_seminar_studiengang` ADD INDEX `studiengang_id` ( `studiengang_id` )");
+		$this->db->query("ALTER TABLE `admission_seminar_user` ADD INDEX `seminar_id` ( `seminar_id`, `studiengang_id`, `status` )");
+		$this->db->query("ALTER TABLE `seminar_user` ADD INDEX `user_id` ( `user_id`, `status` )");
+		$this->db->query("ALTER TABLE `seminar_user` ADD INDEX `Seminar_id` ( `Seminar_id`, `admission_studiengang_id )");
+
+		$this->announce("done.");
+		
+	}
+	
+	function down () {
+		$this->announce("delete keys...");
+		$this->db->query("ALTER TABLE `admission_seminar_studiengang` DROP INDEX `studiengang_id`");
+		$this->db->query("ALTER TABLE `admission_seminar_user` DROP INDEX `seminar_id`");
+		$this->db->query("ALTER TABLE `seminar_user` DROP INDEX `user_id`");
+		$this->db->query("ALTER TABLE `seminar_user` DROP INDEX `Seminar_id`");
+		
+		$this->announce("done.");
+	}
+}
+?>
