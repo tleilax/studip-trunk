e47590fbcda4ea11dee2d17b8d5af0eb124e99d6
Elmar Ludwig <ludwig@uni-osnabrueck.de>
2008-04-29 08:49:30 +0000

    ignore errors in this migration when adding or deleting keys

--- db/migrations/17_db_optimierung_kontingentierung.php
+++ db/migrations/17_db_optimierung_kontingentierung.php
@@ -7,20 +7,29 @@ class DbOptimierungKontingentierung extends DBMigration {
 
 	function up () {
 		$this->announce("add keys...");
-		$this->db->query("ALTER TABLE `admission_seminar_studiengang` ADD INDEX `studiengang_id` ( `studiengang_id` )");
-		$this->db->query("ALTER TABLE `admission_seminar_user` ADD INDEX `seminar_id` ( `seminar_id`, `studiengang_id`, `status` )");
-		$this->db->query("ALTER TABLE `seminar_user` ADD INDEX `Seminar_id` ( `Seminar_id`, `admission_studiengang_id` )");
+
+		$db = DBManager::get();
+		$mode = $db->getAttribute(PDO::ATTR_ERRMODE);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+		$db->query("ALTER TABLE `admission_seminar_studiengang` ADD INDEX `studiengang_id` ( `studiengang_id` )");
+		$db->query("ALTER TABLE `admission_seminar_user` ADD INDEX `seminar_id` ( `seminar_id`, `studiengang_id`, `status` )");
+		$db->query("ALTER TABLE `seminar_user` ADD INDEX `Seminar_id` ( `Seminar_id`, `admission_studiengang_id` )");
+		$mode = $db->setAttribute(PDO::ATTR_ERRMODE, $mode);
 
 		$this->announce("done.");
-		
 	}
-	
+
 	function down () {
 		$this->announce("delete keys...");
-		$this->db->query("ALTER TABLE `admission_seminar_studiengang` DROP INDEX `studiengang_id`");
-		$this->db->query("ALTER TABLE `admission_seminar_user` DROP INDEX `seminar_id`");
-		$this->db->query("ALTER TABLE `seminar_user` DROP INDEX `Seminar_id`");
-		
+
+		$db = DBManager::get();
+		$mode = $db->getAttribute(PDO::ATTR_ERRMODE);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+		$db->query("ALTER TABLE `admission_seminar_studiengang` DROP INDEX `studiengang_id`");
+		$db->query("ALTER TABLE `admission_seminar_user` DROP INDEX `seminar_id`");
+		$db->query("ALTER TABLE `seminar_user` DROP INDEX `Seminar_id`");
+		$mode = $db->setAttribute(PDO::ATTR_ERRMODE, $mode);
+
 		$this->announce("done.");
 	}
 }
