1436b2ddd579719a0ec315655c4fe413465cbd3c
Till Glöggler <tgloeggl@uos.de>
2008-07-10 10:53:14 +0000

    refs #152 - needs to be reviewed

---
+++ public/datenschutz.php
@@ -0,0 +1,210 @@
+<?php
+require("lib/include/html_head.inc.php");
+?>
+<body>
+<table width=99% align=center bgcolor=white>
+<tr><td>
+<h1>Stud.IP - Erläuterungen zum Datenschutz im Überblick</h1>
+
+<p>Die wichtigsten Informationen auf einen Blick: Wer kann was über mich sehen bzw. herausfinden?</p>
+
+<table border=1 cellpadding=2 cellspacing=1>
+
+<tr>
+<th>&nbsp;</th>
+<th>Ich selbst</th>
+<th>Jeder Student</th>
+<th>Jeder Tutor</th>
+<th>Jeder Dozent</th>
+<th>Jeder Admin</th>
+<th>Mein Tutor</th>
+<th>Mein Dozent</th>
+<th>Mein Admin</th>
+<th>Root</th>
+</tr>
+
+<tr>
+<td bgcolor="#aaaaaa">Mich suchen</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+</tr>
+
+<tr>
+<td bgcolor="#aaaaaa">E-Mail-Adresse</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+</tr>
+
+<tr>
+<td bgcolor="#aaaaaa">Matrikelnummer</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+</tr>
+
+<tr>
+<td bgcolor="#aaaaaa">Studiengänge</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+</tr>
+
+<tr>
+<td bgcolor="#aaaaaa">Fachsemester</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+</tr>
+
+<tr>
+<td bgcolor="#aaaaaa">Stud.IP-Punkte</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+</tr>
+
+<tr>
+<td bgcolor="#aaaaaa">Teilname an einer best. VA</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+</tr>
+
+<tr>
+<td bgcolor="#aaaaaa">Liste aller besuchten VA</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+</tr>
+
+<tr>
+<td bgcolor="#aaaaaa">Tage seit letztem Login</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+</tr>
+
+<tr>
+<td bgcolor="#aaaaaa">Online-Status</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+</tr>
+
+<tr>
+<td bgcolor="#aaaaaa">Mein Name an meinen<br>Beiträgen, Dateien, ...</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+<td bgcolor="green">&nbsp;</td>
+</tr>
+
+<tr>
+<td bgcolor="#aaaaaa">Telefonnummer, Adresse, ...</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+<td bgcolor="yellow">&nbsp;</td>
+</tr>
+
+<tr>
+<td bgcolor="#aaaaaa">Welche Dateien, Beiträge,<br />
+News,...  habe ich gelesen?</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+<td bgcolor="red">&nbsp;</td>
+</tr>
+
+
+
+</table>
+
+<h2>Legende:</h2>
+
+<table border=1 cellpadding=1 cellspacing=2>
+<tr><td width=50 bgcolor="green">&nbsp;</td><td>Immer möglich.</td></tr>
+<tr><td width=50 bgcolor="yellow">&nbsp;</td><td>Mit meiner Zustimmung möglich.</td></tr>
+<tr><td width=50 bgcolor="red">&nbsp;</td><td>Nicht möglich.</td></tr>
+</table>
+
+</td></tr></table>
+</body>
+</html>
