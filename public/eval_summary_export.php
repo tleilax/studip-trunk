bf9889e3f8fba6ccdcaa56a4f4dac3165c0a1909
Stefan Suchi <suchi@data-quest.de>
2008-08-23 16:47:20 +0000

    fixes #230

--- public/eval_summary_export.php
+++ public/eval_summary_export.php
@@ -517,10 +517,6 @@ if ($db->next_record()) {
 	
 	$str = $FOP_SH_CALL." $tmp_path_export/evalsum".$db->f("eval_id").$auth->auth["uid"].".fo $pdffile";
 	
-	if ($JAVA_ENV_CALL){
-		$str = ". $JAVA_ENV_CALL && " . $str;
-	}
-	
 	$err = exec($str);
 	
 	if (file_exists($pdffile) && filesize($pdffile)) {
