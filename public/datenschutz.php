65fcb0ed3a2ddf6c6a4d0f1580946f3d8691cde5
Elmar Ludwig <ludwig@uni-osnabrueck.de>
2009-01-02 12:19:59 +0000

    fix layout of privacy guidelines, fixes #376

--- public/datenschutz.php
+++ public/datenschutz.php
@@ -1,210 +1,26 @@
 <?php
-require("lib/include/html_head.inc.php");
+/*
+ * datenschutz.php - privacy guidelines for Stud.IP
+ * Copyright (C) 2009  Elmar Ludwig
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ */
+
+page_open(array('sess' => 'Seminar_Session',
+                'auth' => 'Seminar_Default_Auth',
+                'perm' => 'Seminar_Perm',
+                'user' => 'Seminar_User'));
+
+$_language_path = init_i18n($_language);
+
+$CURRENT_PAGE = _('Erläuterungen zum Datenschutz');
+
+$template = $template_factory->open('privacy');
+$layout   = $template_factory->open('layouts/base_without_infobox');
+$template->set_layout($layout);
+
+echo $template->render();
 ?>
-<body>
-<table width=99% align=center bgcolor=white>
-<tr><td>
-<h1>Stud.IP - Erläuterungen zum Datenschutz im Überblick</h1>
-
-<p>Die wichtigsten Informationen auf einen Blick: Wer kann was über mich sehen bzw. herausfinden?</p>
-
-<table border=1 cellpadding=2 cellspacing=1>
-
-<tr>
-<th>&nbsp;</th>
-<th>Ich selbst</th>
-<th>Jeder Student</th>
-<th>Jeder Tutor</th>
-<th>Jeder Dozent</th>
-<th>Jeder Admin</th>
-<th>Mein Tutor</th>
-<th>Mein Dozent</th>
-<th>Mein Admin</th>
-<th>Root</th>
-</tr>
-
-<tr>
-<td bgcolor="#aaaaaa">Mich suchen</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-</tr>
-
-<tr>
-<td bgcolor="#aaaaaa">E-Mail-Adresse</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-</tr>
-
-<tr>
-<td bgcolor="#aaaaaa">Matrikelnummer</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-</tr>
-
-<tr>
-<td bgcolor="#aaaaaa">Studiengänge</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-</tr>
-
-<tr>
-<td bgcolor="#aaaaaa">Fachsemester</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-</tr>
-
-<tr>
-<td bgcolor="#aaaaaa">Stud.IP-Punkte</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-</tr>
-
-<tr>
-<td bgcolor="#aaaaaa">Teilname an einer best. VA</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-</tr>
-
-<tr>
-<td bgcolor="#aaaaaa">Liste aller besuchten VA</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-</tr>
-
-<tr>
-<td bgcolor="#aaaaaa">Tage seit letztem Login</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-</tr>
-
-<tr>
-<td bgcolor="#aaaaaa">Online-Status</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-</tr>
-
-<tr>
-<td bgcolor="#aaaaaa">Mein Name an meinen<br>Beiträgen, Dateien, ...</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-<td bgcolor="green">&nbsp;</td>
-</tr>
-
-<tr>
-<td bgcolor="#aaaaaa">Telefonnummer, Adresse, ...</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-<td bgcolor="yellow">&nbsp;</td>
-</tr>
-
-<tr>
-<td bgcolor="#aaaaaa">Welche Dateien, Beiträge,<br />
-News,...  habe ich gelesen?</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-<td bgcolor="red">&nbsp;</td>
-</tr>
-
-
-
-</table>
-
-<h2>Legende:</h2>
-
-<table border=1 cellpadding=1 cellspacing=2>
-<tr><td width=50 bgcolor="green">&nbsp;</td><td>Immer möglich.</td></tr>
-<tr><td width=50 bgcolor="yellow">&nbsp;</td><td>Mit meiner Zustimmung möglich.</td></tr>
-<tr><td width=50 bgcolor="red">&nbsp;</td><td>Nicht möglich.</td></tr>
-</table>
-
-</td></tr></table>
-</body>
-</html>
