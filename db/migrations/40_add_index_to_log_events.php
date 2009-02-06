bbff122df01e58a834374c2c8e6b0887edab9abd
Elmar Ludwig <ludwig@uni-osnabrueck.de>
2009-02-06 11:47:50 +0000

    add down() to migration (not required, but I need this for testing), re #441

--- db/migrations/40_add_index_to_log_events.php
+++ db/migrations/40_add_index_to_log_events.php
@@ -6,6 +6,10 @@ class AddIndexToLogEvents extends Migration {
 	}
 
 	function up() {
-		 DBManager::get()->query("ALTER TABLE `log_events` ADD INDEX ( `action_id` )");
+		 DBManager::get()->query("ALTER TABLE `log_events` ADD INDEX `action_id` ( `action_id` )");
+	}
+
+	function down() {
+		 DBManager::get()->query("ALTER TABLE `log_events` DROP INDEX `action_id`");
 	}
 }
