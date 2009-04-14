a01f24cfb675d70b4bf7eeeb3f4ed794db7ec2e9
Elmar Ludwig <ludwig@uni-osnabrueck.de>
2009-04-14 10:15:18 +0000

    don't check admission_turnout for closed courses, fixes #558

--- public/admin_seminare1.php
+++ public/admin_seminare1.php
@@ -588,7 +588,7 @@ if ($s_send) {
 	}
 
 	//Checks for admission turnout (only important if an admission is set)
-	if ($db->f("admission_type") && !LockRules::Check($s_id, 'admission_turnout')) {
+	if ($db->f("admission_type") != 0 && $db->f("admission_type") != 3 && !LockRules::Check($s_id, 'admission_turnout')) {
 		if ($turnout < 1) {
 			$msg .= "error§" . _("Diese Veranstaltung ist teilnahmebeschr&auml;nkt. Daher m&uuml;ssen Sie wenigstens einen Teilnehmenden zulassen!") . "§";
 			$run=FALSE;
