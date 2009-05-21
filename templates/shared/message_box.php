5875539b432cb1ff7c64142fdc888c0c65e7257f
luniki <github@luniki.de>
2009-05-21 14:23:31 +0000

    simplify messagebox, refs #594

--- templates/shared/message_box.php
+++ templates/shared/message_box.php
@@ -1,20 +1,26 @@
-<div id="m<?=$id?>" class="messagebox_outer">
+<div class="messagebox <?= $class ?>">
 	<div class="messagebox_buttons">
-		<? if(!empty($details)):?>
-		<a href="#" onclick="Effect.toggle('d<?=$id?>', 'blind'); $('min<?=$id?>').toggle(); $('max<?=$id?>').toggle(); return false;"><?=Assets::img('icons/maximize_inv.png', array('id'=> 'max'.$id, 'alt' => 'show details', 'title' => _('Details anzeigen')));?><?=Assets::img('icons/minimize_inv.png', array('id'=> 'min'.$id, 'alt' => 'close details', 'title' => _('Details ausblenden'), 'style'=>'display: none;'));?></a>
-		<?endif;?>
-		<a href="#" onclick="$('m<?=$id?>').fade(); return false;"><?=Assets::img('icons/cross_inv.png', array('alt' => 'close', 'title' => _('Nachrichtenbox schließen')));?></a>
+		<? if (!empty($details)) : ?>
+		<a href="#" onclick="Effect.toggle($(this).up().next('.messagebox_details'), 'blind'); $(this).select('img').each(Element.toggle); return false;">
+			<?= Assets::img('icons/maximize_inv.png', array('alt' => _('Details anzeigen'), 'title' => _('Details anzeigen'))) ?>
+			<?= Assets::img('icons/minimize_inv.png', array('alt' => _('Details ausblenden'), 'title' => _('Details ausblenden'), 'style'=>'display: none;')) ?>
+		</a>
+		<? endif ?>
+		<a href="#" onclick="$(this).up('.messagebox').fade(); return false;">
+			<?= Assets::img('icons/cross_inv.png', array('alt' => 'close', 'title' => _('Nachrichtenbox schließen'))) ?>
+		</a>
 	</div>
-	<div class="messagebox <?=$class?>">
-		<div class="messagebox_text"><?=$message?></div>
-		<? if(!empty($details)):?>
-			<div id="d<?=$id?>" style="display: none;">
-				<ul>
-				<?foreach($details as $li): ?>
-					<li><?=$li?></li>
-				<?endforeach;?>
-				</ul>
-			</div>
-		<? endif;?>
+
+	<div class="messagebox_text">
+		<?= $message ?>
 	</div>
+
+	<? if (!empty($details)) : ?>
+		<ul class="messagebox_details" style="display: none;">
+			<? foreach ($details as $li) : ?>
+				<li><?= $li ?></li>
+			<? endforeach ?>
+		</ul>
+	<? endif ?>
+
 </div>
