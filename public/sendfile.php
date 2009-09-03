552c1214a1c70fe9856b7749593a34e6e42f0700
André Noack <noackorama@gmail.com>
2009-09-03 00:10:16 +0000

    bugfix, aufgeräumt, geliftet fixes #704, re #82, re #483

--- public/sendfile.php
+++ public/sendfile.php
@@ -1,13 +1,14 @@
 <?php
-# Lifter002: TODO
-# Lifter007: TODO
-# Lifter003: TODO
+# Lifter002: TEST
+# Lifter007: TEST
+# Lifter003: TEST
 /**
 * sendfile.php
 *
 * Send files to the browser an does permchecks
 *
-* @author		Cornelis Kater <ckater@gwdg.de>, Suchi & Berg GmbH <info@data-quest.de>, André Noack André Noack <andre.noack@gmx.net>
+* @author		Cornelis Kater <ckater@gwdg.de>, Suchi & Berg GmbH <info@data-quest.de>,
+*               André Noack <andre.noack@gmx.net>
 * @version		$Id$
 * @access		public
 * @package		studip_core
@@ -34,125 +35,106 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // +---------------------------------------------------------------------------+
-if (ini_get('zlib.output_compression')){
-	@ini_set('zlib.output_compression','0');
-}
-if (!ini_get('allow_url_fopen')){
-	@ini_set('allow_url_fopen','1');
-}
+
 ob_start();
-page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Default_Auth", "perm" => "Seminar_Perm", "user" => "Seminar_User"));
+page_open(array("sess" => "Seminar_Session",
+				"auth" => "Seminar_Default_Auth",
+				"perm" => "Seminar_Perm",
+				"user" => "Seminar_User"));
 
-require_once ('config.inc.php');
-require_once ('lib/datei.inc.php');
-require_once ('lib/visual.inc.php');
-require_once 'lib/functions.php';
-require_once ('lib/classes/StudipLitList.class.php');
+require_once 'config.inc.php';
+require_once 'lib/datei.inc.php';
+require_once 'lib/classes/StudipLitList.class.php';
 
-$db=new DB_Seminar;
-$db2=new DB_Seminar;
+//basename() needs setlocale()
+init_i18n($_SESSION['_language']);
 
-if (isset($file_id))
-	$file_id = escapeshellcmd(basename($file_id));
+$db = DBManager::get();
+$file_id = escapeshellcmd(basename(Request::option('file_id')));
+$type = Request::int('type');
+if($type < 0 || $type > 7) $type = 0;
 
-//determine the type of the object we want to download a file from (only in type=0 mode!)
-$db->query("SELECT seminar_id AS object_id, filesize, range_id FROM dokumente WHERE dokument_id = '".$file_id."' ");
-$db->next_record();
-$object_id = $db->f('object_id');
+$document = new StudipDocument($file_id);
 
-$skip_check=FALSE;
-if ($type == 0 || $type == 6) {
+$object_id = $document->getValue('seminar_id');
+
+$no_access = true;
+
+//download from course or institute
+if ($object_id && in_array($type, array(0,6))){
 	$object_type = get_object_type($object_id);
+	//download from institute is always allowed
 	if ($object_type == "inst" || $object_type == "fak"){
-		$skip_check = TRUE;
+		$no_access = false;
 	}
-	$folder_tree =& TreeAbstract::GetInstance('StudipDocumentTree', array('range_id' => $object_id));
-	if (!$folder_tree->isDownloadFolder($db->f('range_id'), $GLOBALS['user']->id)) {
-		$no_access = true;
-		$skip_check = TRUE;
+	//download from course is allowed if course is free for all or user is participant
+	if($object_type == 'sem'){
+		$no_access = !$perm->have_studip_perm('user', $object_id);
+		$seminar = Seminar::GetInstance($object_id);
+		if($seminar->read_level == 0){
+			$no_access = false;
+		}
 	}
-	if(!check_protected_download($file_id)){
-		$no_access = true;
-		$skip_check = true;
+	//if allowed basically, check for closed folders and protected documents
+	if($no_access == false){
+		$folder_tree = TreeAbstract::GetInstance('StudipDocumentTree', array('range_id' => $object_id));
+		if (!$folder_tree->isDownloadFolder($document->getValue('range_id'), $GLOBALS['user']->id)) {
+			$no_access = true;
+		}
+		if(!check_protected_download($file_id)){
+			$no_access = true;
+		}
 	}
 }
-
-// Rechtecheck
-//////////////
-
+//download from archive, allowed if former participant
+if ($type == 1){
+	$archiv_seminar_id = $db->query("SELECT seminar_id FROM archiv WHERE archiv_file_id = ".$db->quote($file_id))
+							->fetchColumn();
+	$no_access = !archiv_check_perm($archiv_seminar_id);
+}
+//download bibliography
 if ($type == 5){
-	$skip_check = true;
-	if (!($range_id == $user->id) && !$perm->have_studip_perm('tutor', $range_id)){
-		$no_access = true;
-	} else {
+	$range_id = Request::option('range_id');
+	$list_id = Request::option('list_id');
+	if ($range_id == $user->id || $perm->have_studip_perm('tutor', $range_id)){
+		$no_access = false;
 		$the_data = StudipLitList::GetTabbedList($range_id, $list_id);
 	}
 }
-//permcheck
-if (($type != 2) && ($type != 3) && ($type != 4) && (!$skip_check)) { //if type 2, 3 or 4 we download from some tmp directory and skip permchecks
-	if (!$perm->have_perm("user")) {
-		if ($type == 0 || $type == 6) {
-			$db->query("SELECT Lesezugriff FROM seminare LEFT JOIN dokumente USING (seminar_id) WHERE dokument_id = '".$file_id."' ");
-			$db->next_record();
-			if ($db->f("Lesezugriff") != 0)
-				$no_access=TRUE;
-		} else {
-			$no_access=TRUE; //nobody darf nie an das Archiv
-		}
-	} elseif (!$perm->have_perm("root")) {
-		if ($type == 1) {
-			$db->query ("SELECT archiv.seminar_id FROM archiv LEFT JOIN archiv_user USING (seminar_id) WHERE archiv_file_id = '".$file_id."' ");
-			if (!$db->next_record()){
-				$no_access = TRUE;
-			} else {
-				$no_access = (archiv_check_perm($db->f("seminar_id")) ? FALSE : TRUE);
-			}
-		// File is attached to a Stud.IP-Message. Check if message belongs to current user.
-		} elseif ($type == 7) {
-			$db->query("SELECT dokument_id FROM dokumente INNER JOIN message_user ON message_id=range_id AND message_user.user_id='{$user->id}' WHERE dokument_id = '".$file_id."' ");
-			if (!$db->next_record()){
-				$no_access = TRUE;
-			}
-		} else {
-			$no_access = ($perm->have_studip_perm('user', $object_id) ? FALSE : TRUE);
-		}
-	}
+//download message attachment
+if ($type == 7){
+	$st = $db->prepare("SELECT dokument_id FROM dokumente 
+		INNER JOIN message_user ON message_id=range_id AND message_user.user_id = ?
+		WHERE dokument_id = ?");
+	$st->execute(array($user->id, $file_id));
+	$no_access = !($st->fetchColumn());
+}
+//download ad hoc created files, always allowed
+if(in_array($type, array(2,3,4))){
+	$no_access = false;
 }
 
-//Nachricht bei verbotenem Download
+//if download not allowed throw exception to terminate script
 if ($no_access) {
-	if ($type)
-		$add_msg= sprintf(_("%sZur&uuml;ck%s zum Archiv"), '<a href="archiv.php?back=TRUE"><b>&nbsp;', '</b></a>') . '<br>&nbsp;' ;
-	else
-		$add_msg= sprintf(_("%sZur&uuml;ck%s zum Downloadbereich"), '<a href="folder.php?back=TRUE"><b>&nbsp;', '</b></a>') . '<br>&nbsp;' ;
-
-	// Start of Output
-	include ('lib/include/html_head.inc.php'); // Output of html head
-	include ('lib/include/header.php');   // Output of Stud.IP head
-
-	parse_window('error§' . _("Sie haben keine Zugriffsberechtigung f&uuml;r diesen Download!"), '§', _("Download nicht m&ouml;glich"), $add_msg);
-	include ('lib/include/html_end.inc.php');
-	page_close();
-	die;
+	throw new Studip_AccessDeniedException(_("Sie haben keine Zugriffsberechtigung für diesen Download!"));
 }
-
 
 switch ($type) {
 	//We want to download from the archive (this mode performs perm checks)
 	case 1:
-		$path_file=$ARCHIV_PATH."/".$file_id;
+		$path_file = $ARCHIV_PATH."/".$file_id;
 	break;
 	//We want to download from the tmp/export-folder
 	case 2:
-		$path_file=$TMP_PATH."/export/".$file_id;
+		$path_file = $TMP_PATH."/export/".$file_id;
 	break;
 	//We want to download an XSL-Script
 	case 3:
-		$path_file=$STUDIP_BASE_PATH . "/" . $PATH_EXPORT . "/".$file_id;
+		$path_file = $STUDIP_BASE_PATH . "/" . $PATH_EXPORT . "/".$file_id;
 	break;
 	//we want to download from the studip-tmp folder (this mode performs perm checks)
 	case 4:
-		$path_file=$TMP_PATH . "/".$file_id;
+		$path_file = $TMP_PATH . "/".$file_id;
 	break;
 	//download lit list as tab delimited text file
 	case 5:
@@ -173,9 +155,9 @@ switch ($type) {
 }
 
 //replace bad charakters to avoid problems when saving the file
-$file_name = prepareFilename(basename(stripslashes(rawurldecode($file_name))));
+$file_name = prepareFilename(basename(Request::get('file_name')));
 
-if ($zip && is_file($path_file)) {
+if (Request::int('zip') && is_file($path_file)) {
 	$tmp_id = md5(uniqid("suppe"));
 	$zip_path_file = "$TMP_PATH/$tmp_id";
 	$tmp_file_name = escapeshellcmd("$TMP_PATH/$file_name");
@@ -183,10 +165,7 @@ if ($zip && is_file($path_file)) {
 	if (create_zip_from_file( $tmp_file_name, "$zip_path_file.zip") === false) {
 		@unlink($zip_path_file . '.zip');
 		@unlink($tmp_file_name);
-		parse_window('error§' . _("Fehler beim Erstellen des Zip-Archivs!"), '§', _("Download nicht m&ouml;glich"), $add_msg);
-		include ('lib/include/html_end.inc.php');
-		page_close();
-		die;
+		throw new Exception(_("Fehler beim Erstellen des Zip-Archivs!"));
 	} else {
 		$file_name = $file_name . ".zip";
 		$path_file = $zip_path_file . ".zip";
@@ -194,161 +173,105 @@ if ($zip && is_file($path_file)) {
 	}
 }
 
-
-
-if ($force_download) {
-	$content_type="application/octet-stream";
-	$content_disposition="attachment";
+if (Request::int('force_download')) {
+	$content_type = "application/octet-stream";
+	$content_disposition = "attachment";
 } else {
+	$content_disposition = "inline";
 	switch (strtolower(getFileExtension ($file_name))) {
 		case "txt":
-			$content_type="text/plain";
-			$content_disposition="inline";
+			$content_type = "text/plain";
 		break;
 		case "css":
-			$content_type="text/css";
-			$content_disposition="inline";
+			$content_type = "text/css";
 		break;
 		case "gif":
-			$content_type="image/gif";
-			$content_disposition="inline";
+			$content_type = "image/gif";
 		break;
 		case "jpeg":
-			$content_type="image/jpeg";
-			$content_disposition="inline";
-		break;
 		case "jpg":
-			$content_type="image/jpeg";
-			$content_disposition="inline";
-		break;
 		case "jpe":
-			$content_type="image/jpeg";
-			$content_disposition="inline";
+			$content_type = "image/jpeg";
 		break;
 		case "bmp":
-			$content_type="image/x-ms-bmp";
-			$content_disposition="inline";
+			$content_type = "image/x-ms-bmp";
 		break;
 		case "png":
-			$content_type="image/png";
-			$content_disposition="inline";
+			$content_type = "image/png";
 		break;
 		case "wav":
-			$content_type="audio/x-wav";
-			$content_disposition="inline";
+			$content_type = "audio/x-wav";
 		break;
 		case "ra":
-			$content_type="application/x-pn-realaudio";
-			$content_disposition="inline";
+			$content_type = "application/x-pn-realaudio";
 		break;
 		case "ram":
-			$content_type="application/x-pn-realaudio";
-			$content_disposition="inline";
+			$content_type = "application/x-pn-realaudio";
 		break;
 		case "mpeg":
-			$content_type="video/mpeg";
-			$content_disposition="inline";
-		break;
 		case "mpg":
-			$content_type="video/mpeg";
-			$content_disposition="inline";
-		break;
 		case "mpe":
-			$content_type="video/mpeg";
-			$content_disposition="inline";
+			$content_type = "video/mpeg";
 		break;
 		case "qt":
-			$content_type="video/quicktime";
-			$content_disposition="inline";
-		break;
 		case "mov":
-			$content_type="video/quicktime";
-			$content_disposition="inline";
+			$content_type ="video/quicktime";
 		break;
 		case "avi":
-			$content_type="video/x-msvideo";
-			$content_disposition="inline";
+			$content_type = "video/x-msvideo";
 		break;
 		case "rtf":
-			$content_type="application/rtf";
-			$content_disposition="inline";
+			$content_type = "application/rtf";
 		break;
 		case "pdf":
-			$content_type="application/pdf";
-			$content_disposition="inline";
+			$content_type = "application/pdf";
 		break;
 		case "doc":
-			$content_type="application/msword";
-			$content_disposition="inline";
+			$content_type = "application/msword";
 		break;
 		case "xls":
-			$content_type="application/ms-excel";
-			$content_disposition="inline";
+			$content_type = "application/ms-excel";
 		break;
 		case "ppt":
-			$content_type="application/ms-powerpoint";
-			$content_disposition="inline";
+			$content_type = "application/ms-powerpoint";
 		break;
 		case "tgz":
 		case "gz":
-			$content_type="application/x-gzip";
-			$content_disposition="inline";
+			$content_type = "application/x-gzip";
 		break;
 		case "bz2":
-			$content_type="application/x-bzip2";
-			$content_disposition="inline";
+			$content_type = "application/x-bzip2";
 		break;
 		case "zip":
-			$content_type="application/zip";
-			$content_disposition="inline";
+			$content_type = "application/zip";
 		break;
 		case "swf":
-			$content_type="application/x-shockwave-flash";
-			$content_disposition="inline";
+			$content_type = "application/x-shockwave-flash";
 		break;
 		case "csv":
-			$content_type="text/csv";
-			$content_disposition="inline";
+			$content_type = "text/csv";
 		break;
 		default:
-			$content_type="application/octet-stream";
-			$content_disposition="inline";
+			$content_type = "application/octet-stream";
 		break;
-		}
-	}
-
-//override disposition, if available
-if ($disposition)
-	$content_disposition=$disposition;
-
-
-// Check bei verlinkten Dateien ob sie erreichbar sind
-if ($type == 6) {
-	$link_data = parse_link($path_file);
-	if (!($link_data['HTTP/1.0 200 OK'] || $link_data['HTTP/1.1 200 OK'])) {
-		include ('lib/include/html_head.inc.php'); // Output of html head
-		include ('lib/include/header.php');   // Output of Stud.IP head
-		$add_msg= sprintf(_("%sZur&uuml;ck%s zum Downloadbereich"), '<a href="folder.php?back=TRUE"><b>&nbsp;', '</b></a>') . '<br>&nbsp;' ;
-		parse_window('error§' . _("Diese Datei wird von einem externen Server geladen und ist dort momentan nicht erreichbar!"), '§', _("Download nicht m&ouml;glich"), $add_msg);
-		include ('lib/include/html_end.inc.php');
-		page_close();
-		die;
 	}
 }
 
-
-//Datei verschicken
+// check if linked file is obtainable
 if ($type == 6) {
-	$db->query("SELECT filesize FROM dokumente WHERE dokument_id = '$file_id'");
-	if ($db->next_record())
-		$filesize = $db->f("filesize");
-	if (!$filesize || $filesize==0)
-		$filesize = FALSE;
+	$link_data = parse_link($path_file);
+	if ($link_data['response_code'] != 200) {
+		throw new Exception(_("Diese Datei wird von einem externen Server geladen und ist dort momentan nicht erreichbar!"));
+	}
+	$filesize = $link_data['Content-Length'];
+	if (!$filesize) $filesize = false;
 } elseif ($type != 5){
 	$filesize = @filesize($path_file);
 } else {
 	$filesize = strlen($the_data);
 }
+// close session, download will mostly be a parallel action
+page_close();
 
 header("Expires: Mon, 12 Dec 2001 08:00:00 GMT");
 header("Last-Modified: " . gmdate ("D, d M Y H:i:s") . " GMT");
@@ -369,20 +292,16 @@ header("Content-Disposition: $content_disposition; filename=\"$file_name\"");
 ob_end_flush();
 
 if ($type != 5){
-	@readfile_chunked($path_file);
-	if(!in_array($type, array(1,2,3,4,5))){
+	@readfile($path_file);
+	if(in_array($type, array(0,6))){
 		TrackAccess($file_id, 'dokument');
 	}
 } else {
 	echo $the_data;
 }
 
-
 //remove temporary file after zipping
-if ($zip || $type == 4) {
+if (Request::int('zip') || $type == 4) {
 	@unlink($path_file);
 }
-
-// Save data back to database.
-page_close();
 ?>
