8946285bf993ac06b05c335e8e8f8c4d6a145c65
Till Glöggler <tgloeggl@uos.de>
2009-11-02 07:48:40 +0000

    revert [13723], which reverted [12496], fixes #827, re #68, fixes it with enabled Lifter001

--- public/show_bereich.php
+++ public/show_bereich.php
@@ -1,5 +1,5 @@
 <?php
-# Lifter001: TODO
+# Lifter001: TEST
 # Lifter002: TODO
 # Lifter007: TODO
 # Lifter003: TODO
@@ -31,34 +31,32 @@ require_once('lib/visual.inc.php');
 require_once 'lib/classes/SemBrowse.class.php';
 
 
-	$sess->register ("show_bereich_data");
 	$db=new DB_Seminar;
 	$intro_text = $head_text = '';
 	if ($id){
-		$show_bereich_data["id"]=$id;
-		$show_bereich_data['level'] = $level;
+		URLHelper::bindLinkParam('id',$id);
+		URLHelper::bindLinkParam('level',$level);
 	}
 
-	if (!$_REQUEST['group_by']){
-		$_REQUEST['group_by'] = 0;
-	}
-	unset($_REQUEST['level']);
-	$save_me = $sem_browse_data;
-	unset($sem_browse_data);
+	$level = Request::get('level');
+	$id = Request::get('id');
+
+	$group_by = Request::int('group_by', 0);
+
 	$sem_browse_obj = new SemBrowse();
 	$sem_browse_obj->sem_browse_data['default_sem'] = "all";
 	$sem_browse_obj->sem_number = false;
 	$sem_browse_obj->target_url="details.php";	//teilt der nachfolgenden Include mit, wo sie die Leute hinschicken soll
 	$sem_browse_obj->target_id="sem_id"; 		//teilt der nachfolgenden Include mit, wie die id die &uuml;bergeben wird, bezeichnet werden soll
-	$sem_browse_obj->sem_browse_data['level'] = $show_bereich_data['level'];
-	switch ($show_bereich_data['level']) {
+	$sem_browse_obj->sem_browse_data['level'] = $level;
+	switch ($level) {
 		case "sbb":
 			$the_tree =& TreeAbstract::GetInstance("StudipSemTree", array('visible_only' => !$GLOBALS['perm']->have_perm(get_config('SEM_VISIBILITY_PERM'))));
 			$bereich_typ = _("Studienbereich");
 			$head_text = _("Übersicht aller Veranstaltungen eines Studienbereichs");
 			$intro_text = sprintf(_("Alle Veranstaltungen, die dem Studienbereich: <br><b>%s</b><br> zugeordnet wurden."),
-							htmlReady($the_tree->getShortPath($show_bereich_data["id"])));
-			$sem_ids = $the_tree->getSemIds($show_bereich_data["id"],false);
+							htmlReady($the_tree->getShortPath($id)));
+			$sem_ids = $the_tree->getSemIds($id, false);
 			if (is_array($sem_ids)){
 				$sem_browse_obj->sem_browse_data['search_result'] = array_flip($sem_ids);
 			} else {
@@ -66,17 +64,17 @@ require_once 'lib/classes/SemBrowse.class.php';
 			}
 			$sem_browse_obj->show_result = true;
 			$sem_browse_obj->sem_browse_data['sset'] = false;
-			$sem_browse_obj->sem_browse_data['start_item_id'] = $show_bereich_data["id"];
+			$sem_browse_obj->sem_browse_data['start_item_id'] = $id;
 			break;
 		case "s":
 			$bereich_typ=_("Einrichtung");
-			$db->query("SELECT Name FROM Institute WHERE Institut_id='".$show_bereich_data["id"]."'");
+			$db->query("SELECT Name FROM Institute WHERE Institut_id='".$id."'");
 			$db->next_record();
 			$head_text = _("Übersicht aller Veranstaltungen einer Einrichtung");
 			$intro_text = sprintf(_("Alle Veranstaltungen der Einrichtung <b>%s</b>"), htmlReady($db->f("Name")));
 			$db->query("SELECT seminar_inst.seminar_id FROM seminar_inst
 			LEFT JOIN seminare ON (seminar_inst.seminar_id=seminare.Seminar_id)
-			WHERE seminar_inst.Institut_id='".$show_bereich_data["id"]."'" . (!$GLOBALS['perm']->have_perm(get_config('SEM_VISIBILITY_PERM')) ? " AND seminare.visible='1'" : ""));
+			WHERE seminar_inst.Institut_id='".$id."'" . (!$GLOBALS['perm']->have_perm(get_config('SEM_VISIBILITY_PERM')) ? " AND seminare.visible='1'" : ""));
 
 			$sem_browse_obj->sem_browse_data['search_result'] = array();
 			while ($db->next_record()){
@@ -97,7 +95,7 @@ if (isset($_REQUEST['send_excel'])){
 ob_end_flush();
 // Start of Output
 $HELP_KEYWORD="Basis.Informationsseite";
-$CURRENT_PAGE = ($show_bereich_data['level'] == "s" ? $SessSemName["header_line"]." - " : "").$head_text;
+$CURRENT_PAGE = ($level == "s" ? $SessSemName["header_line"]." - " : "").$head_text;
 if (($SessSemName[1]) && ($SessSemName["class"] == "inst")) {
 	Navigation::activateItem('/course/main/courses');
 }
@@ -117,13 +115,13 @@ $sem_browse_obj->print_result();
 <?
 $goup_by_links = "";
 for ($i = 0; $i < count($sem_browse_obj->group_by_fields); ++$i){
-	if($sem_browse_data['group_by'] != $i){
-		$group_by_links .= "<a href=\"$PHP_SELF?group_by=$i\"><img src=\"".$GLOBALS['ASSETS_URL']."images/blank.gif\" width=\"10\" height=\"20\" border=\"0\">";
+	if($group_by != $i){
+		$group_by_links .= "<a href=\"".URLHelper::getLink("",array('group_by'=>$i))."\"><img src=\"".$GLOBALS['ASSETS_URL']."images/blank.gif\" width=\"10\" height=\"20\" border=\"0\">";
 	} else {
 		$group_by_links .= "<img src=\"".$GLOBALS['ASSETS_URL']."images/forumrot.gif\" border=\"0\" align=\"bottom\">";
 	}
 	$group_by_links .= "&nbsp;" . $sem_browse_obj->group_by_fields[$i]['name'];
-	if($sem_browse_data['group_by'] != $i){
+	if($group_by != $i){
 		$group_by_links .= "</a>";
 	}
 	$group_by_links .= "<br>";
@@ -132,26 +130,26 @@ $infobox[] = 	array(	"kategorie" => _("Anzeige gruppieren:"),
 						"eintrag" => array(array(	"icon" => "blank.gif",
 													"text" => $group_by_links))
 				);
-if (($EXPORT_ENABLE) AND ($show_bereich_data['level'] == "s") AND ($perm->have_perm("tutor")))
+if (($EXPORT_ENABLE) AND ($level == "s") AND ($perm->have_perm("tutor")))
 {
 	include_once($PATH_EXPORT . "/export_linking_func.inc.php");
 	$infobox[] = 	array(	"kategorie" => _("Daten ausgeben:"),
 							"eintrag" => array(array(	"icon" => "blank.gif",
 														"text" => export_link($SessSemName[1], "veranstaltung", $SessSemName[0])),
 												array( 'icon' => 'blank.gif',
-														"text" => '<a href="'.$PHP_SELF.'?send_excel=1&group_by='.(int)$_REQUEST['group_by'].'"><img src="'.$GLOBALS['ASSETS_URL'].'images/xls-icon.gif" align="absbottom" border="0">&nbsp;'._("Download als Excel Tabelle").'</a>')
+														"text" => '<a href="'.$PHP_SELF.'?send_excel=1&group_by='.(int)$group_by.'"><img src="'.$GLOBALS['ASSETS_URL'].'images/xls-icon.gif" align="absbottom" border="0">&nbsp;'._("Download als Excel Tabelle").'</a>')
 
 														)
 					);
 }
-if (($EXPORT_ENABLE) AND ($show_bereich_data['level'] == "sbb") AND ($perm->have_perm("tutor")))
+if (($EXPORT_ENABLE) AND ($level == "sbb") AND ($perm->have_perm("tutor")))
 {
 	include_once($PATH_EXPORT . "/export_linking_func.inc.php");
 	$infobox[] = 	array(	"kategorie" => _("Daten ausgeben:"),
 							"eintrag" => array(array(	"icon" => "blank.gif",
-														"text" => export_link($show_bereich_data["id"], "veranstaltung", $show_bereich_data["id"])),
+														"text" => export_link($id, "veranstaltung", $id)),
 												array( 'icon' => 'blank.gif',
-														"text" => '<a href="'.$PHP_SELF.'?send_excel=1&group_by='.(int)$_REQUEST['group_by'].'"><img src="'.$GLOBALS['ASSETS_URL'].'images/xls-icon.gif" align="absbottom" border="0">&nbsp;'._("Download als Excel Tabelle").'</a>')
+														"text" => '<a href="'.$PHP_SELF.'?send_excel=1&group_by='.(int)$group_by.'"><img src="'.$GLOBALS['ASSETS_URL'].'images/xls-icon.gif" align="absbottom" border="0">&nbsp;'._("Download als Excel Tabelle").'</a>')
 
 														)
 					);
@@ -166,7 +164,5 @@ print_infobox ($infobox,"browse.jpg");
 </table>
 
 <?
-$sem_browse_data = $save_me;
 include ('lib/include/html_end.inc.php');
-page_close()
-?>
+page_close();
