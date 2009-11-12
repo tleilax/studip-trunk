01c837036d7cc9f037250b51334f5edad8310d8b
Elmar Ludwig <ludwig@uni-osnabrueck.de>
2009-11-12 12:38:09 +0000

    don't change ID of role management plugin, fixes #846

--- db/migrations/41_step_00157_rolemanagment.php
+++ db/migrations/41_step_00157_rolemanagment.php
@@ -8,17 +8,15 @@ class Step00157RoleManagment extends Migration
 
     function up ()
     {
-        DBManager::get()->exec("DELETE FROM `plugins` WHERE `pluginclassname`='de_studip_core_UserManagementPlugin'");
-    	DBManager::get()->exec("UPDATE `plugins` SET `pluginid` = '2', `pluginclassname` = 'RoleManagementPlugin', `navigationpos` = '1' WHERE `pluginclassname`='de_studip_core_RoleManagementPlugin'");
-    	DBManager::get()->exec("UPDATE `plugins_activated` SET `pluginid`='2' WHERE `pluginid`='3'");
-        DBManager::get()->exec("DELETE FROM `roles_plugins` WHERE `pluginid`='3'");
-	}
+        DBManager::get()->exec("UPDATE plugins SET pluginclassname = 'RoleManagementPlugin' WHERE pluginclassname = 'de_studip_core_RoleManagementPlugin'");
+        DBManager::get()->exec("DELETE FROM plugins WHERE pluginclassname = 'de_studip_core_UserManagementPlugin'");
+        DBManager::get()->exec("DELETE FROM plugins_activated WHERE pluginid = 2");
+        DBManager::get()->exec("DELETE FROM roles_plugins WHERE pluginid = 2");
+    }
 
     function down ()
     {
-     	DBManager::get()->exec("UPDATE `plugins` SET `pluginid` = '3', `pluginclassname` = 'de_studip_core_RoleManagementPlugin', `navigationpos` = '2' WHERE `pluginid`=2");
-    	DBManager::get()->exec("INSERT INTO `plugins` (`pluginid`, `pluginclassname`, `pluginpath`, `pluginname`, `plugindesc`, `plugintype`, `enabled`, `navigationpos`, `dependentonid`) VALUES (2, 'de_studip_core_UserManagementPlugin', 'core', 'UserManagement', '', 'Core', 'yes', 1, 1)");
-    	DBManager::get()->exec("UPDATE `plugins_activated` SET `pluginid`='3' WHERE `pluginid`='2'");
+        DBManager::get()->exec("UPDATE plugins SET pluginclassname = 'de_studip_core_RoleManagementPlugin' WHERE pluginclassname = 'RoleManagementPlugin'");
     }
 }
 ?>
