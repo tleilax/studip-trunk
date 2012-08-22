941ff2beff08a9783c94bab9765fa0df06ba95cd
hackl <hackl>
2012-08-22 07:28:29 +0000

    Waiting list with position information

--- lib/classes/admission/WaitingList.class.php
+++ lib/classes/admission/WaitingList.class.php
@@ -20,41 +20,40 @@ class WaitingList
     // --- OPERATIONS ---
 
     /**
-     * Adds the user with the given ID to the waiting list.
+     * Adds the user with the given ID to the waiting list. If no explicit
+     * position is given, the user is inserted at the bottom of the waiting
+     * list.
      *
      * @param  String userId
      * @return WaitingList
      */
-    public static function addUser($userId, $courseSetId, $courseId) {
-        $stmt = DBManager::get()->prepare("INSERT INTO `waitinglist` 
-            (`set_id`, `seminar_id`, `user_id`, `mkdate`) 
-            VALUES (?, ?, ?, ?)");
-        return $stmt->execute(array($courseSetId, $courseId, $userId, time()));
+    public static function addUser($userId, $courseId, $position=0) {
+        if ($position==0) {
+            $query = "INSERT INTO `waitinglist` 
+                (`seminar_id`, `user_id`, `position`, `mkdate`) ( 
+                SELECT ?, ?, MAX(`position`)+1, ? FROM `waitinglist` 
+                WHERE `seminar_id`=?)";
+            $parameters = array($courseId, $userId, time(), $courseId);
+        } else {
+            $query = "INSERT INTO `waitinglist` 
+                (`seminar_id`, `user_id`, `position`, `mkdate`) 
+                VALUES (?, ?, ?, ?)";
+            $parameters = array($courseId, $userId, $position, time());
+        }
+        $stmt = DBManager::get()->prepare($query);
+        return $stmt->execute($parameters);
     }
 
     /**
-     * Gets the waiting list for a single course.
+     * Gets the waiting list for the given course.
      * 
      * @param  String courseId
      * @return Array
      */
-    public static function getCourseWaitingList($courseId) {
+    public static function getWaitingList($courseId) {
         $stmt = DBManager::get()->prepare("SELECT * FROM `waitinglist` 
             WHERE `seminar_id`=? ORDER BY `mkdate` ASC");
         $stmt->execute(array($courseId));
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    /**
-     * Gets the waiting list for a CourseSet.
-     * 
-     * @param  String courseSetId
-     * @return Array
-     */
-    public static function getCourseSetWaitingList($courseSetId) {
-        $stmt = DBManager::get()->prepare("SELECT * FROM `waitinglist` 
-            WHERE `set_id`=? ORDER BY `mkdate` ASC");
-        $stmt->execute(array($courseSetId));
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
@@ -64,19 +63,11 @@ class WaitingList
      * @param  String userId
      * @return WaitingList
      */
-    public static function removeUser($userId, $courseSetId, $courseId) {
-        $query = "DELETE FROM `waitinglist` WHERE `user_id`=?";
-        $parameters = array($userId);
-        if ($courseSetId) {
-            $query .= " AND `set_id`=?";
-            $parameters[] = $courseSetId;
-        }
-        if ($courseId) {
-            $query .= " AND `seminar_id`=?";
-            $parameters[] = $courseId;
-        }
+    public static function removeUser($userId, $courseId) {
+        $query = "DELETE FROM `waitinglist` 
+            WHERE `user_id`=? AND `seminar_id`=?";
         $stmt = DBManager::get()->prepare($query);
-        return $stmt->execute($parameters);
+        return $stmt->execute(array($userId, $courseid));
     }
 
 } /* end of class WaitingList */
