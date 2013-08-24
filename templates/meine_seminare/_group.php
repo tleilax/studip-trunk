6dcb4060a7ead02b80f95e28d43c6aa149eda369
Jan-Hendrik Willms <tleilax+github@gmail.com>
2013-08-24 11:04:56 +0000

    tables: tidy up template

--- templates/meine_seminare/_group.php
+++ templates/meine_seminare/_group.php
@@ -2,36 +2,34 @@
 # Lifter010: TODO
 sort_groups($group_field, $groups);
 $group_names = get_group_names($group_field, $groups);
-foreach ($groups as $group_id => $group_members) {
-    if ($group_field == 'not_grouped') {
+$_my_sem_open['not_grouped'] = $group_field === 'not_grouped';
 
-        $_my_sem_open['not_grouped'] = true;
-
-    } else {
+foreach ($groups as $group_id => $group_members):
+    if (!$_my_sem_open['not_grouped']): 
         $last_modified = check_group_new($group_members, $my_obj);
-        ?>
-    <tbody class="toggleable">
+?>
+    <tbody class="toggleable <? if (!isset($_my_sem_open[$group_id])) echo 'toggled'; ?>">
         <tr>
             <th nowrap colspan="2">
-                <?= Assets::img("blank.gif", array("size" => "1@20")) ?>
                 <? if (isset($_my_sem_open[$group_id])): ?>
-                    <a class="toggle-switch" href="<?= URLHelper::getLink('#' . $group_id, array('view' => $view, 'close_my_sem' => $group_id)) ?>"
-                       <?= tooltip(_("Gruppierung schließen"), true) ?>>
+                    <a class="toggle-switch"
+                       href="<?= URLHelper::getLink('#' . $group_id, array('view' => $view, 'close_my_sem' => $group_id)) ?>"
+                       title="<?= _('Gruppierung schließen') ?>">
 
-                        <?= _("Gruppierung schließen") ?>
+                        <?= _('Gruppierung schließen') ?>
                     </a>
                 <? else: ?>
                     <a class="toggle-switch"
                        href="<?= URLHelper::getLink('#' . $group_id, array('view' => $view, 'open_my_sem' => $group_id)) ?>"
-                       <?= tooltip(_("Gruppierung öffnen"), true) ?>>
+                       title="<?= _('Gruppierung öffnen') ?>">
 
-                        <?= _("Gruppierung öffnen") ?>
+                        <?= _('Gruppierung öffnen') ?>
                     </a>
                 <? endif; ?>
 				<?
                 if (is_array($group_names[$group_id])) {
                     $group_name = $group_names[$group_id][1]
-                                  ? $group_names[$group_id][1] . " > " . $group_names[$group_id][0]
+                                  ? $group_names[$group_id][1] . ' > ' . $group_names[$group_id][0]
                                   : $group_names[$group_id][0];
                 } else {
                     $group_name = $group_names[$group_id];
@@ -39,22 +37,21 @@ foreach ($groups as $group_id => $group_members) {
                 ?>
 
             </th>
-            <th align="left" valign="middle"
-                colspan="<?= $view == 'ext' ? 3 : 1 ?>">
+            <th colspan="<?= $view == 'ext' ? 3 : 1 ?>">
 
-                <a class="tree" <?= $_my_sem_open[$group_id] ? 'style="font-weight:bold"' : '' ?>
+                <a class="tree"
                    name="<?= $group_id ?>"
                    href="<?= URLHelper::getLink('#' . $group_id, array('view' => $view, ($_my_sem_open[$group_id] ? 'close_my_sem' : 'open_my_sem' ) => $group_id)) ?>"
-                   <?= tooltip(_("Gruppierung öffnen"), true) ?>>
+                   title="<?= _('Gruppierung öffnen') ?>">
 
                     <?= htmlReady($group_field == "sem_tree_id" ? $group_names[$group_id][0] : $group_names[$group_id]) ?>
                 </a>
 
             <? if ($group_field == "sem_tree_id"): ?>
                 <br>
-                <span style="font-size:0.8em">
+                <small>
                     <sup><?= htmlReady($group_name) ?></sup>
-                </span>
+                </small>
             <? endif; ?>
             </th>
 
@@ -64,13 +61,9 @@ foreach ($groups as $group_id => $group_members) {
             <? endif; ?>
             </th>
         </tr>
-    <?
-    }
-
-    if (isset($_my_sem_open[$group_id])) {
-        echo $this->render_partial("meine_seminare/_course", compact("group_members"));
-    }
-    ?>
+    <? endif; ?>
+    <? if (isset($_my_sem_open[$group_id])): ?>
+        <?= $this->render_partial('meine_seminare/_course', compact('group_members')) ?>
+    <? endif; ?>
     </tbody>
-    <?
-}
+<? endforeach; ?>
