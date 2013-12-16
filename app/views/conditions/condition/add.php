1b215b622b634d4e0852bca364fb324defac168a
hackl <hackl>
2013-12-16 13:38:52 +0000

    Rename Conditions to Userfilter

--- app/views/conditions/condition/add.php
+++
@@ -1,7 +0,0 @@
-<div class="condition" id="<?= $condition->getId() ?>">
-    <?= $via_ajax ? studip_utf8encode($condition->toString()) : $condition->toString() ?>
-    <input type="hidden" name="conditions[]" value="<?= $via_ajax? studip_utf8encode(htmlentities(serialize($condition))) : htmlentities(serialize($condition)) ?>"/>
-    <a href="#" onclick="return STUDIP.Conditions.removeConditionField($(this).parent())"
-            class="conditionfield_delete">
-        <?= Assets::img('icons/16/blue/trash.png'); ?></a>
-</div>
