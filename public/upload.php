e2f9d807774f37216b4cd099a0b40fae01733bf1
Robert Costa <zabbarob@gmail.com>
2013-12-23 17:46:08 +0000

    cleaned up session management in upload.php and removed debug code

--- public/upload.php
+++ public/upload.php
@@ -16,40 +16,15 @@
  * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL version 2
  * @category    Stud.IP
  */
-//require_once('bootstrap.php');
-//include ('lib/seminar_open.php'); // initialise Stud.IP-Session
-
-require '../lib/bootstrap.php';
-
-ob_start();
-page_open(array("sess" => "Seminar_Session",
-    "auth" => "Seminar_Auth",
-    "perm" => "Seminar_Perm", "" .
-    "user" => "Seminar_User"));
-
-include ('lib/seminar_open.php'); // initialise Stud.IP-Session
-
-checkObject();
-checkObjectModule('documents');
-object_set_visit_module('documents');
-
-// -- here you have to put initialisations for the current page
-require_once('lib/datei.inc.php');
-require_once('lib/msg.inc.php');
-require_once('lib/visual.inc.php');
-require_once('config.inc.php');
-require_once 'lib/functions.php';
-require_once('lib/classes/StudipDocumentTree.class.php');
-require_once 'lib/raumzeit/Issue.class.php';
-
-
-error_log('upload.php');
-error_log($user->id);
-error_log(print_r($GLOBALS['auth'], TRUE));
-
+require_once('../lib/bootstrap.php');
 require_once('../lib/utils.php');
-post_file();
-error_log('upload.php EXIT');
+
+page_open(array("sess" => "Seminar_Session",  // init session management
+                "auth" => "Seminar_Auth",
+                "perm" => "Seminar_Perm",
+                "user" => "Seminar_User"));
+post_file();                                  // upload files
+page_close();                                 // stop session management
 
 /**
  * Handle folder creation and file upload requests.
@@ -64,33 +39,27 @@ error_log('upload.php EXIT');
  * or to update a folder's description.
  */
 function post_file() {
-    error_log('post_file()');
     Utils\verifyPostRequest();
     Utils\verifyPermission('autor');  // minimum permission level for uploading
     CSRFProtection::verifyUnsafeRequest();
-    error_log('verified');
 
     // store uploaded files as StudIP documents
     $folder_id = post_folder();  // StudIP document folder for storing files
     $response = array(); // data for HTTP response
-    error_log('foreach');
     foreach (Utils\FILES() as $file) {
         try {
-            error_log('file');
             $newfile = Utils\uploadFile($file, $folder_id);
             $response['files'][] = Array(
                 'name' => utf8_encode($newfile['filename']),
                 'type' => $file['type'],
                 'url' => GetDownloadLink($newfile->getId(), $newfile['filename']));
         } catch (AccessDeniedException $e) { // creation of Stud.IP doc failed
-            error_log('catch');
             $response['files'][] = Array(
                 'name' => $file['name'],
                 'type' => $file['type'],
                 'error' => $e->getMessage());
         }
     }
-    error_log('sendAsJson');
     Utils\sendAsJson($response);
 }
 
@@ -112,7 +81,6 @@ function post_file() {
  * TODO if no folder is given, return root folder ID
  */
 function post_folder() {
-    error_log('post_folder');
     // TODO change this, so root folder is returned if folder not set
     return Utils\getFolderId(Utils\utf8POST('folder', TRUE),
                              Utils\utf8POST('folder_description'));
