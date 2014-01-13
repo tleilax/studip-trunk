1dc97094b4bcf5d58d51c1a9be2182f75366f8c8
Robert Costa <zabbarob@gmail.com>
2014-01-13 17:19:30 +0000

    merged back to revision 28312

--- public/assets/javascripts/ckeditor/plugins/studip-wiki/dialogs/wikilink.js
+++ public/assets/javascripts/ckeditor/plugins/studip-wiki/dialogs/wikilink.js
@@ -103,14 +103,13 @@ CKEDITOR.dialog.add('wikiDialog', function (editor) {
                     + " und darf nur folgende Zeichen enthalten:"
                     + " a-z A-Z ÄÖÜ äöü ß 0-9 -_:.( )/@#§ und das Leerzeichen."),
                 setup: function(link) {
-//                    this.setValue(getWikiPage(link.getAttribute('href')));
+                    this.setValue(getWikiPage(link.getAttribute('href')));
                 },
                 commit: function(link) {
-                    var wikiLink = '[[' + this.getValue() + ']]';
-//                    var href = getWikiLink(this.getValue());
-//                    link.setAttribute('href', href);
-//                    link.setAttribute('data-cke-saved-href', href);
-//                    link.setAttribute('class', 'wiki-link');
+                    var href = getWikiLink(this.getValue());
+                    link.setAttribute('href', href);
+                    link.setAttribute('data-cke-saved-href', href);
+                    link.setAttribute('class', 'wiki-link');
                 }
             }, {
                 type: 'text',
@@ -121,120 +120,43 @@ CKEDITOR.dialog.add('wikiDialog', function (editor) {
                     "Die schließende eckige Klammer, ], ist im angezeigten"
                     + " Text leider nicht erlaubt."),
                 setup: function(link) {
-//                    var usertext = link.getText(),
-//                        wikipage = getWikiPage(link.getAttribute('href'));
-//                    this.setValue(usertext === wikipage ? '' : usertext);
+                    var usertext = link.getText(),
+                        wikipage = getWikiPage(link.getAttribute('href'));
+                    this.setValue(usertext === wikipage ? '' : usertext);
                 },
                 commit: function(link) {
-//                    var usertext = this.getValue(),
-//                        wikipage = this._.dialog.getValueOf('tab-link', 'wikipage');
-//                    link.setText(usertext || wikipage);
+                    var usertext = this.getValue(),
+                        wikipage = this._.dialog.getValueOf('tab-link', 'wikipage');
+                    link.setText(usertext || wikipage);
                 }
             }]
         }],
         onShow: function() {
-            // selection empty (_ = cursor position)
-            // _                    empty text       ==> insert mode
-            // ab_cd                not in wiki link ==> insert mode
-            // [[ab_cd]]            in wiki link     ==> extend selection
-            // [_[abcd]]            in wiki link     ==> extend selection
-            // [[abcd]_]            in wiki link     ==> extend selection
-            // _[[abcd]]            at wiki link     ==> extend selection
-            // [[abcd]]_            at wiki link     ==> extend selection
-            //
-            // selection empty, link empty ==> extend selection
-            // _[[]]
-            // [_[]]
-            // [[_]]
-            // [[]_]
-            // [[]]_
-            //
-            // selection real (_ = selection start/end)
-            // a_bcd_e              not in wiki link ==> text -> link
-            // [[a_bc_d]]           in wiki link     ==> extend selection
-            // [_[abc]_]            in wiki link     ==> extend selection
-            // _[[abd]]_            enclosing wiki link ==> shrink selection
-            // _a[[bc]]d_           enclosing wiki link ==> shring selection
-            // a_b[[c_d]]           enclosing wiki link ==> extend/shrink
-            // [[a_b]]c_d           enclosing wiki link ==> extend/shrink
-            // [[a_b]][[c_d]]       invalid selection ==> deactivate button
-            // 
-            // simplification: set end to start for real selections
-
-            // get selection
-            var selection = editor.getSelection();
-            if (selection.getSelectedText().length) {
-                console.log('!selectionEmpty');
+            // get selected element
+            var element = editor.getSelection().getStartElement();
+            if (element) {
+                element = element.getAscendant('a', true);
             }
 
-            // find link's start and end position
-            var range = selection.getRanges()[0],
-                offset = range.startOffset,
-                text = range.startContainer.getText(),
-                linkStart = text.lastIndexOf('[[', offset + 2),
-                linkEnd = text.indexOf(']]', offset - 2);
-
-            // check that link doesn't overlap with another link
-            var overlapEnd = text.lastIndexOf(']]', offset + 2),
-                overlapStart = text.indexOf('[[', offset - 2),
-                overlaps = (overlapEnd > linkStart && overlapEnd < linkEnd)
-                    || (overlapStart < linkEnd && overlapStart > linkStart);
-
-            // NOTE since real selections are ignored (only start position is
-            // used) the only way to find an overlap is: ]]_[[ therefore switch
-            // to insert mode in the overlap case
-
-            // select complete link
-            if (overlaps || linkStart < 0 || linkEnd < 0) {
-                linkStart = linkEnd = offset;
+            // if no link is selected, insert a new one
+            this.insertMode = !element || element.getName() != 'a';
+            if (this.insertMode) {
+                element = editor.document.createElement('a');
+                var text = editor.getSelection().getSelectedText();
+                if (text) {
+                    this.setValueOf('tab-link', 'wikipage', text);
+                }
             } else {
-                linkEnd += 2;
+                this.setupContent(element);
             }
-            range.setStart(range.startContainer, linkStart);
-            range.setEnd(range.startContainer, linkEnd);
-            selection.selectRanges([range]);
-            console.log(selection.getSelectedText());
 
-            // regexp matches [[page]] and [[page|text]]
-            var existingLink = /^\[\[([^\[\|]*)(\|([^\]]*))?\]\]/
-                    .exec(selection.getSelectedText());
-
-            if (existingLink && existingLink.length > 1) {
-                page = existingLink[1];
-                text = existingLink.length > 3 ? existingLink[3] : '';
-                this.setValueOf('tab-link', 'wikipage', page);
-                this.setValueOf('tab-link', 'usertext', text);
-            }
-            
-//            var element = editor.getSelection().getStartElement();
-//            if (element) {
-//                element = element.getAscendant('a', true);
-//            }
-//
-//            // if no link is selected, insert a new one
-//            this.insertMode = !element || element.getName() != 'a';
-//            if (this.insertMode) {
-//                element = editor.document.createElement('a');
-//                var text = editor.getSelection().getSelectedText();
-//                if (text) {
-//                    this.setValueOf('tab-link', 'wikipage', text);
-//                }
-//            } else {
-//                this.setupContent(element);
-//            }
-//
-//            this.link = element;
+            this.link = element;
         },
         onOk: function() {
-            var page = this.getValueOf('tab-link', 'wikipage'),
-                text = this.getValueOf('tab-link', 'usertext'),
-                link = '[[' + page + '|' + text + ']]';
-            //selection.insertText(link);
-            editor.insertText(link);
-//            this.commitContent(this.link);
-//            if (this.insertMode) {
-//                editor.insertElement(this.link);
-//            }
+            this.commitContent(this.link);
+            if (this.insertMode) {
+                editor.insertElement(this.link);
+            }
         }
     };
 });
