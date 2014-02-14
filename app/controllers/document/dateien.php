b855e297aff11713f8259450b851ed71c1898201
Nico Müller <nico.mueller@uni-oldenburg.de>
2014-02-14 09:05:48 +0000

    documents: resolve api conflicts and adjust to new conditions

--- app/controllers/document/dateien.php
+++ app/controllers/document/dateien.php
@@ -1,20 +1,20 @@
 <?php
 
 /**
- * dateien.php 
- * 
+ * dateien.php
+ *
  * Der Controller stellt angemeldeten Benutzer/innen einen Dateimanager
- * fuer deren persoenlichen Dateibereich im Stud.IP zur Verfuegung.   
+ * fuer deren persoenlichen Dateibereich im Stud.IP zur Verfuegung.
  *
  *
  * TODO:
- * 
+ *
  * - Lifter 010: Unterstuetzung einer barrierefreien Nutzung
- * 
+ *
  *
  * @category    Stud.IP
  * @version     3.1
- * 
+ *
  * @author      Gerd Hoffmann <gerd.hoffmann@uni-oldenburg.de>
  * @license     http://www.gnu.org/licenses/gpl-3.0
  * @copyright   2014 Carl von Ossietzky Universitaet Oldenburg
@@ -22,126 +22,126 @@
 
 
 require_once 'app/controllers/authenticated_controller.php';
-require_once 'lib/classes/document/UserToAPI.class.php';
+#require_once 'lib/classes/document/UserToAPI.class.php';
 
 
 class Document_DateienController extends AuthenticatedController
- {  
+ {
    private $userConfig, $quota;
-   
-   
+
+
    public function before_filter(&$action, &$args)
-    {    
+    {
      global $user, $USER_DOC_PATH;
-      
-     parent::before_filter($action, $args);    
+
+     parent::before_filter($action, $args);
      Navigation::activateItem('/document/dateien');
-           
+
      $user_id = $user -> id;
-            
-     //Configurations for the Documentarea for this user 
+
+     //Configurations for the Documentarea for this user
      $this -> userConfig = DocUsergroupConfig::getUserConfig($GLOBALS['user'] -> user_id);
-     
+
      if (!empty($this -> userConfig))
       {
        $measure = $this -> userConfig['quota'];
        $this -> quota = $this -> formatiere($measure);
       }
-     
-     $this -> userAPI = new UserToAPI();
-     $user_exists = $this -> userAPI -> authUser($user_id, 'DB');
-     
-     if (!$user_exists)
-      $this -> userAPI -> initUser($user_id, 'DB');
-      
+
+#     $this -> userAPI = new UserToAPI();
+#     $user_exists = $this -> userAPI -> authUser($user_id, 'DB');
+
+#     if (!$user_exists)
+#      $this -> userAPI -> initUser($user_id, 'DB');
+
      PageLayout::setTitle(_('Dateiverwaltung'));
-     PageLayout::setHelpKeyword('Basis.Dateien');      
+     PageLayout::setHelpKeyword('Basis.Dateien');
      //PageLayout::addScript('./javascripts/application_diff.js');
      //PageLayout::addScript('./javascripts/jquery/jquery-ui.1.9.1.custom.js');
-     
+
      $this -> set_layout($GLOBALS['template_factory'] -> open('layouts/base'));
     }
-  
- 
+
+
   public function index_action()
-   {                          
+   {
     $this -> redirect("document/dateien/list");
    }
-   
- 
+
+
   public function list_action()
-   {    
+   {
     $this -> flash['count'] = 0;
-        
+
     $this -> flash['quota'] = $this -> quota;
     $this -> flash['closed'] = $this -> userConfig['area_close'];
     $this -> flash['notification'] = $this -> userConfig['area_close_text'];
-    
+
     $this -> render_action('index');
    }
-   
+
 
   public function oeffnen_action($type)
-   { 
+   {
     //
    }
-   
-  
+
+
   public function teilen_action($type)
-   { 
+   {
     $this -> flash['share'] = $type;
     $this -> redirect("document/dateien/list");
    }
-   
-  
+
+
   public function bearbeiten_action($type)
-   { 
+   {
     $this -> flash['workOn'] = $type;
     $this -> redirect("document/dateien/list");
    }
-   
-   
+
+
   public function verwalten_action($type)
-   { 
+   {
     $this -> flash['admin'] = $type;
     $this -> redirect("document/dateien/list");
    }
-   
-   
+
+
   public function loeschen_action($type)
-   { 
+   {
     $this -> flash['delete'] = $type;
     $this -> redirect("document/dateien/list");
    }
-   
-   
-   
+
+
+
   public function crazy_action()
    {
-    $in = $_GET["test"];   
+    $in = $_GET["test"];
     $this -> redirect("document/dateien/list");
    }
-   
-   
-   
+
+
+
   public function hochladen_action()
    {
     //
    }
-   
-   
+
+
   public function erstellen_action()
    {
     //
    }
-   
-  
+
+
   public function konfigurieren_action()
    {
     //
    }
-   
-   
+
+
   private function formatiere($bytes)
    {
     if ($bytes >= 1073741824)
@@ -168,8 +168,7 @@ class Document_DateienController extends AuthenticatedController
       {
        $groesse = '0 Byte';
       }
- 
+
     return $groesse;
    }
  }
- 
--- lib/classes/UserManagement.class.php
+++ lib/classes/UserManagement.class.php
@@ -33,7 +33,6 @@ require_once 'lib/messaging.inc.php';   // remove messages send or recieved by u
 require_once 'lib/contact.inc.php'; // remove user from adressbooks
 require_once 'lib/classes/DataFieldEntry.class.php';    // remove extra data of user
 require_once 'lib/classes/auth_plugins/StudipAuthAbstract.class.php';
-require_once 'lib/classes/document/UserToAPI.class.php'; //remove private file-space of user
 require_once 'lib/object.inc.php';
 require_once 'lib/log_events.inc.php';  // Event logging
 require_once 'app/models/studygroup.php';
@@ -899,23 +898,10 @@ class UserManagement
                     $temp_count++;
                 }
             }
-            
-            //delete private file-space of this user with all documents contained
-            $user_id = $this -> user_data['auth_user_md5.user_id'];
-            $user_exists = StudipDocumentAPI::authEntity('user', $user_id, 'DB');
-     
-            if ($user_exists) {
-               $res = StudipDocumentAPI::deleteEntity('user',$user_id, 'DB');
-            }
-            
-            if ($temp_count && $res) {
-                $this->msg .= "info§" . sprintf(_("%s Dokumente und pers&ouml;nlicher 
-                    Dateibereich gel&ouml;scht."), $temp_count) . "§";
-            } 
-            elseif ($temp_count) {
-                $this->msg .= "info§" . sprintf(_("%s Veranstaltungs- und Gruppen-Dokumente 
-                    gel&ouml;scht."), $temp_count) . "§";
-            }  
+
+            // Remove private file space of this user
+            $root_dir = new RootDirectory($this->user_data['auth_user_md5.user_id']);
+            $root_dir->delete();
 
             // delete empty folders of this user
             $temp_count = 0;
