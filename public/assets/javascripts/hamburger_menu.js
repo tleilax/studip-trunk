ec4f407919b1fffac3be7578ab7a8b95ac297c53
David Siegfried <ds.siegfried@gmail.com>
2014-12-17 07:48:02 +0000

    responsive: add sidebar

--- public/assets/javascripts/hamburger_menu.js
+++ public/assets/javascripts/hamburger_menu.js
@@ -1,7 +1,7 @@
 /*jslint browser: true, white: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, newcap: true, immed: true, indent: 4, onevar: false */
 /*global window, $, jQuery, _ */
 
-jQuery(function() {
+jQuery(function () {
     jQuery("#hamburgerNavigation input[type=checkbox]").on("change", function () {
         if (jQuery(this).is(":checked")) {
             jQuery(this).closest("li").siblings(":not(#hamburgerNavigation > li)").slideUp();
@@ -11,10 +11,12 @@ jQuery(function() {
         } else {
             jQuery(this).closest("li").siblings().slideDown();
         }
-    });
-    jQuery("#hamburgerNavigation .subnavigation input[type=checkbox]").each(function (index, input) {
-        if (jQuery(input).siblings(".subnavigation").find("input:checked").length > 0) {
-            jQuery(input).closest("li").siblings().hide();
-        }
+    }).trigger("change");
+
+    if (jQuery('#layout-sidebar').length == 0) {
+        jQuery('#sidebar-menu').hide();
+    }
+    jQuery('#sidebar-menu').click(function () {
+        jQuery('#layout-sidebar').toggleClass('visible-sidebar');
     });
 });
