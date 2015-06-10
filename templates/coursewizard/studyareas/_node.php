3ff5bc691228223e99fdf98d8799909ebbf1bff1
hackl <hackl>
2015-06-10 12:28:47 +0000

    use Assets correctly and not like a drunk maniac

--- templates/coursewizard/studyareas/_node.php
+++ templates/coursewizard/studyareas/_node.php
@@ -1,6 +1,7 @@
 <li class="sem-tree-<?= htmlReady($node->id) ?> keep-node" data-id="<?= $node->id ?>">
     <?php if ($node->isAssignable()) : ?>
-        <img src="<?= Assets::img('icons/yellow/arr_2left.svg') ?>"/>
+    <a href="" onclick="return STUDIP.CourseWizard.assignNode('<?= $node->id ?>')">
+        <?= Assets::img('icons/yellow/arr_2left.svg') ?></a>
     <?php endif ?>
     <input type="checkbox" id="<?= htmlReady($node->sem_tree_id) ?>"/>
     <label for="<?= htmlReady($node->sem_tree_id) ?>" onclick="return STUDIP.CourseWizard.getTreeChildren('<?= htmlReady($node->sem_tree_id) ?>', true)">
