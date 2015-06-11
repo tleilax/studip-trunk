fa42c153f9f135623c33303165aff3ff81a9b650
hackl <hackl>
2015-06-11 07:00:10 +0000

    correct tree structure for elements without children

--- templates/coursewizard/studyareas/_node.php
+++ templates/coursewizard/studyareas/_node.php
@@ -3,11 +3,13 @@
     <a href="" onclick="return STUDIP.CourseWizard.assignNode('<?= $node->id ?>')">
         <?= Assets::img('icons/yellow/arr_2left.svg') ?></a>
     <?php endif ?>
+    <?php if ($node->hasChildren()) : ?>
     <input type="checkbox" id="<?= htmlReady($node->sem_tree_id) ?>"/>
     <label for="<?= htmlReady($node->sem_tree_id) ?>" onclick="return STUDIP.CourseWizard.getTreeChildren('<?= htmlReady($node->sem_tree_id) ?>', true)">
+    <?php endif ?>
         <?= htmlReady($node->name) ?>
-    </label>
     <?php if ($node->hasChildren()) : ?>
+    </label>
     <ul></ul>
     <?php endif ?>
 </li>
