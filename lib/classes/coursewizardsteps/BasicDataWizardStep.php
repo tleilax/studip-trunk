150a8966d80a2d9f9d6247e9bd9fd38dc1babbf2
hackl <hackl>
2015-07-10 14:53:27 +0000

    studygroup creation working again, link to new assi

--- lib/classes/coursewizardsteps/BasicDataWizardStep.php
+++ lib/classes/coursewizardsteps/BasicDataWizardStep.php
@@ -27,11 +27,12 @@ class BasicDataWizardStep implements CourseWizardStep
      */
     public function getStepTemplate($values, $stepnumber, $temp_id)
     {
+        $studygroup = $values['studygroup'];
         // We only need our own stored values here.
         $values = $values[__CLASS__];
         // Load template from step template directory.
         $factory = new Flexi_TemplateFactory($GLOBALS['STUDIP_BASE_PATH'].'/app/views/course/wizard/steps');
-        if ($values['studygroup']) {
+        if ($studygroup) {
             $tpl = $factory->open('basicdata/index_studygroup');
             $values['lecturers'][$GLOBALS['user']->id] = 1;
         } else {
@@ -44,7 +45,7 @@ class BasicDataWizardStep implements CourseWizardStep
         {
             $class = $type->getClass();
             // Creates a studygroup.
-            if ($values['studygroup']) {
+            if ($studygroup) {
                 // Get all studygroup types.
                 if ($class['studygroup_mode']) {
                     $typestruct[$class['name']][] = $type;
