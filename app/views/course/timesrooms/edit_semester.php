2028568995a494776f05865fdd0ad40b15002672
David Siegfried <ds.siegfried@gmail.com>
2015-08-25 07:07:44 +0000

    raumzeit: fix views, re #5772

--- app/views/course/timesrooms/edit_semester.php
+++ app/views/course/timesrooms/edit_semester.php
@@ -1,36 +1,37 @@
-<form action="<?= $controller->url_for('course/timesrooms/set_semester/' . $course->id) ?>" method="post" class="studip-form"
-    <?= Request::isXhr() ? 'data-dialog="size=big"' : ''?>>
-    <section <?= !Request::isXhr() ? 'style="float: left; width: 45%"' : '' ?>>
-        <label for="startSemester"><?= _('Startsemester') ?>:</label>
-        <select class="size-xl" name="startSemester" id="startSemester">
-            <? foreach ($semester as $sem) : ?>
-                <option
-                    value="<?= $sem->semester_id ?>" <?= $sem->semester_id == $course->start_semester->semester_id ? 'selected' : '' ?>>
-                    <?= htmlReady($sem->name) ?>
-                </option>
-            <? endforeach; ?>
-        </select>
-    </section>
-    <section <?= !Request::isXhr() ? 'style="float: right; width: 45%"' : '' ?>>
-        <label for="endSemester"><?= _('Dauer') ?>:</label>
-        <select class="size-xl" name="endSemester" id="endSemester">
-            <option value="0"
-                <?= $course->__get('metadate')->seminarDurationTime == 0 ? 'selected' : '' ?>>
-                <?= _('ein Semester') ?></option>
-            <? foreach ($semester as $sem) : ?>
-                <? if ($sem->beginn >= $current_semester->beginn) : ?>
-                    <option value="<?= $sem->semester_id ?>"
-                        <?= $course->__get('metadate')->seminarDurationTime == $sem->semester_id ? 'selected' : '' ?>>
+<form action="<?= $controller->url_for('course/timesrooms/set_semester/' . $course->id) ?>" method="post"
+      class="studip-form" <?= Request::isXhr() ? 'data-dialog="size=big"' : '' ?>>
+    <section>
+        <section <?= !Request::isXhr() ? 'style="float: left; width: 45%"' : '' ?>>
+            <label for="startSemester"><?= _('Startsemester') ?>:</label>
+            <select class="size-xl" name="startSemester" id="startSemester">
+                <? foreach ($semester as $sem) : ?>
+                    <option
+                        value="<?= $sem->semester_id ?>" <?= $sem->semester_id == $course->start_semester->semester_id ? 'selected' : '' ?>>
                         <?= htmlReady($sem->name) ?>
                     </option>
-                <? endif; ?>
-            <? endforeach; ?>
-            <option value="-1"
-                <?= $course->__get('metadate')->seminarDurationTime == -1 ? 'selected' : '' ?>>
-                <?= _('unbegrenzt') ?></option>
-        </select>
+                <? endforeach; ?>
+            </select>
+        </section>
+        <section <?= !Request::isXhr() ? 'style="float: right; width: 45%"' : '' ?>>
+            <label for="endSemester"><?= _('Dauer') ?>:</label>
+            <select class="size-xl" name="endSemester" id="endSemester">
+                <option value="0"
+                    <?= $course->__get('metadate')->seminarDurationTime == 0 ? 'selected' : '' ?>>
+                    <?= _('ein Semester') ?></option>
+                <? foreach ($semester as $sem) : ?>
+                    <? if ($sem->beginn >= $current_semester->beginn) : ?>
+                        <option value="<?= $sem->semester_id ?>"
+                            <?= $course->__get('metadate')->seminarDurationTime == $sem->semester_id ? 'selected' : '' ?>>
+                            <?= htmlReady($sem->name) ?>
+                        </option>
+                    <? endif; ?>
+                <? endforeach; ?>
+                <option value="-1"
+                    <?= $course->__get('metadate')->seminarDurationTime == -1 ? 'selected' : '' ?>>
+                    <?= _('unbegrenzt') ?></option>
+            </select>
+        </section>
     </section>
-
     <? if (!Request::isXhr()) : ?>
         <footer>
             <?= Studip\Button::createAccept(_('Semester speichern'), 'save', $semesterFormParams) ?>
@@ -43,4 +44,5 @@
             <?= Studip\Button::createAccept(_('Semester speichern'), 'save_close') ?>
         </div>
     <? endif ?>
+
 </form>
