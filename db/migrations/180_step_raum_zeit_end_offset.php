89caa2786ff242075143c4116c888b4dde22f6aa
Jan-Hendrik Willms <tleilax+github@gmail.com>
2015-11-19 15:23:41 +0000

    raumzeit: clear sorm cache in migration and adjust migration number, re #5772

--- db/migrations/180_step_raum_zeit_end_offset.php
+++ db/migrations/180_step_raum_zeit_end_offset.php
@@ -10,7 +10,9 @@ class StepRaumZeitEndOffset extends Migration
     function up()
     {
         DBManager::get()->exec('ALTER TABLE `seminar_cycle_dates` ADD COLUMN `end_offset` TINYINT(3) NULL AFTER `week_offset`');
-        
+
+        SimpleORMap::expireTableScheme();
+
         //CHANGE route entry in help_content from raumzeit.php to new dispatch.php/course/timesrooms
         $query = 'UPDATE `help_content` SET route = :new WHERE route = :old';
         $stm = DBManager::get()->prepare($query);
@@ -20,7 +22,9 @@ class StepRaumZeitEndOffset extends Migration
     function down()
     {
         DBManager::get()->exec('ALTER TABLE `seminar_cycle_dates` DROP COLUMN `end_offset`');
-        
+
+        SimpleORMap::expireTableScheme();
+
         //CHANGE route entry in help_content from dispatch.php/course/timesrooms back to raumzeit.php
         $query = 'UPDATE `help_content` SET route = :old WHERE route = :new';
         $stm = DBManager::get()->prepare($query);
