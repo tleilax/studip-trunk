8ca08022a5bd0f87682a15653b8fcb29e9884161
Andre Klassen <andre.klassen@gmail.com>
2016-03-03 13:09:52 +0000

    working on #5177 (implement caching for stream)

--- lib/activities/Stream.php
+++ lib/activities/Stream.php
@@ -34,36 +34,32 @@ class Stream implements \ArrayAccess, \Countable, \IteratorAggregate
             throw new \InvalidArgumentException();
         }
 
+        $cached_activities = self::getCachedActivities($observer_id, $filter, $contexts);
 
-        // load all activities
-        $this->activities = array_flatten(array_map(
-                                              function ($context) use($observer_id, $filter) {
-                                                  return $context->getActivities($observer_id, $filter);
-                                              }, $contexts));
 
-        usort($this->activities, function($a, $b) {
-            if ($a->getMkdate() == $b->getMkdate()) {
-                return 0;
-            }
-
-            return ($a->getMkdate() > $b->getMkdate()) ? -1 : 1;
-        });
-
+        //var_dump($cached_activities);
 
         $new_activities = array();
 
-        foreach ($this->activities as $key => $activity) {
-            // generate an id for the activity, considering some basic object parameters
-            $object = $activity->getObject();
-            $id = md5($activity->getProvider() . serialize($activity->getDescription()) . $activity->getVerb() . $object['objectType'] . $activity->getMkdate());
+        foreach ( $cached_activities as $activities) {
 
-            if ($new_activities[$id]) {
-                list($url, $name) = each($object['url']);
-                $new_activities[$id]->addUrl($url, $name);
-            } else {
-                $new_activities[$id] = $activity;
+            if(!empty($activities)) {
+                foreach ( $activities as $key => $activity) {
+                    // generate an id for the activity, considering some basic object parameters
+                    $object = $activity->getObject();
+                    $id = md5($activity->getProvider() . serialize($activity->getDescription()) . $activity->getVerb() . $object['objectType'] . $activity->getMkdate());
+
+                    if ($new_activities[$id]) {
+                        list($url, $name) = each($object['url']);
+                        $new_activities[$id]->addUrl($url, $name);
+                    } else {
+                        $new_activities[$id] = $activity;
+                    }
+                }
             }
+
         }
+
 
         $this->activities = $new_activities;
     }
@@ -125,5 +121,68 @@ class Stream implements \ArrayAccess, \Countable, \IteratorAggregate
         }
 
         return $activities;
+    }
+
+
+    static function getCachedActivities($observer_id, $filter, $contexts) {
+
+        $cache =\StudipCacheFactory::getCache();
+
+
+        $end_date = new \DateTime();
+        $end_date->setTimestamp(($filter->getEndDate()-(24*3600)));
+
+        $start_date = new \DateTime();
+        $start_date->setTimestamp($filter->getStartDate());
+
+        $diff = $end_date->diff($start_date);
+        $ret = array();
+
+        for($i=1; $i<=$diff->days; $i++) {
+            date_sub($end_date, date_interval_create_from_date_string('1 day'));
+            $cachekey = 'activities_' . $observer_id . '_' . $end_date->format('Y-m-d');
+
+            $tmp_stmp = $end_date->getTimestamp();
+
+            $cached_activities = unserialize($cache->read($cachekey));
+
+            if ($cached_activities) {
+
+                $ret[$cachekey] = $cached_activities;
+
+            } else {
+
+                $filter2 = new Filter();
+                $filter2->setStartDate($tmp_stmp);
+                $filter2->setEndDate($tmp_stmp + ((23 * 3600) + 35999));
+
+
+                // load all activities
+                $activities = array_flatten(array_map(
+                    function ($context) use ($observer_id, $filter2) {
+                        return $context->getActivities($observer_id, $filter2);
+                    }, $contexts));
+
+
+                $new_activities = array();
+
+                foreach ($activities as $key => $activity) {
+                    // generate an id for the activity, considering some basic object parameters
+                    $object = $activity->getObject();
+                    $id = md5($activity->getProvider() . serialize($activity->getDescription()) . $activity->getVerb() . $object['objectType'] . $activity->getMkdate());
+
+                    if ($new_activities[$id]) {
+                        list($url, $name) = each($object['url']);
+                        $new_activities[$id]->addUrl($url, $name);
+                    } else {
+                        $new_activities[$id] = $activity;
+                    }
+                }
+
+                $cache->write($cachekey, serialize($new_activities));
+                $ret[$cachekey] = $new_activities;
+            }
+        }
+        return $ret;
     }
 }
