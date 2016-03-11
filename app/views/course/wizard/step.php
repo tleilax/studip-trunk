e6f531237499cd624685af4b5212719e3722914d
hackl <hackl>
2016-03-11 20:31:41 +0000

    use form footer only when not in dialog, fixes #6422

--- app/views/course/wizard/step.php
+++ app/views/course/wizard/step.php
@@ -1,7 +1,9 @@
 <?php if ($content) : ?>
     <form class="default" action="<?= $controller->url_for('course/wizard/process', $stepnumber, $temp_id) ?>" method="post">
         <?= $content ?>
+        <?php if (!Request::isXhr()) : ?>
         <footer>
+        <?php endif ?>
             <input type="hidden" name="step" value="<?= $stepnumber ?>"/>
             <?php if (!$first_step) { ?>
                 <?= Studip\Button::create(_('Zurück'), 'back',
@@ -9,7 +11,9 @@
             <?php } ?>
             <?= Studip\Button::create(_('Weiter'), 'next',
                 $dialog ? array('data-dialog' => 'size=50%', 'data-dialog-button' => true) : array()) ?>
+        <?php if (!Request::isXhr()) : ?>
         </footer>
+        <?php endif ?>
     </form>
 <?php else : ?>
     <?= Studip\LinkButton::createCancel(_('Zurück zu meiner Veranstaltungsübersicht'),
