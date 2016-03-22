34b2da93d70e0e48fcd88bafded311b4f0f3b3c1
Jan-Hendrik Willms <tleilax+github@gmail.com>
2016-03-22 12:33:19 +0000

    fix input icon, re #6217

--- app/views/calendar/single/_jump_to.php
+++ app/views/calendar/single/_jump_to.php
@@ -2,7 +2,7 @@
     <span style="font-size: small; color: #555555;"><?= _('Gehe zu:') ?> </span>
     <input type="hidden" name="action" value="<?= $action ?>">
     <input size="10" type="text" id="jmp_date" name="jmp_date" type="text" value="<?= strftime('%x', $atime)?>">
-    <?= Icon::create('accept', 'clickable')->asImg(['class' => 'text-top']) ?>
+    <?= Icon::create('accept', 'clickable')->asInput(['class' => 'text-top']) ?>
 </form>
 <script>
     jQuery('#jmp_date').datepicker();
