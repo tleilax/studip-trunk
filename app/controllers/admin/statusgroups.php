8c18a3227f8c7b47c0ff258635192f5d7cfbf3c5
Elmar Ludwig <ludwig@uni-osnabrueck.de>
2016-03-30 10:51:41 +0000

    delete datafields when moving/deleting group, fixes #3289

--- app/controllers/admin/statusgroups.php
+++ app/controllers/admin/statusgroups.php
@@ -105,7 +105,10 @@ class Admin_StatusgroupsController extends AuthenticatedController
             $group = new Statusgruppen($group_id);
             if ($group->isNew()) {
                 $group->range_id = $_SESSION['SessionSeminar'];
+            } else {
+                DataFieldEntry::removeAll(array('', $group->statusgruppe_id));
             }
+
             $group->name       = Request::get('name');
             $group->name_w     = Request::get('name_w');
             $group->name_m     = Request::get('name_m');
@@ -231,6 +234,8 @@ class Admin_StatusgroupsController extends AuthenticatedController
 
             //remove users
             $this->group->removeAllUsers();
+            //remove datafields
+            DataFieldEntry::removeAll(array('', $this->group->statusgruppe_id));
 
             //goodbye group
             $this->group->delete();
