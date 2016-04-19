5f77da70215e7898b667e33fc754f3871bc03685
André Noack <noackorama@gmail.com>
2016-04-19 09:43:40 +0000

    fixes #6547

--- templates/shared/opengraphinfo_wide.php
+++ templates/shared/opengraphinfo_wide.php
@@ -1,11 +1,27 @@
-<? $videofiles = $og->getVideoFiles() ?>
-<? $audiofiles = $og->getAudioFiles() ?>
+<?php
+$videofiles = $og->getVideoFiles();
+$audiofiles = $og->getAudioFiles();
+$og['image'] = filter_var($og['image'], FILTER_VALIDATE_URL) ? $og['image'] : '';
+if (Config::get()->LOAD_EXTERNAL_MEDIA === "proxy" && Seminar_Session::is_current_session_authenticated()) {
+    $media_url_func = function ($url) {
+        return $GLOBALS['ABSOLUTE_URI_STUDIP'] . 'dispatch.php/media_proxy?url=' . urlencode($url);
+    };
+} elseif (Config::get()->LOAD_EXTERNAL_MEDIA === "deny") {
+    $media_url_func = function ($url) {
+        return '';
+    };
+} else {
+    $media_url_func = function ($url) {
+        return $url;
+    };
+}
+?>
 <div class="opengraph <? if (count($videofiles) > 0) echo 'video'; ?> <? if (count($audiofiles) > 0) echo 'audio'; ?>">
 <? if ($og['image'] && count($videofiles) === 0): ?>
     <a href="<?= URLHelper::getLink($og['url'], array(), false) ?>" class="image" target="_blank"
-       style="background-image:url(<?= $og['image'] ?>)">
+       style="background-image:url(<?= htmlReady($media_url_func($og['image'])) ?>)">
     </a>
-<? endif; ?>  
+<? endif; ?>
     <a href="<?= URLHelper::getLink($og['url'], array(), false) ?>" class="info" target="_blank">
         <strong><?= htmlReady($og['title']) ?></strong>
     <? if (!count($videofiles)) : ?>
@@ -17,14 +33,14 @@
     <? if (in_array($videofiles[0][1], array("text/html", "application/x-shockwave-flash"))) : ?>
         <a href="<?= htmlReady($videofiles[0][0]) ?>"
            class="flash-embedder"
-           style="background-image: url('<?= htmlReady($og['image']) ?>');"
+           style="background-image: url('<?= htmlReady($media_url_func($og['image'])) ?>');"
            title="<?= _("Video abspielen") ?>">
             <?= Icon::create('play', 'clickable')->asImg(80, ["class" => "play"])?>
         </a>
     <? else : ?>
         <video width="100%" height="200px" controls>
         <? foreach ($videofiles as $file) : ?>
-            <source src="<?= htmlReady($file[0]) ?>"<?= $file[1] ? ' type="'.htmlReady($file[1]).'"' : "" ?>></source>
+            <source src="<?= htmlReady($media_url_func($file[0])) ?>"<?= $file[1] ? ' type="'.htmlReady($file[1]).'"' : "" ?>></source>
         <? endforeach ?>
         </video>
     <? endif ?>
@@ -35,14 +51,14 @@
     <? if (in_array($audiofiles[0][1], array("text/html", "application/x-shockwave-flash"))) : ?>
         <a href="<?= htmlReady($audiofiles[0][0]) ?>"
            class="flash-embedder"
-           style="background-image: url('<?= htmlReady($og['image']) ?>');"
+           style="background-image: url('<?= htmlReady($media_url_func($og['image'])) ?>');"
            title="<?= _("Audio abspielen") ?>">
             <?= Icon::create('play', 'clickable')->asImg(100)?>
         </a>
     <? else : ?>
         <audio width="100%" height="50px" controls>
         <? foreach ($audiofiles as $file) : ?>
-            <source src="<?= htmlReady($file[0]) ?>"<?= $file[1] ? ' type="'.htmlReady($file[1]).'"' : "" ?>></source>
+            <source src="<?= htmlReady($media_url_func($file[0])) ?>"<?= $file[1] ? ' type="'.htmlReady($file[1]).'"' : "" ?>></source>
         <? endforeach ?>
         </audio>
     <? endif ?>
