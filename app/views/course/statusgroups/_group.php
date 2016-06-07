b3d9d0b0969dc37e0533275d0099d0504577d439
hackl <hackl>
2016-06-07 14:19:12 +0000

    use action menu in members table, fine tune positioning, re #6590

--- app/views/course/statusgroups/_group.php
+++ app/views/course/statusgroups/_group.php
@@ -27,7 +27,7 @@
                             <div class="action-title">
                                 <?= _('Aktionen') ?>
                             </div>
-                            <?= Icon::create('action', 'clickable', array('title' => _('Aktionen'))) ?>
+                            <?= Icon::create('action', 'clickable', array('title' => _('Aktionen'), 'class' => 'action-icon')) ?>
                             <ul>
                                 <li>
                                     <a href="<?= $controller->url_for('messages/write', array(
@@ -177,69 +177,9 @@
                 <tbody>
                 <?php $i = 1; $invisible = 0; foreach ($members as $m) : ?>
                     <?php if ($is_tutor || $m->user_id == $GLOBALS['user']->id || $m->visible != 'no') : ?>
-                        <tr>
-                            <?php if ($is_tutor && !$is_locked) : ?>
-                                <td>
-                                    <input aria-label="<?= _('Mitglieder auswählen') ?>"
-                                           type="checkbox" name="group[<?= $group->id ?>][<?= $m->user_id ?>]"
-                                           class="groupmembers-<?= $group->id ?>" value="1" />
-                                </td>
-                            <?php endif ?>
-                            <td>
-                                <?= str_pad($i, 2, '0', STR_PAD_LEFT) ?>
-                            </td>
-                            <td>
-                                <a href="<?= $controller->url_for(sprintf('profile?username=%s',$m->username)) ?>">
-                                    <?= Avatar::getAvatar($m->user_id, $m->username)->getImageTag(Avatar::SMALL,
-                                        array('style' => 'margin-right: 5px',
-                                            'title' => htmlReady($m->getUserFullname('full_rev')))); ?>
-                                    <?= htmlReady($m->getUserFullname('full_rev')) ?>
-                                    <?php if ($user_id == $m->user_id && $m->visible == 'no') : ?>
-                                        (<?= _('unsichtbar') ?>)
-                                    <?php endif ?>
-                                </a>
-                            </td>
-                            <?php if($is_tutor) : ?>
-                                <td>
-                                    <?= strftime('%x %X', $m->mkdate) ?>
-                                </td>
-                                <td>
-                                    <?= $this->render_partial('course/members/_studycourse.php',
-                                        array('study_courses' => UserModel::getUserStudycourse($m->user_id))) ?>
-                                </td>
-                            <?php endif ?>
-                            <td class="memberactions">
-                                <?php if ($is_tutor || $m->user_id != $GLOBALS['user']->id) : ?>
-                                    <a href="<?= $controller->url_for('messages/write', array(
-                                        'rec_uname' => $m->user->username,
-                                        'default_subject' => $course_title
-                                    )) ?>" data-dialog="size=auto;">
-                                        <?= Icon::create('mail', 'clickable',
-                                            array('title' => sprintf(_('Nachricht an %s schicken'),
-                                                $m->user->getFullname()))) ?></a>
-                                <?php endif ?>
-                                <?php if ($is_tutor) : ?>
-                                    <a href="<?= $controller->url_for('course/statusgroups/move_member',
-                                        $m->user_id, $group->id) ?>" data-dialog="size=auto;">
-                                        <?= Icon::create('person+move_right', 'clickable',
-                                            array('title' => sprintf(_('%s in eine andere Gruppe verschieben'),
-                                                $m->user->getFullname()))) ?></a>
-                                <?php endif ?>
-                                <?php if ($group->id != 'nogroup' &&
-                                    ($is_tutor || $m->user_id == $GLOBALS['user']->id)) : ?>
-                                    <a href="<?= $controller->url_for('course/statusgroups/delete_member',
-                                        $m->user_id, $group->id) ?>"
-                                       data-confirm="<?= sprintf(
-                                           _('Soll %s wirklich aus der Gruppe %s entfernt werden?'),
-                                           htmlReady($m->getUserFullname()), htmlReady($group->name)) ?>">
-                                        <?= Icon::create('trash', 'clickable',
-                                            array('title' => sprintf(_('%s aus Gruppe %s entfernen'),
-                                                htmlReady($m->getUserFullname()),
-                                                htmlReady($group->name)))) ?></a>
-                                <?php endif ?>
-                            </td>
-                        </tr>
-                        <?php $i++; else : $invisible++; endif ?>
+                        <?= $this->render_partial('course/statusgroups/_member',
+                            array('m' => $m, 'i' => $i, 'is_tutor' => $is_tutor, 'is_locked' => $is_locked)); $i++ ?>
+                    <?php else : $invisible++; endif ?>
                 <?php endforeach ?>
                 </tbody>
                 <tfoot>
--- public/assets/javascripts/actionmenu.js
+++ public/assets/javascripts/actionmenu.js
@@ -10,10 +10,34 @@
 
     $(document).ready(function () {
         // Open submenu trigger.
-        $('ul.actionmenu').children('li').on('click', function(e) {
+        $('ul.actionmenu').find('li').on('click', function(e) {
             // Hide all other open actionmenus.
             $('ul.actionmenu').not($(this).parents('ul.actionmenu')).children('li').removeClass('active');
+            $('ul.actionmenu').removeClass('positioncorrected');
+            $('ul.actionmenu').css('left', '');
             $(this).toggleClass('active');
+            var parent = $(this).parent();
+            if (parent.hasClass('actionmenu')) {
+                var td = parent.closest('td');
+
+                // Adjust menu top item width.
+                if ($(this).hasClass('active')) {
+                    var itemWidth = $(this).children('ul').children('li').width();
+                    $(this).children('div.action-title').width(itemWidth - 15);
+
+                    /*
+                     * We need to check if we are inside a table because
+                     * then the positioning must be changed if we do not
+                     * want the td to grow along with the menu.
+                     */
+                    if (td.length > 0) {
+                        td.css('position', 'relative');
+                        td.css('height', '35px');
+                        parent.addClass('positioncorrected');
+                        parent.css('left', '-' + (itemWidth - 55) + 'px');
+                    }
+                }
+            }
         });
     });
 
