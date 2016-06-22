d6b27b78505bf6651457764b71c1552589914d3b
Till Glöggler <tgloeggl@uos.de>
2016-06-22 09:12:19 +0000

    add docblocks to filter methods

--- lib/activities/Filter.php
+++ lib/activities/Filter.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @author      Till Glöggler <tgloeggl@uos.de>
  * @author      André Klaßen <klassen@elan-ev.de>
@@ -16,41 +15,73 @@ class Filter
         $type,
         $verb;
 
+    /**
+     *
+     * @param string $start_date
+     */
     public function setStartDate($start_date)
     {
         $this->start_date = $start_date;
     }
 
+    /**
+     *
+     * @return string
+     */
     public function getStartDate()
     {
         return $this->start_date;
     }
 
+    /**
+     *
+     * @param string $end_date
+     */
     public function setEndDate($end_date)
     {
         $this->end_date = $end_date;
     }
 
+    /**
+     *
+     * @return string
+     */
     public function getEndDate()
     {
         return $this->end_date;
     }
 
+    /**
+     *
+     * @param string $type
+     */
     public function setType($type)
     {
         $this->type = $type;
     }
 
+    /**
+     *
+     * @return string
+     */
     public function getType()
     {
         return $this->type;
     }
 
+    /**
+     *
+     * @return string
+     */
     public function getVerb()
     {
         return $this->verb;
     }
 
+    /**
+     *
+     * @param string $verb
+     */
     public function setVerb($verb)
     {
         $this->verb = $verb;
