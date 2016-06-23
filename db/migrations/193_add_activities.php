7142f17ceeee008f6b49f1db30fad84b88fe8c4a
André Noack <noackorama@gmail.com>
2016-06-23 14:24:27 +0000

    optimize migration re #5177

--- db/migrations/193_add_activities.php
+++ db/migrations/193_add_activities.php
@@ -3,7 +3,7 @@
 class AddActivities extends Migration
 {
 
-    public function description() 
+    public function description()
     {
         return 'add table for activities';
     }
@@ -14,18 +14,20 @@ class AddActivities extends Migration
 
         $db->exec("CREATE TABLE `activities` (
             `id` int(11) NOT NULL AUTO_INCREMENT,
-            `object_id` varchar(255) COLLATE latin1_german1_ci NOT NULL,
-            `context` enum('system','course','institute','user') COLLATE latin1_german1_ci NOT NULL,
-            `context_id` varchar(255) COLLATE latin1_german1_ci NOT NULL,
-            `provider` varchar(255) COLLATE latin1_german1_ci NOT NULL,
-            `actor_type` varchar(255) COLLATE latin1_german1_ci NOT NULL,
-            `actor_id` varchar(255) COLLATE latin1_german1_ci NOT NULL,
-            `verb` enum('answered','attempted','attended','completed','created','deleted','edited','experienced','failed','imported','interacted','passed','shared','sent','voided') COLLATE latin1_german1_ci NOT NULL DEFAULT 'experienced',
-            `content` text COLLATE latin1_german1_ci NULL,
-            `object_type` varchar(255) COLLATE latin1_german1_ci NOT NULL,
+            `object_id` varchar(255) NOT NULL,
+            `context` enum('system','course','institute','user') NOT NULL,
+            `context_id` varchar(32) NOT NULL,
+            `provider` varchar(255) NOT NULL,
+            `actor_type` varchar(255) NOT NULL,
+            `actor_id` varchar(255) NOT NULL,
+            `verb` enum('answered','attempted','attended','completed','created','deleted','edited','experienced','failed','imported','interacted','passed','shared','sent','voided') NOT NULL DEFAULT 'experienced',
+            `content` text NULL,
+            `object_type` varchar(255) NOT NULL,
             `mkdate` int(11) NOT NULL,
-            PRIMARY KEY (`id`)
-        )");
+            PRIMARY KEY (`id`),
+            KEY `context_id` (`context_id`),
+            KEY `mkdate` (`mkdate`)
+        ) ENGINE = InnoDB ROW_FORMAT=DYNAMIC");
     }
 
     public function down()
