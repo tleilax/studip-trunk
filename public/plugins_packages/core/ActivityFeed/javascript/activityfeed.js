67b9c1ac0132d154945687e2402d38b72225f35f
Till Glöggler <tgloeggl@uos.de>
2016-07-08 11:31:34 +0000

    autoload additional activities if styled window-height is not yet reached

--- public/plugins_packages/core/ActivityFeed/javascript/activityfeed.js
+++ public/plugins_packages/core/ActivityFeed/javascript/activityfeed.js
@@ -4,15 +4,18 @@
         polling: null,
         initial: true,
         scrolledfrom: null,
+        maxheight: null,
 
         init: function() {
-            STUDIP.ActivityFeed.loadFeed('' ,false, STUDIP.ActivityFeed.scrolledfrom);
+            STUDIP.ActivityFeed.maxheight = parseInt($('#stream-container').css('max-height').replace(/[^-\d\.]/g, ''));
+
+            STUDIP.ActivityFeed.loadFeed('');
 
             $('#stream-container').scroll(function () {
-                if((($('#stream-container').prop('scrollHeight') < ($('#stream-container').scrollTop() + 250))
-                        && !STUDIP.ActivityFeed.polling) && STUDIP.ActivityFeed.scrolledfrom ) {
-                    STUDIP.ActivityFeed.loadFeed('' ,true, STUDIP.ActivityFeed.scrolledfrom);
-                    STUDIP.ActivityFeed.polling = true;
+                var scrollBottom = $('#stream-container').scrollTop() + $('#stream-container').height() + 250;
+
+                if ($('#stream-container').prop('scrollHeight') < scrollBottom) {
+                    STUDIP.ActivityFeed.loadFeed('');
                 }
             });
         },
@@ -21,36 +24,40 @@
             return _.template($("script." + name).html());
         }),
 
-        loadFeed: function(filtertype, append, scrollfrom) {
+        loadFeed: function(filtertype) {
             if (STUDIP.ActivityFeed.user_id === null) {
                 console.log('Could not retrieve activities, no valid user id found!');
-                return;
+                return false;
             }
 
+            if (STUDIP.ActivityFeed.polling || !STUDIP.ActivityFeed.scrolledfrom) {
+                return false;
+            }
+
+            STUDIP.ActivityFeed.polling = true;
+
             $.ajax(STUDIP.URLHelper.resolveURL('api.php/user/' + STUDIP.ActivityFeed.user_id
-                    + '/activitystream?filtertype=' + filtertype + '&scrollfrom=' + scrollfrom), {
+                    + '/activitystream?filtertype=' + filtertype + '&scrollfrom=' + STUDIP.ActivityFeed.scrolledfrom), {
                 success: function(data) {
                     var stream        = STUDIP.ActivityFeed.getTemplate('activity_stream');
                     var activity      = STUDIP.ActivityFeed.getTemplate('activity');
                     var activity_urls = STUDIP.ActivityFeed.getTemplate('activity-urls');
                     var activities    = data;
 
-
-                    STUDIP.ActivityFeed.initial = false;
-
                     var num_entries   = Object.keys(activities).length;
-                    STUDIP.ActivityFeed.polling = false;
+
 
                     var lastelem = $(activities).last();
 
                     if(lastelem[0]) {
                         STUDIP.ActivityFeed.scrolledfrom  = lastelem[0].mkdate;
-                    } else STUDIP.ActivityFeed.scrolledfrom = false;
-
-                    if (!append) {            // replace data in DOM
-                        $('#stream-container').html('');
+                    } else {
+                        STUDIP.ActivityFeed.scrolledfrom = false;
                     }
 
+                    if (STUDIP.ActivityFeed.initial) {            // replace data in DOM
+                        $('#stream-container').html('');
+                    }
 
                     $('#stream-container').append(stream({
                         stream        : activities,
@@ -63,6 +70,15 @@
                     $('.provider_circle').click(function() {
                         $(this).parent().parent().children('.activity-content').toggle();
                     });
+
+                    STUDIP.ActivityFeed.initial = false;
+                    STUDIP.ActivityFeed.polling = false;
+
+
+                    if ($('#stream-container').height() < STUDIP.ActivityFeed.maxheight) {
+                        console.log('jetzatlet');
+                        STUDIP.ActivityFeed.loadFeed('');
+                    }
                 }
             });
 
