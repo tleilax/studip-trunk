9007a38a01b639d5fc237404368096c68d452e76
Jan-Hendrik Willms <tleilax+github@gmail.com>
2016-07-26 11:26:56 +0000

    activities: add system checks for api and route upon plugin enabling, re #5177

--- public/plugins_packages/core/ActivityFeed/ActivityFeed.php
+++ public/plugins_packages/core/ActivityFeed/ActivityFeed.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * @author      AndrÈ Klaﬂen <klassen@elan-ev.de>
- * @author      Till Glˆggler <tgloeggl@uos.de>
+ * @author      Andr√© Kla√üen <klassen@elan-ev.de>
+ * @author      Till Gl√∂ggler <tgloeggl@uos.de>
  * @license     GPL 2 or later
  */
 class ActivityFeed extends StudIPPlugin implements PortalPlugin
 {
     public function getPluginName()
     {
-        return _('Aktivit‰ten');
+        return _('Aktivit√§ten');
     }
 
     public function getPortalTemplate()
@@ -23,5 +23,34 @@ class ActivityFeed extends StudIPPlugin implements PortalPlugin
         $template->scrolledfrom = strtotime('+1 day');
 
         return $template;
+    }
+
+    public static function onEnable($plugin_id)
+    {
+        $errors = [];
+        if (!Config::get()->API_ENABLED) {
+            $errors[] = sprintf(
+                _('Die REST-API ist nicht aktiviert (%s "API_ENABLED")'),
+                formatReady(sprintf('[%s]%s',
+                    _('Konfiguration'),
+                    URLHelper::getLink('dispatch.php/admin/configuration/configuration')
+                ))
+            );
+        } elseif (!RESTAPI\ConsumerPermissions::get('global')->check('/user/:user_id/activitystream', 'get')) {
+            $errors[] = sprintf(
+                _('Die REST-API-Route ist nicht aktiviert (%s "/user/:user_id/activitystream"")'),
+                formatReady(sprintf('[%s]%s',
+                    _('Konfiguration'),
+                    URLHelper::getLink('dispatch.php/admin/api/permissions')
+                ))
+            );
+        }
+
+        if (count($errors) > 0) {
+            PageLayout::postInfo(
+                _('Das Aktivit√§ten-Plugin konnte nicht vollst√§ndig aktiviert werden.'),
+                $errors
+            );
+        }
     }
 }
