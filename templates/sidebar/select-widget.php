1ead6aae945e7852c72b8ea417268662595b69dd
Jan-Hendrik Willms <tleilax+github@gmail.com>
2016-08-11 15:58:55 +0000

    SelectGroupElement is now derived from SelectElement, re #6801

--- templates/sidebar/select-widget.php
+++ templates/sidebar/select-widget.php
@@ -4,18 +4,18 @@
 <? endforeach; ?>
     <select class="sidebar-selectlist <?= $class ?> <? if ($__is_nested): ?>nested-select<? endif; ?>" name="<?= htmlReady($name) ?>" <? if ($size) printf('size="%u"', $size); ?> <?= $attributes ?>>
     <? foreach ($elements as $element): ?>
-        <? if ($element instanceof SelectElement): ?>
-            <option value="<?= htmlReady($element->getId()) ?>" <? if ($element->isActive()) echo 'selected'; ?> class="<? if ($element->getIndentLevel()): ?>nested-item nested-item-level-<?= $element->getIndentLevel() + 1 ?><? endif; ?> <? if ($element->isHeader()): ?>nested-item-header<? endif; ?>">
-                <?= htmlReady(my_substr($element->getLabel(), 0, $max_length)) ?>
-            </option>
-        <? elseif (count($element->getElements()) > 0): ?>
+        <? if ($element instanceof SelectGroupElement && count($element->getElements()) > 0): ?>
             <optgroup label="<?= htmlReady($element->getLabel() ) ?>">
             <? foreach ($element->getElements() as $option): ?>
-                <option value="<?= htmlReady($option->getId()) ?>" <? if ($option->isActive()) echo 'selected'; ?> class="<? if ($element->getIndentLevel()): ?>nested-item nested-item-level-<?= $element->getIndentLevel() + 1 ?><? endif; ?>  <? if ($element->isHeader()): ?>nested-item-header<? endif; ?>">">
+                <option value="<?= htmlReady($option->getId()) ?>" <? if ($option->isActive()) echo 'selected'; ?> class="<? if ($element->getIndentLevel()): ?>nested-item nested-item-level-<?= $element->getIndentLevel() + 1 ?><? endif; ?>  <? if ($element->isHeader()): ?>nested-item-header<? endif; ?>">
                     <?= htmlReady(my_substr($option->getLabel(), 0, $max_length)) ?>
                 </option>
             <? endforeach; ?>
             </optgroup>
+        <? elseif (!($element instanceof SelectGroupElement)): ?>
+            <option value="<?= htmlReady($element->getId()) ?>" <? if ($element->isActive()) echo 'selected'; ?> class="<? if ($element->getIndentLevel()): ?>nested-item nested-item-level-<?= $element->getIndentLevel() + 1 ?><? endif; ?> <? if ($element->isHeader()): ?>nested-item-header<? endif; ?>">
+                <?= htmlReady(my_substr($element->getLabel(), 0, $max_length)) ?>
+            </option>
         <? endif; ?>
     <? endforeach; ?>
     </select>
