28ea04a6ced7e0c329de01c2f4bc3ddd34d2173c
Timo Hartge <hartge@data-quest.de>
2016-08-25 09:39:09 +0000

    modul description dialog link on modul search result, fixes #6828

--- public/plugins_packages/core/Modulverwaltung/views/search/module/_modul.php
+++ public/plugins_packages/core/Modulverwaltung/views/search/module/_modul.php
@@ -1,8 +1,12 @@
 <tbody  class="<?= ($modul_id == $modul->id ? 'not-collapsed' : 'collapsed') ?>">
     <tr class="table-header header-row" id="modul_<?= $modul->id ?>">
             <td class="toggle-indicator">
-                <a class="mvv-load-in-new-row" href="<?= $controller->url_for('/details/' . $modul->id . '/' . '#' . $modul->id) ?>">
+                <a class="mvv-load-in-new-row" style="display: inline;" href="<?= $controller->url_for('/details/' . $modul->id . '/' . '#' . $modul->id) ?>">
                     <?= htmlReady($modul->getDisplayName(true, true)) ?>
+                </a> 
+                <a style="display: inline; background-image: none; padding-left: 5px; "
+                	data-dialog title="<?= htmlReady($modul->getDisplayName(true, true)) . ' (' . _('Vollständige Modulbeschreibung') . ')' ?>" href="<?= $controller->url_for('shared/modul/description/' . $modul->id) ?>">
+                    <?= Assets::img('icons/16/blue/info-circle.png') ?>
                 </a>
             </td>
             <td class="dont-hide">
