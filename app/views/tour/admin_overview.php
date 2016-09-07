e6efdab101d2cb352ea1966e6f0ada490c8d42f4
David Siegfried <ds.siegfried@gmail.com>
2016-09-07 09:58:39 +0000

    cleanup, add actionmenu, replace form-css, re #6894, re #6823

--- app/views/tour/admin_overview.php
+++ app/views/tour/admin_overview.php
@@ -1,90 +1,101 @@
 <? use Studip\Button, Studip\LinkButton; ?>
-<?=$delete_question?>
-<div class="tour_admin">
-<h2><?= _('Verwalten von Touren') ?></h2>
-<table cellspacing="0" cellpadding="0" width="100%">
-    <tr>
-        <td width="100%" class="blank"><p class="info">
-        <form action="<?=$controller->url_for('tour/admin_overview')?>" id="admin_tour_form" method="POST">
-        <input type="hidden" name="tour_filter" value="set">
-        <input type="hidden" name="tour_filter_term" value="<?=htmlReady($tour_searchterm)?>">
-        <?=CSRFProtection::tokenTag(); ?>
+<?= $delete_question ?>
+<form action="<?= $controller->url_for('tour/admin_overview') ?>" id="admin_tour_form" method="POST">
+    <input type="hidden" name="tour_filter" value="set">
+    <input type="hidden" name="tour_filter_term" value="<?= htmlReady($tour_searchterm) ?>">
+    <?= CSRFProtection::tokenTag(); ?>
+    <? if ($filter_text) : ?>
         <table class="default">
-            <? if ($filter_text) : ?>
-                <tr><td colspan="1">
-                <?=$filter_text?>
-                </td><td><div class="tour_reset_filter">
-                <?=Button::create(_('Auswahl aufheben'), 'reset_filter')?>
-                </div>
-                </td></tr>
-            <? endif ?>
+            <tr>
+                <td colspan="1">
+                    <?= $filter_text ?>
+                </td>
+                <td>
+                    <div class="tour_reset_filter">
+                        <?= Button::create(_('Auswahl aufheben'), 'reset_filter') ?>
+                    </div>
+                </td>
+            </tr>
         </table>
+    <? endif ?>
+
+    <table class="default">
+        <caption>
+            <div class="tour_list_title"><?= _('Touren') ?></div>
+        </caption>
+        <colgroup>
+            <col width="20">
+            <col>
+            <col width="10%">
+            <col width="10%">
+            <col width="10%">
+            <col width="20%">
+            <col width="10%">
+            <col width="80">
+        </colgroup>
+        <thead>
+            <tr>
+                <th><?= _('Aktiv') ?></th>
+                <th><?= _('Überschrift') ?></th>
+                <th><?= _('Sprache') ?></th>
+                <th><?= _('Typ') ?></th>
+                <th><?= _('Zugang') ?></th>
+                <th><?= _('Startseite') ?></th>
+                <th><?= _('Anzahl der Schritte') ?></th>
+                <th><?= _('Aktion') ?></th>
+            </tr>
+        </thead>
         <? if (count($tours)) : ?>
-            <table class="default">
-                <caption>
-                    <div class="tour_list_title"><?=_('Touren')?></div>
-                </caption>
-                <colgroup>
-                    <col width="20">
-                    <col>
-                    <col width="10%">
-                    <col width="10%">
-                    <col width="10%">
-                    <col width="20%">
-                    <col width="10%">
-                    <col width="80">
-                </colgroup>                   
-                <thead><tr>
-                    <th><?=_("Aktiv")?></th>
-                    <th><?=_("Überschrift")?></th>
-                    <th><?=_("Sprache")?></th>
-                    <th><?=_("Typ")?></th>
-                    <th><?=_("Zugang")?></th>
-                    <th><?=_("Startseite")?></th>
-                    <th><?=_("Anzahl der Schritte")?></th>
-                    <th><?=_("Aktion")?></th>
-                </tr></thead>
-                <tbody>
+            <tbody>
                 <? foreach ($tours as $tour_id => $tour) : ?>
                     <tr>
-                    <td><input type="CHECKBOX" name="tour_status_<?=$tour_id?>" value="1" aria-label="<?= _('Status der Tour (aktiv oder inaktiv)')?>" <?=tooltip(_("Status der Tour (aktiv oder inaktiv)"),false)?><?=($tour->settings->active) ? ' checked' : ''?>></td>
-                    <td><a href="<?=URLHelper::getURL('dispatch.php/tour/admin_details/'.$tour_id)?>">
-                    <?=htmlReady($tour->name)?>
-                    </a></td>
-                    <td><?=$tour->language?></td>
-                    <td><?=$tour->type?></td>
-                    <td><?=$tour->settings->access?></td>
-                    <td><?=(count($tour->steps)) ? htmlReady($tour->steps[0]->route) : ''?></td>
-                    <td><?=count($tour->steps)?></td>
-                    <td>
-                    <a href="<?=URLHelper::getURL('dispatch.php/tour/admin_details/'.$tour_id)?>" <?=tooltip(_('Tour bearbeiten'))?>>
-                    <?= Icon::create('edit', 'clickable')->asImg() ?></a>
-                    <?= Icon::create('trash', 'clickable', ['title' => _('Tour löschen')])->asInput(array('name'=>'tour_remove_'.$tour_id,)) ?>
-                    </td>
+                        <td>
+                            <input type="checkbox" name="tour_status_<?= $tour_id ?>" value="1"
+                                   aria-label="<?= _('Status der Tour (aktiv oder inaktiv)') ?>" <?= tooltip(_("Status der Tour (aktiv oder inaktiv)"), false) ?><?= ($tour->settings->active) ? ' checked' : '' ?>>
+                        </td>
+                        <td>
+                            <a href="<?= URLHelper::getURL('dispatch.php/tour/admin_details/' . $tour_id) ?>">
+                                <?= htmlReady($tour->name) ?>
+                            </a>
+                        </td>
+                        <td><?= $tour->language ?></td>
+                        <td><?= $tour->type ?></td>
+                        <td><?= $tour->settings->access ?></td>
+                        <td><?= (count($tour->steps)) ? htmlReady($tour->steps[0]->route) : '' ?></td>
+                        <td><?= count($tour->steps) ?></td>
+                        <td class="actions">
+                            <? $actionMenu = ActionMenu::get() ?>
+                            <? $actionMenu->addLink(
+                                    $controller->url_for('tour/admin_details/' . $tour_id),
+                                    _('Tour bearbeiten'),
+                                    Icon::create('edit', 'clickable', ['title' => _('Tour bearbeiten')]))
+                            ?>
+                            <? $actionMenu->addButton(
+                                    'tour_remove_' . $tour_id,
+                                    _('Tour löschen'),
+                                    Icon::create('trash', 'clickable', ['title'        => _('Tour löschen')]))
+                            ?>
+                            <? Icon::create('trash', 'clickable', ['title' => _('Tour löschen')])->asInput(['name' => 'tour_remove_' . $tour_id,]) ?>
+                            <?= $actionMenu->render() ?>
+                        </td>
                     </tr>
                 <? endforeach ?>
-                </tbody>
-                <tfoot>
-                <tr><td colspan="8">
-                <?=Button::createAccept(_('Speichern'), 'save_tour_settings') ?>
-                </td></tr></tfoot>
-            </table>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="8">
+                        <?= Button::createAccept(_('Speichern'), 'save_tour_settings') ?>
+                    </td>
+                </tr>
+            </tfoot>
         <? else : ?>
-            <?=_('Keine Touren vorhanden.')?>
+            <tbody>
+                <tr>
+                    <td colspan="8" style="text-align: center">
+                        <?= _('Keine Touren vorhanden.') ?>
+                    </td>
+                </tr>
+            </tbody>
         <? endif ?>
-        </form><br><br></p></td>
-    </tr>
-</table>
-</div>
-<?
-$sidebar = Sidebar::get();
-$widget = new ViewsWidget();
-$widget->addLink(_('Übersicht'), URLHelper::getURL('dispatch.php/tour/admin_overview'))->setActive(true);
-$widget->addLink(_('Konflikte'), URLHelper::getURL('dispatch.php/tour/admin_conflicts'));
-$sidebar->addWidget($widget);
-$widget = new ActionsWidget();
-$widget->addLink(_('Tour erstellen'), URLHelper::getLink('dispatch.php/tour/admin_details'), Icon::create('add', 'clickable'), array());
-$sidebar->addWidget($widget);
-$search = new SearchWidget('?apply_tour_filter=1');
-$search->addNeedle(_('Suchbegriff'), 'tour_searchterm');
-$sidebar->addWidget($search);
+    </table>
+</form>
--- app/views/tour/edit_step.php
+++ app/views/tour/edit_step.php
@@ -1,105 +1,135 @@
-<? use Studip\Button, Studip\LinkButton ?>
+<? use Studip\Button, Studip\LinkButton; ?>
 <div id="edit_tour_step" class="edit_tour_step">
-<? if (Request::isXhr()) : ?>
-    <? foreach (PageLayout::getMessages() as $msg) : ?>
-        <?=$msg?>
-    <? endforeach ?>
-<? endif ?>
-<form id="edit_tour_form" class="studip_form" action="<?=URLHelper::getURL('dispatch.php/tour/edit_step/'.$tour_id.'/'.$step->step.'/save')?>" method="POST">
-    <?=CSRFProtection::tokenTag(); ?>
-    <fieldset>
-        <input type="hidden" name="tour_step_nr" value="<?=$step->step?>">
-        <input type="hidden" name="tour_step_editmode" value="<?=$mode?>">
-        <legend><?= sprintf(_('Schritt %s'), $step->step) ?></legend>
-        <label for="step_title" class="caption">
-            <?= _('Titel:') ?>
-        </label>
-        <input type="text" size="60" maxlength="255" name="step_title"
-            value="<?= $step ? htmlReady($step->title) : '' ?>"
-            placeholder="<?= _('Bitte geben Sie einen Titel für den Schritt an') ?>"/>
-        <label for="step_tip" class="caption">
-            <?= _('Inhalt:') ?>
-        </label>
-        <textarea cols="60" rows="5" name="step_tip"
-            placeholder="<?= _('Bitte geben Sie den Text für diesen Schritt ein') ?>"><?= $step ? htmlReady($step->tip) : '' ?></textarea>
-        <label for="step?interactive" class="caption">
-            <?= _('Art:') ?>
-            <span class="required">*</span>
-        </label>
-        <select name="step_interactive">
-            <option value="0" <?= $step->interactive == 0 ? ' selected="selected"' : '' ?>>
-            <?= _('Geführt') ?>
-            </option>
-            <option value="1" <?= $step->interactive == 1 ? ' selected="selected"' : '' ?>>
-            <?= _('Interaktiv') ?>
-            </option>
-        </select>
-        <? if ($force_route) : ?>
-            <input type="hidden" name="step_route" value="<?= $force_route ?>">
-            <input type="hidden" name="step_css" value="<?= $step->css_selector ?>">
-        <? else : ?>
-            <label for="step_route" class="caption">
-                <?= _('Seite:') ?>
-                <span class="required">*</span>
+    <? if (Request::isXhr()) : ?>
+        <? foreach (PageLayout::getMessages() as $msg) : ?>
+            <?= $msg ?>
+        <? endforeach ?>
+    <? endif ?>
+    <form id="edit_tour_form" class="default"
+          action="<?= $controller->url_for('tour/edit_step/' . $tour_id . '/' . $step->step . '/save') ?>"
+          method="post">
+        <?= CSRFProtection::tokenTag(); ?>
+        <fieldset>
+            <input type="hidden" name="tour_step_nr" value="<?= $step->step ?>">
+            <input type="hidden" name="tour_step_editmode" value="<?= $mode ?>">
+            <legend><?= sprintf(_('Schritt %s'), $step->step) ?></legend>
+            <label>
+                <?= _('Titel:') ?>
+                <input type="text" size="60" maxlength="255" name="step_title"
+                       value="<?= $step ? htmlReady($step->title) : '' ?>"
+                       placeholder="<?= _('Bitte geben Sie einen Titel für den Schritt an') ?>">
             </label>
-            <input type="text" size="60" maxlength="255" name="step_route"
-                value="<?= $step ? htmlReady($step->route) : '' ?>"
-                placeholder="<?= _('Route für den Schritt (z.B. "dispatch.php/profile")') ?>"/>
-            <label for="step_css" class="caption">
-                <?= _('CSS-Selektor:') ?>
+
+            <label>
+                <?= _('Inhalt:') ?>
+                <textarea cols="60" rows="5" name="step_tip"
+                          placeholder="<?= _('Bitte geben Sie den Text für diesen Schritt ein') ?>"><?= $step ? htmlReady($step->tip) : '' ?></textarea>
             </label>
-            <input type="text" size="60" maxlength="255" name="step_css"
-                value="<?= $step ? htmlReady($step->css_selector) : '' ?>"
-                placeholder="<?= _('Selektor, an dem der Schritt angezeigt wird') ?>"/>
+
+            <label>
+                <span class="required"><?= _('Art:') ?></span>
+                <select name="step_interactive">
+                    <option value="0" <?= $step->interactive == 0 ? ' selected="selected"' : '' ?>>
+                        <?= _('Geführt') ?>
+                    </option>
+                    <option value="1" <?= $step->interactive == 1 ? ' selected="selected"' : '' ?>>
+                        <?= _('Interaktiv') ?>
+                    </option>
+                </select>
+            </label>
+
+            <? if ($force_route) : ?>
+                <input type="hidden" name="step_route" value="<?= $force_route ?>">
+                <input type="hidden" name="step_css" value="<?= $step->css_selector ?>">
+            <? else : ?>
+                <label for="step_route" class="caption">
+                    <span class="required"><?= _('Seite:') ?></span>
+                    <input type="text" size="60" maxlength="255" name="step_route"
+                           value="<?= $step ? htmlReady($step->route) : '' ?>"
+                           placeholder="<?= _('Route für den Schritt (z.B. "dispatch.php/profile")') ?>">
+                </label>
+                <label>
+                    <?= _('CSS-Selektor:') ?>
+                    <input type="text" size="60" maxlength="255" name="step_css"
+                           value="<?= $step ? htmlReady($step->css_selector) : '' ?>"
+                           placeholder="<?= _('Selektor, an dem der Schritt angezeigt wird') ?>"/>
+                </label>
+
+            <? endif ?>
+        </fieldset>
+        <? if ($step->css_selector) : ?>
+            <fieldset>
+                <legend><?= _('Orientierung:') ?></legend>
+                <div class="tour_step_orientation"
+                     style="<?= $step->css_selector ? 'display: block' : 'display: none' ?>">
+                    <table>
+                        <tr>
+                            <td></td>
+                            <td><input type="radio" name="step_orientation"
+                                       value="TL" <?= ($step->orientation == 'TL') ? 'checked' : '' ?>><?= _('oben (links)') ?>
+                            </td>
+                            <td><input type="radio" name="step_orientation"
+                                       value="T" <?= ($step->orientation == 'T') ? 'checked' : '' ?>><?= _('oben') ?>
+                            </td>
+                            <td><input type="radio" name="step_orientation"
+                                       value="TR" <?= ($step->orientation == 'TR') ? 'checked' : '' ?>><?= _('oben (rechts)') ?>
+                            </td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td><input type="radio" name="step_orientation"
+                                       value="LT" <?= ($step->orientation == 'LT') ? 'checked' : '' ?>><?= _('links (oben)') ?>
+                            </td>
+                            <td colspan="3"></td>
+                            <td><input type="radio" name="step_orientation"
+                                       value="RT" <?= ($step->orientation == 'RT') ? 'checked' : '' ?>><?= _('rechts (oben)') ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><input type="radio" name="step_orientation"
+                                       value="L" <?= ($step->orientation == 'L') ? 'checked' : '' ?>><?= _('links') ?>
+                            </td>
+                            <td colspan="3" style="text-align: center"><?= _('Selektiertes Element') ?></td>
+                            <td><input type="radio" name="step_orientation"
+                                       value="R" <?= ($step->orientation == 'R') ? 'checked' : '' ?>><?= _('rechts') ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><input type="radio" name="step_orientation"
+                                       value="RB" <?= ($step->orientation == 'RB') ? 'checked' : '' ?>><?= _('links (unten)') ?>
+                            </td>
+                            <td colspan="3"></td>
+                            <td><input type="radio" name="step_orientation"
+                                       value="LB" <?= ($step->orientation == 'LB') ? 'checked' : '' ?>><?= _('rechts (unten)') ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><input type="radio" name="step_orientation"
+                                       value="BL" <?= ($step->orientation == 'BL') ? 'checked' : '' ?>><?= _('unten (links)') ?>
+                            </td>
+                            <td><input type="radio" name="step_orientation"
+                                       value="B" <?= ($step->orientation == 'B') ? 'checked' : '' ?>><?= _('unten') ?>
+                            </td>
+                            <td><input type="radio" name="step_orientation"
+                                       value="BR" <?= ($step->orientation == 'BR') ? 'checked' : '' ?>><?= _('unten (rechts)') ?>
+                            </td>
+                            <td></td>
+                        </tr>
+                    </table>
+                </div>
+            </fieldset>
         <? endif ?>
-        <label for="step_orientation" class="caption tour_step_orientation" style="<?=$step->css_selector ? 'display: block' : 'display: none' ?>">
-            <?= _('Orientierung:') ?>
-        </label>
-        <div class="tour_step_orientation" style="<?=$step->css_selector ? 'display: block' : 'display: none' ?>">
-            <table>
-                <tr>
-                    <td></td>
-                    <td><input type="radio" name="step_orientation" value="TL" <?= ($step->orientation == 'TL') ? 'checked' : ''?>><?=_('oben (links)')?></td>
-                    <td><input type="radio" name="step_orientation" value="T" <?= ($step->orientation == 'T') ? 'checked' : ''?>><?=_('oben')?></td>
-                    <td><input type="radio" name="step_orientation" value="TR" <?= ($step->orientation == 'TR') ? 'checked' : ''?>><?=_('oben (rechts)')?></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td><input type="radio" name="step_orientation" value="LT" <?= ($step->orientation == 'LT') ? 'checked' : ''?>><?=_('links (oben)')?></td>
-                    <td colspan="3"></td>
-                    <td><input type="radio" name="step_orientation" value="RT" <?= ($step->orientation == 'RT') ? 'checked' : ''?>><?=_('rechts (oben)')?></td>
-                </tr>
-                <tr>
-                    <td><input type="radio" name="step_orientation" value="L" <?= ($step->orientation == 'L') ? 'checked' : ''?>><?=_('links')?></td>
-                    <td colspan="3" style="text-align: center"><?=_('Selektiertes Element')?></td>
-                    <td><input type="radio" name="step_orientation" value="R" <?= ($step->orientation == 'R') ? 'checked' : ''?>><?=_('rechts')?></td>
-                </tr>
-                <tr>
-                    <td><input type="radio" name="step_orientation" value="RB" <?= ($step->orientation == 'RB') ? 'checked' : ''?>><?=_('links (unten)')?></td>
-                    <td colspan="3"></td>
-                    <td><input type="radio" name="step_orientation" value="LB" <?= ($step->orientation == 'LB') ? 'checked' : ''?>><?=_('rechts (unten)')?></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><input type="radio" name="step_orientation" value="BL" <?= ($step->orientation == 'BL') ? 'checked' : ''?>><?=_('unten (links)')?></td>
-                    <td><input type="radio" name="step_orientation" value="B" <?= ($step->orientation == 'B') ? 'checked' : ''?>><?=_('unten')?></td>
-                    <td><input type="radio" name="step_orientation" value="BR" <?= ($step->orientation == 'BR') ? 'checked' : ''?>><?=_('unten (rechts)')?></td>
-                    <td></td>
-                </tr>
-            </table>
-        </div>
-        <br>
-        <div "data-dialog-button" = "1">
+        <footer data-dialog-button>
             <?= CSRFProtection::tokenTag() ?>
             <? if ($via_ajax): ?>
-                <?= Button::create(_('Speichern'), 'confirm', array('data-dialog' => '1', 'data-dialog-button' => '1')) ?>
+                <?= Button::create(_('Speichern'), 'confirm', ['data-dialog' => '1', 'data-dialog-button' => '1']) ?>
             <? else: ?>
                 <?= Button::createAccept(_('Speichern'), 'submit') ?>
-                <?= LinkButton::createCancel(_('Abbrechen'), $controller->url_for('tour/admin_overview'), array()) ?>
+                <?= LinkButton::createCancel(_('Abbrechen'), $controller->url_for('tour/admin_overview'), []) ?>
             <? endif; ?>
-        </div>
-    </fieldset>
-</form>
+        </footer>
+    </form>
 </div>
 <script>
     jQuery(document).on('change', 'input[name=step_css]', function (event) {
@@ -112,14 +142,5 @@
         jQuery('#tour_controls').hide();
         jQuery('#tour_tip').hide();
         jQuery('#selector_overlay').hide();
-        jQuery('#edit_tour_step').parent().dialog({
-            beforeClose: function( event, ui ) {
-                jQuery('#tour_controls').show();
-                jQuery('#tour_tip').show();
-                <? if ($step->css_selector) : ?>
-                    jQuery('#selector_overlay').show();
-                <? endif ?>
-            }
-        });
     }
 </script>
