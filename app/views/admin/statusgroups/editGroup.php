d1f0425efcc83be165aacd02c9e54eeab76750d6
David Siegfried <ds.siegfried@gmail.com>
2016-09-09 07:21:33 +0000

    add new form-css, re #6894

--- app/views/admin/statusgroups/editGroup.php
+++ app/views/admin/statusgroups/editGroup.php
@@ -1,24 +1,24 @@
-<form class="studip_form" action="<?= $controller->url_for('admin/statusgroups/editGroup/' . $group->id) ?>#group-<?= $group->id ?>" method="post">
-    <label class="caption">
-        <?= _('Gruppenname') ?>
+<form class="default" action="<?= $controller->url_for('admin/statusgroups/editGroup/' . $group->id) ?>#group-<?= $group->id ?>" method="post">
+    <label>
+       <span class="required"><?= _('Gruppenname') ?></span>
         <input required type="text" name="name" class="groupname" size="50"
                 value="<?= htmlReady($group->name) ?>"
                 placeholder="<?= _('Mitarbeiterinnen und Mitarbeiter') ?>">
     </label>
-    <label class="caption">
+    <label>
         <?= _('Weibliche Bezeichnung') ?>
         <input type="text" name="name_w" size="50"
                 value="<?= htmlReady($group->name_w) ?>"
                placeholder="<?= _('Mitarbeiterin') ?>">
     </label>
-    <label class="caption">
+    <label>
         <?= _('Männliche Bezeichnung') ?>
         <input type="text" name="name_m" size="50"
                value="<?= htmlReady($group->name_m) ?>"
                placeholder="<?= _('Mitarbeiter') ?>">
     </label>
 <? if ($type['needs_size']): ?>
-    <label class="caption">
+    <label>
         <?= _('Größe') ?>
         <input name="size" type="text" size="10"
                value="<?= htmlReady($group->size) ?>"
@@ -26,27 +26,27 @@
     </label>
 <? endif; ?>
 <? foreach ($group->getDatafields() as $field): ?>
-    <label class="caption">
+    <label>
         <?= htmlReady($field->getName()) ?>
         <?= $field->getHTML('datafields') ?>
     </label>
 <? endforeach; ?>
 <? if ($type['needs_self_assign']): ?>
-    <label class="caption">
+    <label>
         <?= _('Selbsteintrag') ?>
         <input name="selfassign" type="checkbox" value="1"
                <? if ($group->selfassign) echo 'checked'; ?>>
     </label>
 <? endif; ?>
     <noscript>
-        <label class="caption">
+        <label>
             <?= _('Position') ?>
             <input name="size" type="text" size="10"
                    value="<?= htmlReady($group->position) ?>"
                    placeholder="0">
         </label>
     </noscript>
-    <label class="caption">
+    <label>
         <?= _('Einordnen unter') ?>
         <select name="range_id" class="nested-select">
             <option value="<?= htmlReady($_SESSION['SessionSeminar']) ?>">
@@ -55,8 +55,8 @@
             <?= $this->render_partial("admin/statusgroups/_edit_subgroupselect.php", array('groups' => $groups, 'selected' => $group, 'level' => 0)) ?>
         </select>
     </label>
-    <div data-dialog-button>
+    <footer data-dialog-button>
         <?= Studip\Button::createAccept(_('Speichern'), 'save') ?>
         <?= Studip\LinkButton::createCancel(_('Abbrechen'), $controller->url_for('admin/statusgroups')) ?>
-    </div>
+    </footer>
 </form>
