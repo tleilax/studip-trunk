c8513fd0e72ffa1e23512bc4a7d03a66bdfa9520
David Siegfried <ds.siegfried@gmail.com>
2016-09-09 06:48:06 +0000

    add new form, re #6894

--- app/views/admin/api/config.php
+++ app/views/admin/api/config.php
@@ -1,18 +1,18 @@
 <? use Studip\Button, Studip\LinkButton; ?>
 
-<form class="studip_form" action="<?= $controller->url_for('admin/api/config') ?>" method="post">
+<form class="default" action="<?= $controller->url_for('admin/api/config') ?>" method="post">
     <fieldset>
-        <legend><?= _('Einstellungen') ?></legend>
+        <legend><?= _('Konfiguration') ?></legend>
+
+        <input type="hidden" name="active" value="0">
+        <label>
+            <input type="checkbox" name="active" value="1" <? if ($config['API_ENABLED']) echo 'checked'; ?>>
+            <?= _('REST-API aktiviert') ?>
+        </label>
         
-        <div class="studip_input_wrapper">
-            <input type="hidden" name="active" value="0">
-            <label>
-                
-                <input type="checkbox" name="active" value="1" <? if ($config['API_ENABLED']) echo 'checked'; ?>>
-            <?= _('REST-API aktiviert') ?></label>
-        </div>
         
-        <label class="caption" for="auth"><?= _('Standard-Authentifizierung beim Login') ?>
+        <label class="caption" for="auth">
+            <?= _('Standard-Authentifizierung beim Login') ?>
             <select name="auth" id="auth">
             <? foreach ($GLOBALS['STUDIP_AUTH_PLUGIN'] as $plugin): ?>
                 <option <? if ($config['API_OAUTH_AUTH_PLUGIN'] === $plugin) echo 'selected'; ?>>
@@ -22,8 +22,8 @@
             </select>
         </label>
     </fieldset>
-    <div class="submit_wrapper">
+    <footer>
         <?= Button::createAccept(_('Speichern')) ?>
         <?= LinkButton::createCancel(_('Abbrechen'), $controller->url_for('admin/api')) ?>
-    </div>
+    </footer>
 </form>
