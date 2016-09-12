d303b1cc6303915dd61de4ee3978b877f5b37735
David Siegfried <ds.siegfried@gmail.com>
2016-09-12 12:27:41 +0000

    migrate to new default-forms, re #6894

--- app/views/admin/configuration/edit_user_config.php
+++ app/views/admin/configuration/edit_user_config.php
@@ -3,38 +3,22 @@
     <?= htmlReady(User::find($user_id)->getFullname()) ?>
 </h2>
 
-<form action="<?= $controller->url_for('admin/configuration/edit_user_config/' . $user_id . '?id=' . $field) ?>" method="post" data-dialog>
+<form action="<?= $controller->url_for('admin/configuration/edit_user_config/' . $user_id . '?id=' . $field) ?>" method="post" data-dialog class="default">
     <?= CSRFProtection::tokenTag() ?>
 
-<table class="default">
-    <tbody>
-        <tr>
-            <td><?= _('Name:') ?> (<em>field</em>)</td>
-            <td><?= htmlReady($field) ?></td>
-        </tr>
-        <tr>
-            <td>
-                <label for="item-value">
-                    <?= _('Inhalt:') ?> (<em>value</em>)
-                </label>
-            </td>
-            <td>
-                <?= $this->render_partial('admin/configuration/type-edit.php', $config) ?>
-            </td>
-        </tr>
-        <tr>
-            <td><?= _('Beschreibung:') ?> (<em>description</em>)</td>
-            <td><?= htmlReady($config['description']) ?></td>
-        </tr>
-    </tbody>
-    <tfoot data-dialog-button>
-        <tr>
-            <td colspan="2">
-                <?= Studip\Button::createAccept(_('Speichern')) ?>
-                <?= Studip\LinkButton::createCancel(_('Abbrechen'),
-                        $controller->url_for('admin/configuration/user_configuration', compact('user_id'))) ?>
-            </td>
-        </tr>
-    </tfoot>
-</table>
+    <fieldset>
+        <legend>
+            <?= htmlReady($field) ?>
+        </legend>
+        <?= $this->render_partial('admin/configuration/type-edit.php', $config) ?>
+        <label>
+            <?= _('Beschreibung:') ?> (<em>description</em>)
+            <textarea name="descriptio" readonly><?= htmlReady($config['description']) ?></textarea>
+        </label>
+    </fieldset>
+    <footer data-dialog-button>
+        <?= Studip\Button::createAccept(_('Speichern')) ?>
+        <?= Studip\LinkButton::createCancel(_('Abbrechen'),
+                $controller->url_for('admin/configuration/user_configuration', compact('user_id'))) ?>
+    </footer>
 </form>
