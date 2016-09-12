3178c73fd2ace499ff69b61e451c2ebe7a9033b1
David Siegfried <ds.siegfried@gmail.com>
2016-09-12 11:18:14 +0000

    replace studip_form with default, re #6894

--- app/views/course/admission/_change_admission.php
+++ app/views/course/admission/_change_admission.php
@@ -1,13 +1,13 @@
 <? foreach(PageLayout::getMessages() as $pm) : ?>
     <?= $pm ?>
 <? endforeach; ?>
-<form class="studip_form" action="<?= $controller->link_for() ?>" method="post">
+<form class="default" action="<?= $controller->link_for() ?>" method="post">
 <?= CSRFProtection::tokenTag()?>
 <? foreach(array_filter($request, function ($r) {return $r !== false;}) as $k => $v) : ?>
     <?= addHiddenFields($k, $v) ?>
 <? endforeach ?>
-<div data-dialog-button>
+<footer data-dialog-button>
     <?= Studip\Button::create(_("Ja"), $button_yes, array('data-dialog' => ''))?>
     <?= Studip\Button::create(_("Nein"), $button_no, array('data-dialog' => ''))?>
-</div>
+</footer>
 </form>
