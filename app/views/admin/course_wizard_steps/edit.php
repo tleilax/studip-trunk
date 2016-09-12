5691e93108b5cf61510199c6eb784815b4187328
David Siegfried <ds.siegfried@gmail.com>
2016-09-12 11:49:37 +0000

    migrate from studip_form to default

--- app/views/admin/course_wizard_steps/edit.php
+++ app/views/admin/course_wizard_steps/edit.php
@@ -1,38 +1,38 @@
-<form class="studip_form" action="<?= $controller->url_for('admin/coursewizardsteps/save', $step->id) ?>" method="post">
-    <label class="caption">
-        <?= _('Name des Schrittes') ?>:
-        <input type="text" name="name" size="50" maxlength="255" value="<?= htmlReady($step->name) ?>" required/>
+<form class="default" action="<?= $controller->url_for('admin/coursewizardsteps/save', $step->id) ?>" method="post">
+    <?= CSRFProtection::tokenTag() ?>
+    <label>
+        <span class="required"><?= _('Name des Schrittes') ?></span>
+        <input type="text" name="name" size="50" maxlength="255" value="<?= htmlReady($step->name) ?>" required>
     </label>
-    <label class="caption" for="classname">
-        <?= _('PHP-Klasse') ?>:
+    <label for="classname">
+        <span class="required"><?= _('PHP-Klasse') ?></span>
         <input type="text" name="classname" size="50" maxlength="255" value="<?=
-            htmlReady($step->classname) ?>" required/>
+        htmlReady($step->classname) ?>" required>
     </label>
     <? if ($availableClasses && count($availableClasses)) : ?>
-    <div>
-        <ul class="clean">
-            <? foreach ($availableClasses as $className) : ?>
-            <li>
-                <a href="#" onClick="jQuery('input[name=classname]').val('<?= htmlReady($className) ?>');">
-                    <?= Icon::create('arr_2up', 'info')->asImg(['class' => "text-bottom"]) ?>
-                    <?= htmlReady($className) ?>
-                </a>
-            </li>
-            <? endforeach ?>
-        </ul>
-    </div>
+        <div>
+            <ul class="clean">
+                <? foreach ($availableClasses as $className) : ?>
+                    <li>
+                        <a href="#" onClick="jQuery('input[name=classname]').val('<?= htmlReady($className) ?>');">
+                            <?= Icon::create('arr_2up', 'info')->asImg(['class' => "text-bottom"]) ?>
+                            <?= htmlReady($className) ?>
+                        </a>
+                    </li>
+                <? endforeach ?>
+            </ul>
+        </div>
     <? endif ?>
-    <label class="caption">
-        <?= _('Nummer des Schritts im Assistenten') ?>:
-        <input type="number" name="number" size="4" maxlength="2" value="<?= $step->number ?>" required/>
+    <label>
+        <span class="required">  <?= _('Nummer des Schritts im Assistenten') ?></span>
+        <input type="number" name="number" size="4" maxlength="2" value="<?= $step->number ?>" required>
     </label>
-    <label class="caption">
-        <input type="checkbox" name="enabled"<?= $step->enabled ? ' checked="checked"' : '' ?>/>
+    <label>
+        <input type="checkbox" name="enabled"<?= $step->enabled ? ' checked="checked"' : '' ?>>
         <?= _('Schritt ist aktiv') ?>
     </label>
-    <?= CSRFProtection::tokenTag() ?>
-    <div data-dialog-button>
+    <footer data-dialog-button>
         <?= Studip\Button::createAccept(_('Speichern'), 'submit') ?>
-        <?= Studip\Button::createCancel(_('Abbrechen'), 'cancel', array('data-dialog' => 'close')) ?>
-    </div>
+        <?= Studip\Button::createCancel(_('Abbrechen'), 'cancel', ['data-dialog' => 'close']) ?>
+    </footer>
 </form>
