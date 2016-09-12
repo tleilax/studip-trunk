379926314ab6e3383ebef321aebaefd52942d1be
David Siegfried <ds.siegfried@gmail.com>
2016-09-12 11:00:37 +0000

    add actionmenu, migrate to new form-css, re #6823, re #6894

--- app/views/admin/specification/index.php
+++ app/views/admin/specification/index.php
@@ -1,54 +1,59 @@
-<?
-# Lifter010: TODO
-?>
-<? if (isset($flash['error'])) : ?>
-    <?= MessageBox::error($flash['error'], $flash['error_detail']) ?>
-<? elseif (isset($flash['message'])): ?>
-    <?= MessageBox::info($flash['message']) ?>
-<? elseif (isset($flash['success'])) : ?>
-    <?= MessageBox::success($flash['success'], $flash['success_detail']) ?>
-<? elseif (isset($flash['delete'])) : ?>
-    <?= createQuestion(sprintf(_('Wollen Sie die Regel "%s" wirklich löschen?'), $flash['delete']['name']), array('delete' => 1), array('back' => 1), $controller->url_for('admin/specification/delete/'.$flash['delete']['lock_id'])) ?>
-<? endif; ?>
-
-<table class="default">
-    <caption>
-        <?= _('Verwaltung von Zusatzangaben') ?>
-    </caption>
-    <colgroup>
-        <col width="45%">
-        <col width="45%">
-        <col width="10%">
-    </colgroup>
-    <thead>
-    <tr>
-        <th><?= _('Name') ?></th>
-        <th><?= _('Beschreibung') ?></th>
-        <th><?= _('Aktionen') ?></th>
-    </tr>
-    </thead>
-    <tbody>
-   <? foreach ($allrules as $index=>$rule) : ?>
-    <tr>
-        <td>
-            <?= htmlReady($rule['name']) ?>
-        </td>
-        <td>
-            <?= htmlReady($rule['description']) ?>
-        </td>
-        <td class="actions">
-            <a href="<?=$controller->url_for('admin/specification/edit/'.$rule['lock_id']) ?>">
-                <?= Icon::create('edit', 'clickable', ['title' => _('Regel bearbeiten')])->asImg() ?>
-            </a>
-            <a href="<?=$controller->url_for('admin/specification/delete/'.$rule['lock_id'])?>">
-                <?= Icon::create('trash', 'clickable', ['title' => _('Regel löschen')])->asImg() ?>
-            </a>
-        </td>
-    </tr>
-    <? endforeach ?>
-    </tbody>
-</table>
-
+<form method="post">
+    <?= CSRFProtection::tokenTag() ?>
+    <table class="default">
+        <caption>
+            <?= _('Verwaltung von Zusatzangaben') ?>
+        </caption>
+        <colgroup>
+            <col width="45%">
+            <col width="45%">
+            <col width="10%">
+        </colgroup>
+        <thead>
+            <tr>
+                <th><?= _('Name') ?></th>
+                <th><?= _('Beschreibung') ?></th>
+                <th><?= _('Aktionen') ?></th>
+            </tr>
+        </thead>
+        <tbody>
+            <? if (!empty($allrules)): ?>
+                <? foreach ($allrules as $index => $rule) : ?>
+                    <tr>
+                        <td>
+                            <?= htmlReady($rule['name']) ?>
+                        </td>
+                        <td>
+                            <?= htmlReady($rule['description']) ?>
+                        </td>
+                        <td class="actions">
+                            <? $actionMenu = ActionMenu::get() ?>
+                            <? $actionMenu->addLink($controller->url_for('admin/specification/edit/' . $rule['lock_id']),
+                                    _('Regel bearbeiten'),
+                                    Icon::create('edit', 'clickable'))
+                            ?>
+                            <? $actionMenu->addButton(
+                                    'delete_rule',
+                                    _('Regel löschen'),
+                                    Icon::create('trash', 'clickable',
+                                            ['title'        => _('Regel löschen'),
+                                             'formaction'   => $controller->url_for('admin/specification/delete/' . $rule['lock_id']),
+                                             'data-confirm' => sprintf(_('Wollen Sie die Regel "%s" wirklich löschen?'), htmlReady($rule['name']))]))
+                            ?>
+                            <?= $actionMenu->render() ?>
+                        </td>
+                    </tr>
+                <? endforeach ?>
+            <? else : ?>
+                <tr>
+                    <td colspan="3" style="text-align: center">
+                        <?= _('Es wurden noch keine Zusatzangaben definiert.') ?>
+                    </td>
+                </tr>
+            <? endif ?>
+        </tbody>
+    </table>
+</form>
 <?
 
 $sidebar = Sidebar::Get();
