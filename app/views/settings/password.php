dc9451d7fb8f7d24b5f839bf7cbe8531ecdc4a35
David Siegfried <ds.siegfried@gmail.com>
2016-09-13 10:31:46 +0000

    migrate from table-form to default-form, re #6894

--- app/views/settings/password.php
+++ app/views/settings/password.php
@@ -1,55 +1,28 @@
 <? use Studip\Button; ?>
 
-<form id="edit_password" method="post" action="<?= $controller->url_for('settings/password/store') ?>">
+<form id="edit_password" method="post" action="<?= $controller->url_for('settings/password/store') ?>" class="default">
     <?= CSRFProtection::tokenTag() ?>
     <input type="hidden" name="studipticket" value="<?= get_ticket() ?>">
-
-    <table class="default">
-        <colgroup>
-            <col width="50%">
-            <col width="50%">
-        </colgroup>
-        <thead>
-            <tr>
-                <th colspan="2"><?= _('Passwort ändern') ?></th>
-            </tr>
-        </thead>
-        <tbody class="maxed">
-            <tr>
-                <td>
-                    <label for="password"><?= _('Aktuelles Passwort') ?></label>
-                </td>
-                <td>
-                    <input required type="password" id="password" name="password">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label for="new_password"><?= _('Neues Passwort') ?></label>
-                </td>
-                <td>
-                    <input required type="password" pattern=".{4,}"
-                           id="new_password" name="new_password"
-                           data-message="<?= _('Das Passwort ist zu kurz - es sollte mindestens 4 Zeichen lang sein.') ?>">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label for="new_password_confirm"><?= _('Passwort bestätigen') ?></label>
-                </td>
-                <td>
-                    <input required type="password" pattern=".{4,}"
-                           id="new_password_confirm" name="new_password_confirm"
-                           data-must-equal="#new_password">
-                </td>
-            </tr>
-        </tbody>
-        <tfoot>
-            <tr>
-                <td colspan="2">
-                    <?= Button::create(_('Übernehmen'), 'store', array('title' => _('Änderungen übernehmen'))) ?>
-                </td>
-            </tr>
-        </tfoot>
-    </table>
+    <fieldset>
+        <legend>
+            <?= _('Passwort ändern') ?>
+        </legend>
+        <label>
+            <span class="required"><?= _('Aktuelles Passwort') ?></span>
+            <input required type="password" id="password" name="password">
+        </label>
+        <label>
+            <span class="required"><?= _('Neues Passwort') ?></span>
+            <input required type="password" pattern=".{4,}"
+                   id="new_password" name="new_password"
+                   data-message="<?= _('Das Passwort ist zu kurz - es sollte mindestens 4 Zeichen lang sein.') ?>">
+        </label>
+        <label>
+            <span class="required"><?= _('Passwort bestätigen') ?></span>
+            <input required type="password" pattern=".{4,}"
+                   id="new_password_confirm" name="new_password_confirm"
+                   data-must-equal="#new_password">
+        </label>
+        <footer><?= Button::create(_('Übernehmen'), 'store', ['title' => _('Änderungen übernehmen')]) ?></footer>
+    </fieldset>
 </form>
