e3a80628770b84bd77a5073352b3bad4f4c7bbb4
David Siegfried <ds.siegfried@gmail.com>
2016-09-22 13:30:41 +0000

    replace StudipForm, re #6885

--- app/controllers/course/cancel_dates.php
+++ app/controllers/course/cancel_dates.php
@@ -1,90 +1,79 @@
 <?php
-/**
-* cancel_dates.php
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License as
-* published by the Free Software Foundation; either version 2 of
-* the License, or (at your option) any later version.
-*
-* @author      André Noack <noack@data-quest.de>
-* @license     http://www.gnu.org/licenses/gpl-2.0.html GPL version 2
-* @category    Stud.IP
-* @package     admin
-*/
 
+/**
+ * cancel_dates.php
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * @author      André Noack <noack@data-quest.de>
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL version 2
+ * @category    Stud.IP
+ * @package     admin
+ */
 class Course_CancelDatesController extends AuthenticatedController
 {
     protected $utf8decode_xhr = true;
-
+    
     /**
      * common tasks for all actions
      */
-    function before_filter (&$action, &$args)
+    public function before_filter(&$action, &$args)
     {
         global $perm;
-
+        
         parent::before_filter($action, $args);
-
+        
         if (Request::get('termin_id')) {
-            $this->dates[0] = new SingleDate(Request::option('termin_id'));
+            $this->dates[0]  = new SingleDate(Request::option('termin_id'));
             $this->course_id = $this->dates[0]->range_id;
         }
         
         if (Request::get('issue_id')) {
-            $this->issue_id = Request::option('issue_id');
-            $this->dates = array_values(
-                            array_map(function ($data) 
-                            {
-                                $d = new SingleDate();
-                                $d->fillValuesFromArray($data);
-                                return $d;
-                            },
-                            IssueDB::getDatesforIssue(Request::option('issue_id'))
-                            )
-            );
+            $this->issue_id  = Request::option('issue_id');
+            $this->dates     = array_values(array_map(function ($data) {
+                $d = new SingleDate();
+                $d->fillValuesFromArray($data);
+                return $d;
+            }, IssueDB::getDatesforIssue(Request::option('issue_id'))));
             $this->course_id = $this->dates[0]->range_id;
         }
-        if (!get_object_type($this->course_id, array('sem')) ||
-                !$perm->have_studip_perm("tutor", $this->course_id)) {
+        if (!get_object_type($this->course_id, ['sem']) || !$perm->have_studip_perm("tutor", $this->course_id)) {
             throw new Trails_Exception(400);
         }
-        PageLayout::setHelpKeyword("Basis.VeranstaltungenVerwaltenAendernVonZeitenUndTerminen");
-        PageLayout::setTitle(Course::findCurrent()->getFullname()." - " . _("Veranstaltungstermine absagen"));
+        PageLayout::setHelpKeyword('Basis.VeranstaltungenVerwaltenAendernVonZeitenUndTerminen');
+        PageLayout::setTitle(Course::findCurrent()->getFullname() . " - " . _('Veranstaltungstermine absagen'));
     }
-
-    function index_action()
+    
+    public function index_action()
     {
-            $form_fields['comment'] = array('caption' => _("Kommentar"), 'type' => 'textarea', 'attributes' => array('rows' => 4, 'style' => 'width:100%'));
-            $form_fields['snd_message'] = array('caption' => _("Benachrichtigung über ausfallende Termine an alle Teilnehmer verschicken"),'type' => 'checkbox', 'attributes' => array('style' => 'vertical-align:middle'));
-            
-            $form_buttons['save_close'] = array('caption' => _('OK'), 'info' => _("Termine absagen und Dialog schließen"));
-
-            $form = new StudipForm($form_fields, $form_buttons, 'cancel_dates', false);
-
-            if ($form->isClicked('save_close')) {
-                $sem = Seminar::getInstance($this->course_id);
-                $comment = $form->getFormFieldValue('comment');
-                foreach ($this->dates as $date) {
-                    $sem->cancelSingleDate($date->getTerminId(), $date->getMetadateId());
-                    $date->setComment($comment);
-                    $date->setExTermin(true);
-                    $date->store();
-                }
-                if ($form->getFormFieldValue('snd_message') && count($this->dates)) {
-                    $snd_messages = raumzeit_send_cancel_message($comment, $this->dates);
-                    if ($snd_messages) {
-                        $msg = sprintf(_("Es wurden %s Benachrichtigungen gesendet."), $snd_messages);
-                    }
-                }
-                PageLayout::postMessage(MessageBox::success(_("Folgende Termine wurden abgesagt") . ($msg ? ' (' . $msg . '):' : ':'),
-                     array_map(function ($d) {return $d->toString();}, $this->dates)));
-                $this->redirect($this->url_for('course/dates'));
-            }
-            $this->form = $form;
     }
-
-    function after_filter($action, $args)
+    
+    public function store_action()
+    {
+        $sem     = Seminar::getInstance($this->course_id);
+        foreach ($this->dates as $date) {
+            $sem->cancelSingleDate($date->getTerminId(), $date->getMetadateId());
+            $date->setComment(Request::get('cancel_dates_comment'));
+            $date->setExTermin(true);
+            $date->store();
+        }
+        if (Request::int('cancel_dates_snd_message') && count($this->dates)) {
+            $snd_messages = raumzeit_send_cancel_message(Request::get('cancel_dates_comment'), $this->dates);
+            if ($snd_messages) {
+                $msg = sprintf(_('Es wurden %s Benachrichtigungen gesendet.'), $snd_messages);
+            }
+        }
+        PageLayout::postSuccess(_('Folgende Termine wurden abgesagt') . ($msg ? ' (' . $msg . '):' : ':'), array_map(function ($d) {
+            return $d->toString();
+        }, $this->dates));
+        
+        $this->redirect($this->url_for('course/dates'));
+    }
+    
+    public function after_filter($action, $args)
     {
         if (Request::isXhr()) {
             foreach ($this->response->headers as $k => $v) {
@@ -98,5 +87,5 @@ class Course_CancelDatesController extends AuthenticatedController
         }
         parent::after_filter($action, $args);
     }
-
+    
 }
--- app/views/course/cancel_dates/index.php
+++ app/views/course/cancel_dates/index.php
@@ -1,35 +1,32 @@
-<? foreach(PageLayout::getMessages() as $pm) : ?>
-    <?= $pm ?>
-<? endforeach; ?>
-<?= $form->getFormStart($controller->url_for('course/cancel_dates')) ?>
-<h3>
-<?=_("Folgende Veranstaltungstermine ausfallen lassen")?>:
-</h3>
-<div style="padding: 5px; margin: 5px;font-weight: bold;">
-<? echo join(', ', array_map(function ($d) {return $d->toString();}, $dates)); ?>
-</div>
-<div style="font-weight:bold;padding-top:5px;">
-<?= $form->getFormFieldCaption('comment')?>:
-</div>
-    <div>
-        <div>
-        <?= _("Wenn Sie die nicht stattfindenden Termine mit einem Kommentar versehen, werden die Ausfalltermine im Ablaufplan weiterhin dargestellt und auch im Terminkalender eingeblendet.")?>
+<form action="<?= $controller->url_for('course/cancel_dates/store') ?>" method="post" class="default" name="cancel_dates">
+    <?= CSRFProtection::tokenTag() ?>
+    <fieldset>
+        <legend>
+            <?= _('Folgende Veranstaltungstermine ausfallen lassen') ?>
+        </legend>
+        <div style="padding: 5px; margin: 5px;font-weight: bold;">
+            <? echo join(', ', array_map(function ($d) {
+                return $d->toString();
+            }, $dates)); ?>
         </div>
-        <div>
-        <?= $form->getFormField('comment') ?>
-        </div>
-    </div>
-<div style="font-weight:bold;padding-top:5px;">
-    <?= $form->getFormFieldCaption('snd_message')?>:
-    <?= $form->getFormField('snd_message') ?>
-</div>
 
-<div style="text-align:center" data-dialog-button>
-     <?= $form->getFormButton('save_close')?>
-</div>
-<? if ($issue_id) : ?>
-    <input type="hidden" name="issue_id" value="<?= $issue_id ?>">
-<? else : ?>
-    <input type="hidden" name="termin_id" value="<?= $dates[0]->getTerminId() ?>">
-<? endif ?>
-<?= $form->getFormEnd() ?>
+        <label>
+            <?= _('Kommentar') ?>
+            <?= tooltipIcon(_('Wenn Sie die nicht stattfindenden Termine mit einem Kommentar versehen, werden die Ausfalltermine im Ablaufplan weiterhin dargestellt und auch im Terminkalender eingeblendet.')) ?>
+            <textarea wrap="virtual" name="cancel_dates_comment" id="cancel_dates_comment"></textarea>
+        </label>
+        <label>
+            <input type="checkbox" name="cancel_dates_snd_message" value="1">
+            <?= _('Benachrichtigung über ausfallende Termine an alle Teilnehmer verschicken') ?>
+        </label>
+    </fieldset>
+    <? if ($issue_id) : ?>
+        <input type="hidden" name="issue_id" value="<?= $issue_id ?>">
+    <? else : ?>
+        <input type="hidden" name="termin_id" value="<?= $dates[0]->getTerminId() ?>">
+    <? endif ?>
+    <footer data-dialog-button>
+        <?= Studip\Button::createAccept(_('Speichern')) ?>
+    </footer>
+</form>
+
