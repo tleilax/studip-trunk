70f461ceb714af0b83e1750bba484fd51a2984db
Rasmus Fuhse <Krassmus@gmail.com>
2016-10-28 15:48:06 +0000

    MOADB: fix edit

--- app/views/file/edit.php
+++ app/views/file/edit.php
@@ -1,26 +1,25 @@
-<form enctype="multipart/form-data" method="post" class="studip_form"
+<form enctype="multipart/form-data"
+      method="post"
+      class="default"
       action="<?= $controller->url_for('/edit') ?>">
 
     <?= CSRFProtection::tokenTag() ?>
     <input type="hidden" name="fileref_id" value="<?=htmlReady($fileref_id)?>">
     <input type="hidden" name="folder_id" value="<?=htmlReady($folder_id)?>">
     <fieldset>
-        <fieldset>
-            <label>
-                <?= _('Lizenz') ?>
-                <select name="licence">
-                	<option value="1">Placeholder1</option>
-                	<option value="2">Placeholder2</option>
-                </select>
-            </label>
-        </fieldset>
+        <legend><?= _("Datei bearbeiten") ?></legend>
+        <label>
+            <?= _('Lizenz') ?>
+            <select name="licence">
+                <option value="1">Placeholder1</option>
+                <option value="2">Placeholder2</option>
+            </select>
+        </label>
 
-        <fieldset>
-            <label>
-                <?= _('Beschreibung') ?>
-                <textarea name="description" placeholder="<?= _('Optionale Beschreibung') ?>"><?= htmlReady($description); ?></textarea>
-            </label>
-        </fieldset>
+        <label>
+            <?= _('Beschreibung') ?>
+            <textarea name="description" placeholder="<?= _('Optionale Beschreibung') ?>"><?= htmlReady($description); ?></textarea>
+        </label>
 
         <?/*
         <fieldset>
