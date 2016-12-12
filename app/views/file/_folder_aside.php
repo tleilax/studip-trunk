a86460863b59f8506031d5bb3b0aa50dd205c4f2
Rasmus Fuhse <Krassmus@gmail.com>
2016-12-12 15:46:56 +0000

    MOADB: work on file edit window

--- app/views/file/_folder_aside.php
+++ app/views/file/_folder_aside.php
@@ -3,15 +3,15 @@
     <h3><?= htmlReady($folder->name) ?></h3>
     <table class="default">
         <tr>
-            <th><?= _('Erstellt') ?></th>
+            <td><?= _('Erstellt') ?></td>
             <td><?= date('d.m.Y H:i', $folder->mkdate) ?></td>
         </tr>
         <tr>
-            <th><?= _('Geändert') ?></th>
+            <td><?= _('Geändert') ?></td>
             <td><?= date('d.m.Y H:i', $folder->chdate) ?></td>
         </tr>
         <tr>
-            <th><?= _('Besitzer/-in') ?></th>
+            <td><?= _('Besitzer/-in') ?></td>
             <td>
             <? if($folder->owner): ?>
             <?= htmlReady($folder->owner->getFullName()) ?>
--- app/views/file/edit_folder.php
+++ app/views/file/edit_folder.php
@@ -1,25 +1,27 @@
-<?= $this->render_partial('file/_folder_aside.php',
-    [
-        'folder' => $folder
-    ])  ?>
-<div id="file_management_forms">
-    <form method="post" class="default"
-        action="<?= $controller->url_for('/edit/' . $folder_id) ?>"
-        <? if(Request::isDialog()): ?>
-        data-dialog="reload-on-close;size=auto"
-        <? endif ?>
-        >
-        <?= CSRFProtection::tokenTag() ?>
-        <?= $this->render_partial('file/new_edit_folder_form.php',
-            [
-                'name' => $name,
-                'description' => $description
-            ]) ?>
-        <div data-dialog-button>
-            <?= Studip\Button::createAccept(_('Speichern')) ?>
-            <?= Studip\LinkButton::createCancel(_('Abbrechen'), $controller->url_for('/goto/' . $parent_folder_id)) ?>
-        </div>
-    </form>
+<div id="file_edit_window">
+    <?= $this->render_partial('file/_folder_aside.php',
+        [
+            'folder' => $folder
+        ])  ?>
+    <div id="file_management_forms">
+        <form method="post" class="default"
+            action="<?= $controller->url_for('/edit/' . $folder_id) ?>"
+            <? if(Request::isDialog()): ?>
+            data-dialog="reload-on-close;size=auto"
+            <? endif ?>
+            >
+            <?= CSRFProtection::tokenTag() ?>
+            <?= $this->render_partial('file/new_edit_folder_form.php',
+                [
+                    'name' => $name,
+                    'description' => $description
+                ]) ?>
+            <div data-dialog-button>
+                <?= Studip\Button::createAccept(_('Speichern')) ?>
+                <?= Studip\LinkButton::createCancel(_('Abbrechen'), $controller->url_for('/goto/' . $parent_folder_id)) ?>
+            </div>
+        </form>
+    </div>
 </div>
 
 <script type="text/javascript">
