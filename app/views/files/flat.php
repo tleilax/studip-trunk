4cfe6eddd0e624c7b93d93c167e8c346703247fd
arne <arne>
2016-12-12 11:08:24 +0000

    change tablesorter filter

--- app/views/files/flat.php
+++ app/views/files/flat.php
@@ -1,4 +1,3 @@
-<?=$last_visitdate?>
 <form method="post">
 <table class="default documents">
     <?= $this->render_partial("files/_files_thead.php") ?>
@@ -35,17 +34,12 @@
 
 <? ob_start(); ?>
 <div align="center">
-<input class="tablesorterfilter filter-select" placeholder="Name" data-column="2" type="search" style="width: 100%; margin-bottom: 5px;"><br>
-<input class="tablesorterfilter" placeholder="Autor/in" data-column="4" type="search" style="width: 100%; margin-bottom: 5px;"><br>
-<select class="tablesorterfilter filter-select" data-column="5">
-<option></option>
-<option value="<10000">kleiner 10kB</option>
-<option value="1">1</option>
-</select>
+<input class="tablesorterfilter filter-select" placeholder="<?=_('Name oder Autor/-in')?>" data-column="2,4" type="search" style="width: 100%; margin-bottom: 5px;"><br>
 </div>
 <? $content = ob_get_clean();
 $sidebar = Sidebar::get();
 $widget = new SidebarWidget();
 $widget->setTitle(_('Filter'));
 $widget->addElement(new WidgetElement($content));
-$sidebar->addWidget($widget); ?>
+$sidebar->addWidget($widget); 
+?>
