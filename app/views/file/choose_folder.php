89cb797506cf80de757afb9bb8b820d2672e6e62
Timo Hartge <hartge@data-quest.de>
2016-12-13 14:59:09 +0000

    MOADB: fixed layout in copy/move dialog

--- app/views/file/choose_folder.php
+++ app/views/file/choose_folder.php
@@ -60,7 +60,7 @@ if (Request::get("copymode")) {
 <table class="default">
     <thead>
         <tr>
-            <th><?= _("Typ") ?></th>
+            <th width="20px"><?= _("Typ") ?></th>
             <th><?= _("Name") ?></th>
         </tr>
     </thead>
