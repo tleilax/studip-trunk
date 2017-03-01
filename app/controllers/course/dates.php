b9555b131d2986e9fc842745a2f520bd9966d374
Elmar Ludwig <ludwig@uni-osnabrueck.de>
2017-03-01 17:23:24 +0000

    add room info to export, fixes #7238

--- app/controllers/course/dates.php
+++ app/controllers/course/dates.php
@@ -328,7 +328,8 @@ class Course_DatesController extends AuthenticatedController
                         'title' => $title,
                         'description' => $description,
                         'start' => $singledate->getStartTime(),
-                        'related_persons' => $singledate->getRelatedPersons()
+                        'related_persons' => $singledate->getRelatedPersons(),
+                        'room' => $singledate->getRoom() ?: $singledate->raum
                     );
                 } elseif ($singledate->getComment()) {
                     $dates[] = array(
@@ -336,7 +337,8 @@ class Course_DatesController extends AuthenticatedController
                         'title' => _('fällt aus') . ' (' . _('Kommentar:') . ' ' . $singledate->getComment() . ')',
                         'description' => '',
                         'start' => $singledate->getStartTime(),
-                        'related_persons' => array()
+                        'related_persons' => array(),
+                        'room' => ''
                     );
                 }
             }
@@ -346,6 +348,7 @@ class Course_DatesController extends AuthenticatedController
         $template = $factory->open($this->get_default_template("export"));
 
         $template->set_attribute('dates', $dates);
+        $template->lecturer_count = $course->countMembersWithStatus('dozent');
         $content = $template->render();
 
         $content = mb_encode_numericentity($content, array(0x80, 0xffff, 0, 0xffff), 'cp1252');
