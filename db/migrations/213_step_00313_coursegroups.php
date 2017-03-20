c278c1e356ecd5fa5a00ccc3a79dbb86ba638a2e
hackl <hackl>
2017-03-20 08:22:03 +0000

    renumber migration, re #7242

---
+++ db/migrations/213_step_00313_coursegroups.php
@@ -0,0 +1,29 @@
+<?php
+class StEP00313Coursegroups extends Migration
+{
+    public function up()
+    {
+        DBManager::get()->exec(
+            "ALTER TABLE `sem_classes` ADD `is_group` TINYINT(1) NOT NULL DEFAULT '0' AFTER `show_raumzeit`");
+        DBManager::get()->exec(
+            "ALTER TABLE `seminare` ADD `parent_course` VARCHAR(32) NULL DEFAULT NULL AFTER `public_topics`");
+
+        StudipLog::registerAction('SEM_ADD_TO_GROUP', 'Veranstaltung zu Gruppe hinzufügen',
+            '%user ordnet Veranstaltung %sem(%affected) der Gruppe %sem(%coaffected) zu.', null);
+        StudipLog::registerAction('SEM_DEL_FROM_GROUP', 'Veranstaltung aus Gruppe entfernen',
+            '%user entfernt Veranstaltung %sem(%affected) aus der Gruppe %sem(%coaffected).', null);
+
+        SimpleORMap::expireTableScheme();
+    }
+
+    public function down()
+    {
+        DBManager::get()->exec("ALTER TABLE `sem_classes` DROP `is_group`");
+        DBManager::get()->exec("ALTER TABLE `seminare` DROP `parent_course`");
+
+        StudipLog::unregisterAction('SEM_ADD_TO_GROUP');
+        StudipLog::unregisterAction('SEM_DEL_FROM_GROUP');
+
+        SimpleORMap::expireTableScheme();
+    }
+}
