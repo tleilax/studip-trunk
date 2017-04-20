978fcb62df0f6e933b0d1e7c7c037c45e23721a7
David Siegfried <ds.siegfried@gmail.com>
2017-04-20 09:44:01 +0000

    replace navigation with views-widget, fixes #7399

--- app/controllers/search/courses.php
+++ app/controllers/search/courses.php
@@ -27,7 +27,7 @@ class Search_CoursesController extends AuthenticatedController
         }
 
         Request::set('view', $_SESSION['sem_portal']['bereich']);
-        Navigation::activateItem('/search/courses/'.$_SESSION['sem_portal']['bereich']);
+        Navigation::activateItem('/search/courses');
 
         if (Request::option('choose_toplist')) {
             $_SESSION['sem_portal']['toplist'] = Request::option('choose_toplist');
@@ -36,6 +36,21 @@ class Search_CoursesController extends AuthenticatedController
         if (!$_SESSION['sem_portal']['toplist']) {
             $_SESSION['sem_portal']['toplist'] = 4;
         }
+
+        $views = new ViewsWidget();
+        $views->addLink(
+            _('Alle'),
+                URLHelper::getURL('dispatch.php/search/courses?reset_all=TRUE&cmd=qs',
+                    ['reset_all' => 'TRUE', 'cmd' => 'qs', 'view' => 'all'])
+        )->setActive(!Request::get('view') || Request::get('view') == 'all');
+        foreach ($GLOBALS['SEM_CLASS'] as $key => $val) {
+            $views->addLink(
+                $val['name'],
+                    URLHelper::getURL('dispatch.php/search/courses?reset_all=TRUE&cmd=qs',
+                        ['reset_all' => 'TRUE', 'cmd' => 'qs', 'view' => $key])
+            )->setActive(Request::get('view') == $key);
+        }
+        Sidebar::Get()->addWidget($views);
     }
 
     public function index_action()
