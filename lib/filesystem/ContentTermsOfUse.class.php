2ae9a2d80de4273784ed365fd58caa27205a9d14
André Noack <noackorama@gmail.com>
2017-05-22 14:30:21 +0000

    fixes #7416

--- lib/filesystem/ContentTermsOfUse.class.php
+++ lib/filesystem/ContentTermsOfUse.class.php
@@ -68,96 +68,42 @@ class ContentTermsOfUse extends SimpleORMap
      *   The user's ID must therefore match the user_id attribute
      *   of the FileRef object.
      */
-    public function fileIsDownloadable(FileRef $file_ref, $user_id = null)
+    public function fileIsDownloadable(FileRef $file_ref, $allow_owner = true, $user_id = null)
     {
-        if($this->download_condition == 0) {
+        $user_id = $user_id || $GLOBALS['user_id'];
+        if ($allow_owner && ($file_ref->user_id == $GLOBALS['user']->id || $GLOBALS['perm']->have_perm('root', $user_id))) {
             return true;
-        } elseif($this->download_condition == 1) {
+        }
+        if ($this->download_condition == 1) {
             $folder = $file_ref->folder;
-            if(!$folder) {
-                //no folder: we can't check if the file is downloadable
-                //when this download condition is set!
-                return false;
-            }
 
             //the content is only downloadable when the user is inside a closed group
             //(referenced by range_id). If download_condition is set to 2
             //the group must also have a terminated signup deadline.
-            if($folder->range_id && $folder->range_type) {
+            if ($folder->range_id && $folder->range_type) {
                 //check where this range_id comes from:
-                if($folder->range_type == 'course') {
-                    $course = Course::find($folder->range_id);
-                    //check if course exists:
-                    if(!$course) {
-                        //A deleted course's file can't be checked for its
-                        //download status:
-                        return false;
+                if ($folder->range_type == 'course') {
+                    $seminar = Seminar::GetInstance($folder->range_id);
+                    $timed_admission = $seminar->getAdmissionTimeFrame();
+
+                    if ($seminar->admission_prelim == 1 || $seminar->isPasswordProtected() ||
+                        $seminar->isAdmissionLocked()
+                        || (is_array($timed_admission) && $timed_admission['end_time'] > 0 && $timed_admission['end_time'] < time())) {
+                        return true;
                     }
-                    
-                    //ok, range is a course:
-                    $status = $course->getParticipantStatus($user_id);
-                    if(!$status || ($status == 'awaiting')) {
-                        //the user is not a member in the course
-                        //or he is awaiting membership to the course.
-                        //However, this means the user isn't permitted
-                        //to download the content!
-                        return false;
-                    } else {
-                        //the user has the status 'accepted' (applicants_seminar_user table)
-                        //or is one of 'user', 'autor', 'tutor', 'dozent' (seminar_user table).
-                        //If download condition is set to 1 we must also check
-                        //if the course admission is closed!
-
-                        $seminar = new Seminar($course);
-
-
-                        if($seminar->isPasswordProtected() || $seminar->isAdmissionLocked()) {
-                            //course is password protected or locked:
-                            return true;
-                        } else {
-                            //There are two last chances to permit the download:
-                            //Either the course has a timed admission that
-                            //is closed or the course is a study group
-                            //without open admission.
-
-                            $admission_timeframe = $seminar->getAdmissionTimeFrame();
-
-                            if(is_array($admission_timeframe)) {
-                                //There is a timed admission for this course:
-                                //Check if it has ended.
-                                if($admission_timeframe['end_time'] > 0 &&
-                                    $admission_timeframe['end_time'] < time()) {
-                                    //it has ended!
-                                    return true;
-                                }
-
-                            } else {
-                                //There is no timed admission: One last chance:
-                                //Is the course a closed studygroup?
-                                //(a studygroup without open admission)
-                                if(StudygroupModel::isStudygroup($folder->range_id) &&
-                                    ($seminar->admission_prelim == 1)) {
-                                    //The course is a studygroup without open admission!
-                                    return true;
-                                }
-                            }
-                            //in case the above if-statements didn't validate
-                            //to true we can't permit to download the file:
-                            return false;
-                        }
-                    }
+                } else {
+                    return false;
                 }
             } else {
                 //no range_id set: we can't check if the content is downloadable
                 //so it won't be downloadable at all!
                 return false;
             }
-        } elseif($this->download_condition == 2) {
-            //can only be downloaded if the user is the owner of the file:
-            return ($file_ref->user_id == $user_id);
-        } else {
-            //invalid download_condition ID!
+        } elseif ($this->download_condition == 2) {
+            return (bool)Config::get()->ALLOW_DOWNLOAD_FOR_UNKNOWN_LICENSE;
+        } elseif ($this->download_condition == 3) {
             return false;
         }
+        return true;
     }
 }
