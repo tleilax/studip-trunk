526e519214534ce83040056488d96b74a059e7ba
Rasmus Fuhse <Krassmus@gmail.com>
2017-05-23 07:10:39 +0000

    MOADB: add back button

--- app/views/file/edit_folder.php
+++ app/views/file/edit_folder.php
@@ -8,16 +8,19 @@
             <?= $this->render_partial('file/new_edit_folder_form.php') ?>
             <div data-dialog-button>
                 <?= Studip\Button::createAccept(_('Speichern'), 'edit') ?>
+                <? if (!Request::isDialog()) : ?>
+                    <?= \Studip\LinkButton::create(_("Abbrechen"), $controller->url_for(($folder->range_type === "course" ? "course/" : ($folder->range_type === "institute" ? "institute/" : "")).'files/index/' . $folder->parent_id)) ?>
+                <? endif ?>
             </div>
         </form>
     </div>
 </div>
 
 <script type="text/javascript">
-//On focus the whole folder name shall be selected.
-//The input field with id edit_folder_name is in the view new_edit_folder_form!
-var folder_name_edit_input = document.getElementById('edit_folder_name');
-if(folder_name_edit_input) {
-    folder_name_edit_input.select();
-}
+    //On focus the whole folder name shall be selected.
+    //The input field with id edit_folder_name is in the view new_edit_folder_form!
+    var folder_name_edit_input = document.getElementById('edit_folder_name');
+    if (folder_name_edit_input) {
+        folder_name_edit_input.select();
+    }
 </script>
