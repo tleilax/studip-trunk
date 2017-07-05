ac2f705d1791f741b29aa394f35eb4347b88eab6
David Siegfried <ds.siegfried@gmail.com>
2017-07-05 14:29:52 +0000

    refine terms of use, fixes #7552

---
+++ templates/register/step1.php
@@ -0,0 +1,36 @@
+<section class="contentbox">
+    <header>
+        <h1><?= _('Stimmen Sie den Nutzungsbedinungen zu?') ?></h1>
+    </header>
+    <section>
+        <?= _('Stud.IP ist ein Open Source Projekt und steht unter der Gnu General Public License (GPL). Das System befindet sich in der ständigen Weiterentwicklung.') ?>
+
+        <? printf(_('Für Vorschläge und Kritik findet sich immer ein Ohr. Wenden Sie sich hierzu entweder an die %sStud.IP Crew%s oder direkt an die %sEntwickler%s.'),
+            "<a href=\"mailto:studip-users@lists.sourceforge.net\">", "</a>",
+            "<a href=\"dispatch.php/siteinfo/show\">", "</a>") ?>
+        <br><br>
+        <?= _('Um den vollen Funktionsumfang von Stud.IP nutzen zu können, müssen Sie sich am System anmelden.') ?><br>
+        <?= _('Das hat viele Vorzüge:') ?><br>
+        <blockquote>
+            <ul>
+                <li><?= _('Zugriff auf Ihre Daten von jedem internetfähigen Rechner weltweit,') ?>
+                <li><?= _('Anzeige neuer Mitteilungen oder Dateien seit Ihrem letzten Besuch,') ?>
+                <li><?= _('Eine eigenes Profil im System,') ?>
+                <li><?= _('die Möglichkeit anderen Personen Nachrichten zu schicken oder mit ihnen zu chatten,') ?>
+                <li><?= _('und vieles mehr.') ?></li>
+        </blockquote>
+        <br>
+        </ul>
+
+        <?= _('Mit der Anmeldung werden die nachfolgenden Nutzungsbedingungen akzeptiert:') ?>
+    </section>
+</section>
+
+<? include('locale/' . $GLOBALS['_language_path'] . '/LC_HELP/pages/nutzung.html'); ?>
+
+<footer>
+    <div class="button-group">
+        <?= Studip\LinkButton::create(_('Ich erkenne die Nutzungsbedingungen an'), URLHelper::getLink('register2.php')) ?>
+        <?= Studip\LinkButton::create(_('Registrierung abbrechen'), URLHelper::getLink('index.php')) ?>
+    </div>
+</footer>
