02bae2d7eb16adb7aeb014b2a899bcc7bfd4f6bd
Jan-Hendrik Willms <tleilax+github@gmail.com>
2017-08-04 17:24:07 +0000

    avatar menu is now a real stud.ip navigation, fixes #7439

---
+++ lib/navigation/AvatarNavigation.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * AvatarNavigation.php - navigation for menu "below" the avatar in header
+ *
+ * @author  Jan-Hendrik Willms <tleilax+studip@gmail.com>
+ * @license GPL2 or any later version
+ */
+class AvatarNavigation extends Navigation
+{
+    /**
+     * Initialize a new Navigation instance.
+     */
+    public function __construct()
+    {
+        parent::__construct(_('Avatar-Menü'));
+    }
+
+    /**
+     * Initialize the subnavigation of this item. This method
+     * is called once before the first item is added or removed.
+     */
+    public function initSubNavigation()
+    {
+        parent::initSubNavigation();
+
+        // Link to profile
+        $navigation = new Navigation(_('Profil'), 'dispatch.php/profile/index');
+        $navigation->setImage(Icon::create('person'));
+        $this->addSubNavigation('profile', $navigation);
+
+        // Link to personal files
+        $navigation = new Navigation(_('Meine Dateien'), 'dispatch.php/files');
+        $navigation->setImage(Icon::create('folder-empty'));
+        $this->addSubNavigation('files', $navigation);
+
+        // Link to user data
+        $navigation = new Navigation(_('Nutzerdaten'), 'dispatch.php/settings/account');
+        $navigation->setImage(Icon::create('key'));
+        $this->addSubNavigation('account', $navigation);
+
+        // Link to user settings
+        $navigation = new Navigation(_('Einstellungen'), 'dispatch.php/settings/general');
+        $navigation->setImage(Icon::create('admin'));
+        $this->addSubNavigation('settings', $navigation);
+
+        // Link to logout
+        $navigation = new Navigation(_('Logout'), 'logout.php');
+        $navigation->setImage(Icon::create('door-leave'));
+        $this->addSubNavigation('logout', $navigation);
+    }
+}
