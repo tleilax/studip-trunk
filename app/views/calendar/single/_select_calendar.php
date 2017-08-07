531af428ac8a88dfb8018b6b63b228268e8668a4
Peter Thienel <thienel@data-quest.de>
2017-08-07 15:26:27 +0000

    names of calendar owners in full_rev order, re #7677

--- app/views/calendar/single/_select_calendar.php
+++ app/views/calendar/single/_select_calendar.php
@@ -30,7 +30,7 @@
                                 continue;
                             } ?>
                             <option value="<?= $calendar_user->owner_id ?>"<?= ($range_id == $calendar_user->owner_id ? ' selected' : '') ?>>
-                                <?= htmlReady($calendar_user->owner->getFullname()) ?>
+                                <?= htmlReady($calendar_user->owner->getFullname('full-rev')) ?>
                             </option>
                         <? endforeach ?>
                         </optgroup>
