4b3b325e1b6d99969cccb38a81e00220a59548c2
hackl <hackl>
2017-10-13 07:32:06 +0000

    include lib/seminar_open.php so that plugins are loaded, re #7824

--- public/sendfile.php
+++ public/sendfile.php
@@ -44,6 +44,7 @@ page_open(array("sess" => "Seminar_Session",
                 "perm" => "Seminar_Perm",
                 "user" => "Seminar_User"));
 
+include 'lib/seminar_open.php';
 
 //basename() needs setlocale()
 init_i18n($_SESSION['_language']);
