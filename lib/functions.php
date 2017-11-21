57b760b0ac5ef4c799b48a070b7aa05c04a3e086
Elmar Ludwig <ludwig@uni-osnabrueck.de>
2017-11-21 15:25:18 +0000

    use rawurlencode and fix comments, re #7913

--- lib/functions.php
+++ lib/functions.php
@@ -1456,10 +1456,10 @@ function studip_json_encode($data, $options = 0)
 function encode_header_parameter($name, $value)
 {
     if (preg_match('/[\200-\377]/', $value)) {
-        // use RFC 2616 encoding (ext-parameter)
-        return $name . "*=UTF-8''" . urlencode($value);
+        // use RFC 5987 encoding (ext-parameter)
+        return $name . "*=UTF-8''" . rawurlencode($value);
     } else {
-        // use RFC 5987 encoding (quoted-string)
+        // use RFC 2616 encoding (quoted-string)
         return $name . '="' . addslashes($value) . '"';
     }
 }
