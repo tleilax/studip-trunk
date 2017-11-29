68ae592f461c1e1a21ca95728990b67e9e13f564
Jan-Hendrik Willms <tleilax+github@gmail.com>
2017-11-29 11:04:20 +0000

    js secure forms: treat data-secure='false' as if the attribute was not set at all, re #7954

--- public/assets/javascripts/studip-secure-forms.js
+++ public/assets/javascripts/studip-secure-forms.js
@@ -58,7 +58,7 @@
      */
     function normalizeConfig(input) {
         var config = {
-            always: false,
+            always: null,
             exists: false
         };
         if ($.isPlainObject(input)) {
@@ -95,7 +95,7 @@
 
             if (config.always === true) {
                 changed = true;
-            } else {
+            } else if (config.always !== false) {
                 items.filter('[name]').each(function () {
                     changed = changed
                            || (this.defaultValue !== undefined && this.value !== this.defaultValue);
