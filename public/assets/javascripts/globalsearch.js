b97abcfbc9555f72924668c31f8a503bff39b9b6
hackl <hackl>
2017-12-05 14:53:18 +0000

    don't just use html() because of XSS problems, re #7893

--- public/assets/javascripts/globalsearch.js
+++ public/assets/javascripts/globalsearch.js
@@ -21,12 +21,14 @@ STUDIP.GlobalSearch = {
         var searchterm = $('#globalsearch-input').val();
         if ($('#globalsearch-input').val() != '') {
             var resultsDiv = $('#globalsearch-results');
+            // Call AJAX endpoint and get search results.
             $.ajax(
                 STUDIP.ABSOLUTE_URI_STUDIP +'dispatch.php/globalsearch/find',
                 {
                     data: {
                         'search': encodeURIComponent(searchterm)
                     },
+                    // Display spinner symbol, user should always see something is happening.
                     beforeSend: function(xhr, settings) {
                         resultsDiv.attr('align', 'center');
                         resultsDiv.html('');
@@ -39,32 +41,39 @@ STUDIP.GlobalSearch = {
                             attr('src', STUDIP.ASSETS_URL + 'images/ajax-indicator-black.svg').
                             attr('id', 'globalsearch-loading-icon'));
                     },
+                    // Success: show search results.
                     success: function (data, status, xhr) {
-                        $('#globalsearch-loading-text').remove();
-                        $('#globalsearch-loading-icon').remove();
+                        resultsDiv.html('');
+                        // Some results found...
                         if ($(data).length > 0) {
                             resultsDiv.attr('align', null);
-                            resultsDiv.css('max-height', ($('html').height() - 150));
+                            $('#globalsearch-list').css('max-height', ($('html').height() - 100));
                             // Iterate over each result category.
                             $.each(data, function(name, value) {
                                 // Create an <article> for category...
                                 var category = $('<article>').
                                     append($('<header>').
-                                        html(value.name + ' (' + value.content.length + ')'));
+                                        text(value.name + ' (' + value.content.length + ')'));
                                 resultsDiv.append(category);
 
                                 // Process results and create corresponding entries.
                                 $.each(value.content, function(index, result) {
                                     // Build detail text.
-                                    var detail = '';
+                                    var description = null;
                                     if (result.description != null) {
-                                        detail += result.description + '<br>';
+                                        description = $('<div>').
+                                            attr('class', 'globalsearch-result-description').
+                                            html($.parseHTML(result.description));
                                     }
+                                    var additional = null;
                                     if (result.additional != null) {
-                                        detail += result.additional + '<br>';
+                                        additional = $('<div>').
+                                            attr('class', 'globalsearch-result-additional').
+                                            html($.parseHTML(result.additional));
                                     }
                                     // Create single result entry.
                                     var single = $('<section>');
+                                    // Optional image...
                                     if (result.img != null) {
                                         single.append($('<div class="globalsearch-result-img">').
                                             append(singleImg = $('<img>').
@@ -72,6 +81,7 @@ STUDIP.GlobalSearch = {
                                                 attr('width', '36').
                                                 attr('src', result.img)));
                                     }
+                                    // Name/title
                                     var dataDiv = $('<div>').
                                         attr('class', 'globalsearch-result-data');
                                     single.append(dataDiv);
@@ -79,16 +89,25 @@ STUDIP.GlobalSearch = {
                                         attr('class', 'globalsearch-result-link').
                                         append($('<a>').
                                             attr('href', result.url).
-                                            html(result.name)));
+                                            html($.parseHTML(result.name))));
+                                    // Details like:
                                     var singleDetails = $('<div>').
-                                        attr('class', 'globalsearch-result-details').
-                                        html(detail);
+                                        attr('class', 'globalsearch-result-details');
+                                    // Descriptional text
+                                    if (description != null) {
+                                        singleDetails.append(description);
+                                    }
+                                    // Additional information
+                                    if (additional != null) {
+                                        singleDetails.append(additional);
+                                    }
                                     dataDiv.append(singleDetails);
+                                    // Date/Time of entry
                                     if (result.date != null) {
                                         var singleTime = $('<div>').
                                             attr('class', 'globalsearch-result-time').
                                             css('max-width', '20%').
-                                            html(result.date);
+                                            html($.parseHTML(result.date));
                                         single.append(singleTime);
                                     }
                                     category.append(single);
@@ -105,6 +124,7 @@ STUDIP.GlobalSearch = {
             );
         }
     }
+
 };
 
 $(function () {
