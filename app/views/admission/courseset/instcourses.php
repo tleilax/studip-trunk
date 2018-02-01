c821b22676a811f4166580c59dc29633dc6e717e
David Siegfried <ds.siegfried@gmail.com>
2018-02-01 09:04:16 +0000

    cleanup and fixes #7764

--- app/views/admission/courseset/instcourses.php
+++ app/views/admission/courseset/instcourses.php
@@ -1,56 +1,54 @@
 <table id="courselist" class="default">
     <thead>
         <colgroup>
-            <col width="15"/>
-            <col width="75"/>
-            <col/>
+            <col width="15">
+            <col width="75">
+            <col>
         </colgroup>
         <tr>
-            <th colspan="3">
+            <th>
+                <input type="checkbox" data-proxyfor="[name^=courses]">
+            </th>
+            <th colspan="2">
                 <?= _('Veranstaltungszuordnung:') ?>
-                <span class="actions">
-                    <a href="#" onclick="return STUDIP.Admission.checkUncheckAll('courses[]', 'check');"><?= _('alle') ?></a>
-                    |
-                    <a href="#" onclick="return STUDIP.Admission.checkUncheckAll('courses[]', 'uncheck');"><?= ('keine') ?></a>
-                    |
-                    <a href="#" onclick="return STUDIP.Admission.checkUncheckAll('courses[]', 'invert');"><?= ('Auswahl umkehren') ?></a>
-                </span>
             </th>
         </tr>
     </thead>
     <tbody>
-        <?php foreach ($allCourses as $course) {
-            $title = $course['Name'];
-            $title .= (!$course['visible'] ? ' (' . _("versteckt") . ')' : '');
-            $title .= " (" . (int)$course['admission_turnout'] . ")";
-            if (in_array($course['seminar_id'], $selectedCourses)) {
-                $selected = ' checked="checked"';
-            } else {
-                $selected = '';
-            }
-        ?>
-        <tr class="course">
-            <td width="10">
-                <input type="checkbox" name="courses[]" id="<?= $course['seminar_id'] ?>" value="<?= $course['seminar_id'] ?>"<?= $selected ?>/>
-            </td>
-            <td>
-                <label for="<?= $course['seminar_id'] ?>">
-                    <?= htmlReady($course['VeranstaltungsNummer']) ?>
-                </label>
-            </td>
-            <td>
-                <label for="<?= $course['seminar_id'] ?>">
-                <a href="<?=URLHelper::getScriptLink('dispatch.php/course/details/index/' . $course['seminar_id']) ?>"  data-dialog>
-                <?= Icon::create('info-circle', 'inactive', ['title' =>_('Veranstaltungsdetails aufrufen')])->asImg()?>
-                </a>
-                    <?= htmlReady($title) ?>
-                <? if ($course['admission_type']) : ?>
-                <? $typename = call_user_func($course['admission_type'] . '::getName') ?>
-                    <?= Icon::create('exclaim-circle', 'attention', ['title' => sprintf(_("vorhandene Anmelderegel: %s"), $typename)])->asImg(); ?>
-                <? endif ?>
-                </label>
-            </td>
-        </tr>
-        <?php } ?>
+        <? if(!empty($allCourses)) : ?>
+            <?foreach ($allCourses as $course) {
+                $title = $course['Name'];
+                $title .= (!$course['visible'] ? ' (' . _("versteckt") . ')' : '');
+                $title .= " (" . (int)$course['admission_turnout'] . ")";
+                if (in_array($course['seminar_id'], $selectedCourses)) {
+                    $selected = ' checked="checked"';
+                } else {
+                    $selected = '';
+                }
+                ?>
+                <tr class="course">
+                    <td>
+                        <input type="checkbox" name="courses[]" id="<?= $course['seminar_id'] ?>" value="<?= $course['seminar_id'] ?>"<?= $selected ?>>
+                    </td>
+                    <td>
+                        <label for="<?= $course['seminar_id'] ?>">
+                            <?= htmlReady($course['VeranstaltungsNummer']) ?>
+                        </label>
+                    </td>
+                    <td>
+                        <label for="<?= $course['seminar_id'] ?>">
+                            <a href="<?=URLHelper::getScriptLink('dispatch.php/course/details/index/' . $course['seminar_id']) ?>"  data-dialog>
+                                <?= Icon::create('info-circle', 'inactive', ['title' =>_('Veranstaltungsdetails aufrufen')])->asImg()?>
+                            </a>
+                            <?= htmlReady($title) ?>
+                            <? if ($course['admission_type']) : ?>
+                                <? $typename = call_user_func($course['admission_type'] . '::getName') ?>
+                                <?= Icon::create('exclaim-circle', 'attention', ['title' => sprintf(_("vorhandene Anmelderegel: %s"), $typename)])->asImg(); ?>
+                            <? endif ?>
+                        </label>
+                    </td>
+                </tr>
+            <?php } ?>
+        <? endif?>
     </tbody>
 </table>
