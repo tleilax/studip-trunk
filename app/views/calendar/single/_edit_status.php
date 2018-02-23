3fa7ca8aa493e0481cbc3cde614f7541b28c0654
Moritz Strohm <strohm@data-quest.de>
2018-02-23 10:01:44 +0000

    improved text string in calendar/single/_edit_status, fixes #8222

--- app/views/calendar/single/_edit_status.php
+++ app/views/calendar/single/_edit_status.php
@@ -1,7 +1,7 @@
 <? use Studip\Button, Studip\LinkButton; ?>
 <form action="" method="post">
     <div>
-        <b><?= _('Eigener Teilnehmendenstatus') ?>:</b>
+        <b><?= _('Eigener Teilnahmestatus') ?>:</b>
         <? $group_status = array(
             CalendarEvent::PARTSTAT_TENTATIVE => _('Abwartend'),
             CalendarEvent::PARTSTAT_ACCEPTED => _('Angenommen'),
