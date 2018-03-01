8f7680aa77d13ab9a3a8232453d1bc2c48def483
David Siegfried <ds.siegfried@gmail.com>
2018-03-01 10:30:55 +0000

    fixes #8233

--- templates/dates/seminar_html_location.php
+++ templates/dates/seminar_html_location.php
@@ -3,18 +3,29 @@ if (!isset($link)) $link = true;
 
 // condense regular dates by room
 if (is_array($dates['regular']['turnus_data'])) foreach ($dates['regular']['turnus_data'] as $cycle) :
+    $first_date   = sprintf(_("ab %s"), strftime('%x', $cycle['first_date']['date']));
+    $cycle_output = $cycle['tostring'] . ' (' . $first_date . ')';
+    if ($cycle['desc'])
+        $cycle_output .= ', <i>' . htmlReady($cycle['desc']) . '</i>';
+
+    if ($show_room) :
+        $cycle_output .= $this->render_partial('dates/_seminar_rooms', ['assigned' => $cycle['assigned_rooms'],
+                                                                        'freetext' => $cycle['freetext_rooms'],
+                                                                        'link'     => $link
+        ]);
+    endif;
+
     if (is_array($cycle['assigned_rooms'])) foreach ($cycle['assigned_rooms'] as $room_id => $count) :
         $resObj = ResourceObject::Factory($room_id);
         if ($link) {
-            $output[$resObj->getFormattedLink(TRUE, TRUE, TRUE)][] = $cycle['tostring_short'] .' ('. $count .'x)';
+            $output[$resObj->getFormattedLink(true, true, true)][] = $cycle_output .' ('. $count .'x)';
         } else {
-            $output[htmlReady($resObj->getName())][] = $cycle['tostring_short'] .' ('. $count .'x)';
+            $output[htmlReady($resObj->getName())][] = $cycle_output .' ('. $count .'x)';
         }
     endforeach;
-
     if (is_array($cycle['freetext_rooms'])) foreach ($cycle['freetext_rooms'] as $room => $count) :
         if ($room) :
-            $output['('. htmlReady($room) .')'][] = $cycle['tostring_short']  .' ('. $count .'x)';
+            $output['(' . htmlReady($room) . ')'][] = $cycle['tostring'] . ' (' . $count . 'x)';
         endif;
     endforeach;
 endforeach;
@@ -29,44 +40,48 @@ if (is_array($dates['irregular'])) foreach ($dates['irregular'] as $date) :
     endif;
 endforeach;
 
+
 // now shrink the dates for each room/freetext and add them to the output
 if (is_array($output_dates)) foreach ($output_dates as $dates) :
     if ($dates[0]['resource_id']) :
         $resObj = ResourceObject::Factory($dates[0]['resource_id']);
         if ($link) {
-            $output[$resObj->getFormattedLink(TRUE, TRUE, TRUE)][] = implode('<br>', shrink_dates($dates));
+            $output[$resObj->getFormattedLink(true, true, true)][] = implode('<br>', shrink_dates($dates));
         } else {
             $output[htmlReady($resObj->getName())][] = implode('<br>', shrink_dates($dates));
         }
     elseif ($dates[0]['raum']) :
-        $output['('. htmlReady($dates[0]['raum']) .')'][] = implode('<br>', shrink_dates($dates));
+        $output['(' . htmlReady($dates[0]['raum']) . ')'][] = implode('<br>', shrink_dates($dates));
     endif;
 endforeach;
 ?>
 
+
 <? if (sizeof($output) == 0) : ?>
     <?= htmlReady($ort) ?: _("nicht angegeben") ?>
 <? else: ?>
-<table class="default">
-  <? foreach ($output as $room => $dates) : ?>
-  <tr>
-    <td style="vertical-align: top"><?= $room ?></td>
-    <td>
-        <? $dates = implode('<br>', $dates)?>
+    <table class="default">
+        <? foreach ($output
 
-        <? if(mb_strlen($dates) > 222) : ?>
-            <?= mb_substr($dates, 0, 228)?>
-            <div class="more-location-dates-infos" style="display:none">
-                <?=$dates?>
-            </div>
-            <div>
-                <span class='more-location-digits'>...</span>
-                <a class="more-location-dates" style="cursor: pointer; margin-left: 3px">(mehr)</a>
-            </div>
-        <? else : ?>
-            <?= $dates ?>
-        <? endif ?>
-    </td>
-  <? endforeach ?>
-</table>
+        as $room => $dates) : ?>
+        <tr>
+            <td style="vertical-align: top"><?= $room ?></td>
+            <td>
+                <? $dates = implode('<br>', $dates) ?>
+
+                <? if (mb_strlen($dates) > 222) : ?>
+                    <?= mb_substr($dates, 0, 228) ?>
+                    <div class="more-location-dates-infos" style="display:none">
+                        <?= $dates ?>
+                    </div>
+                    <div>
+                        <span class='more-location-digits'>...</span>
+                        <a class="more-location-dates" style="cursor: pointer; margin-left: 3px">(mehr)</a>
+                    </div>
+                <? else : ?>
+                    <?= $dates ?>
+                <? endif ?>
+            </td>
+            <? endforeach ?>
+    </table>
 <? endif ?>
