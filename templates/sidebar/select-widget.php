aba068594bb0d6a21ed27c9016411b23c02b5e35
Stefan Osterloh <s.osterloh@uni-oldenburg.de>
2018-03-17 15:09:42 +0000

    remove !SelectorWidget from course/admission/index and add title attr for select-widget, re: #8336

--- templates/sidebar/select-widget.php
+++ templates/sidebar/select-widget.php
@@ -1,19 +1,23 @@
 <form action="<?= $url ?>" method="<?= $method ?>">
     <?= \SelectWidget::arrayToHiddenInput($params) ?>
     <select class="sidebar-selectlist <?= $class ?> <? if ($__is_nested): ?>nested-select<? endif; ?>" <? if ($size) printf('size="%u"', $size); ?> <?= $attributes ?>
-        name="<?= sprintf('%s%s',htmlReady($name), $multiple ? '[]' : '') ?>" <? if ($multiple) : ?>multiple<? endif; ?>>
+        name="<?= sprintf('%s%s', htmlReady($name), $multiple ? '[]' : '') ?>" <? if ($multiple) echo 'multiple'; ?>>
 
     <? foreach ($elements as $element): ?>
         <? if ($element instanceof SelectGroupElement && count($element->getElements()) > 0): ?>
             <optgroup label="<?= htmlReady($element->getLabel() ) ?>">
             <? foreach ($element->getElements() as $option): ?>
-                <option value="<?= htmlReady($option->getId()) ?>" <? if ($option->isActive()) echo 'selected'; ?> class="<? if ($element->getIndentLevel()): ?>nested-item nested-item-level-<?= $element->getIndentLevel() + 1 ?><? endif; ?>  <? if ($element->isHeader()): ?>nested-item-header<? endif; ?>">
+                <option value="<?= htmlReady($option->getId()) ?>" <? if ($option->isActive()) echo 'selected'; ?>
+                    class="<? if ($element->getIndentLevel()): ?>nested-item nested-item-level-<?= $element->getIndentLevel() + 1 ?><? endif; ?>  <? if ($element->isHeader()): ?>nested-item-header<? endif; ?>">
                     <?= htmlReady(my_substr($option->getLabel(), 0, $max_length)) ?>
                 </option>
             <? endforeach; ?>
             </optgroup>
         <? elseif (!($element instanceof SelectGroupElement)): ?>
-            <option value="<?= htmlReady($element->getId()) ?>" <? if ($element->isActive()) echo 'selected'; ?> class="<? if ($element->getIndentLevel()): ?>nested-item nested-item-level-<?= $element->getIndentLevel() + 1 ?><? endif; ?> <? if ($element->isHeader()): ?>nested-item-header<? endif; ?>">
+            <option value="<?= htmlReady($element->getId()) ?>" <? if ($element->isActive()) echo 'selected'; ?>
+                class="<? if ($element->getIndentLevel()): ?>nested-item nested-item-level-<?= $element->getIndentLevel() + 1 ?><? endif; ?> <? if ($element->isHeader()): ?>nested-item-header<? endif; ?>"
+                title="<?= htmlReady($element->getTooltip()  !== null ? $element->getTooltip() : $element->getLabel()) ?>">
+
                 <?= htmlReady(my_substr($element->getLabel(), 0, $max_length)) ?>
             </option>
         <? endif; ?>
