80a5f9d31bd7aac36f99d45f2298a75104359ea5
Stefan Osterloh <s.osterloh@uni-oldenburg.de>
2018-03-17 15:02:54 +0000

    use Context::getId(), fixes: #8352

--- app/controllers/course/admission.php
+++ app/controllers/course/admission.php
@@ -70,7 +70,7 @@ class Course_AdmissionController extends AuthenticatedController
                     new SelectElement(
                         $seminar['Seminar_id'],
                         $seminar['Name'],
-                        $seminar['Seminar_id'] === $_SESSION['SessionSeminar'],
+                        $seminar['Seminar_id'] === Context::getId(),
                         $seminar['VeranstaltungsNummer'] . ' ' . $seminar['Name']
                     ),
                     'select-' . $seminar['Seminar_id']
