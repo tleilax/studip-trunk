5ddbc0c9f3c8e02f0be3864355bd9178aba698ca
David Siegfried <ds.siegfried@gmail.com>
2018-03-22 14:43:24 +0000

    fix indentation re#7624

--- app/views/help_content/admin_conflicts.php
+++ app/views/help_content/admin_conflicts.php
@@ -9,36 +9,36 @@
                     <col width="50%">
                 </colgroup>
                 <thead>
-                <? $keys = array_keys($conflict); ?>
-                <tr>
-                    <th colspan="2"><?= _('Seite:') ?> <?= $conflict[$keys[0]]->route ?></th>
-                </tr>
+                    <? $keys = array_keys($conflict); ?>
+                    <tr>
+                        <th colspan="2"><?= _('Seite:') ?> <?= $conflict[$keys[0]]->route ?></th>
+                    </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <th><?= sprintf(_("Lokale Version (%s)"), $conflict[$keys[0]]->studip_version) ?></th>
-                    <th><?= sprintf(_("Offizielle Version (%s)"), $conflict[$keys[1]]->studip_version) ?></th>
-                </tr>
-                <tr>
-                    <td>
-                        <?= $conflict[$keys[0]]->content ?>
-                    </td>
-                    <td>
-                        <?= $conflict[$keys[1]]->content ?>
-                    </td>
-                </tr>
-                <tr>
-                </tr>
+                    <tr>
+                        <th><?= sprintf(_("Lokale Version (%s)"), $conflict[$keys[0]]->studip_version) ?></th>
+                        <th><?= sprintf(_("Offizielle Version (%s)"), $conflict[$keys[1]]->studip_version) ?></th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <?= $conflict[$keys[0]]->content ?>
+                        </td>
+                        <td>
+                            <?= $conflict[$keys[1]]->content ?>
+                        </td>
+                    </tr>
+                    <tr>
+                    </tr>
                 </tbody>
                 <tfoot>
-                <tr>
-                    <td><?= LinkButton::create(_('Übernehmen'), $controller->url_for('help_content/resolve_conflict/' . $conflict[$keys[0]]->getId() . '/accept')) ?></td>
-                    <td><?= LinkButton::create(_('Übernehmen'), $controller->url_for('help_content/resolve_conflict/' . $conflict[$keys[0]]->getId() . '/delete')) ?></td>
-                </tr>
+                    <tr>
+                        <td><?= LinkButton::create(_('Übernehmen'), $controller->url_for('help_content/resolve_conflict/' . $conflict[$keys[0]]->getId() . '/accept')) ?></td>
+                        <td><?= LinkButton::create(_('Übernehmen'), $controller->url_for('help_content/resolve_conflict/' . $conflict[$keys[0]]->getId() . '/delete')) ?></td>
+                    </tr>
                 </tfoot>
             </table>
         <? endforeach ?>
     <? else : ?>
-        <?= MessageBox::info(_('Keine Konflikte vorhanden.'))?>
+        <?= MessageBox::info(_('Keine Konflikte vorhanden.')) ?>
     <? endif ?>
 </form>
--- app/views/help_content/admin_overview.php
+++ app/views/help_content/admin_overview.php
@@ -1,7 +1,7 @@
 <? use Studip\Button; ?>
 <? if ($count = count($help_contents)) : ?>
     <form action="<?= $controller->url_for('help_content/store_settings') ?>" method="post">
-        <input type="hidden" name="help_content_searchterm" value="<?= $help_content_searchterm?>">
+        <input type="hidden" name="help_content_searchterm" value="<?= $help_content_searchterm ?>">
         <?= CSRFProtection::tokenTag() ?>
         <table class="default">
             <caption>
@@ -15,43 +15,45 @@
                 <col width="80">
             </colgroup>
             <thead>
-            <tr>
-                <th><?= _("Aktiv") ?></th>
-                <th><?= _("Seite") ?></th>
-                <th><?= _("Sprache") ?></th>
-                <th><?= _("Inhalt") ?></th>
-                <th><?= _("Aktion") ?></th>
-            </tr>
+                <tr>
+                    <th><?= _("Aktiv") ?></th>
+                    <th><?= _("Seite") ?></th>
+                    <th><?= _("Sprache") ?></th>
+                    <th><?= _("Inhalt") ?></th>
+                    <th><?= _("Aktion") ?></th>
+                </tr>
             </thead>
             <tbody>
-            <? foreach ($help_contents as $help_content_id => $help_content) : ?>
-                <tr>
-                    <td><input type="checkbox" name="help_content_status_<?= $help_content_id ?>" id="help_content_status_<?= $help_content_id ?>" value="1" class="studip-checkbox help_on"
-                               <?= tooltip(_("Status der Hilfe (aktiv oder inaktiv)"), false) ?><?= ($help_content->visible) ? ' checked' : '' ?>>
-                        <label for="help_content_status_<?= $help_content_id ?>">
-                            <? _('Status der Hilfe (aktiv oder inaktiv)')?>
-                        </label>
-                    </td>
-                    <td><?= htmlReady($help_content->route) ?></td>
-                    <td><?= htmlReady($help_content->language) ?></td>
-                    <td><?= formatReady($help_content->content) ?></td>
-                    <td>
-                        <a href="<?= URLHelper::getURL('dispatch.php/help_content/edit/' . $help_content_id) ?>" <?= tooltip(_('Hilfe-Text bearbeiten')) ?>
-                           data-dialog="size=auto;reload-on-close">
-                            <?= Icon::create('edit', 'clickable')->asImg() ?></a>
-                        <a href="<?= URLHelper::getURL('dispatch.php/help_content/delete/' . $help_content_id) ?>" <?= tooltip(_('Hilfe-Text löschen')) ?>
-                           data-dialog="size=auto;reload-on-close">
-                            <?= Icon::create('trash', 'clickable')->asImg() ?></a>
-                    </td>
-                </tr>
-            <? endforeach ?>
+                <? foreach ($help_contents as $help_content_id => $help_content) : ?>
+                    <tr>
+                        <td><input type="checkbox" name="help_content_status_<?= $help_content_id ?>"
+                                   id="help_content_status_<?= $help_content_id ?>" value="1"
+                                   class="studip-checkbox help_on"
+                                <?= tooltip(_("Status der Hilfe (aktiv oder inaktiv)"), false) ?><?= ($help_content->visible) ? ' checked' : '' ?>>
+                            <label for="help_content_status_<?= $help_content_id ?>">
+                                <? _('Status der Hilfe (aktiv oder inaktiv)') ?>
+                            </label>
+                        </td>
+                        <td><?= htmlReady($help_content->route) ?></td>
+                        <td><?= htmlReady($help_content->language) ?></td>
+                        <td><?= formatReady($help_content->content) ?></td>
+                        <td>
+                            <a href="<?= URLHelper::getURL('dispatch.php/help_content/edit/' . $help_content_id) ?>" <?= tooltip(_('Hilfe-Text bearbeiten')) ?>
+                               data-dialog="size=auto;reload-on-close">
+                                <?= Icon::create('edit', 'clickable')->asImg() ?></a>
+                            <a href="<?= URLHelper::getURL('dispatch.php/help_content/delete/' . $help_content_id) ?>" <?= tooltip(_('Hilfe-Text löschen')) ?>
+                               data-dialog="size=auto;reload-on-close">
+                                <?= Icon::create('trash', 'clickable')->asImg() ?></a>
+                        </td>
+                    </tr>
+                <? endforeach ?>
             </tbody>
             <tfoot>
-            <tr>
-                <td colspan="6">
-                    <?= Button::createAccept(_('Speichern'), 'save_help_content_settings') ?>
-                </td>
-            </tr>
+                <tr>
+                    <td colspan="6">
+                        <?= Button::createAccept(_('Speichern'), 'save_help_content_settings') ?>
+                    </td>
+                </tr>
             </tfoot>
         </table>
     </form>
