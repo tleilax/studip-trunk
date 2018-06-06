56af943df56497dc5554872af30496fc170b71a3
hackl <hackl>
2018-06-06 10:33:49 +0000

    change appearance according to GUI review proposal, re #8346

--- app/views/avatar/update.php
+++ app/views/avatar/update.php
@@ -1,34 +1,50 @@
-<form class="default avatar" enctype="multipart/form-data"
+<form class="default settings-avatar" enctype="multipart/form-data"
         action="<?= $controller->url_for('avatar/upload', $type, $id) ?>" method="post">
-    <?= CSRFProtection::tokenTag() ?>
-    <section>
-        <div id="avatar-buttons" class="hidden-js">
-            <a href="" id="avatar-zoom-in" title="<?= _('Vergrößern') ?>">
-                <?= Icon::create('add')->asImg(24) ?>
-            </a>
-            <a href="" id="avatar-zoom-out" title="<?= _('Verkleinern') ?>">
-                <?= Icon::create('remove')->asImg(24) ?>
-            </a>
-            <a href="" id="avatar-rotate-clockwise" title="<?= _('Im Uhrzeigersinn drehen') ?>">
-                <?= Icon::create('arr_1right')->asImg(24) ?>
-            </a>
-            <a href="" id="avatar-rotate-counter-clockwise" title="<?= _('Gegen den Uhrzeigersinn drehen') ?>">
-                <?= Icon::create('arr_1left')->asImg(24) ?>
-            </a>
-        </div>
-    </section>
-    <section>
-        <div id="avatar-preview">
-            <img id="new-avatar" src="<?= $avatar ?>">
-        </div>
+    <fieldset>
+        <legend>
+            <?= $type == 'user' ? _('Profilbild') :
+                ($type == 'course' ? _('Veranstaltungsbild') : _('Einrichtungsbild')) ?>
+        </legend>
+        <div class="form-group">
+            <div id="avatar-preview">
+                <img class="avatar-normal" id="new-avatar" src="<?= $avatar ?>">
+            </div>
 
-        <label class="avatar-upload">
-            <?= _('Laden Sie hier ein neues Bild hoch') ?>
-            <input type="file" name="avatar-img" id="avatar-upload">
-        </label>
+            <label class="file-upload">
+                <?= _('Wählen Sie ein Bild von Ihrer Festplatte aus.') ?>
+                <input name="avatar-img" type="file" id="avatar-upload" accept="image/gif,image/png,image/jpeg"
+                       data-max-size="<?= Avatar::MAX_FILE_SIZE ?>"
+                       data-message-too-large="<?= _('Die hochgeladene Datei ist zu groß. Bitte wählen Sie ein anderes Bild.') ?>">
 
-        <input type="hidden" name="cropped-image" id="cropped-image" value="">
-    </section>
+                <p class="form-text">
+                    <?= sprintf(
+                        _('Die Bilddatei darf max. %s groß sein, es sind nur Dateien mit den Endungen .jpg, .png oder .gif erlaubt!'),
+                        relsize(Avatar::MAX_FILE_SIZE)
+                    ) ?>
+                </p>
+
+                <a onclick="javascript:void 0" class=button>Auswählen</a>
+            </label>
+
+            <input type="hidden" name="cropped-image" id="cropped-image" value="">
+
+            <div id="avatar-buttons" class="hidden-js">
+                <a href="" id="avatar-zoom-in" title="<?= _('Vergrößern') ?>">
+                    <?= Icon::create('add')->asImg(24) ?>
+                </a>
+                <a href="" id="avatar-zoom-out" title="<?= _('Verkleinern') ?>">
+                    <?= Icon::create('remove')->asImg(24) ?>
+                </a>
+                <a href="" id="avatar-rotate-clockwise" title="<?= _('Im Uhrzeigersinn drehen') ?>">
+                    <?= Icon::create('arr_1right')->asImg(24) ?>
+                </a>
+                <a href="" id="avatar-rotate-counter-clockwise" title="<?= _('Gegen den Uhrzeigersinn drehen') ?>">
+                    <?= Icon::create('arr_1left')->asImg(24) ?>
+                </a>
+            </div>
+        </div>
+        <?= CSRFProtection::tokenTag() ?>
+    </fieldset>
     <footer data-dialog-button>
         <?= Studip\Button::createAccept(_('Absenden'), 'upload', ['id' => 'submit-avatar']) ?>
         <? if ($customized): ?>
--- public/assets/javascripts/avatar.js
+++ public/assets/javascripts/avatar.js
@@ -6,13 +6,6 @@
     STUDIP.Avatar = {
 
         cropControls: function () {
-            var container = $('div#avatar-preview'),
-                dialog = container.closest('div[role="dialog"]');
-            if (container.length > 0) {
-                // Adjust maximal cropper container height to dialog dimensions.
-                container.css('max-height', dialog.height() - 200);
-                container.css('max-width', dialog.width() - 50);
-            }
             $('#avatar-upload').on('change', function () {
                 STUDIP.Avatar.readFile(this);
             });
@@ -23,41 +16,59 @@
                 var reader = new window.FileReader(),
                     cropper;
 
-                reader.onload = function (event) {
-                    $('#new-avatar').attr('src', event.target.result);
-                    var image = document.getElementById('new-avatar');
-                    cropper = new Cropper(image, {
-                        aspectRatio: 1, // 1 / 1,
-                        viewMode: 2
+                if (input.files[0].size <= $(input).data('max-size')) {
+
+                    var container = $('div#avatar-preview'),
+                        dialog = container.closest('div[role="dialog"]');
+                    if (container.length > 0) {
+                        // Adjust maximal cropper container height to dialog dimensions.
+                        container.css('height', dialog.height() - 200);
+                        container.css('width', dialog.width() - 100);
+                        container.css('max-height', dialog.height() - 200);
+                        container.css('max-width', dialog.width() - 100);
+                    }
+
+                    reader.onload = function (event) {
+
+                        $('#new-avatar').attr('src', event.target.result);
+                        var image = document.getElementById('new-avatar');
+                        cropper = new Cropper(image, {
+                            aspectRatio: 1, // 1 / 1,
+                            viewMode: 2
+                        });
+                    };
+
+                    reader.readAsDataURL(input.files[0]);
+
+                    $('#avatar-buttons').removeClass('hidden-js');
+                    $('label.file-upload').hide();
+                    $('#avatar-zoom-in').on('click', function () {
+                        cropper.zoom(0.1);
+                        return false;
                     });
-                };
+                    $('#avatar-zoom-out').on('click', function () {
+                        cropper.zoom(-0.1);
+                        return false;
+                    });
+                    $('#avatar-rotate-clockwise').on('click', function () {
+                        cropper.rotate(90);
+                        return false;
+                    });
+                    $('#avatar-rotate-counter-clockwise').on('click', function () {
+                        cropper.rotate(-90);
+                        return false;
+                    });
 
-                reader.readAsDataURL(input.files[0]);
+                    $('#submit-avatar').on('click', function () {
+                        $('#cropped-image').attr('value', cropper.getCroppedCanvas().toDataURL());
+                    });
 
-                $('#avatar-buttons').removeClass('hidden-js');
-                $('label.avatar-upload').hide();
-                $('#avatar-zoom-in').on('click', function () {
-                    cropper.zoom(0.1);
-                    return false;
-                });
-                $('#avatar-zoom-out').on('click', function () {
-                    cropper.zoom(-0.1);
-                    return false;
-                });
-                $('#avatar-rotate-clockwise').on('click', function () {
-                    cropper.rotate(90);
-                    return false;
-                });
-                $('#avatar-rotate-counter-clockwise').on('click', function () {
-                    cropper.rotate(-90);
-                    return false;
-                });
+                } else {
+                    alert($(input).data('message-too-large'));
+                }
 
-                $('#submit-avatar').on('click', function () {
-                    $('#cropped-image').attr('value', cropper.getCroppedCanvas().toDataURL());
-                });
             } else {
-                window.alert("Sorry - your browser doesn't support the FileReader API");
+                alert("Sorry - your browser doesn't support the FileReader API");
             }
         }
     };
