fed07b31145998b59836ac254c4b6b1b153706d0
luniki <github@luniki.de>
2018-06-28 10:20:12 +0000

    repair again, fix #8681

--- resources/assets/javascripts/entry-avatar.js
+++ resources/assets/javascripts/entry-avatar.js
@@ -1,15 +1,25 @@
 /*global jQuery, STUDIP */
 STUDIP.Avatar = {
-
     image: '',
 
-    readFile: function (input) {
+    init: function(inputSelector) {
+        $(document).on('change', inputSelector, function() {
+            STUDIP.Avatar.readFile(this);
+
+            $(this)
+                .off('submit.avatar', 'form.settings-avatar')
+                .on('submit.avatar', 'form.settings-avatar', function() {
+                    var data = STUDIP.Avatar.image.cropper('getData');
+                    return STUDIP.Avatar.checkImageSize(data);
+                });
+        });
+    },
+
+    readFile: function(input) {
         if (window.FileReader && input.files && input.files[0]) {
-            var reader = new window.FileReader(),
-                cropper;
+            var reader = new window.FileReader();
 
             if (input.files[0].size <= jQuery(input).data('max-size')) {
-
                 var container = jQuery('div#avatar-preview'),
                     dialog = container.closest('div[role="dialog"]');
                 // We are in a modal dialog
@@ -19,7 +29,7 @@ STUDIP.Avatar = {
                     container.css('width', dialog.width() - 220);
                     container.css('max-height', dialog.height() - 200);
                     container.css('max-width', dialog.width() - 220);
-                // No dialog, full page.
+                    // No dialog, full page.
                 } else {
                     dialog = jQuery('#layout_content');
                     // Responsive view.
@@ -29,7 +39,7 @@ STUDIP.Avatar = {
                         container.css('width', 0.95 * dialog.width());
                         container.css('max-height', dialog.height() * 220);
                         container.css('max-width', 0.95 * dialog.width());
-                    // Non-dialog, non-responsive view.
+                        // Non-dialog, non-responsive view.
                     } else {
                         // Adjust maximal cropper container height to page dimensions.
                         container.css('height', dialog.height() - 100);
@@ -39,83 +49,64 @@ STUDIP.Avatar = {
                     }
                 }
 
-                reader.onload = function (event) {
-
+                reader.onload = function(event) {
                     STUDIP.Avatar.image = jQuery('#new-avatar');
                     STUDIP.Avatar.image.attr('src', event.target.result);
                     STUDIP.Avatar.image.cropper({
                         aspectRatio: 1, // 1 / 1,
                         viewMode: 2
                     });
-
-                    cropper = STUDIP.Avatar.image.data('cropper');
                 };
 
                 reader.readAsDataURL(input.files[0]);
 
                 jQuery('#avatar-buttons').removeClass('hidden-js');
                 jQuery('label.file-upload').hide();
-                jQuery('#avatar-zoom-in').on('click', function () {
-                    cropper.zoom(0.1);
+                jQuery('#avatar-zoom-in').on('click', function() {
+                    STUDIP.Avatar.image.cropper('zoom', 0.1);
                     return false;
                 });
-                jQuery('#avatar-zoom-out').on('click', function () {
-                    cropper.zoom(-0.1);
+                jQuery('#avatar-zoom-out').on('click', function() {
+                    STUDIP.Avatar.image.cropper('zoom', -0.1);
                     return false;
                 });
-                jQuery('#avatar-rotate-clockwise').on('click', function () {
-                    cropper.rotate(90);
+                jQuery('#avatar-rotate-clockwise').on('click', function() {
+                    STUDIP.Avatar.image.cropper('rotate', 90);
                     return false;
                 });
-                jQuery('#avatar-rotate-counter-clockwise').on('click', function () {
-                    cropper.rotate(-90);
-                    return false;
-                });
+                jQuery('#avatar-rotate-counter-clockwise').on(
+                    'click',
+                    function() {
+                        STUDIP.Avatar.image.cropper('rotate', -90);
+                        return false;
+                    }
+                );
 
-                jQuery('#submit-avatar').on('click', function () {
-                    jQuery('#cropped-image').attr('value', STUDIP.Avatar.image.cropper('getCroppedCanvas').toDataURL());
+                jQuery('#submit-avatar').on('click', function() {
+                    jQuery('#cropped-image').attr(
+                        'value',
+                        STUDIP.Avatar.image
+                            .cropper('getCroppedCanvas')
+                            .toDataURL()
+                    );
                 });
-
             } else {
                 alert(jQuery(input).data('message-too-large'));
             }
-
         } else {
             alert("Sorry - your browser doesn't support the FileReader API");
         }
     },
 
-    checkImageSize: function () {
-        var data = STUDIP.Avatar.image.cropper('getData');
-
+    checkImageSize: function(data) {
         // Show a warning if cropped area is smaller than 250x250px.
         if (data.width < 250 || data.height < 250) {
             return confirm(jQuery('#new-avatar').data('message-too-small'));
         }
         return true;
     }
-
 };
-/*jslint browser: true */
-(function ($, STUDIP) {
 
-    'use strict';
-
-    jQuery(function () {
-        jQuery(document).on('dialog-update', function () {
-            jQuery('#avatar-upload').on('change', function () {
-                STUDIP.Avatar.readFile(this);
-            });
-            jQuery('form.settings-avatar').on('submit', function(event) {
-                return STUDIP.Avatar.checkImageSize(event);
-            });
-        });
-        jQuery('#avatar-upload').on('change', function () {
-            STUDIP.Avatar.readFile(this);
-        });
-        jQuery('form.settings-avatar').on('submit', function(event) {
-            return STUDIP.Avatar.checkImageSize(event);
-        });
-    });
-
-}(jQuery, STUDIP));
+jQuery(function() {
+    STUDIP.Avatar.init('#avatar-upload');
+});
