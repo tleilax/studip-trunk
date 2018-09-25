aff422439a18ca797b497658e198d42082900f9a
Moritz Strohm <strohm@data-quest.de>
2018-09-25 09:46:01 +0000

    use DB queries instead of Config class in migration 243, re #8773

--- db/migrations/243_tic8773_sort_news_by_chdate.php
+++ db/migrations/243_tic8773_sort_news_by_chdate.php
@@ -8,20 +8,32 @@ class Tic8773SortNewsByChdate extends Migration
 {
     public function up()
     {
-        Config::get()->create(
-            'SORT_NEWS_BY_CHDATE',
-            [
-                'type' => 'boolean',
-                'value' => false,
-                'section' => 'view',
-                'range' => 'global',
-                'description' => 'Wenn diese Einstellung gesetzt ist werden Ankündigungen nach ihrem letzten Änderungsdatum statt ihrem Erstellungsdatum sortiert angezeigt.'
-            ]
+        $db = DBManager::get();
+
+        $db->exec(
+            "INSERT INTO `config`
+            (
+                `field`, `value`, `type`, `range`, `section`,
+                `mkdate`, `chdate`,
+                `description`
+            )
+            VALUES
+            (
+                'SORT_NEWS_BY_CHDATE', 'false', 'boolean', 'global', 'view',
+                UNIX_TIMESTAMP(), UNIX_TIMESTAMP(),
+                'Wenn diese Einstellung gesetzt ist werden Ankündigungen nach ihrem letzten Änderungsdatum statt ihrem Erstellungsdatum sortiert angezeigt.'
+            );"
         );
+
+        $db = null;
     }
 
     public function down()
     {
-        Config::get()->delete('SORT_NEWS_BY_CHDATE');
+        $db = DBManager::get();
+
+        $db->exec("DELETE FROM config WHERE field = 'SORT_NEWS_BY_CHDATE';");
+
+        $db = null;
     }
 }
