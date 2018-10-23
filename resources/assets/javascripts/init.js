57a61fd7d49aa6fdf4a0c99775ee043f1c38d4eb
luniki <github@luniki.de>
2018-10-23 12:17:24 +0000

    move overlay JS code, refs #8975

--- resources/assets/javascripts/init.js
+++ resources/assets/javascripts/init.js
@@ -1,5 +1,6 @@
 import HeaderMagic from './lib/header_magic.js';
 import NavigationShrinker from './lib/navigation_shrinker.js';
+import Overlay from './lib/overlay.js';
 import PersonalNotifications from './lib/personal_notifications.js';
 import Sidebar from './lib/sidebar.js';
 import SmileyPicker from './lib/smiley_picker.js';
@@ -9,6 +10,7 @@ import Toolbar from './lib/toolbar.js';
 window.STUDIP = _.assign(window.STUDIP || {}, {
     HeaderMagic,
     NavigationShrinker,
+    Overlay,
     PersonalNotifications,
     Sidebar,
     SmileyPicker,
---
+++ resources/assets/javascripts/lib/overlay.js
@@ -0,0 +1,106 @@
+const Overlay = {
+    delay: 300,
+    element: null,
+    selector: '.ui-front.modal-overlay',
+    timeout: null
+};
+
+Overlay.reset = function() {
+    if (this.timeout !== null) {
+        clearTimeout(this.timeout);
+        this.timeout = null;
+    }
+};
+
+Overlay.schedule = function(callback, delay) {
+    this.reset();
+    if (delay !== undefined && !delay) {
+        callback.call(this);
+    } else {
+        this.timeout = setTimeout(callback.bind(this), this.delay);
+    }
+};
+
+Overlay.show = function(ajax, containment, secure, callback, delay) {
+    this.schedule(function() {
+        if (this.element === null) {
+            containment = containment || 'body';
+
+            this.element = $('<div class="ui-front modal-overlay">');
+            if (ajax) {
+                this.element.addClass('modal-overlay-ajax');
+                if (ajax === 'dark') {
+                    this.element.addClass('modal-overlay-dark');
+                }
+            }
+            if (containment !== 'body') {
+                this.element.addClass('modal-overlay-local');
+            } else {
+                // Blur background
+                $('#layout_wrapper').addClass('has-overlay');
+            }
+            this.element.appendTo(containment);
+        }
+
+        if (secure) {
+            $(window).on('beforeunload.overlay', Overlay.securityHandler);
+        }
+        if ($.type(callback) === 'function') {
+            callback.call(this);
+        }
+    }, delay);
+};
+
+Overlay.hide = function() {
+    this.schedule(function() {
+        if (this.element !== null) {
+            this.element.remove();
+            this.element = null;
+        }
+
+        $('#layout_wrapper').removeClass('has-overlay');
+        $(window).off('beforeunload.overlay');
+    });
+};
+
+// Secure the overlay
+Overlay.securityHandler = function(event) {
+    event = event || window.event || {};
+    event.returnValue = 'Ihre Eingaben wurden bislang noch nicht gespeichert.'.toLocaleString();
+    return event.returnValue;
+};
+
+// Allows progress information
+Overlay.showProgress = function(title, ajax, secure, delay) {
+    this.show(
+        ajax,
+        null,
+        secure,
+        function() {
+            if ($('h1', this.selector).length === 0) {
+                $(this.selector)
+                    .append($('<h1>').text(title))
+                    .append('<progress max="100" value="0">')
+                    .append('<ul class="overlay-progress-log">');
+            }
+        },
+        delay
+    );
+};
+
+Overlay.updateProgress = function(percent, message) {
+    $('progress', this.selector).val(percent);
+    if (message) {
+        this.progressInfo(message);
+    }
+};
+
+Overlay.progressInfo = function(message) {
+    var li = $('<li>').text(message);
+    $('.overlay-progress-log', this.selector).prepend(li);
+    li.delay(1000).hide('fade', 300, function() {
+        $(this).remove();
+    });
+};
+
+export default Overlay;
