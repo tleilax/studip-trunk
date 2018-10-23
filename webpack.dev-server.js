cc8fd637e41e7d514fe2ddac1da3820abad0ff7f
luniki <github@luniki.de>
2018-10-23 11:36:56 +0000

    add trailing slash, fixes #8976

--- webpack.dev-server.js
+++ webpack.dev-server.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     output: {
-        publicPath: 'http://localhost:8123'
+        publicPath: 'http://localhost:8123/'
     },
     devServer: {
         contentBase: path.join(__dirname, './public/assets'),
