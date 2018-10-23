a8af0567f0c96562a2d4e6df686ea72d8caf32f7
luniki <github@luniki.de>
2018-10-23 12:53:45 +0000

    move restapi JS code, refs #8975

--- resources/assets/javascripts/init.js
+++ resources/assets/javascripts/init.js
@@ -4,18 +4,23 @@ import NavigationShrinker from './lib/navigation_shrinker.js';
 import Overlay from './lib/overlay.js';
 import parseOptions from './lib/parse_options.js';
 import PersonalNotifications from './lib/personal_notifications.js';
+import RESTAPI from './lib/restapi.js';
 import Sidebar from './lib/sidebar.js';
 import SmileyPicker from './lib/smiley_picker.js';
 import Table from './lib/table.js';
 import Toolbar from './lib/toolbar.js';
 
+const api = new RESTAPI()
+
 window.STUDIP = _.assign(window.STUDIP || {}, {
+    api,
     Dialog,
     HeaderMagic,
     NavigationShrinker,
     Overlay,
     parseOptions,
     PersonalNotifications,
+    RESTAPI,
     Sidebar,
     SmileyPicker,
     Table,
