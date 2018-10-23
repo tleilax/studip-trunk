b876639c57e2ab4c99fc829faa4028f8d916c541
luniki <github@luniki.de>
2018-10-23 12:45:59 +0000

    move dialog JS code, refs #8975

--- resources/assets/javascripts/init.js
+++ resources/assets/javascripts/init.js
@@ -1,6 +1,8 @@
+import Dialog from './lib/dialog.js';
 import HeaderMagic from './lib/header_magic.js';
 import NavigationShrinker from './lib/navigation_shrinker.js';
 import Overlay from './lib/overlay.js';
+import parseOptions from './lib/parse_options.js';
 import PersonalNotifications from './lib/personal_notifications.js';
 import Sidebar from './lib/sidebar.js';
 import SmileyPicker from './lib/smiley_picker.js';
@@ -8,9 +10,11 @@ import Table from './lib/table.js';
 import Toolbar from './lib/toolbar.js';
 
 window.STUDIP = _.assign(window.STUDIP || {}, {
+    Dialog,
     HeaderMagic,
     NavigationShrinker,
     Overlay,
+    parseOptions,
     PersonalNotifications,
     Sidebar,
     SmileyPicker,
