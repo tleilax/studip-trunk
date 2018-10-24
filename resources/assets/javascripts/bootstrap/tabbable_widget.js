86efb63c987efbc155c4cdfc7967021aacd34970
luniki <github@luniki.de>
2018-10-24 05:44:58 +0000

    move tabbable widget JS code, refs #8975

--- resources/assets/javascripts/bootstrap/tabbable_widget.js
+++ resources/assets/javascripts/bootstrap/tabbable_widget.js
@@ -1,18 +1,19 @@
-/*jslint browser: true */
-/*global jQuery, STUDIP */
-(function ($, STUDIP) {
-    'use strict';
-
-    $(document).on('click', '.tabbable-widget > nav > a', function (event) {
+$(document)
+    .on('click', '.tabbable-widget > nav > a', function(event) {
         var selector = $(this).attr('href');
-        $(this).addClass('active').siblings().removeClass('active');
-        $(selector).addClass('active')
-                   .siblings('section').removeClass('active')
-                   .end();
+        $(this)
+            .addClass('active')
+            .siblings()
+            .removeClass('active');
+        $(selector)
+            .addClass('active')
+            .siblings('section')
+            .removeClass('active')
+            .end();
 
         // Delay resetting of scroll top until browser is no longer busy
         // (otherwise the scrolled element will not reset - at least in FF)
-        setTimeout(function () {
+        setTimeout(function() {
             $(selector).scrollTop(0);
         }, 0);
 
@@ -21,11 +22,11 @@
         }
 
         event.preventDefault();
-    }).ready(function () {
+    })
+    .ready(function() {
         if (!location.hash) {
             return;
         }
 
         $('.tabbable-widget > nav > a[href="' + location.hash + '"]').click();
     });
-}(jQuery, STUDIP));
