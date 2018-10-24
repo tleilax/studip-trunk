d22fca37f670e75802d8b5f5b4c081efb3e06ddb
luniki <github@luniki.de>
2018-10-24 05:44:34 +0000

    move register JS code, refs #8975

--- resources/assets/javascripts/init.js
+++ resources/assets/javascripts/init.js
@@ -19,6 +19,7 @@ import Overlay from './lib/overlay.js';
 import parseOptions from './lib/parse_options.js';
 import PersonalNotifications from './lib/personal_notifications.js';
 import QuickSearch from './lib/quick_search.js';
+import register from './lib/register.js';
 import RESTAPI from './lib/restapi.js';
 import Sidebar from './lib/sidebar.js';
 import SmileyPicker from './lib/smiley_picker.js';
@@ -52,6 +53,7 @@ window.STUDIP = _.assign(window.STUDIP || {}, {
     parseOptions,
     PersonalNotifications,
     QuickSearch,
+    register,
     RESTAPI,
     Sidebar,
     SmileyPicker,
