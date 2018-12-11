d47426a62d9be3d50e83994a07ac0f4e7c9feb4a
Jan-Hendrik Willms <tleilax+github@gmail.com>
2018-12-11 09:38:28 +0000

    wiki: show edit button in footer, re #9071

--- templates/wiki/show.php
+++ templates/wiki/show.php
@@ -9,22 +9,26 @@
                     <? $actionMenu->addLink(
                            URLHelper::getURL('', ['keyword' => $wikipage->keyword, 'view' => 'edit']),
                            _('Bearbeiten'),
-                           Icon::create('edit')) ?>
+                           Icon::create('edit')
+                    ) ?>
                 <? endif ?>
                 <? if ($GLOBALS['perm']->have_studip_perm('tutor', Context::getId()) && !$wikipage->isNew()): ?>
                     <? $actionMenu->addLink(
                            URLHelper::getURL('dispatch.php/wiki/change_pageperms', ['keyword' => $wikipage->keyword]),
                            _('Seiten-Einstellungen'),
                            Icon::create('admin'),
-                           ['data-dialog' => 'size=auto']) ?>
+                           ['data-dialog' => 'size=auto']
+                    ) ?>
                     <? $actionMenu->addLink(
                            URLHelper::getURL('', ['keyword' => $wikipage->keyword, 'cmd' => 'delete', 'version' => 'latest']),
                            _('Löschen'),
-                           Icon::create('trash')) ?>
+                           Icon::create('trash')
+                    ) ?>
                     <? $actionMenu->addLink(
                            URLHelper::getURL('', ['keyword' => $wikipage->keyword, 'cmd' => 'delete_all']),
                            _('Alle Versionen löschen'),
-                           Icon::create('trash')) ?>
+                           Icon::create('trash')
+                    ) ?>
                 <? endif ?>
                 <?= $actionMenu->render() ?>
             <? endif ?>
@@ -33,4 +37,12 @@
     <section>
         <?= $content ?>
     </section>
+<? if ($wikipage->isEditableBy($GLOBALS['user'])): ?>
+    <footer>
+        <?= Studip\LinkButton::create(
+            _('Bearbeiten'),
+            URLHelper::getURL('', ['keyword' => $wikipage->keyword, 'view' => 'edit'])
+        ) ?>
+    </footer>
+<? endif; ?>
 </article>
