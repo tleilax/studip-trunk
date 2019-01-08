bf02ee7c72bb274fe0c147771dc182ac33ee4b62
luniki <github@luniki.de>
2019-01-08 08:26:51 +0000

    rebuild JS, refs #9114 and #9115

---
+++ public/assets/javascripts/tablesorter-f8421ec5962df843613d.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{197:function(a,r,t){"use strict";t.r(r);t(194),t(195),t(196);jQuery.tablesorter.addParser({id:"htmldata",is:function(a,r,t,e){var n=r.config.parserMetadataName||"sortValue";return void 0!==$(t).data(n)},format:function(a,r,t){var e=r.config.parserMetadataName||"sortValue";return $(t).data(e)},type:"numeric"})}}]);
+//# sourceMappingURL=tablesorter-f8421ec5962df843613d.chunk.js.map
