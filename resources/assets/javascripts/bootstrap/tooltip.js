efa40d59c3b1f3d69cf1e4cac93ab9508eecf767
Jan-Hendrik Willms <tleilax+github@gmail.com>
2019-01-29 08:46:48 +0000

    convert tooltip code to es6, re #9155

--- resources/assets/javascripts/bootstrap/tooltip.js
+++ resources/assets/javascripts/bootstrap/tooltip.js
@@ -1,3 +1,5 @@
+/*jslint esversion: 6*/
+
 // Attach global hover handler for tooltips.
 // Applies to all elements having a "data-tooltip" attribute.
 // Tooltip may be provided in the data-attribute itself or by
@@ -8,58 +10,49 @@ var timeout = null;
 
 STUDIP.Tooltip.threshold = 6;
 
-jQuery(document)
-    .on('mouseenter mouseleave', '[data-tooltip]', function(event) {
-        var data = $(this).data(),
-            visible = event.type === 'mouseenter',
-            content,
-            offset = $(this).offset(),
-            x = offset.left + $(this).outerWidth(true) / 2,
-            y = offset.top,
-            tooltip;
+$(document).on('mouseenter mouseleave', '[data-tooltip]', function(event) {
+    let  data = $(this).data();
 
-        if (!data.tooltipObject) {
-            // If tooltip has not yet been created (first hover), obtain it's
-            // contents and create the actual tooltip object.
-            content =
-                $('<div/>')
-                    .text(data.tooltip || $(this).attr('title'))
-                    .html() ||
-                $(this)
-                    .find('.tooltip-content')
-                    .remove()
-                    .html();
-            $(this).attr('title', '');
+    const visible = event.type === 'mouseenter';
+    const offset = $(this).offset();
+    const x = offset.left + $(this).outerWidth(true) / 2;
+    const y = offset.top;
 
-            tooltip = new STUDIP.Tooltip(x, y, content);
+    let content;
+    let tooltip;
 
-            data.tooltipObject = tooltip;
-
-            $(this).on('remove', function() {
-                tooltip.remove();
-            });
-        } else if (visible) {
-            // If tooltip has already been created, update it's position.
-            // This is neccessary if the surrounding content is scrollable AND has
-            // been scrolled. Otherwise the tooltip would appear at it's previous
-            // and now wrong location.
-            data.tooltipObject.position(x, y);
+    if (!data.tooltipObject) {
+        // If tooltip has not yet been created (first hover), obtain it's
+        // contents and create the actual tooltip object.
+        content = $('<div/>').text(data.tooltip || $(this).attr('title')).html();
+        if (!content) {
+            content = $(this).find('.tooltip-content').remove().html();
         }
+        $(this).attr('title', '');
 
-        if (visible) {
-            $('.studip-tooltip')
-                .not(data.tooltipObject)
-                .hide();
-            data.tooltipObject.show();
-        } else {
-            timeout = setTimeout(function() {
-                data.tooltipObject.hide();
-            }, 300);
-        }
-    })
-    .on('mouseenter', '.studip-tooltip', function() {
-        clearTimeout(timeout);
-    })
-    .on('mouseleave', '.studip-tooltip', function() {
-        $(this).hide();
-    });
+        tooltip = new STUDIP.Tooltip(x, y, content);
+
+        data.tooltipObject = tooltip;
+
+        $(this).on('remove', function() {
+            tooltip.remove();
+        });
+    } else if (visible) {
+        // If tooltip has already been created, update it's position.
+        // This is neccessary if the surrounding content is scrollable AND has
+        // been scrolled. Otherwise the tooltip would appear at it's previous
+        // and now wrong location.
+        data.tooltipObject.position(x, y);
+    }
+
+    if (visible) {
+        $('.studip-tooltip').not(data.tooltipObject).hide();
+        data.tooltipObject.show();
+    } else {
+        timeout = setTimeout(() => data.tooltipObject.hide(), 300);
+    }
+}).on('mouseenter', '.studip-tooltip', () => {
+    clearTimeout(timeout);
+}).on('mouseleave', '.studip-tooltip', function() {
+    $(this).hide();
+});
