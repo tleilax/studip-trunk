a52da1d9719fa1b267838d23a622043d367c8c92
Jan-Hendrik Willms <tleilax+github@gmail.com>
2019-03-28 11:58:07 +0000

    provide compiled assets, fixes #9413

---
+++ public/assets/javascripts/tablesorter-1ec47d3e4651d66168ef.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"c/R+":function(a,r,t){"use strict";t.r(r);t("nQco"),t("3zzX"),t("WPsp");jQuery.tablesorter.addParser({id:"htmldata",is:function(a,r,t,e){var n=r.config.parserMetadataName||"sortValue";return void 0!==$(t).data(n)},format:function(a,r,t){var e=r.config.parserMetadataName||"sortValue";return $(t).data(e)},type:"numeric"})}}]);
+//# sourceMappingURL=tablesorter-1ec47d3e4651d66168ef.chunk.js.map
