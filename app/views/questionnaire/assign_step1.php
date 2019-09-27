2ecb343ec2494b98cd5cb9d1ba292a4bfa02947e
Moritz Strohm <strohm@data-quest.de>
2019-09-27 15:11:13 +0000

    added views from AskALotPlugin, re #9832

---
+++ app/views/questionnaire/assign_step1.php
@@ -0,0 +1,56 @@
+<? if ($found_courses): ?>
+    <table class="default">
+        <thead>
+            <tr>
+                <th>
+                    <input type="checkbox" data-proxyfor=".FoundCourseListItem"
+                           id="AskALotPlugin_course_select_proxy"
+                           data-proxyfor="input[name='course_id_list[]']"
+                           data-activates="#questionnaire_assign_step1_button">
+                </th>
+                <th><?= dgettext('AskALotPlugin', 'Veranstaltung') ?></th>
+                <th><?= dgettext('AskALotPlugin', 'Lehrende') ?></th>
+            </tr>
+        </thead>
+        <tbody>
+            <? foreach ($found_courses as $found_course): ?>
+                <? $teachers = CourseMember::findByCourseAndStatus(
+                    $found_course->id,
+                    'dozent'
+                );
+                $teacher_arr = [];
+                foreach ($teachers as $teacher) {
+                    $teacher_arr[] = $teacher->getUserFullName();
+                } ?>
+                <tr>
+                    <td>
+                        <input type="checkbox" name="course_id_list[]"
+                               class="FoundCourseListItem"
+                               value="<?= htmlReady($found_course->id) ?>">
+                    </td>
+                    <td>
+                        <a href="<?= $controller->link_for(
+                                 'course/details',
+                                 ['cid' => $found_course->id]
+                                 ) ?>" data-dialog="1">
+                            <?= htmlReady($found_course->getFullName()) ?>
+                        </a>
+                    </td>
+                    <td><?= htmlReady(implode(', ', $teacher_arr)) ?></td>
+                </tr>
+            <? endforeach ?>
+        </tbody>
+    </table>
+    <?= \Studip\Button::create(
+        _('Auswählen'),
+        'select_courses',
+        [
+            'id' => 'questionnaire_assign_step1_button',
+            'disabled' => 'disabled'
+        ]
+    ) ?>
+<? else: ?>
+    <?= MessageBox::info(
+        _('Es wurden keine zur Suche passenden Veranstaltungen gefunden!')
+    ) ?>
+<? endif ?>
---
+++ app/views/questionnaire/assign_step2.php
@@ -0,0 +1,77 @@
+<h1><?= _('Ausgewählte Veranstaltungen') ?></h1>
+<ul>
+    <? if ($selected_courses): ?>
+        <? foreach ($selected_courses as $selected_course): ?>
+            <li>
+                <? if ($GLOBALS['perm']->have_perm('root')): ?>
+                    <a href="<?= URLHelper::getLink(
+                             'dispatch.php/course/details',
+                             [
+                                 'cid' => $found_course->id
+                             ]
+                             ) ?>" data-dialog="1">
+                        <?= htmlReady($selected_course->getFullName()) ?>
+                    </a>
+                <? else: ?>
+                    <?= htmlReady($selected_course->getFullName()) ?>
+                <? endif ?>
+            </li>
+        <? endforeach ?>
+    <? endif ?>
+</ul>
+<h1><?= _('Auswählbare Fragebögen') ?></h1>
+<table class="default sortable-table" data-sortlist="[[2, 1]]">
+    <thead>
+        <tr>
+            <th>
+                <input type="checkbox"
+                       data-proxyfor="input[name='selected_questionnaire_ids[]']"
+                       data-activates="#questionnaire-assign-form .step2-button">
+            </th>
+            <th data-sort="text">
+                <?= _('Titel') ?>
+            </th>
+            <th data-sort="htmldata">
+                <?= _('Erstellungsdatum') ?>
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        <? foreach ($questionnaires as $questionnaire): ?>
+            <tr>
+                <td>
+                    <input type="checkbox" name="selected_questionnaire_ids[]"
+                           value="<?= htmlReady($questionnaire->id) ?>"
+                           <?= in_array($questionnaire->id, $selected_questionnaires)
+                                      ? 'checked="checked"'
+                                      : '' ?>>
+                </td>
+                <td><?= htmlReady($questionnaire->title) ?></td>
+                <td data-sort-value="<?= htmlReady($questionnaire->mkdate) ?>">
+                    <?= date('d.m.Y', $questionnaire->mkdate) ?>
+                </td>
+            </tr>
+        <? endforeach ?>
+    </tbody>
+</table>
+<label>
+    <input type="checkbox" name="delete_dates" value="1"
+           <?= $delete_dates ? 'checked="checked"' : '' ?>>
+    <?= _('Kopierte Fragebögen händisch starten und enden lassen') ?>
+</label>
+<?= \Studip\Button::create(
+    _('Zuweisen'),
+    'assign',
+    [
+        'class' => 'step2-button',
+        'disabled' => 'disabled'
+    ]
+) ?>
+<?= \Studip\Button::create(
+    _('In Veranstaltungen kopieren'),
+    'copy',
+    [
+        'class' => 'step2-button',
+        'disabled' => 'disabled'
+    ]
+) ?>
