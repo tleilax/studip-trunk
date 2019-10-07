0f0a3a01b81050aa986310a7b459c4b5c079d7f2
André Noack <noackorama@gmail.com>
2019-10-07 16:01:58 +0000

    fixes #9843

--- templates/filesystem/hidden_folder/description.php
+++ templates/filesystem/hidden_folder/description.php
@@ -1,7 +1,8 @@
-<div>
-    <div><?= _('Ein unsichtbarer Ordner, welcher nur von Lehrenden und TutorInnen gesehen werden kann.') ?></div>
+<div style="font-style: italic">
+    <?= _('Ein unsichtbarer Ordner, welcher nur von Lehrenden und TutorInnen gesehen werden kann.') ?>
 </div>
 <? if ($folderdata['description']) : ?>
+<hr>
     <div>
         <?= formatReady($folderdata['description']) ?>
     </div>
--- templates/filesystem/permission_enabled_folder/description.php
+++ templates/filesystem/permission_enabled_folder/description.php
@@ -1,15 +1,15 @@
-<div><?= htmlReady($type )?>:</div>
-<label>
+<div style="font-style: italic"><?= htmlReady($type )?>:
+
     <input disabled type="checkbox" <? if ($folder->isReadable()) echo 'checked'; ?>>
-    <strong>r</strong> - <?= _('Lesen (Dateien können heruntergeladen werden)') ?>
-</label>
-<br>
-<label>
+    <strong>Lesen</strong>
     <input disabled type="checkbox" <? if ($folder->isWritable()) echo 'checked'; ?>>
-    <strong>w</strong> - <?= _('Schreiben (Dateien können hochgeladen werden)') ?>
-</label>
-<br>
-<label>
+    <strong>Schreiben</strong>
     <input disabled type="checkbox" <? if ($folder->isVisible()) echo 'checked'; ?>>
-    <strong>x</strong> - <?= _('Sichtbarkeit (Ordner wird angezeigt)') ?>
-</label>
+    <strong>Sichtbarkeit</strong>
+</div>
+<? if ($folderdata['description']) : ?>
+<hr>
+    <div>
+        <?= formatReady($folderdata['description']) ?>
+    </div>
+<? endif ?>
