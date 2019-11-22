b2d808586b9b78850b4e912445edd08cf4f2f776
André Noack <noackorama@gmail.com>
2019-11-22 11:16:45 +0000

    re #9713

--- templates/mail/text.php
+++ templates/mail/text.php
@@ -18,6 +18,9 @@
 
 
 --
-<?= sprintf(_("Diese E-Mail ist eine Kopie einer systeminternen Nachricht, die in Stud.IP an %s versendet wurde."), $rec_fullname) ?>
-
+<? if ($snd_fullname) : ?>
+    <?= sprintf(_('Diese E-Mail ist eine Kopie einer systeminternen Nachricht, die in Stud.IP von %s (%s) an %s (%s) versendet wurde.'), $snd_fullname, $snd_email, $rec_fullname, $rec_email) ?>
+<? else : ?>
+    <?= sprintf(_("Diese E-Mail ist eine Kopie einer systeminternen Nachricht, die in Stud.IP an %s versendet wurde."), $rec_fullname) ?>
+<? endif ?>
 <?= sprintf(_("Sie erreichen Stud.IP unter %s"), $GLOBALS['ABSOLUTE_URI_STUDIP']) ?>
